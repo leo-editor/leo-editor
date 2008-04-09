cd40e9adf302fdc140599e530dc2bfde743e58f1
Edward K. Ream <edreamleo@gmail.com>
2008-04-09 08:02:49 -0500

    improved leoKeyEvent.__repr__

--- leo/src/leoTkinterGui.py
+++ leo/src/leoTkinterGui.py
@@ -745,7 +745,8 @@ class tkinterGui(leoGui.leoGui):
 
         def __repr__ (self):
 
-            return 'tkGui.leoKeyEvent: char: %s, keysym: %s' % (repr(self.char),repr(self.keysym))
+            return 'tkGui.leoKeyEvent: char: %s, keysym: %s, widget: %s' % (
+                repr(self.char),repr(self.keysym),self.widget)
     #@nonl
     #@-node:ekr.20061112152012.2:class leoKeyEvent (tkGui)
     #@-others
