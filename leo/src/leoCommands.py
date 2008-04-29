e2c05409a482b03a99396f619346ea0b1650d9fb
Edward K. Ream <edreamleo@gmail.com>
2008-04-29 12:36:01 -0500

    removed failing test in check-outline command when in unified-nodes world

--- leo/src/leoCommands.py
+++ leo/src/leoCommands.py
@@ -3565,11 +3565,13 @@ class baseCommands:
 
                     assert parent_v.t.children[n] == p.v,'fail 1'
 
-                    assert parent_v in p.v.parents,'fail 2: parent_v: %s\nparents: %s' % (
-                        parent_v,g.listToString(p.v.parents))
+                    if not g.unified_nodes:
 
-                    for z in p.v.parents:
-                        assert p.v in z.t.children,'fail 3'
+                        assert parent_v in p.v.parents,'fail 2: parent_v: %s\nparents: %s' % (
+                            parent_v,g.listToString(p.v.parents))
+
+                        for z in p.v.parents:
+                            assert p.v in z.t.children,'fail 3'
                     #@-node:ekr.20080426051658.1:assert consistency of t.parent and t.children arrays
                     #@-others
                     #@-node:ekr.20040323155951:<< do full tests >>
