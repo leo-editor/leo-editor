fe0ef43cf8d558496bb29ab71fef98dc54581e23
Edward K. Ream <edreamleo@gmail.com>
2008-07-30 17:47:29 -0500

    removed commented-out code

--- leo/core/leoGlobals.py
+++ leo/core/leoGlobals.py
@@ -5170,7 +5170,7 @@ def importFromPath (name,path,pluginName=None,verbose=False):
     path = g.toEncodedString(path,app and app.tkEncoding or 'ascii')
 
     # g.trace(verbose,name,pluginName)
-    module = sys.modules.get(moduleName) # or sys.modules.get('leo.src.%s' % moduleName)
+    module = sys.modules.get(moduleName)
     if not module:
         try:
             theFile = None
