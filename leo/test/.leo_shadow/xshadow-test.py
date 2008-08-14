362b57bb06dbc524ec845cc3028b9fcf6acdb630
Edward K. Ream <edreamleo@gmail.com>
2008-08-14 10:26:23 -0500

    cleaned up test files, and do not ignore shadow files in leo\test\.leo_shadow

---
+++ leo/test/.leo_shadow/xshadow-test.py
@@ -0,0 +1,12 @@
+#@+leo-ver=4
+#@+node:@shadow shadow-test.py
+#@+others
+#@+node:spam
+def spam():
+    pass #
+#@nonl
+#@-node:spam
+#@-others
+#@nonl
+#@-node:@shadow shadow-test.py
+#@-leo
---
+++ leo/test/.leo_shadow/xuA_test_shadow_file.py
@@ -0,0 +1,18 @@
+#@+leo-ver=4-thin
+#@+node:ekr.20080806211453.1:@shadow uA_test_shadow_file.py
+#@+others
+#@+node:ekr.20080806211453.2:child1
+pass
+#@nonl
+#@+node:ekr.20080806211453.3:grandChild1
+pass
+#@+node:ekr.20080806211453.4:greatGrandChild1
+pass
+#@nonl
+#@-node:ekr.20080806211453.4:greatGrandChild1
+#@-node:ekr.20080806211453.3:grandChild1
+#@-node:ekr.20080806211453.2:child1
+#@-others
+#@nonl
+#@-node:ekr.20080806211453.1:@shadow uA_test_shadow_file.py
+#@-leo
