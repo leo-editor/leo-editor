17b45c4d5dc5750273ba5b5ecb97eae092f719fd
Edward K. Ream <edreamleo@gmail.com>
2008-08-27 10:38:58 -0500

    update aux files

---
+++ leo/dist/pre-install-script.py
@@ -0,0 +1,8 @@
+#@+leo-ver=4-thin
+#@+node:ekr.20080827092958.3:@thin pre-install-script.py
+import sys
+print ("This is Leo's pre-install script")
+print ("sys.executable: %s" % sys.executable)
+#@nonl
+#@-node:ekr.20080827092958.3:@thin pre-install-script.py
+#@-leo
