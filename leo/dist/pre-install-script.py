64012431a1c3f9d85a69627e87c62e4dc80526ae
Edward K. Ream <edreamleo@gmail.com>
2008-08-27 11:15:26 -0500

    commit doc files during make

--- leo/dist/pre-install-script.py
+++ leo/dist/pre-install-script.py
@@ -3,8 +3,10 @@
 import sys
 print ("This is Leo's pre-install script")
 print ("sys.executable: %s" % sys.executable)
-print ("dir(sys): %s" % dir(sys))
-print sys.argv # Generates an error so I can see sys.executable.
+# print ("dir(sys): %s" % dir(sys))
+print ("sys.path")
+for z in sys.path: print z
+print sys.argv # Generates an error so I can see results.
 # print ("sys.argv...")
 # for z in sys.argv:
     # print z
