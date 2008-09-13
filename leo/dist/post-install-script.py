fcde8cf9153d26916cbe2f8473a36c2686b84a46
Edward K. Ream <edreamleo@gmail.com>
2008-09-13 13:35:42 -0500

    commit doc files during make

--- leo/dist/post-install-script.py
+++ leo/dist/post-install-script.py
@@ -3,8 +3,7 @@
 import sys
 print 'post-install-script.py'
 print 'sys.argv',sys.argv
-
-print 'argv',argv
+print 'sys.executable',sys.executable
 #@nonl
 #@-node:ekr.20080913110741.1:@thin post-install-script.py
 #@-leo
