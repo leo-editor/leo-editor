2bc5ed4e13af9e2f4f7c72abe47944305f611772
Edward K. Ream <edreamleo@gmail.com>
2008-09-13 11:46:12 -0500

    added post install script so it will be included in distribution

---
+++ leo/dist/post-install-script.py
@@ -0,0 +1,10 @@
+#@+leo-ver=4-thin
+#@+node:ekr.20080913110741.1:@thin post-install-script.py
+import sys
+print 'post-install-script.py'
+print 'sys.argv',sys.argv
+
+print 'argv',argv
+#@nonl
+#@-node:ekr.20080913110741.1:@thin post-install-script.py
+#@-leo
