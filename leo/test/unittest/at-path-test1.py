6e0895e75a88c39dec0d31828e09039c29a4d629
Edward K. Ream <edreamleo@gmail.com>
2008-09-17 15:23:40 -0500

    Added at-path unit tests to unitTest.leo from the trunk.

--- leo/test/unittest/at-path-test1.py
+++ leo/test/unittest/at-path-test1.py
@@ -1,7 +1,7 @@
 #@+leo-ver=4
 #@+node:@file ../test/unittest/at-path-test1.py
 #@@language python
-# unittest/at-path-test1.py
+# unittest/at-path-test1.py 
 #@nonl
 #@-node:@file ../test/unittest/at-path-test1.py
 #@-leo
--- leo/test/unittest/at-path-test3.py
+++ leo/test/unittest/at-path-test3.py
@@ -1,7 +1,7 @@
 #@+leo-ver=4
 #@+node:@file at-path-test3.py
 #@@language python
-# unittest/at-path-test3.py
+# unittest/at-path-test3.py 
 #@nonl
 #@-node:@file at-path-test3.py
 #@-leo
