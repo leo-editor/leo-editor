43ef27e8eb3b041b27b2b6e2a32d270d4f6e5714
Edward K. Ream <edreamleo@gmail.com>
2008-09-23 14:25:13 -0500

    Removed old scanner helpers.  Hurray.
    Bug fix: g.os_path_finalize_join now returns 
        os.path.normpath(os.path.abspath(g.os_path_join(*args,**keys)))
    That is, it returns the normpathed and abspathed result.

--- leo/core/leoGlobals.py
+++ leo/core/leoGlobals.py
@@ -325,83 +325,6 @@ def startupEncoding ():
 #@-node:ekr.20041117151301.1:startupEncoding
 #@-node:ekr.20050304072744:Compute directories... (leoGlobals)
 #@+node:ekr.20031218072017.1380:g.Directive utils...
-#@+node:ekr.20080923070954.2:To be deleted
-#@+node:ekr.20031218072017.1387:g.scanAtEncodingDirective (to be deleted)
-def scanAtEncodingDirective(theDict):
-
-    """Scan the @encoding directive at s[theDict["encoding"]:].
-
-    Returns the encoding name or None if the encoding name is invalid.
-    """
-
-    encoding = theDict.get('encoding')
-    if not encoding:
-        return None
-
-    if g.isValidEncoding(encoding):
-        # g.trace(encoding)
-        return encoding
-    else:
-        g.es("invalid @encoding:",encoding,color="red")
-        return None
-#@-node:ekr.20031218072017.1387:g.scanAtEncodingDirective (to be deleted)
-#@+node:ekr.20031218072017.1388:g.scanAtLineendingDirective (to be deleted)
-def scanAtLineendingDirective(theDict):
-
-    """Scan the @lineending directive at s[theDict["lineending"]:].
-
-    Returns the actual lineending or None if the name of the lineending is invalid.
-    """
-
-    e = theDict.get('lineending')
-
-    if e in ("cr","crlf","lf","nl","platform"):
-        lineending = g.getOutputNewline(name=e)
-        # g.trace(e,lineending)
-        return lineending
-    else:
-        # g.es("invalid @lineending directive:",e,color="red")
-        return None
-#@-node:ekr.20031218072017.1388:g.scanAtLineendingDirective (to be deleted)
-#@+node:ekr.20031218072017.1389:g.scanAtPagewidthDirective (to be deleted)
-def scanAtPagewidthDirective(theDict,issue_error_flag=False):
-
-    """Scan the @pagewidth directive at s[theDict["pagewidth"]:].
-
-    Returns the value of the width or None if the width is invalid.
-    """
-
-    s = theDict.get('pagewidth')
-    i, val = g.skip_long(s,0)
-
-    if val != None and val > 0:
-        # g.trace(val)
-        return val
-    else:
-        if issue_error_flag and not g.app.unitTesting:
-            g.es("ignoring @pagewidth",s,color="red")
-        return None
-#@-node:ekr.20031218072017.1389:g.scanAtPagewidthDirective (to be deleted)
-#@+node:ekr.20031218072017.1390:g.scanAtTabwidthDirective (to be deleted)
-def scanAtTabwidthDirective(theDict,issue_error_flag=False):
-
-    """Scan the @tabwidth directive at s[theDict["tabwidth"]:].
-
-    Returns the value of the width or None if the width is invalid.
-    """
-
-    s = theDict.get('tabwidth')
-    junk,val = g.skip_long(s,0)
-
-    if val != None and val != 0:
-        # g.trace(val)
-        return val
-    else:
-        if issue_error_flag:
-            g.es("ignoring",s,color="red")
-        return None
-#@-node:ekr.20031218072017.1390:g.scanAtTabwidthDirective (to be deleted)
-#@-node:ekr.20080923070954.2:To be deleted
 #@+node:EKR.20040504150046.4:g.comment_delims_from_extension
 def comment_delims_from_extension(filename):
 
@@ -3280,7 +3203,8 @@ def os_path_finalize_join (*args,**keys):
     if c: args = [g.os_path_expandExpression(path,**keys)
         for path in args if path]
 
-    return g.os_path_join(*args,**keys) # Handles expanduser
+    return os.path.normpath(os.path.abspath(
+        g.os_path_join(*args,**keys))) # Handles expanduser
 #@-node:ekr.20080921060401.14:os_path_finalize & os_path_finalize_join
 #@+node:ekr.20031218072017.2150:os_path_getmtime
 def os_path_getmtime(path,encoding=None):
