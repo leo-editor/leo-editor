0e42c0ca1298c6a382cffd1632ff938190731e3b
Terry Brown <terry_n_brown@yahoo.com>
2008-12-23 13:19:35 -0600

    first release

--- leo/plugins/backlink.py
+++ leo/plugins/backlink.py
@@ -2,15 +2,14 @@
 # 
 # gnxs won't work, because they belong to tnodes, not vnodes
 # 
-# backling will store all its stuff in v.unknownAttributes['_bklnk']
+# Backlink will store all its stuff in v.unknownAttributes['_bklnk']
 # 
-# the vnode's id will be v.unknownAttributes['_bklnk']['id']
+# The vnode's id will be v.unknownAttributes['_bklnk']['id']
 # 
-# unless Edward decides otherwise, backlink will use 
+# Unless Edward decides otherwise, backlink will use 
 # leo.core.leoNodes.nodeIndices.getNewIndex() to make these ids
 # 
-# when nodes are copied and pasted unknownAttributes are duplicated
-# 
+# When nodes are copied and pasted unknownAttributes are duplicated.
 # during load, backlink will create a dict. of vnode ids.  Duplicates
 # will be split, so that a node linking to a node which is copied and
 # pasted will link to both nodes after the paste, *after* a save and
@@ -22,7 +21,7 @@
 
 __version__ = '0.1'
 # 
-# Put notes about each version here.
+# 0.1 - initial release - TNB
 
 import leo.core.leoGlobals as g
 import leo.core.leoPlugins as leoPlugins
@@ -51,7 +50,7 @@ def onCreate (tag, keys):
     c = keys.get('c')
     if not c: return
     
-    thePluginController = backlinkController(c)
+    backlinkController(c)
 class backlinkController:
     
     def __init__ (self,c):
@@ -59,18 +58,19 @@ class backlinkController:
         self.c = c
         self.initIvars()
         leoPlugins.registerHandler('open2', self.loadLinks)
-        # already missed initial 'open2' because of after-create-leo-frame
+        # already missed initial 'open2' because of after-create-leo-frame, so
         self.loadLinksInt()
 
         if Tk:
-            #leoPlugins.registerHandler('iconrclick2', self.showMenu)
-            #leoPlugins.registerHandler('select3', self.showLinksLog)
-            leoPlugins.registerHandler('select3', self.updateTkTab)
             self.makeTkTab()
+            leoPlugins.registerHandler('select3', self.updateTkTab)
+            # deprecated right button menu methods
+            # leoPlugins.registerHandler('iconrclick2', self.showMenu)
+            # leoPlugins.registerHandler('select3', self.showLinksLog)
         if Qt:
             # for now, remove when Qt UI works
             leoPlugins.registerHandler('select3', self.showLinksLog)
-    
+
             uiPath = g.os_path_join(g.app.leoDir, 'plugins', 'Backlink.ui')
             form_class, base_class = uic.loadUiType(uiPath)
 
@@ -119,14 +119,48 @@ class backlinkController:
         self.linkSource = None
         self.vnode = {}
         self.positions = {}
-    def makeTkTab(self):
+        self.messageUsed = False
+    def showMessage(self, msg, **kargs):
+        """Show the message using whatever u.i. is available"""
+
+        optional = False
+        if 'optional' in kargs:
+            optional = kargs['optional']
+            del kargs['optional']
+        
+        if self.messageUsed and optional:
+            return
+        
+        self.messageUsed = not optional
     
+        if Tk and hasattr(self, 'message'):
+            for i in kargs:
+                if i == 'color':
+                    kargs['fg'] = kargs[i]
+                    del kargs[i]
+            if 'fg' not in kargs:
+                kargs['fg'] = 'black'
+
+            self.message.configure(text = msg, **kargs)
+        else:
+            g.es('backlink: '+msg, **kargs)
+    def makeTkTab(self):
+
         c = self.c
         c.frame.log.createTab('Links', createText=False)
         w = c.frame.log.frameDict['Links']
+    
+        f = Tk.Frame(w)
+        scrollbar = Tk.Scrollbar(f, orient=Tk.VERTICAL)
+        self.listbox = Tk.Listbox(f, height=4, yscrollcommand=scrollbar.set)
+    
+        scrollbar.config(command=self.listbox.yview)
+        scrollbar.pack(side=Tk.RIGHT, fill=Tk.Y)
 
-        self.listbox = Tk.Listbox(w)
-        self.listbox.pack(side=Tk.TOP, fill=Tk.BOTH, expand=True)
+        self.listbox.pack(side=Tk.RIGHT, fill=Tk.BOTH, expand=True)
+
+        f.pack(side=Tk.TOP, fill=Tk.BOTH, expand=True)
+
         self.listbox.bind("<ButtonRelease-1>", self.tkListClicked)
 
         commands = [
@@ -143,9 +177,18 @@ class backlinkController:
             f = Tk.Frame(w)
             for j in range(2):
                 txt, com = comms.next()
-                b = Tk.Button(f, text=txt, width=10, command=com)
+                b = Tk.Button(f, text=txt, width=10, height=1, command=com)
                 b.pack(side=Tk.LEFT, fill=Tk.BOTH)
             f.pack(side=Tk.TOP, fill=Tk.BOTH)
+
+        f = Tk.Frame(w)
+        self.message = Tk.Label(f, text='no msg.')
+        self.message.pack(side=Tk.LEFT)
+        self.delete = Tk.IntVar()
+        self.deleteButton = Tk.Checkbutton(f, text='Delete link', variable=self.delete,
+            command=self.tkDeleteClicked)
+        self.deleteButton.pack(side=Tk.RIGHT)
+        f.pack(side=Tk.TOP, fill=Tk.BOTH)
     def updateTkTab(self,tag,k):
 
         if k['c'] != self.c: return  # not our problem
@@ -159,6 +202,13 @@ class backlinkController:
 
         self.listbox.delete(0,Tk.END)
 
+        self.messageUsed = False
+
+        self.delete.set(0)
+
+        self.deleteButton.configure(state=Tk.DISABLED)
+        self.showMessage('', optional=True)
+
         if hasattr(v, 'unknownAttributes') and '_bklnk' in v.unknownAttributes:
             i = 0
             links = v.unknownAttributes['_bklnk']['links']
@@ -168,12 +218,14 @@ class backlinkController:
                 otherV = self.vnode[other]
                 otherP = self.vnodePosition(otherV)
                 if not otherP:
-                    g.es('Deleting lost link')
+                    self.showMessage('Lost link(s) deleted', color='red')
                     del links[i]
                 else:
                     i += 1
                     dests.append((linkType, otherP))
             if dests:
+                self.deleteButton.configure(state=Tk.NORMAL)
+                self.showMessage('Click a link to follow it', optional=True)
                 for i in dests:
                     def goThere(where = i[1]): c.selectPosition(where)
                     txt = {'S':'->','D':'<-','U':'--'}[i[0]] + ' ' + i[1].headString()
@@ -181,14 +233,34 @@ class backlinkController:
                     def delLink(on=v,
                         to=i[1].v.unknownAttributes['_bklnk']['id'],
                         type_=i[0]): self.deleteLink(on,to,type_)
-                self.dests = dests
+                self.dests = dests            
     def tkListClicked(self, event):
-    
+
         selected = self.listbox.curselection()
+    
         if not selected:
             return  # click on empty list of unlinked node
+
         selected = int(selected[0])  # not some fancy smancy Tk value
-        self.c.selectPosition(self.dests[selected][1])
+
+        if not self.delete.get():
+            assert self.c.positionExists(self.dests[selected][1])
+            self.c.selectPosition(self.dests[selected][1])
+            return
+    
+        if self.delete.get():
+            self.deleteLink(
+                self.c.currentPosition().v,
+                self.dests[selected][1].v.unknownAttributes['_bklnk']['id'],
+                self.dests[selected][0]
+            )
+            self.updateTkTabInt()
+    def tkDeleteClicked(self):
+
+        if self.delete.get():
+            self.showMessage('Click a link to DELETE it', color='red')
+        else:
+            self.showMessage('Click a link to follow it')
     def initBacklink(self, v):
 
         if not hasattr(v, 'unknownAttributes'):
@@ -217,7 +289,7 @@ class backlinkController:
         # /here id should be a dict of lists of "aliases"
 
         for p in c.allNodes_iter():
-            self.positions[p.v] = p
+            self.positions[p.v] = p.copy()
             v = p.v
             if hasattr(v, 'unknownAttributes') and '_bklnk' in v.unknownAttributes:
                 vid = v.unknownAttributes['_bklnk']['id']
@@ -237,12 +309,13 @@ class backlinkController:
                 while nvid() in idsSeen and idx <= 100:
                     idx += 1
                 if nvid() in idsSeen:
+                    # use g.es rather than showMessage here
                     g.es('backlink: Massive duplication of vnode ids', color='red')
                 idsSeen.add(nvid())
                 rvid[i].append(x)
                 x.unknownAttributes['_bklnk']['id'] = nvid()
                 self.vnode[nvid()] = x
-    
+
         for vnode in self.vnode:  # just the vnodes with link info.
             links = self.vnode[vnode].unknownAttributes['_bklnk']['links']
             nl = []
@@ -251,6 +324,7 @@ class backlinkController:
                     lt = ('to', 'from')
                     if link[0] == 'S':
                         lt = ('from', 'to')
+                    # use g.es rather than showMessage here
                     g.es('backlink: link %s %s %s ??? lost' % (
                         lt[0], self.vnode[vnode].headString(), lt[1]), color='red')
                     continue
@@ -258,8 +332,10 @@ class backlinkController:
                     nl.append((link[0], x.unknownAttributes['_bklnk']['id']))
             self.vnode[vnode].unknownAttributes['_bklnk']['links'] = nl
     
-        g.es('backlink: link info. loaded on %d nodes' % len(self.vnode))
+        self.showMessage('Link info. loaded on %d nodes' % len(self.vnode))
     def showMenu(self,tag,k):
+
+        # deprecated
 
         g.app.gui.killPopupMenu()
 
@@ -268,7 +344,7 @@ class backlinkController:
         p = k['p']
         self.c.selectPosition(p)
         v = p.v
-    
+
         c = self.c
 
         # Create the menu.
@@ -319,7 +395,7 @@ class backlinkController:
                 continue
             c.add_command(menu,label=text,
                 underline=0,command=com)
-        
+    
 
         # Show the menu.
         event = k['event']
@@ -327,6 +403,8 @@ class backlinkController:
 
         return None # 'break' # EKR: Prevent other right clicks.
     def showLinksLog(self,tag,k):
+    
+        # deprecated
 
         if k['c'] != self.c: return  # not our problem
 
@@ -339,11 +417,11 @@ class backlinkController:
             dests = []
             while i < len(links):
                 linkType, other = links[i]
-        
+    
                 if other not in self.vnode:
                     return
                     # called before load hook?
-        
+    
                 otherV = self.vnode[other]
                 otherP = self.vnodePosition(otherV)
                 if not otherP:
@@ -376,38 +454,41 @@ class backlinkController:
                         del links[n]
                         break
                 else:
-                    g.es("backlink: Note: couldn't find other side of link")
+                    self.showMessage("Couldn't find other side of link")
                 break
         else:
-            g.es("backlink: Error: no such link")
-    def vnodePosition(self, v):
-
-        if v in self.positions:
-            p = self.positions[v]
-            if p.v is v and self.c.positionExists(p):
-                return p
-
-        for p in self.c.allNodes_iter():
-            self.positions[v] = p
-            if p.v is v:
-                return p
-
-        return None
+            self.showMessage("Error: no such link")
     def markSrc(self):
         self.linkSource = self.c.currentPosition().copy()
+    def vnodePosition(self, v):
+        """Return a position for vnode v, if there is one"""
+
+        # first check the cache
+        if False and v in self.positions:
+            p = self.positions[v]
+            if p.v is v and self.c.positionExists(p):
+                return p.copy()
+
+        # else search for one, abort search as soon as it's found
+        for p in self.c.allNodes_iter():
+            self.positions[v] = p.copy()  # update cache
+            if p.v is v:
+                return p.copy()
+
+        return None
     def markDst(self):
         self.linkDestination = self.c.currentPosition().copy()
     def linkDst(self):
 
         if not self.linkDestination or not self.c.positionExists(self.linkDestination):
-            g.es('Link destination not specified or no longer valid', color='red')
+            self.showMessage('Link destination not specified or no longer valid', color='red')
             return
 
         self.link(self.c.currentPosition(), self.linkDestination)
     def linkSrc(self):
 
         if not self.linkSource or not self.c.positionExists(self.linkSource):
-            g.es('Link source not specified or no longer valid', color='red')
+            self.showMessage('Link source not specified or no longer valid', color='red')
             return
 
         self.link(self.linkSource, self.c.currentPosition())
@@ -417,7 +498,7 @@ class backlinkController:
         if self.linkSource and self.c.positionExists(self.linkSource):
             source = self.linkSource
         elif not self.linkDestination or not self.c.positionExists(self.linkDestination):
-            g.es('Link source/dest. not specified or no longer valid', color='red')
+            self.showMessage('Link source/dest. not specified or no longer valid', color='red')
             return
         else:
             source = self.linkDestination
