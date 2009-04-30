3ed658ee4b1cea978bb8807e7430a8de84f8ff0d
Edward K. Ream <edreamleo@gmail.com>
2009-04-30 12:21:03 -0500

    updated rst3 docs

--- leo/doc/html/conf.py
+++ leo/doc/html/conf.py
@@ -123,7 +123,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+#html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
