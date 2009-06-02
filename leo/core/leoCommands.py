0e1feb51f497042633d02fd25f09a09a07ef316f
Edward K. Ream <edreamleo@gmail.com>
2009-06-02 12:19:04 -0500

    added a more-informative version: pre-beta 2

--- leo/core/leoCommands.py
+++ leo/core/leoCommands.py
@@ -421,7 +421,7 @@ class baseCommands (object):
 
     def getSignOnLine (self):
         c = self
-        return "Leo 4.6 beta 1, build %s, March 24, 2009" % c.getBuildNumber()
+        return "Leo 4.6 pre-beta 2, build %s, March 24, 2009" % c.getBuildNumber()
     #@-node:ekr.20040629121554.1:getSignOnLine (Contains hard-coded version info)
     #@+node:ekr.20040629121554.2:initVersion
     def initVersion (self):
