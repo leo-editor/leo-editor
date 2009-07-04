992e9a451e0bcefe3e1136d1a3f552a77b35bdfd
Edward K. Ream <edreamleo@gmail.com>
2009-07-04 03:18:55 -0500

    Added leo\test\tnodeList.py, generated at present by test.leo. This is not a proper unit test; it's an emergency measure.

---
+++ leo/test/tnodeList.py
@@ -0,0 +1,17 @@
+#@+leo-ver=4
+#@+node:@file tnodeList.py
+#@+others
+#@+node:spam
+def spam():
+    pass
+#@nonl
+#@-node:spam
+#@+node:eggs
+def eggs():
+    pass
+#@nonl
+#@-node:eggs
+#@-others
+#@nonl
+#@-node:@file tnodeList.py
+#@-leo
