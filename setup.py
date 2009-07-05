ec363af894fa8bceaa8bb061533a6c4e420397e3
Ville M. Vainio <ville@debian>
2009-07-05 22:41:32 +0300

    doc tweaks (mention debs, remove obsolete stuff from setup.py description)

--- setup.py
+++ setup.py
@@ -104,26 +104,19 @@ datapats = ['.tix', '.GIF', '.dbm', '.conf', '.TXT', '.xml', '.gif', '*.leo', '.
 
 setup(
     name = 'leo-editor',
-    version = "0.1",
-    author = "Edward Ream",
+    version = "4.6",
+    author = "Edward K. Ream",
     author_email = 'edreamleo@gmail.com',
     url = 'http://webpages.charter.net/edreamleo/front.html',
     packages = packages,
     data_files = data_files,
     package_data = {'leo.plugins' : datapats },
-    description = "A programmer's editor, and much more",
+    description = "A programmer's editor/outliner, and much more",
     long_description = """
 Leo is an outline-oriented editor written in 100% pure Python.
-Leo works on any platform that supports Python 2.2.1 and Tk 8.4 or above.
-You may download Python from http://python.org/ and
-tcl/Tk from http://tcl.activestate.com/software/tcltk/
-Leo features a multi-window outlining editor, Python colorizing,
+Leo features a multi-window outlining editor, syntax colorizing,
 powerful outline commands and many other things, including 
-Unlimited Undo/Redo and an integrated Python shell(IDLE) window.
-Leo will place its own icon in Leo windows provided that you have
-installed Fredrik Lundh's PIL and tkIcon packages:
-Download PIL from http://www.pythonware.com/downloads/index.htm#pil
-Download tkIcon from http://www.effbot.org/downloads/#tkIcon
+unlimited Undo/Redo and scriptability.
     """,
     scripts = ['leo/scripts/leo'],
 
