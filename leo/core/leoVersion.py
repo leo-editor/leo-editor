3076fe3ba878d121f066959d8100e7d06d827695
Edward K. Ream <edreamleo@gmail.com>
2009-07-17 13:09:21 -0500

    add file to encapsulate version info

---
+++ leo/core/leoVersion.py
@@ -0,0 +1,20 @@
+#@+leo-ver=4-thin
+#@+node:ekr.20090717092906.12765:@thin leoVersion.py
+'''A module holding version-related info.'''
+
+#@@language python
+#@@tabwidth -4
+
+#@+others
+#@+node:ekr.20090717092906.12766:leoVersion.getVersion
+def getVersion():
+
+    return (
+        "Leo 4.7 devel", # Version number.
+        "July 17, 2009" # Date.
+    )
+#@-node:ekr.20090717092906.12766:leoVersion.getVersion
+#@-others
+
+#@-node:ekr.20090717092906.12765:@thin leoVersion.py
+#@-leo
