fddf4b02002eea2dc161160e75707e5846e55e63
Edward K. Ream <edreamleo@gmail.com>
2009-07-17 08:52:16 -0500

    changed signon to 4.7 devel

--- leo/core/leoCommands.py
+++ leo/core/leoCommands.py
@@ -423,7 +423,7 @@ class baseCommands (object):
 
     def getSignOnLine (self):
         c = self
-        return "Leo 4.6 post rc1, build %s, July 12, 2009" % c.getBuildNumber()
+        return "Leo 4.7 devel, build %s, July 17, 2009" % c.getBuildNumber()
     #@-node:ekr.20040629121554.1:getSignOnLine (Contains hard-coded version info)
     #@+node:ekr.20040629121554.2:initVersion
     def initVersion (self):
