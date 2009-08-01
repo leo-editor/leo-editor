3c9f4ce17df41948241344fc36fdb480f523411f
Ville M. Vainio <vivainio@gmail.com>
2009-08-01 20:31:24 +0300

    tkGui.py: missing pmw fails in a cleaner fashion with tk ui

--- leo/plugins/tkGui.py
+++ leo/plugins/tkGui.py
@@ -61,6 +61,9 @@ def init():
     if not Tk:
         return False
 
+    if not Pmw:
+        g.pr("WARNING: Pmw is not available. Tk ui will not work. Install python-pmw to fix")
+        return False
     if g.app.gui:
         return g.app.gui.guiName() == 'tkinter'
     else:
@@ -589,8 +592,8 @@ class tkinterGui(leoGui.leoGui):
     #@-node:ekr.20081121110412.385:Font
     #@+node:ekr.20081121110412.387:getFullVersion (tkGui)
     def getFullVersion (self):
-
-        return 'Tk %s, Pmw %s' % (Tk.TkVersion,Pmw.version())
+        pmwver = Pmw and Pmw.version() or "UNAVAILABLE"
+        return 'Tk %s, Pmw %s' % (Tk.TkVersion,pmwver)
     #@-node:ekr.20081121110412.387:getFullVersion (tkGui)
     #@+node:ekr.20081121110412.388:Icons
     #@+node:ekr.20081121110412.389:attachLeoIcon & createLeoIcon
