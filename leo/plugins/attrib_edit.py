ca73683099bdb4d6344549c91b8b8a5e8ea93787
Terry Brown <terry_n_brown@yahoo.com>
2009-10-28 15:11:50 -0500

    added attrib manager

--- leo/plugins/attrib_edit.py
+++ leo/plugins/attrib_edit.py
@@ -1,5 +1,5 @@
 #@+leo-ver=4-thin
-#@+node:tbrown.20091009210724.10971:@thin attrib_edit.py
+#@+node:tbrown.20091026203923.1334:@thin /mnt/usr1/usr1/home/tbrown/.gnome-desktop/Package/leo/bzr/leo.repo/attrib_edit/leo/plugins/attrib_edit.py
 #@<< docstring >>
 #@+node:tbrown.20091009210724.10972:<< docstring >>
 '''attrib_edit.py  -- Edit certain attributes in v.uA
@@ -50,7 +50,6 @@ def init():
     leoPlugins.registerHandler('after-create-leo-frame',onCreate)
     g.plugin_signon(__name__)
     return True
-
 #@-node:tbrown.20091009210724.10975:init
 #@+node:tbrown.20091009210724.10976:onCreate
 def onCreate (tag,key):
@@ -64,13 +63,14 @@ class editWatcher(object):
     """class to supply widget for editing attribute and handle
     its textChanged signal"""
 
-    def __init__(self, v, name, value, path, type_):
+    def __init__(self, c, v, name, value, path, type_):
         """v - node whose attribute we edit
         name - name of edited attribute
         value - initial value of edited attribute
         path - dictionary key path to attribute in v.u
         type_ - attribute type
         """
+        self.c = c
         self.v = v
         self.name = name
         self.value = value
@@ -84,11 +84,19 @@ class editWatcher(object):
             self._widget = QtGui.QLineEdit(str(self.value))
             QtCore.QObject.connect(self._widget, 
                 QtCore.SIGNAL("textChanged(QString)"), self.updateValue)
+            # self._widget.focusOutEvent = self.lostFocus
+            # see lostFocus()
         return self._widget
 
     def updateValue(self, newValue):
         """copy value from widget to v.u"""
         self.setValue(self.v.u, self.path, self.type_(newValue))
+        self.v.setDirty()
+
+    def lostFocus(self, event):
+        """Can link this in in widget(), but it stops tabbing through
+        the attributes"""
+        self.c.redraw()
 
     @staticmethod
     def setValue(a, path, value):
@@ -98,8 +106,66 @@ class editWatcher(object):
         for i in path[:-1]:
             a = a.setdefault(i, {})
         a[path[-1]] = value
-        print path,value
 #@-node:tbrown.20091010211613.5257:class editWatcher
+#@+node:tbrown.20091028131637.1353:class ListDialog
+class ListDialog(QtGui.QDialog):
+
+    #@    @+others
+    #@+node:tbrown.20091028131637.1354:__init__
+    def __init__(self, parent, title, text, entries):
+
+        self.entries = entries
+
+        QtGui.QDialog.__init__(self, parent)
+
+        vbox = QtGui.QVBoxLayout()
+
+
+        sa = QtGui.QScrollArea()
+        salo = QtGui.QVBoxLayout()
+        frame = QtGui.QFrame()
+        frame.setLayout(salo)
+
+        self.buttons = []
+
+        for entry in entries:
+            hbox = QtGui.QHBoxLayout()
+            cb = QtGui.QCheckBox(entry[0])
+            self.buttons.append(cb)
+            if entry[1]:
+                cb.setCheckState(Qt.Checked)
+            hbox.addWidget(cb)
+            salo.addLayout(hbox)
+
+        sa.setWidget(frame)
+        vbox.addWidget(sa)
+
+        hbox = QtGui.QHBoxLayout()
+
+        ok = QtGui.QPushButton("Ok")
+        cancel = QtGui.QPushButton("Cancel")
+
+        QtCore.QObject.connect(ok, QtCore.SIGNAL('clicked(bool)'), self.writeBack)
+        QtCore.QObject.connect(cancel, QtCore.SIGNAL('clicked(bool)'), self.reject)
+
+        hbox.addWidget(ok)
+        hbox.addWidget(cancel)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+
+    #@-node:tbrown.20091028131637.1354:__init__
+    #@+node:tbrown.20091028131637.1359:writeBack
+    def writeBack(self, event=None):
+
+        for n,i in enumerate(self.buttons):
+            self.entries[n][1] = (i.checkState() == Qt.Checked)
+
+        self.accept()
+    #@-node:tbrown.20091028131637.1359:writeBack
+    #@-others
+#@-node:tbrown.20091028131637.1353:class ListDialog
 #@+node:tbrown.20091009210724.10979:class attrib_edit_Controller
 class attrib_edit_Controller:
 
@@ -130,6 +196,7 @@ class attrib_edit_Controller:
 
         if self.guiMode == 'body':
             self.holder = QtGui.QSplitter(QtCore.Qt.Vertical)
+            self.holder.setMinimumWidth(300)
             os = c.frame.top.leo_body_frame.parent()
             self.holder.addWidget(c.frame.top.leo_body_frame)
             os.addWidget(self.holder)
@@ -166,6 +233,7 @@ class attrib_edit_Controller:
 
         pnl = QtGui.QFrame()
         self.form = QtGui.QFormLayout()
+        self.form.setVerticalSpacing(0)
         pnl.setLayout(self.form)
         pnl.setAutoFillBackground(True)
         w.addWidget(pnl)
@@ -173,23 +241,29 @@ class attrib_edit_Controller:
     #@+node:tbrown.20091009210724.11047:updateEditor
     def updateEditor(self,tag,k):
         """update edit panel when new node selected"""
-        c = self.c
 
         if k['c'] != self.c: return  # not our problem
+
+        self.updateEditorInt()
+    #@-node:tbrown.20091009210724.11047:updateEditor
+    #@+node:tbrown.20091028100922.1493:updateEditorInt
+    def updateEditorInt(self):
+
+        c = self.c
 
         self.initForm()
 
         for attr in self.getAttribs():
-            if len(attr) == 2:  # read only attribute
-                name, value = attr
+            name, value, path, type_, readonly = attr
+            if readonly:
                 self.form.addRow(QtGui.QLabel(name), QtGui.QLabel(str(value)))
-            else:               # editable attribute
-                name, value, path, type_ = attr
-                editor = editWatcher(c.currentPosition().v, name, value, path, type_)
+
+            else:
+                editor = editWatcher(c, c.currentPosition().v, name, value, path, type_)
                 self.editors.append(editor)
 
                 self.form.addRow(QtGui.QLabel(name), editor.widget())
-    #@-node:tbrown.20091009210724.11047:updateEditor
+    #@-node:tbrown.20091028100922.1493:updateEditorInt
     #@+node:tbrown.20091011151836.14789:recSearch
     def recSearch(self, d, path, ans):
         """recursive search of tree of dicts for values whose
@@ -209,29 +283,22 @@ class attrib_edit_Controller:
                             # ek is '_int' or similar
                             type_ = self.typeMap[ek]
                             for ekt in d[k][ek]:
-                                if k == '_edit':
-                                    ans.append((ekt, d[k][ek][ekt], path+['_edit',ek,ekt], type_))
-                                else:
-                                    ans.append((ekt, d[k][ek][ekt]))
+                                ans.append((ekt, d[k][ek][ekt], path+['_edit',ek,ekt],
+                                    type_, k != '_edit'))
                         else:
-                            if k == '_edit':
-                                ans.append((ek, d[k][ek], path+['_edit',ek], str))
-                            else:
-                                ans.append((ek, d[k][ek]))
+                            ans.append((ek, d[k][ek], path+['_edit',ek], str, k != '_edit'))
     #@-node:tbrown.20091011151836.14789:recSearch
     #@+node:tbrown.20091009210724.11211:getAttribs
     def getAttribs(self):
         """Return a list of tuples describing editable uAs.
 
-        (name, value, [path, type])
+        (name, value, path, type, readonly)
 
-        If only name and value are present then it's a read only attribute
-        for display only.
 
         e.g.
 
-        ('created', '2009-09-23', ['stickynotes','_edit','created'], str),
-        ('cars', 2, ['inventory','_edit','_int','cars'], int)
+        ('created', '2009-09-23', ['stickynotes','_edit','created'], str, False),
+        ('cars', 2, ['inventory','_edit','_int','cars'], int, False)
 
         Changes should be written back to
         v.uA['stickynotes']['_edit']['created'] and
@@ -244,46 +311,72 @@ class attrib_edit_Controller:
 
         return ans
     #@-node:tbrown.20091009210724.11211:getAttribs
-    #@+node:tbrown.20091011151836.14787:getNamespace
-    def getNamespace(self, event=None):
-        # JUNK
-       stateName = 'get-input'
-       k = self.c.k
-       state = k.getState(stateName)
-
-       if state == 0:
-           k.setLabelBlue('Namespace (space separated words): ',protect=True)
-           k.getArg(event,stateName,1,self.getNamespace)
-       else:
-           k.clearState()
-           self.namespace = k.arg
-    #@-node:tbrown.20091011151836.14787:getNamespace
     #@+node:tbrown.20091011151836.14788:createAttrib
-    def createAttrib(self, event=None):
-       stateName = 'get-input'
-       k = self.c.k
-       state = k.getState(stateName)
+    def createAttrib(self, event=None, readonly=False):
 
-       if state == 0:
-           k.setLabelBlue('Namespace (space separated words): ',protect=True)
-           k.getArg(event,stateName,1,self.createAttrib)
-       else:
-           k.clearState()
-           ns = k.arg.split()
-           if '_edit' not in ns:
-               ns.insert(-1, '_edit')
-           editWatcher.setValue(self.c.currentPosition().v.u, ns, '')
+        path,ok = QtGui.QInputDialog.getText(self.holder, "Enter attribute path",
+            "Enter path to attribute (space separated words)")
+
+        ns = str(path).split()  # not the QString
+        if not ok or not ns:
+            g.es("Cancelled")
+            return
+
+        type_ = {True: '_view', False: '_edit'}[readonly]
+
+        if type_ not in ns:
+            ns.insert(-1, type_)
+
+        editWatcher.setValue(self.c.currentPosition().v.u, ns, '')
+        self.c.currentPosition().v.setDirty()
+        self.c.redraw()
+
+        self.updateEditorInt()
     #@-node:tbrown.20091011151836.14788:createAttrib
-    #@+node:tbrown.20091011151836.5259:command attrib-create
+    #@+node:tbrown.20091028131637.1358:manageAttrib
+    def manageAttrib(self):
+
+        dat = [
+          ['foo', False],
+          ['foo', False],
+          ['foo', False],
+          ['foo', False],
+          ['bar', False],
+          ['foo', True],
+          ['foo', False],
+        ]
+
+        res = ListDialog(self.holder, "Enter attribute path",
+            "Enter path to attribute (space separated words)", 
+            dat)
+
+        print dat
+        res.exec_()
+        print dat
+    #@nonl
+    #@-node:tbrown.20091028131637.1358:manageAttrib
+    #@+node:tbrown.20091011151836.5259:command attrib-manage
+    @staticmethod
+    @g.command('attrib-manage')
+    def attrib_create(event):
+        event['c'].attribEditor.manageAttrib()
+    #@-node:tbrown.20091011151836.5259:command attrib-manage
+    #@+node:tbrown.20091028131637.1356:command attrib-create
     @staticmethod
     @g.command('attrib-create')
     def attrib_create(event):
         event['c'].attribEditor.createAttrib()
+    #@-node:tbrown.20091028131637.1356:command attrib-create
+    #@+node:tbrown.20091028100922.1492:command attrib-create-ro
+    @staticmethod
+    @g.command('attrib-create-ro')
+    def attrib_create_ro(event):
+        event['c'].attribEditor.createAttrib(readonly=True)
     #@nonl
-    #@-node:tbrown.20091011151836.5259:command attrib-create
+    #@-node:tbrown.20091028100922.1492:command attrib-create-ro
     #@-others
 #@-node:tbrown.20091009210724.10979:class attrib_edit_Controller
 #@-others
 #@nonl
-#@-node:tbrown.20091009210724.10971:@thin attrib_edit.py
+#@-node:tbrown.20091026203923.1334:@thin /mnt/usr1/usr1/home/tbrown/.gnome-desktop/Package/leo/bzr/leo.repo/attrib_edit/leo/plugins/attrib_edit.py
 #@-leo
