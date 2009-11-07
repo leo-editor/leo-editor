93194a09768e5a44593f2c0d03ddd51265fb9ec5
Edward K. Ream <edreamleo@gmail.com>
2009-11-07 14:49:39 -0600

    Added slide-show-file.py, illustrating an external file.
    More work on the slideshow.

---
+++ leo/test/slide-show-file.py
@@ -0,0 +1,27 @@
+#@+leo-ver=4-thin
+#@+node:ekr.20091107071654.1514:@thin ..\test\slide-show-file.py
+#@@language python
+
+# c, g and p are predefined only for Leo scripts.
+# They are not automatically defined in external files.
+
+#@+others
+#@+node:ekr.20091107071654.1515:class myClass
+class myClass:
+
+    #@    @+others
+    #@+node:ekr.20091107093823.1464:printHi
+    def printHi(self):
+
+        print('Hello from slide-show-file.py')
+    #@-node:ekr.20091107093823.1464:printHi
+    #@-others
+
+
+#@-node:ekr.20091107071654.1515:class myClass
+#@-others
+
+myClass().printHi()
+#@nonl
+#@-node:ekr.20091107071654.1514:@thin ..\test\slide-show-file.py
+#@-leo
