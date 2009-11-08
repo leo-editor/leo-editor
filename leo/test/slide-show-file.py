0942e3706eb8d875cc1a8b1b39ec6dede8f6dd0a
Edward K. Ream <edreamleo@gmail.com>
2009-11-07 18:01:10 -0600

    added short form of slideshow

--- leo/test/slide-show-file.py
+++ leo/test/slide-show-file.py
@@ -1,27 +1,6 @@
 #@+leo-ver=4-thin
-#@+node:ekr.20091107071654.1514:@thin ..\test\slide-show-file.py
-#@@language python
-
-# c, g and p are predefined only for Leo scripts.
-# They are not automatically defined in external files.
-
-#@+others
-#@+node:ekr.20091107071654.1515:class myClass
-class myClass:
-
-    #@    @+others
-    #@+node:ekr.20091107093823.1464:printHi
-    def printHi(self):
-
-        print('Hello from slide-show-file.py')
-    #@-node:ekr.20091107093823.1464:printHi
-    #@-others
-
-
-#@-node:ekr.20091107071654.1515:class myClass
-#@-others
-
-myClass().printHi()
+#@+node:ekr.20091107171305.1516:@thin ..\test\slide-show-file.py
+print('Hello from %s' % __file__)
 #@nonl
-#@-node:ekr.20091107071654.1514:@thin ..\test\slide-show-file.py
+#@-node:ekr.20091107171305.1516:@thin ..\test\slide-show-file.py
 #@-leo
