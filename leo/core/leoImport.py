bafe7877392376871c30e9bbc5b328ac19265f2d
Edward K. Ream <edreamleo@gmail.com>
2009-12-06 20:05:29 -0600

    fixed unicode errors in inport code

--- leo/core/leoImport.py
+++ leo/core/leoImport.py
@@ -365,7 +365,7 @@ class leoImportCommands (scanUtility):
     #@+node:ekr.20031218072017.1462:exportHeadlines
     def exportHeadlines (self,fileName):
 
-        c = self.c ; nl = self.output_newline
+        c = self.c ; nl = g.u(self.output_newline)
         p = c.p
         if not p: return
         self.setEncoding()
@@ -380,14 +380,14 @@ class leoImportCommands (scanUtility):
             return
         for p in p.self_and_subtree():
             head = p.moreHead(firstLevel,useVerticalBar=True)
-            head = g.toEncodedString(head,self.encoding,reportErrors=True)
-            theFile.write(head + nl)
+            s = g.toEncodedString(head + nl,self.encoding,reportErrors=True)
+            theFile.write(s)
         theFile.close()
     #@-node:ekr.20031218072017.1462:exportHeadlines
     #@+node:ekr.20031218072017.1147:flattenOutline
     def flattenOutline (self,fileName):
 
-        c = self.c ; nl = self.output_newline
+        c = self.c ; nl = g.u(self.output_newline)
         p = c.currentVnode()
         if not p: return
         self.setEncoding()
@@ -405,12 +405,12 @@ class leoImportCommands (scanUtility):
 
         for p in p.self_and_subtree():
             head = p.moreHead(firstLevel)
-            head = g.toEncodedString(head,self.encoding,reportErrors=True)
-            theFile.write(head + nl)
+            s = g.toEncodedString(head + nl,encoding=self.encoding,reportErrors=True)
+            theFile.write(s)
             body = p.moreBody() # Inserts escapes.
             if len(body) > 0:
-                body = g.toEncodedString(body,self.encoding,reportErrors=True)
-                theFile.write(body + nl)
+                s = g.toEncodedString(body + nl,self.encoding,reportErrors=True)
+                theFile.write(s)
         theFile.close()
     #@-node:ekr.20031218072017.1147:flattenOutline
     #@+node:ekr.20031218072017.1148:outlineToWeb
