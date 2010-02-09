22e950cb8d1e905d36d0d93e4e2c2024199ffb75
Edward K. Ream <edreamleo@gmail.com>
2010-02-09 11:42:41 -0600

    Removed cruft from leo/test folder.  All unit tests pass.

--- leo/test/qtui_generate.py
+++
@@ -1,4 +0,0 @@
-import os
-os.chdir('../plugins')
-os.system('pyuic4 -o qt_main.py qt_main.ui')
-os.system('pyuic4 -o qt_quicksearch.py qt_quicksearch.ui')
--- leo/test/slide-show-file.py
+++
@@ -1,6 +0,0 @@
-#@+leo-ver=4-thin
-#@+node:ekr.20091107171305.1516:@thin ..\test\slide-show-file.py
-print('Hello from %s' % __file__)
-#@nonl
-#@-node:ekr.20091107171305.1516:@thin ..\test\slide-show-file.py
-#@-leo
