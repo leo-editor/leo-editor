c148cffcffaf28cd9353d405c8ca167857dab21c
Edward K. Ream <edreamleo@gmail.com>
2010-02-11 16:02:41 -0600

    Added leo/test/goto-line-number-test.py, a simple, effective test. Added @test c.gotoLineNumber to unitTest.leo. It's not so easy to run this test externally with Alt-6. The unit test shows obvious bugs.  Will fix asap.

---
+++ leo/test/goto-line-number-test.py
@@ -0,0 +1,14 @@
+class child1:
+    def child1_method1():
+        pass
+    def child1_method2():
+        pass
+
+class child2:
+    def child2_method1():
+        pass
+    def child2_method2():
+        pass
+
+
+# last line
