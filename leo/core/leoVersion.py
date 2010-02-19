b9a483d5d2b44a17d9998d76209d5b67f8fdc11a
Edward K. Ream <edreamleo@gmail.com>
2010-02-18 18:58:46 -0600

    updated version in anticipation of 4.7 final

--- leo/core/leoVersion.py
+++ leo/core/leoVersion.py
@@ -6,12 +6,13 @@
 # Leo 4.6.1 final: July 30, 2009.
 # Leo 4.7 b2: January 29, 2010.
 # Leo 4.7 rc1: February 12, 2010.
+# Leo 4.7 final: February 21, 2010.
 
 #@@language python
 #@@tabwidth -4
 
-build = 2949
-date = "February 12, 2010"
-version = "4.7 rc 1"
+build = 2968
+date = "February 21, 2010"
+version = "4.7 final"
 #@-node:ekr.20090717092906.12765:@thin leoVersion.py
 #@-leo
