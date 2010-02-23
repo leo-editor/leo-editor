7ccd27aa2c6384913a34b588c6f0970765a7ec74
Edward K. Ream <edreamleo@gmail.com>
2010-02-23 06:56:28 -0600

    Preparing for Leo 4.7 final.  Added test files and updated distro files.

--- leo/core/leoVersion.py
+++ leo/core/leoVersion.py
@@ -6,13 +6,13 @@
 # Leo 4.6.1 final: July 30, 2009.
 # Leo 4.7 b2: January 29, 2010.
 # Leo 4.7 rc1: February 12, 2010.
-# Leo 4.7 final: February 21, 2010.
+# Leo 4.7 final: February 23, 2010.
 
 #@@language python
 #@@tabwidth -4
 
-build = 2968
-date = "February 21, 2010"
+build = 2985
+date = "February 23, 2010"
 version = "4.7 final"
 #@-node:ekr.20090717092906.12765:@thin leoVersion.py
 #@-leo
---
+++ leo/test/save-new-test.py
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<?xml-stylesheet ekr_test?>
+<leo_file>
+<leo_header file_format="2" tnodes="0" max_tnode_index="0" clone_windows="0"/>
+<globals body_outline_ratio="0.5" body_secondary_ratio="0.5">
+	<global_window_position top="50" left="50" height="500" width="700"/>
+	<global_log_window_position top="0" left="0" height="0" width="0"/>
+</globals>
+<preferences/>
+<find_panel_settings/>
+<vnodes>
+<v t="ekr.20100223062617.5373"><vh>@chapters</vh></v>
+<v t="ekr.20100223062617.5372"><vh>NewHeadline</vh></v>
+</vnodes>
+<tnodes>
+<t tx="ekr.20100223062617.5372"></t>
+<t tx="ekr.20100223062617.5373"></t>
+</tnodes>
+</leo_file>
