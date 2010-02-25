bd58b4a021e5100b57e182d78495056b09d5627e
Edward K. Ream <edreamleo@gmail.com>
2010-02-25 11:24:10 -0600

    Preparing distribution files for Leo 4.7.1.  Added readme, etc.

--- leo/core/leoVersion.py
+++ leo/core/leoVersion.py
@@ -4,15 +4,13 @@
 
 # Leo 4.5.1 final: September 14, 2008
 # Leo 4.6.1 final: July 30, 2009.
-# Leo 4.7 b2: January 29, 2010.
-# Leo 4.7 rc1: February 12, 2010.
-# Leo 4.7 final: February 23, 2010.
+# Leo 4.7.1 final: February 26, 2010.
 
 #@@language python
 #@@tabwidth -4
 
-build = 2985
-date = "February 23, 2010"
-version = "4.7 final"
+build = 3005
+date = "February 26, 2010"
+version = "4.7.1 final"
 #@-node:ekr.20090717092906.12765:@thin leoVersion.py
 #@-leo
