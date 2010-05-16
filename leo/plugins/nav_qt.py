24dd498e3eb11f3c3acc701eeb289843218421a1
Terry Brown <terry_n_brown@yahoo.com>
2010-05-16 13:03:28 -0500

    fix qt_nav for qt 4.7.2

--- leo/plugins/nav_qt.py
+++ leo/plugins/nav_qt.py
@@ -72,6 +72,7 @@ class pluginController:
     def __init__ (self,c):
 
         self.c = c
+        c._prev_next = self
         self.makeButtons()
         # Warning: hook handlers must use keywords.get('c'), NOT self.c.
     #@-node:ville.20090518182905.5426:__init__
@@ -81,12 +82,10 @@ class pluginController:
         if not ib_w: return # EKR: can be None when unit testing.
         icon_l = ib_w.style().standardIcon(QtGui.QStyle.SP_ArrowLeft)
         icon_r = ib_w.style().standardIcon(QtGui.QStyle.SP_ArrowRight)
-        act_l = QtGui.QAction(icon_l, 'prev', ib_w)           
-        act_r = QtGui.QAction(icon_r, 'next', ib_w)           
+        act_l = QtGui.QAction(icon_l, 'prev', ib_w, triggered=self.clickPrev)   
+        act_r = QtGui.QAction(icon_r, 'next', ib_w, triggered=self.clickNext)           
         self.c.frame.iconBar.add(qaction = act_l, command = self.clickPrev)
         self.c.frame.iconBar.add(qaction = act_r, command = self.clickNext)
-
-
     #@-node:ville.20090518182905.5427:makeButtons
     #@+node:ville.20090518182905.7867:clickPrev
     def clickPrev(self):
