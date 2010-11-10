1bab77ec430c15ad1e92b683e275658833befa80
Edward K. Ream <edreamleo@gmail.com>
2010-11-10 09:51:14 -0600

    comverted mod_speedups to new sentinels

--- leo/plugins/mod_speedups.py
+++ leo/plugins/mod_speedups.py
@@ -1,7 +1,7 @@
-#@+leo-ver=4-thin
-#@+node:ville.20090804155017.7590:@thin mod_speedups.py
-#@<< docstring >>
-#@+node:ville.20090804155017.7591:<< docstring >>
+#@+leo-ver=5-thin
+#@+node:ekr.20101110094759.5843: * @file mod_speedups.py
+#@+<< docstring >>
+#@+node:ville.20090804155017.7591: ** << docstring >>
 ''' Experimental speedups
 
 Various optimizations. Use at your own risk. 
@@ -10,40 +10,34 @@ If stuff breaks, disable this plugin before reporting bugs.
 
 
 '''
-#@-node:ville.20090804155017.7591:<< docstring >>
-#@nl
+#@-<< docstring >>
 
 __version__ = '0.0'
-#@<< version history >>
-#@+node:ville.20090804155017.7592:<< version history >>
+#@+<< version history >>
+#@+node:ville.20090804155017.7592: ** << version history >>
 #@@killcolor
 #@+at
 # 
 # 0.1 VMV first version
-#@-at
-#@-node:ville.20090804155017.7592:<< version history >>
-#@nl
+#@-<< version history >>
 
-#@<< imports >>
-#@+node:ville.20090804155017.7593:<< imports >>
+#@+<< imports >>
+#@+node:ville.20090804155017.7593: ** << imports >>
 import leo.core.leoGlobals as g
 import leo.core.leoPlugins as leoPlugins
 import os.path
 
 # Whatever other imports your plugins uses.
-#@nonl
-#@-node:ville.20090804155017.7593:<< imports >>
-#@nl
+#@-<< imports >>
 
 #@+others
-#@+node:ville.20090804155017.7594:init
+#@+node:ville.20090804155017.7594: ** init
 def init ():
 
     ok = True # This might depend on imports, etc.
 
     return ok
-#@-node:ville.20090804155017.7594:init
-#@+node:ville.20090804155017.7596:g.toUnicodeFileEncoding
+#@+node:ville.20090804155017.7596: ** g.toUnicodeFileEncoding
 def speedup_toUnicodeFileEncoding(s, arg = None):
     #if g:
         #print s,g.callers(10)
@@ -55,8 +49,7 @@ g.toUnicodeFileEncoding = speedup_toUnicodeFileEncoding
 
 
 
-#@-node:ville.20090804155017.7596:g.toUnicodeFileEncoding
-#@+node:ville.20090804155017.12332:os.path shortcuts
+#@+node:ville.20090804155017.12332: ** os.path shortcuts
 g.os_path_basename = os.path.basename
 g.os_path_split = os.path.split
 g.os_path_splitext = os.path.splitext
@@ -69,8 +62,7 @@ g.os_path_exists = os.path.exists
 
 
 
-#@-node:ville.20090804155017.12332:os.path shortcuts
-#@+node:ville.20090804155017.12333:os_path_finalize caching
+#@+node:ville.20090804155017.12333: ** os_path_finalize caching
 
 os_path_finalize_orig = g.os_path_finalize
 os_path_finalize_join_orig = g.os_path_finalize_join
@@ -115,8 +107,5 @@ g.os_path_finalize_join = os_path_finalize_join_cached
 g.os_path_expanduser = os_path_expanduser_cached
 #g.os_path_join = os_path_join_speedup
 
-#@-node:ville.20090804155017.12333:os_path_finalize caching
 #@-others
-#@nonl
-#@-node:ville.20090804155017.7590:@thin mod_speedups.py
 #@-leo
