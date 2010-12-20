9aa17c3a79ffd5cbe3f79d5f5f7109cd13cb50cb
Edward K. Ream <edreamleo@gmail.com>
2010-12-20 16:44:30 -0500

    Use three .txt files to contain most of the contents of unitTest.leo.
    This fixes bug 692352.
    
    All active unit tests pass on Ubuntu.

--- leo/test/unittest/at-thin-test.py
+++ leo/test/unittest/at-thin-test.py
@@ -1,18 +1,12 @@
-#@+leo-ver=4-thin
-#@+node:ekr.20090704085350.5022:@thin ../test/unittest/at-thin-test.py
+#@+leo-ver=5-thin
+#@+node:ekr.20090704085350.5022: * @thin ../test/unittest/at-thin-test.py
 #@@language python
 #@+others
-#@+node:ekr.20090704085350.5023:spam
+#@+node:ekr.20090704085350.5023: ** spam
 def spam():
     pass
-#@nonl
-#@-node:ekr.20090704085350.5023:spam
-#@+node:ekr.20090704085350.5024:eggs
+#@+node:ekr.20090704085350.5024: ** eggs
 def eggs():
     pass
-#@nonl
-#@-node:ekr.20090704085350.5024:eggs
 #@-others
-#@nonl
-#@-node:ekr.20090704085350.5022:@thin ../test/unittest/at-thin-test.py
 #@-leo
--- leo/test/unittest/errorTest.py
+++ leo/test/unittest/errorTest.py
@@ -1,5 +1,5 @@
-#@+leo-ver=4-thin
-#@+node:ekr.20070627082044.808:@thin ../test/unittest/errorTest.py
+#@+leo-ver=5-thin
+#@+node:ekr.20070627082044.808: * @thin ../test/unittest/errorTest.py
 # A file that contains functions with errors in them.
 # This is used to test error reporting in scripts
 
@@ -13,6 +13,4 @@ def testIndexError():
 
 # The next line is used by @test c.checkFileTimeStamp.   
 # timestamp: 1231502468.77
-#@nonl
-#@-node:ekr.20070627082044.808:@thin ../test/unittest/errorTest.py
 #@-leo
