21c1555db82c6727d60ff031990ddbf3ddd64e8d
Ville M. Vainio <vivainio@gmail.com>
2011-04-03 13:12:07 +0300

    more valuespace stuff

--- leo/plugins/valuespace.py
+++ leo/plugins/valuespace.py
@@ -40,10 +40,16 @@ def colorize_headlines_visitor(c,p, item):
         item.setFont(0,f)
     raise leoPlugins.TryNext
 #@+node:ville.20110403115003.10352: ** init
-def init ():
+import types, sys
 
-    import types
+@g.command('vs-reset')
+def vs_reset():    
     g.vs = types.ModuleType('vs')
+    sys.modules['vs'] = g.vs
+    
+def init ():
+    vs_reset()
+
     g.visit_tree_item.add(colorize_headlines_visitor)
     
     return True
@@ -51,6 +57,44 @@ def init ():
 init()
 #@+node:ville.20110403115003.10355: ** Commands
 #@+node:ville.20110403115003.10356: *3* vs-update
+import re
+
+def let(var, val):
+    print "Let ",var,val
+    g.vs.__dict__[var] = val
+
+def parse_body(c,p):
+    body = g.getScript(c,p)
+    print "Body"
+    print body
+    
+    backop = None
+    segs = re.finditer('^@x (.*)$', body,  re.MULTILINE)
+    
+    for mo in segs:
+        op = mo.group(1).strip()
+        print "Oper",op
+        if op.startswith('='):
+            print "Assign", op
+            backop = ('=', mo.group(1).rstrip('{').lstrip('='), mo.end(1))
+        if op == '{':
+            backop = ('runblock', mo.end(1))
+        if op == '}':
+            bo = backop[0]
+            print "backop",bo
+            if bo == '=':
+                let(backop[1], body[backop[2] : mo.start(1)])
+                
+                    
+            
+            
+            
+            
+        
+    
+        
+    print list(segs)
+  
 def update_vs(c, root_p = None):
     if root_p is not None:
         it = root_p.subtree()
@@ -58,14 +102,22 @@ def update_vs(c, root_p = None):
         it = c.all_unique_positions()
         
     for p in it:       
-        h = p.h
+        h = p.h.strip()
         if not h.startswith('@'):
             continue
             
         if h.startswith('@= '):
             var = h[3:].strip()
             g.vs.__dict__[var] = p.b.strip()
-                    
+        if h == '@a' or h.startswith('@a '):                
+            tail = h[2:]
+            parent = p.parent()
+            if tail:
+                g.vs.__dict__[tail] = parent.b.strip()
+                
+            parse_body(c,parent)
+            
+                                
         
         #g.es(p)
         
@@ -85,6 +137,26 @@ def vs_update(event):
     print "update valuaspace"
     
 #@+node:ville.20110403122307.5659: *3* @= foo
-"content"
+
+"""
+@x a = 12
+
+@x =foo {
+
+Some content
+for foo
+
+@x }
+
+
+@x {
+
+print "block"
+print "of commands"
+
+@x }
+
+"""
+#@+node:ville.20110403122307.10351: *4* @a anch
 #@-others
 #@-leo
