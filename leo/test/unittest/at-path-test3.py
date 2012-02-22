7517d209d0424e0e0f1000d0b9c431bf7c345870
Edward K. Ream <edreamleo@gmail.com>
2012-02-22 07:47:22 -0600

    fixed a unit test that failed when run externally

--- leo/test/unittest/at-path-test3.py
+++ leo/test/unittest/at-path-test3.py
@@ -1,5 +1,5 @@
 #@+leo-ver=5-thin
-#@+node:ekr.20120221220113.4837: * @thin at-path-test3.py
+#@+node:ekr.20120222074221.4892: * @thin at-path-test3.py
 #@@language python
 # unittest/at-path-test3.py 
 #@-leo
