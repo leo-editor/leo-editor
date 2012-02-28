2fd60be8544396b8a0078e99195ee8c1d3045570
Edward K. Ream <edreamleo@gmail.com>
2012-02-28 11:57:55 -0600

    Added doc tasks from email items tagged with "pre-writing"

--- leo/test/unittest/at-path-test1.py
+++ leo/test/unittest/at-path-test1.py
@@ -1,5 +1,5 @@
 #@+leo-ver=5-thin
-#@+node:ekr.20120228044415.4834: * @thin ../test/unittest/at-path-test1.py
+#@+node:ekr.20120228111043.4834: * @thin ../test/unittest/at-path-test1.py
 #@@language python
 # unittest/at-path-test1.py 
 #@-leo
--- leo/test/unittest/at-path-test3.py
+++ leo/test/unittest/at-path-test3.py
@@ -1,5 +1,5 @@
 #@+leo-ver=5-thin
-#@+node:ekr.20120228044415.4838: * @thin at-path-test3.py
+#@+node:ekr.20120228111043.4838: * @thin at-path-test3.py
 #@@language python
 # unittest/at-path-test3.py 
 #@-leo
