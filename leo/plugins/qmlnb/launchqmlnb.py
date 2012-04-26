ebc0bc9ed71532d713ca62d65e9714cc7342d981
Ville M. Vainio <vivainio@gmail.com>
2012-04-26 23:51:51 +0300

    hello world runs

--- leo/plugins/qmlnb/launchqmlnb.py
+++ leo/plugins/qmlnb/launchqmlnb.py
@@ -5,42 +5,40 @@ from PyQt4.QtGui import QApplication
 from PyQt4.QtDeclarative import QDeclarativeView
 
 
-# This class will emit the current date and time as a signal when
-# requested.
-class Now(QObject):
+def create_dv():
+    # Create the QML user interface.
+    view = QDeclarativeView()
+    view.setSource(QUrl('qml/leonbmain.qml'))
+    view.setResizeMode(QDeclarativeView.SizeRootObjectToView)
+    # Display the user interface and allow the user to interact with it.
+    view.setGeometry(100, 100, 400, 240)
+    view.show()
+    rootObject = view.rootObject()
+    return view
 
-    now = pyqtSignal(str)
+def main():
+    
 
-    def emit_now(self):
-        formatted_date = QDateTime.currentDateTime().toString()
-        self.now.emit(formatted_date)
-
-
-app = QApplication(sys.argv)
-
-now = Now()
-
-# Create the QML user interface.
-view = QDeclarativeView()
-view.setSource(QUrl('message.qml'))
-view.setResizeMode(QDeclarativeView.SizeRootObjectToView)
+    app = QApplication(sys.argv)
+    v = create_dv()
+    app.exec_()
+    
+if __name__ == '__main__':
+    main()
+    
+    
 
 # Get the root object of the user interface.  It defines a
 # 'messageRequired' signal and JavaScript 'updateMessage' function.  Both
 # can be accessed transparently from Python.
-rootObject = view.rootObject()
 
 # Provide the current date and time when requested by the user interface.
-rootObject.messageRequired.connect(now.emit_now)
+#rootObject.messageRequired.connect(now.emit_now)
 
 # Update the user interface with the current date and time.
-now.now.connect(rootObject.updateMessage)
+#now.now.connect(rootObject.updateMessage)
 
 # Provide an initial message as a prompt.
-rootObject.updateMessage("Click to get the current date and time")
+#rootObject.updateMessage("Click to get the current date and time")
 
-# Display the user interface and allow the user to interact with it.
-view.setGeometry(100, 100, 400, 240)
-view.show()
 
-app.exec_()
