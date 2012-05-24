6f64e46cef15aa1c112f52d65da92a72cf27b6b6
Edward K. Ream <edreamleo@gmail.com>
2012-05-24 08:45:55 -0500

    Fixed bug in vr-hide.  Removed vr-close, and renamed all viewrendered-x commands as vr-x.  All unit tests pass.

--- leo/plugins/viewrendered.py
+++ leo/plugins/viewrendered.py
@@ -265,27 +265,28 @@ def show_scrolled_message(tag, kw):
     
     return True
 #@+node:ekr.20110320120020.14490: ** Commands
-#@+node:ekr.20110917103917.3639: *3* g.command('close-rendering-pane')
-@g.command('viewrendered-close')
+#@+node:ekr.20110917103917.3639: *3* g.command('vr-hide')
+@g.command('vr-close')
 def close_rendering_pane(event):
     
     '''Close the rendering pane.'''
 
+    global controllers
     c = event.get('c')
     if c:
         vr = c.frame.top.findChild(QtGui.QWidget, 'viewrendered_pane')
         if vr:
             vr.deactivate()
             vr.deleteLater()
-#@+node:ekr.20110321085459.14462: *3* g.command('hide-rendering-pane')
-@g.command('viewrendered-hide')
-def hide_rendering_pane(event):
-    
-    '''A synonym for 'close-rendering-pane.'''
-    
-    close_rendering_pane(event)
-#@+node:ekr.20110321072702.14507: *3* g.command('lock-unlock-rendering-pane')
-@g.command('viewrendered-lock-toggle')
+            
+            h = c.hash()
+            if vr == controllers.get(h):
+                del controllers[h]
+            else:
+                g.trace('Can not happen: no controller for %s' % (c))
+            
+#@+node:ekr.20110321072702.14507: *3* g.command('vr-lock-toggle')
+@g.command('vr-lock-toggle')
 def lock_unlock_rendering_pane(event):
     
     '''Pause or play a movie in the rendering pane.'''
@@ -301,8 +302,8 @@ def lock_unlock_rendering_pane(event):
                 vr.unlock()
             else:
                 vr.lock()
-#@+node:ekr.20110320233639.5777: *3* g.command('pause-play-movie')
-@g.command('viewrendered-pause-play')
+#@+node:ekr.20110320233639.5777: *3* g.command('vr-pause-play')
+@g.command('vr-pause-play')
 def pause_play_movie(event):
     
     '''Pause or play a movie in the rendering pane.'''
@@ -319,15 +320,15 @@ def pause_play_movie(event):
                     vp.pause()
                 else:
                     vp.play()
-#@+node:ekr.20110917103917.3637: *3* g.command('show-rendering-pane')
-@g.command('viewrendered-show')
+#@+node:ekr.20110917103917.3637: *3* g.command('vr-show')
+@g.command('vr-show')
 def show_rendering_pane (event):
     
     '''A synonym for viewrendered.'''
     
     viewrendered(event)
-#@+node:ekr.20110317080650.14386: *3* g.command('toggle-rendering-pane')
-@g.command('viewrendered-show-toggle')
+#@+node:ekr.20110317080650.14386: *3* g.command('vr-show-toggle')
+@g.command('vr-show-toggle')
 def toggle_rendering_pane(event):
     
     '''Show or hide the rendering pane.'''
@@ -339,8 +340,8 @@ def toggle_rendering_pane(event):
             hide_rendering_pane(event)
         else:
             viewrendered(event)
-#@+node:ekr.20110321151523.14464: *3* g.command('update-rendering-pane')
-@g.command('viewrendered-update')
+#@+node:ekr.20110321151523.14464: *3* g.command('vr-update')
+@g.command('vr-update')
 def update_rendering_pane (event):
     
     '''Hide the rendering pane, but do not delete it.'''
@@ -352,8 +353,8 @@ def update_rendering_pane (event):
             g.es('Open a viewrendered pane first')
         else:
             vr.update(tag='view',keywords={'c':c,'force':True})
-#@+node:tbrown.20100318101414.5998: *3* g.command('viewrendered')
-@g.command('viewrendered')
+#@+node:tbrown.20100318101414.5998: *3* g.command('vr')
+@g.command('vr')
 def viewrendered(event):
     """Open render view for commander"""
     
@@ -382,12 +383,6 @@ def viewrendered(event):
             vr.resize(600, 600)
             vr.show()
     return vr
-#@+node:ekr.20110320120020.14475: *3* g.command('vr')
-@g.command('vr')
-def viewrendered_alias(event):
-    """A synonynm for the viewrendered command"""
-
-    viewrendered(event)
 #@+node:tbrown.20110629084915.35149: ** class ViewRenderedProvider
 class ViewRenderedProvider:
     #@+others
