32b424cfb6dc8aebbb230725fd937b8ff067d399
Terry Brown <terry_n_brown@yahoo.com>
2013-02-06 14:53:53 -0600

    add button to todo interface to find next todo node

--- leo/plugins/todo.py
+++ leo/plugins/todo.py
@@ -194,7 +194,9 @@ if g.app.gui.guiName() == "qt":
             self.connect(self.UI.butNext, QtCore.SIGNAL("clicked()"),
                 lambda: self.owner.c.selectVisNext())
                 
-
+            self.connect(self.UI.butNextTodo, QtCore.SIGNAL("clicked()"),
+                self.owner.find_todo)
+                
         #@+node:ekr.20111118104929.10203: *3* make_func
         def make_func(self, edit, toggle, method, default):
 
