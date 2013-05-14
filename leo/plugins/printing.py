5d2967b2d18502b49524c874096b672912f1cb9c
Jake Peck <gatesphere@gmail.com>
2013-05-14 09:35:18 -0400

    Added marked and expanded modes.  Also added docstring and leoSettings.leo entries.

--- leo/plugins/printing.py
+++ leo/plugins/printing.py
@@ -1,17 +1,106 @@
 #@+leo-ver=5-thin
-#@+node:peckj.20130513193024.6320: * @file printing.py
+#@+node:peckj.20130513115943.16246: * @file printing.py
 #@@language python
 #@@tabwidth -4
 
 #@+<< docstring >>
 #@+node:peckj.20130513115943.16252: ** << docstring >>
-'''This plugin allows printing nodes and outlines while using the
-PyQt GUI.'''
+'''Supports printing from the Qt GUI.
+
+Jacob M. Peck, 2013
+
+Commands
+========
+
+This plugin supports the following seven commands:
+    
+print-selected-node
+-------------------
+
+Opens up the print dialog to print the selected headline and node.
+
+print-preview-selected-node
+---------------------------
+
+Opens up the print preview dialog to preview the selected headline 
+and node.
+
+print-selected-node-body
+------------------------
+
+Opens up the print dialog to print the selected node body.
+
+print-preview-selected-node-body
+--------------------------------
+
+Opens up the print preview dialog to preview the selected node body.
+
+print-expanded-node
+-------------------
+
+Opens up the print dialog to print the expanded contents of the 
+selected node, with top-level headline.
+
+print-preview-expanded-node
+---------------------------
+
+Opens up the print preview dialog to preview the expanded contents 
+of theselected node, with top-level headline.
+
+print-expanded-node-body
+------------------------
+
+Opens up the print dialog to print the expanded node body.
+
+print-preview-expanded-node-body
+--------------------------------
+
+Opens up the print preview dialog to preview the expanded node 
+body.
+
+print-marked-nodes
+------------------
+
+Opens up the print dialog to print all marked nodes in the current
+outline, with headlines.
+
+print-preview-marked-nodes
+--------------------------
+
+Opens up the print preview dialog to preview all marked nodes in \
+the current outline, with headlines.
+
+print-marked-node-bodies
+------------------------
+
+Opens up the print dialog to print the bodies of all marked nodes
+in the current outline.
+
+print-preview-marked-node-bodies
+--------------------------------
+
+Opens up the print preview dialog to preview the bodies of all 
+marked nodes in the current outline.
+
+Settings
+========
+
+- ``@string printing-font-family = DejaVu Sans Mono``
+  The font family for printing.  A monospaced font is recommended.
+
+- ``@string printing-font-size = 12``
+  The font size for printing bodies, in px.  Due to limitations
+  of PyQt, the size of headlines cannot be changed.
+
+'''
 #@-<< docstring >>
 #@+<< version history >>
 #@+node:peckj.20130513115943.16254: ** << version history >>
 #@+at
 # 
+# version 0.1 (2013-05-14) - initial release
+# 
+#@@c
 #@-<< version history >>
 #@+<< imports >>
 #@+node:peckj.20130513115943.16253: ** << imports >>
@@ -70,6 +159,20 @@ class printingController:
         c.k.registerCommand('print-selected-node',shortcut=None,func=self.print_selected_node)
         c.k.registerCommand('print-preview-selected-node',
                             shortcut=None,func=self.print_preview_selected_node)
+        ## expanded node
+        c.k.registerCommand('print-expanded-node-body',shortcut=None,func=self.print_expanded_node_body)
+        c.k.registerCommand('print-preview-expanded-node-body',
+                            shortcut=None,func=self.print_preview_expanded_node_body)
+        c.k.registerCommand('print-expanded-node',shortcut=None,func=self.print_expanded_node)
+        c.k.registerCommand('print-preview-expanded-node',
+                            shortcut=None,func=self.print_preview_expanded_node)
+        ## marked nodes
+        c.k.registerCommand('print-marked-node-bodies',shortcut=None,func=self.print_marked_node_bodies)
+        c.k.registerCommand('print-preview-marked-node-bodies',
+                            shortcut=None,func=self.print_preview_marked_node_bodies)
+        c.k.registerCommand('print-marked-nodes',shortcut=None,func=self.print_marked_nodes)
+        c.k.registerCommand('print-preview-marked-nodes',
+                            shortcut=None,func=self.print_preview_marked_nodes)
     #@+node:peckj.20130513115943.22457: *3* helpers
     #@+node:peckj.20130513115943.22458: *4* construct stylesheet
     def construct_stylesheet(self):
@@ -80,12 +183,23 @@ class printingController:
     def construct_document(self, text, head=None):
         doc = QtGui.QTextDocument()
         doc.setDefaultStyleSheet(self.stylesheet)
-        text = text.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+        text = self.sanitize_html(text)
         if head:
-            head = head.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+            head = self.sanitize_html(head)
             contents = "<h1>%s</h1>\n<pre>%s</pre>" % (head, text)
         else:
             contents = "<pre>%s<pre>" % text
+        doc.setHtml(contents)
+        return doc
+    #@+node:peckj.20130514082859.5603: *4* construct complex document
+    def construct_complex_document(self, nodes, heads=False):
+        doc = QtGui.QTextDocument()
+        doc.setDefaultStyleSheet(self.stylesheet)
+        contents = ''
+        for n in nodes:
+            if heads:
+                contents += '<h1>%s</h1>\n' % (self.sanitize_html(n.h))
+            contents += '<pre>%s</pre>\n' % (self.sanitize_html(n.b))
         doc.setHtml(contents)
         return doc
     #@+node:peckj.20130513115943.22661: *4* print dialog
@@ -98,6 +212,15 @@ class printingController:
         dialog = QtGui.QPrintPreviewDialog()
         dialog.paintRequested.connect(doc.print_)
         dialog.exec_()
+    #@+node:peckj.20130514082859.5604: *4* sanitize html
+    def sanitize_html(self, html):
+        ''' quick and dirty way to make sure html is escaped properly for printing '''
+        return html.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+    #@+node:peckj.20130514082859.5610: *4* untangle
+    def untangle(self,p):
+        return g.getScript(self.c,p,
+            useSelectedText=False,
+            useSentinels=False)
     #@+node:peckj.20130513115943.22666: *3* selected node
     #@+node:peckj.20130513193024.6336: *4* print_selected_node_body
     def print_selected_node_body (self,event=None):
@@ -122,6 +245,70 @@ class printingController:
         ''' prints the selected node '''
         
         doc = self.construct_document(self.c.p.b, head=self.c.p.h)
+        self.print_preview_doc(doc)
+    #@+node:peckj.20130514082859.5616: *3* expanded selected node
+    #@+node:peckj.20130514082859.5617: *4* print_expanded_node_body
+    def print_expanded_node_body (self,event=None):
+        ''' prints the expanded selected node body '''
+         
+        doc = self.construct_document(self.untangle(self.c.p))
+        self.print_doc(doc)
+    #@+node:peckj.20130514082859.5618: *4* print_preview_expanded_node_body
+    def print_preview_expanded_node_body (self,event=None):
+        ''' print previews the expanded selected node body'''
+        
+        doc = self.construct_document(self.untangle(self.c.p))
+        self.print_preview_doc(doc)
+    #@+node:peckj.20130514082859.5619: *4* print_expanded_node
+    def print_expanded_node (self,event=None):
+        ''' prints the expanded selected node '''
+        
+        p = self.c.p
+        doc = self.construct_document(self.untangle(p), head=p.h)
+        self.print_doc(doc)
+    #@+node:peckj.20130514082859.5620: *4* print_preview_expanded_node
+    def print_preview_expanded_node (self,event=None):
+        ''' prints the expanced selected node '''
+        
+        p = self.c.p
+        doc = self.construct_document(self.untangle(p), head=p.h)
+        self.print_preview_doc(doc)
+    #@+node:peckj.20130514082859.5605: *3* marked nodes
+    #@+node:peckj.20130514082859.5606: *4* print_marked_node_bodies
+    def print_marked_node_bodies (self,event=None):
+        ''' prints the marked node bodies'''
+        nodes = []
+        for n in self.c.all_positions():
+            if n.isMarked():
+                nodes.append(n.v)
+        doc = self.construct_complex_document(nodes)
+        self.print_doc(doc)
+    #@+node:peckj.20130514082859.5607: *4* print_preview_marked_node_bodies
+    def print_preview_marked_node_bodies (self,event=None):
+        ''' print previews the marked node bodies'''
+        nodes = []
+        for n in self.c.all_positions():
+            if n.isMarked():
+                nodes.append(n.v)
+        doc = self.construct_complex_document(nodes)
+        self.print_preview_doc(doc)
+    #@+node:peckj.20130514082859.5608: *4* print_marked_nodes
+    def print_marked_nodes (self,event=None):
+        ''' prints the marked nodes'''
+        nodes = []
+        for n in self.c.all_positions():
+            if n.isMarked():
+                nodes.append(n.v)
+        doc = self.construct_complex_document(nodes, heads=True)
+        self.print_doc(doc)
+    #@+node:peckj.20130514082859.5609: *4* print_preview_marked_nodes
+    def print_preview_marked_nodes (self,event=None):
+        ''' print previews the marked nodes'''
+        nodes = []
+        for n in self.c.all_positions():
+            if n.isMarked():
+                nodes.append(n.v)
+        doc = self.construct_complex_document(nodes, heads=True)
         self.print_preview_doc(doc)
     #@-others
 #@-others
