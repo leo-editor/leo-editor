4c924e5a192723e2224a49cf88b41edcadbc0d33
Edward K. Ream <edreamleo@gmail.com>
2013-11-10 06:23:38 -0600

    completed first draft of vim @data node parsing (creating dicts)

--- leo/core/leoVim.py
+++ leo/core/leoVim.py
@@ -26,55 +26,93 @@ class VimCommands:
             # Valid selectors in @data vim-*-tails.
             
         # Call after setting ivars.
-        self.commands_d = self.create_commands_d()
-    #@+node:ekr.20131109170017.46983: *3* vc.create_commands_d
-    def create_commands_d(self):
-        
-        trace = True
+        self.create_dicts()
+        for ivar in ('command_tails_d','motion_tails_d','motions_d','commands_d'):
+            assert hasattr(self,ivar),ivar
+
+    #@+node:ekr.20131109170017.46983: *3* vc.create_dicts & helpers
+    def create_dicts(self):
+
         dump = True
-        c = self.c
-        def split(s):
-            i = g.skip_id(s,0,chars='_')
-            head = s[:i]
-            tail = s[i:].strip()
-            return head,tail
+        # Compute tails first.
+        self.command_tails_d = self.create_command_tails_d(dump)
+        self.motion_tails_d  = self.create_motion_tails_d(dump)
+        # Then motions.
+        self.motions_d = self.create_motions_d(dump)
+        # Then commands.
+        self.commands_d = self.create_commands_d(dump)
+    #@+node:ekr.20131110050932.16529: *4* create_command_tails_d
+    def create_command_tails_d(self,dump):
+
         # @data vim-command-tails
-        command_tails_d = {}
+        d = {}
         data = self.getData('vim-command-tails')
         for s in data:
-            kind,command = split(s)
+            kind,command = self.split_arg_line(s)
             if kind in self.tail_kinds:
-                command_tails_d[command] = kind
+                d[command] = kind
             else:
                 g.trace('bad kind: %s' % (s))
-        if dump: self.dump('command_tails_d',command_tails_d)
+        if dump: self.dump('command_tails_d',d)
+        return d
+    #@+node:ekr.20131110050932.16530: *4* create_motion_tails_d
+    def create_motion_tails_d(self,dump):
+
         # @data vim-motion-tails
-        motion_tails_d = {}
+        d = {}
         data = self.getData('vim-motion-tails')
         for s in data:
-            kind,command = split(s)
+            kind,command = self.split_arg_line(s)
             command = command.strip()
             if kind in self.tail_kinds:
-                motion_tails_d[command] = kind
+                d[command] = kind
             else:
                 g.trace('bad kind: %s' % (s))
-        if dump: self.dump('motion_tails_d',motion_tails_d)
+        if dump: self.dump('motion_tails_d',d)
+        return d
+    #@+node:ekr.20131110050932.16531: *4* create_motions_d
+    def create_motions_d(self,dump):
+        
         # @data vim-motions
-        motions_d = {}
+        d = {}
         data = self.getData('vim-motions')
         for command in data:
             command = command.strip()
-            motions_d[command] = motion_tails_d.get(command,'')
+            d[command] = self.motion_tails_d.get(command,'')
                 # List of possible tails
-        if dump: self.dump('motions_d',motions_d)
+        if dump: self.dump('motions_d',d)
+        return d
+    #@+node:ekr.20131110050932.16532: *4* create_commands_d
+    def create_commands_d(self,dump):
+        
         # @data vim-commands
         d = {}
         data = self.getData('vim-commands')
         for s in data:
-            func,command = split(s)
+            func,command = self.split_arg_line(s)
             command = command.strip()
             if command:
-                d[command] = [func,command_tails_d.get(command)]
+                ch = command[0]
+                tail = command[1:] or None
+                d2 = d.get(ch,{})
+                if d2:
+                    assert d2.get('ch') == ch
+                else:
+                    d2['ch']=ch
+                # Append the tail (including None) to d2['tail_chars']
+                aList = d2.get('tail_chars',[])
+                if tail in aList:
+                    g.trace('duplicate command tail: %s' % tail)
+                else:
+                    aList.append(tail)
+                    d2['tail_chars']=aList
+                # Also set d2['tail_pattern'] if tail is None.
+                if tail is None:
+                    if d2.get('tail_pattern'):
+                        g.trace('duplicate entry for %r' % (ch))
+                    else:
+                        d2['tail_pattern'] = self.command_tails_d.get(ch)
+                d[ch] = d2
             else:
                 g.trace('missing command chars: %s' % (s))
         if dump: self.dump('command_d',d)
@@ -82,13 +120,28 @@ class VimCommands:
     #@+node:ekr.20131109170017.46984: *3* vc.dump
     def dump(self,name,d):
         '''Dump a dictionary.'''
-        print('')
-        g.trace(name)
+        print('\nDump of %s' % name)
         for key in sorted(d.keys()):
             val = d.get(key)
             if type(val) in (type([]),type((),)):
                 val = ' '.join([z for z in val if z])
-            print('%5s %s' % (key,val))
+                print('%5s %s' % (key,val))
+            elif type(val) == type({}):
+                result = []
+                for key2 in sorted(val.keys()):
+                    val2 = val.get(key2)
+                    if type(val2) in (type([]),type((),)):
+                        val2 = ','.join([repr(z) if z is None else z for z in val2])
+                    pad = ' '*2
+                    if val2:
+                        result.append('%s%s %s' % (pad,key2,val2))
+                val3 = '\n'.join(result)
+                if len(result) == 1:
+                    print('%s %s' % (key,val3))
+                else:
+                    print('%s\n%s' % (key,val3))
+            else:
+                print('%5s %s' % (key,val))
     #@+node:ekr.20131109170017.46985: *3* vc.getData
     def getData(self,s):
         
@@ -102,6 +155,16 @@ class VimCommands:
                 g.trace('not found: %s' % s)
         else:
             return c.config.getData(s)
+    #@+node:ekr.20131110050932.16501: *3* vc.split_arg_line
+    def split_arg_line(self,s):
+        '''
+        Split line s into a head and tail.
+        The head is a python id; the tail is everything else.
+        '''
+        i = g.skip_id(s,0,chars='_')
+        head = s[:i]
+        tail = s[i:].strip()
+        return head,tail
     #@-others
 #@-others
 #@-leo
