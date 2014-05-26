01771a71802c0f13e7b602f9336f4ca8354c70e8
Edward K. Ream <edreamleo@gmail.com>
2014-05-26 12:42:38 -0500

    Revised imports.  Added test function.

--- leo/core/leoRope.py
+++ leo/core/leoRope.py
@@ -1,55 +1,58 @@
 # -*- coding: utf-8 -*-
 #@+leo-ver=5-thin
-#@+node:ekr.20140524144107.15803: * @file leoRope.py
+#@+node:ekr.20140526082700.18440: * @file leoRope.py
 #@@first
 #@+<< leoRope imports >>
-#@+node:ekr.20140525065558.15807: ** << leoRope imports >>
+#@+middle:ekr.20140526123310.17592: ** class RopeController
+#@+node:ekr.20140525065558.15807: *3* << leoRope imports >>
 import leo.core.leoGlobals as g
-import rope.base.project as project
-import rope.base.simplify as simplify
-# import rope.refactor.rename
-import rope.refactor as refactor
 import glob
+import imp
+import rope
+import time
+imp.reload(rope)
 #@-<< leoRope imports >>
+#@+others
+#@+node:ekr.20140526123310.17592: ** class RopeController
 class RopeController:
     #@+others
-    #@+node:ekr.20140525065558.15809: ** ctor
+    #@+node:ekr.20140525065558.15809: *3* ctor
     def __init__(self,c):
         self.c = c
-        self.proj = project.Project(g.app.loadDir)
-    #@+node:ekr.20140525065558.15806: ** modules
+        self.proj = rope.base.project.Project(g.app.loadDir)
+    #@+node:ekr.20140525065558.15806: *3* modules
     def modules(self):
         '''Return full path names of all Leo modules.'''
         aList = glob.glob(g.os_path_join(g.app.loadDir,'*.py'))
         return sorted(aList)
-    #@+node:ekr.20140525065558.15808: ** path
+    #@+node:ekr.20140525065558.15808: *3* path
     def path(self,fn):
         return g.os_path_join(g.app.loadDir,fn)
-    #@+node:ekr.20140525065558.15805: ** refactor
+    #@+node:ekr.20140525065558.15805: *3* refactor
     def refactor(self):
         '''Perform refactorings.'''
         proj = self.proj
         m = proj.get_resource(self.path('leoAtFile.py'))
         s = m.read()
         # Important: get an offset in actual code.
-        s = simplify.real_code(s)
+        s = rope.base.simplify.real_code(s)
         tag1 = 'atFile'
         tag2 = self.pep8_class_name(tag1)
         offset = s.find(tag1)
         if offset > -1:
-            changes = refactor.rename.Rename(proj,m,offset).get_changes(tag2)
+            changes = rope.refactor.rename.Rename(proj,m,offset).get_changes(tag2)
             g.trace(changes.get_description())
         else:
             g.trace('not found',tag)
         # prog.do(changes)
-    #@+node:ekr.20140525065558.15812: ** pep8_class_name
+    #@+node:ekr.20140525065558.15812: *3* pep8_class_name
     def pep8_class_name(self,s):
         '''Return the proper class name for s.'''
         assert s
         if s[0].islower():
             s = s[0].upper()+s[1:]
         return s
-    #@+node:ekr.20140525065558.15810: ** run
+    #@+node:ekr.20140525065558.15810: *3* run
     def run(self):
         '''run the refactorings.'''
         proj = self.proj
@@ -57,4 +60,14 @@ class RopeController:
         self.refactor()
         proj.close()
     #@-others
+#@+node:ekr.20140526123310.17593: ** test
+def test(c):
+    t1 = time.clock()
+    RopeController(c).run()
+    print('done: %s sec.' % g.timeSince(t1))
+#@-others
+
+#@@language python
+#@@tabwidth -4
+#@@pagewidth 70
 #@-leo
