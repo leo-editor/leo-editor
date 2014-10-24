8d5e54267274b094748d229964eef8a027d1f26e
Edward K. Ream <edreamleo@gmail.com>
2014-10-24 10:30:05 -0500

    Use leo.core.leoQt in qt_quicksearch.  This might get overridden by Qt designer, but the change must be made.
    Leo build: 20141024103006

--- leo/plugins/qt_quicksearch.py
+++ leo/plugins/qt_quicksearch.py
@@ -7,7 +7,8 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+### from PyQt4 import QtCore, QtGui
+from leo.core.leoQt import QtCore,QtGui
 
 class Ui_LeoQuickSearchWidget(object):
     def setupUi(self, LeoQuickSearchWidget):
