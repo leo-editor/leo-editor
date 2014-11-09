f34b1f3edc07373af87514d64b7eb05b307aeced
Edward K. Ream <edreamleo@gmail.com>
2014-11-09 07:25:31 -0600

    Added initial, do-nothing, version of watchfiles.py.  See the docstring for the intentions.
    Leo build: 20141109072532

---
+++ leo/plugins/watchfiles.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#@+leo-ver=5-thin
+#@+node:ekr.20141109072314.1: * @file watchfiles.py
+#@@first
+
+#@+<< watchfiles.py docstring >>
+#@+node:ekr.20141109072314.2: ** << watchfiles.py docstring >>
+'''
+A plugin to update Leo nodes when corresponding external files change.
+
+This implements the following wishlit items:
+    
+https://bugs.launchpad.net/leo-editor/+bug/1259974
+Leo should watch @ node contents for external updates
+
+https://bugs.launchpad.net/leo-editor/+bug/1259755
+Leo should keep the external editor's temp file up to date
+
+https://bugs.launchpad.net/leo-editor/+bug/1159490
+"Open with" fails to copy changes by external editor back to Leo-Editor node
+
+'''
+#@-<< watchfiles.py docstring >>
+
+#@+others
+#@-others
+#@@language python
+#@@tabwidth -4
+#@-leo
