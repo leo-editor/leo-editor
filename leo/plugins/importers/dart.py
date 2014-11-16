c18d7f384cbdee05a98f20f5876629c68b6c7cd0
Edward K. Ream <edreamleo@gmail.com>
2014-11-16 10:53:13 -0600

    Added importer/writer plugins for dart language.
    Leo build: 20141116105313

---
+++ leo/plugins/importers/dart.py
@@ -0,0 +1,39 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20141116100154.1: * @file importers/dart.py
+'''The @auto importer for dart.'''
+
+import leo.core.leoGlobals as g
+import leo.plugins.importers.basescanner as basescanner
+
+class DartScanner(basescanner.BaseScanner):
+    '''Scanner for .dart files.'''
+    #@+others
+    #@+node:ekr.20141116100154.7: ** dart.__init__
+    def __init__ (self,importCommands,atAuto):
+        '''Ctor for the DartScanner class.'''
+       
+        basescanner.BaseScanner.__init__(self,importCommands,atAuto=atAuto,language='dart')
+             # Init the base class.
+        # Set the parser delims (adapted from c).
+        self.blockCommentDelim1 = '/*'
+        self.blockCommentDelim2 = '*/'
+        self.blockDelim1 = '{'
+        self.blockDelim2 = '}'
+        self.classTags = ['class',]
+        self.extraIdChars = '$'
+        self.functionTags = []
+        self.lineCommentDelim = '//'
+        # self.lineCommentDelim2 = '#' # A hack: treat preprocess directives as comments(!)
+        self.outerBlockDelim1 = '{'
+        self.outerBlockDelim2 = '}'
+        self.outerBlockEndsDecls = False # To handle extern statement.
+        self.sigHeadExtraTokens = [] ### '*']
+        self.sigFailTokens = [';','=']
+        self.strict = False
+    #@-others
+    
+importer_dict = {
+    'class': DartScanner,
+    'extensions': ['.dart',],
+}
+#@-leo
---
+++ leo/plugins/writers/dart.py
@@ -0,0 +1,41 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20141116100154.2: * @file writers/dart.py
+'''The @auto write code for Emacs org-mode (.org) files.'''
+# pylint: disable=unused-import
+import leo.core.leoGlobals as g
+import leo.plugins.writers.basewriter as basewriter
+
+class DartWriter(basewriter.BaseWriter):
+    '''The writer class for .dart files.'''
+    # def __init__(self,c):
+        # basewriter.BaseWriter.__init__(self,c)
+    #@+others
+    #@+node:ekr.20141116100154.4: ** dart.write
+    def write (self,root):
+        """Write all the *descendants* of an .dart node."""
+        at = self
+        trace = False
+        root_level = root.level()
+        for p in root.subtree():
+            indent = p.level()-root_level
+            self.put('%s %s' % ('*'*indent,p.h))
+            for s in p.b.splitlines(False):
+                if not g.isDirective(s):
+                    if trace: g.trace(s)
+                    self.put(s)
+        root.setVisited()
+        return True
+    #@+node:ekr.20141116100154.18: ** dart.put (not used)
+    # def put(self,s):
+        # '''Write line s using at.os, taking special care of newlines.'''
+        # at = self.at
+        # at.os(s[:-1] if s.endswith('\n') else s)
+        # at.onl()
+    #@-others
+
+writer_dict = {
+    '@auto': [],
+    'class': DartWriter,
+    'extensions': ['.dart',],
+}
+#@-leo
