15a1945a898eef786f90eeb7de3d93b4590718fa
Edward K. Ream <edreamleo@gmail.com>
2014-12-11 07:11:42 -0600

    Bibtex plugin now works with Python 3.

--- leo/plugins/bibtex.py
+++ leo/plugins/bibtex.py
@@ -63,33 +63,36 @@ valid BibTeX file.
 import leo.core.leoGlobals as g
 
 # By Timo Honkasalo: contributed under the same license as Leo.py itself.
+__version__ = '0.5'
 #@+<< change log >>
 #@+node:timo.20050213160555.2: ** <<change log>>
 #@@nocolor-node
-#@+at 
+#@+at
 # 
-# 0.1
+# 0.1 Timo Honkasalo 2005/02/13
 # - @bibtex nodes introduced, writing the contents in a BibTeX format.
-#   Timo Honkasalo 2005/02/13
 # 
-# 0.2
+# 0.2 Timo Honkasalo 2005/02/14
 # - Importing BibTeX files added.
-#   Timo Honkasalo 2005/02/14
-# 
-# 0.3
+#   
+# 0.3 Timo Honkasalo 2005/02/15
 # - Automatic inserting of templates when new entries are created.
-#   Timo Honkasalo 2005/02/15
 # 
-# 0.4
+# 0.4 Timo Honkasalo 2005/03/02
 # - Some changes in writeTreeAsBibTex (better format), added entrytypes in globals.
 # - Greatly simplified and enhanced the performance of readBibTexFileIntoTree.
 # - Fixed parsing of files in readBibTexFileIntoTree: they are now split at '\n@' (whitespace stripped) instead of '@', so that fields may contain '@' (like a 'mailto' field most likely would).
 # - Changed <<write template>> to move cursor to the entry point of first field (16 columns right).
 # - Bugfix: templates now include commas after each field
-#   Timo Honkasalo 2005/03/02
+#   
+# 0.5 EKR: 2014/12/11: This plugin now works with Python 3.
+# - Use p, not v, for positions.
+# - Improved messages and cleaned up code.
+# 
 #@-<< change log >>
-#@+<< globals >>
-#@+node:timo.20050215183130: ** <<globals>>
+
+#@+<< define templates >>
+#@+node:timo.20050215183130: ** <<define templates>>
 templates = {
     '@article':'author       = {},\ntitle        = {},\njournal      = {},\nyear         = ',
     '@book':'author       = {},\ntitle        = {},\npublisher    = {},\nyear         = ',
@@ -105,10 +108,11 @@ templates = {
     '@proceedings':'title        = {},\nyear         = ',
     '@techreport':'author       = {},\ntitle        = {},\ninstitution  = {},\nyear         = ',
     '@unpublished':'author       = {},\ntitle        = {},\nnote         = {}'
-    }
+}
+#@-<< define templates >>
 entrytypes = list(templates.keys())
-entrytypes.append('@string') 
-#@-<< globals >>
+entrytypes.append('@string')
+
 #@+<< to do >>
 #@+node:timo.20050213185039: ** <<to do>>
 #@+at To do list (in approximate order of importance):
@@ -126,7 +130,6 @@ entrytypes.append('@string')
 # - Syntax highligting
 # 
 #@-<< to do >>
-
 #@+others
 #@+node:ekr.20100128073941.5370: ** init
 def init():
@@ -138,48 +141,10 @@ def init():
         g.registerHandler("headkey2",onHeadKey)
         g.plugin_signon(__name__)
     return ok
-#@+node:timo.20050213160555.3: ** onIconDoubleClick
-#
-# this does not check for proper filename syntax.
-# path is the current dir, or the place @folder points to
-# this should probably be changed to @path or so.
-
-def onIconDoubleClick(tag,keywords):
-    """Read or write a bibtex file when the node is double-clicked.
-
-    Write the @bibtex tree as bibtex file when the root node is double-clicked. 
-    If it has no child nodes, read bibtex file."""
-
-    v = keywords.get("p") or keywords.get("v")
-    c = keywords.get("c")
-    h = v.h.strip()
-    if g.match_word(h,0,"@bibtex"):
-        fname = h[8:]
-        if v.hasChildren():
-            #@+<< write bibtex file >>
-            #@+node:timo.20050213160555.6: *3* << write bibtex file >>
-            bibFile = open(fname,'w')
-            writeTreeAsBibTex(bibFile, v, c)
-
-            bibFile.close()
-            g.es('written: '+str(fname))
-            #@-<< write bibtex file >>
-        else:
-            #@+<< read bibtex file >>
-            #@+node:timo.20050214174623: *3* << read bibtex file >>
-            g.es('reading: ' + fname)
-            try: 
-                bibFile = open(fname,'r')
-            except IOError:
-                g.es('IOError: file not found')
-                return    
-            readBibTexFileIntoTree(bibFile, c)
-
-            bibFile.close()
-            #@-<< read bibtex file >>
 #@+node:timo.20050215222802: ** onHeadKey
 def onHeadKey(tag,keywords):
-    """Write template for the entry in body pane.
+    """
+    Write template for the entry in body pane.
 
     If body pane is empty, get template for the entry from a dictionary
     'templates ' and write it in the body pane.
@@ -188,48 +153,50 @@ def onHeadKey(tag,keywords):
     to check which key brought us here.    
     """
     # To do: check for duplicate keys here.
-    v = keywords.get("p") or keywords.get("v")
+    p = keywords.get("p") or keywords.get("v")
     c = keywords.get("c")
-    h = v.h.strip()
-    if h[:h.find(' ')] in templates.keys() and not v.b:
-        for p in v.parents():
+    h = p.h.strip()
+    if h[:h.find(' ')] in templates.keys() and not p.b:
+        for p in p.parents():
             if p.h[:8] == '@bibtex ':
-                #@+<< write template >>
-                #@+node:timo.20050215232157: *3* << write template >>
+                # write template
                 c.setBodyString(v,templates[h[:h.find(' ')]])
                 c.frame.body.wrapper.setInsertPoint(16)
                 return
-                #@-<< write template >>
-#@+node:timo.20050213160555.7: ** writeTreeAsBibTex
-def writeTreeAsBibTex(bibFile, vnode, c):
-    """Write the tree under vnode to the file bibFile"""
-    # body text of @bibtex node is ignored
-    dict = c.scanAllDirectives(p=vnode)
-    encoding = dict.get("encoding",None)
-    if encoding == None:
-        encoding = g.app.config.default_derived_file_encoding
-    strings = ''
-    entries = ''
-    # iterate over nodes in this tree
-    for v in vnode.subtree():    
-        h = v.h
-        h = g.toEncodedString(h,encoding,reportErrors=True)
-        if h.lower() == '@string':
-            typestring = '@string'
+#@+node:timo.20050213160555.3: ** onIconDoubleClick
+#
+# this does not check for proper filename syntax.
+# path is the current dir, or the place @folder points to
+# this should probably be changed to @path or so.
+
+def onIconDoubleClick(tag,keywords):
+    """
+    Read or write a bibtex file when the node is double-clicked.
+
+    Write the @bibtex tree as bibtex file when the root node is double-clicked. 
+    If it has no child nodes, read bibtex file.
+    """
+    p = keywords.get("p") or keywords.get("v")
+    c = keywords.get("c")
+    if not c or not p:
+        return
+    h = p.h.strip()
+    if g.match_word(h,0,"@bibtex"): 
+        fn = g.os_path_finalize_join(g.os_path_dirname(c.fileName() or ''),h[8:])
+        if p.hasChildren():
+            bibFile = open(fn,'w')
+            writeTreeAsBibTex(bibFile,p,c)
+            bibFile.close()
+            g.es('wrote: %s' % fn)
         else:
-            typestring = h[:h.find(' ')].lower()
-        if typestring in entrytypes:
-            s = v.b
-            s = g.toEncodedString(s,encoding,reportErrors=True)
-            if h == '@string': # store string declarations in strings
-                for i in s.split('\n'):
-                    if i and (not i.isspace()):
-                        strings = strings + '@string{' + i + '}\n'
-            else:  # store other stuff in entries  
-                entries = entries + typestring + '{' + h[h.find(' ')+1:]+  ',\n' + s + '}\n\n'
-    if strings:
-        bibFile.write(strings + '\n\n')
-    bibFile.write(entries)  
+            try: 
+                bibFile = open(fn,'r')
+            except IOError:
+                g.es('not found: %s' % fn,color='red')
+                return
+            g.es('reading: ' + fn) 
+            readBibTexFileIntoTree(bibFile,c)
+            bibFile.close()
 #@+node:timo.20050214174623.1: ** readBibTexFileIntoTree
 def readBibTexFileIntoTree(bibFile, c):
     """Read BibTeX file and parse it into @bibtex tree
@@ -240,7 +207,7 @@ def readBibTexFileIntoTree(bibFile, c):
     of tuples ('headline', 'body text') for each entry, all the strings in
     the first element. For each element of biblist, a vnode is created and
     headline and body text put into place."""
-    entrylist = biblist = []
+    entrylist, biblist = [],[]
     strings = ''
     rawstring = '\n'
     # read 'bibFile' by lines, strip leading whitespace and store as one 
@@ -248,9 +215,11 @@ def readBibTexFileIntoTree(bibFile, c):
     for i in [o.lstrip() for o in bibFile.readlines()]:
         rawstring = rawstring + i
     for i in rawstring.split('\n@')[1:]:
-        if i[:6] == 'string': # store all @string declarations into 'strings'
+        if i[:6] == 'string':
+            # store all @string declarations into 'strings'
             strings = strings + i[7:].strip()[:-1] + '\n'
-        else: # store all alse into 'entrylist'
+        else:
+            # store all alse into 'entrylist'
             entrylist.append(('@' + i[:i.find(',')].replace('{',' ').replace('(',
             ' ').replace('\n',''), i[i.find(',')+1:].rstrip().lstrip('\n')[:-1]))
     if strings:
@@ -258,14 +227,42 @@ def readBibTexFileIntoTree(bibFile, c):
     biblist = biblist + entrylist
     p = c.p
     for i in biblist:
-        v = p.insertAsLastChild()
-        c.setHeadString(v,g.toUnicode(i[0]))
-        c.setBodyString(v,g.toUnicode(i[1]))
-
-
-
-
-
+        p2 = p.insertAsLastChild()
+        c.setHeadString(p2,g.toUnicode(i[0]))
+        c.setBodyString(p2,g.toUnicode(i[1]))
+#@+node:timo.20050213160555.7: ** writeTreeAsBibTex
+def writeTreeAsBibTex(bibFile,root,c):
+    """Write root's tree to the file bibFile"""
+    # body text of @bibtex node is ignored
+    dict = c.scanAllDirectives(p=root)
+    encoding = dict.get("encoding",None)
+    if encoding == None:
+        encoding = g.app.config.default_derived_file_encoding
+    strings = ''
+    entries = ''
+    # iterate over nodes in this tree
+    for p in root.subtree():    
+        h = p.h
+        if h.lower() == '@string':
+            typestring = '@string'
+        else:
+            typestring = h[:h.find(' ')].lower()
+        if typestring in entrytypes:
+            s = p.b
+            if h == '@string':
+                # store string declarations in strings
+                for i in s.split('\n'):
+                    if i and (not i.isspace()):
+                        strings = strings + '@string{' + i + '}\n'
+            else:
+                # store other stuff in entries  
+                entries = (entries + typestring +
+                    '{' + h[h.find(' ')+1:]+  ',\n' + s + '}\n\n'
+                )
+    if strings:
+        s = g.toEncodedString(strings+'\n\n',encoding=encoding,reportErrors=True)
+        bibFile.write(s)
+    bibFile.write(entries)  
 #@-others
 #@@language python
 #@@tabwidth -4
