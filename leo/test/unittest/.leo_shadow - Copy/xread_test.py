cdf3393d8f18faee12171aaf5eadf2bdd097b2c6
Edward K. Ream <edreamleo@gmail.com>
2015-02-14 06:49:51 -0600

    Removed all active @shadow files from unitTest.leo and disabled two unit tests that used them. Removed unittest/.leo_shadow and unittest/.leo_shadow - Copy directories. This will make running unit tests on "foreign" machines much easier.

--- leo/test/unittest/.leo_shadow - Copy/xread_test.py
+++
@@ -1,8 +0,0 @@
-#@+leo-ver=4-thin
-#@+node:ekr.20090529115704.4565:@shadow unittest/read_test.py
-#@@language python
-#@@tabwidth -4
-#@+others
-#@-others
-#@-node:ekr.20090529115704.4565:@shadow unittest/read_test.py
-#@-leo
--- leo/test/unittest/.leo_shadow - Copy/xtest_1.py
+++
@@ -1,6 +0,0 @@
-#@+leo-ver=4-thin
-#@+node:ekr.20090529115704.4568:@shadow unittest/test_1.py
-# body of @shadow test node
-# The last line.
-#@-node:ekr.20090529115704.4568:@shadow unittest/test_1.py
-#@-leo
