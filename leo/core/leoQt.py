a360a721fda2a9f1d18f256dadfb47ffb9588cf1
Edward K. Ream <edreamleo@gmail.com>
2015-03-27 20:55:29 -0500

    leoQt.py sets all exported symbols to None when g.in_bridge is True.  Fast and effective.

--- leo/core/leoQt.py
+++ leo/core/leoQt.py
@@ -15,92 +15,101 @@ Callers are expected to use the *PyQt5* spellings of modules:
 
 # pylint: disable=unused-import
 # Define isQt,Qt,QtConst,QtCore,QtDeclarative,QtGui,QtWidgets,QUrl
-try:
-    isQt5 = True
-    from PyQt5 import Qt
-except ImportError:
-    isQt5 = False
-    try:
-        from PyQt4 import Qt
-    except ImportError:
-        print('leoQt.py: can not import either PyQt4 or PyQt5.')
-        raise
-if isQt5:
-    try:
-        from PyQt5 import QtCore
-        from PyQt5 import QtGui
-        from PyQt5 import QtWidgets
-        from PyQt5.QtCore import QUrl
-        QtConst = QtCore.Qt
-    except ImportError:
-        print('leoQt.py: can not fully import PyQt5.')
-else:
-    try:
-        from PyQt4 import QtCore
-        from PyQt4 import QtGui
-        from PyQt4.QtCore import QUrl
-        QtConst = QtCore.Qt
-        QtWidgets = QtGui
-    except ImportError:
-        print('leoQt.py: can not fully import PyQt4.')
-qt_version = QtCore.QT_VERSION_STR
-if 0:
-    import leo.core.leoGlobals as g
-    isNewQt = g.CheckVersion(qt_version,'4.5.0')
-    # print(qt_version,isNewQt)
 
-# Define phonon,Qsci,QtSvg,QtWebKitWidgets,uic.
-# These imports may fail without affecting the isQt5 constant.
-if isQt5:
-    try:
-        import PyQt5.QtDeclarative as QtDeclarative
-    except ImportError:
-        QtDeclarative = None
-    try:
-        import PyQt5.phonon as phonon
-        phonon = phonon.Phonon
-    except ImportError:
-        phonon = None
-    try:
-        from PyQt5 import Qsci
-    except ImportError:
-        Qsci = None
-    try:
-        import PyQt5.QtSvg as QtSvg
-    except ImportError:
-        QtSvg = None
-    try:
-        from PyQt5 import uic
-    except ImportError:
-        uic = None
-    try:
-        import PyQt5.QtWebKitWidgets as QtWebKitWidgets
-    except ImportError:
-        QtWebKitWidgets = None
+import leo.core.leoGlobals as g
+
+if g.in_bridge:
+    isQt5 = False
+    Qt = QtConst = QtCore = QtGui = QtWidgets = QUrl = None
+    QtDeclarative = Qsci = QtSvg = QtWebKitWidgets = None
+    phonon = uic = None
 else:
     try:
-        import PyQt4.QtDeclarative as QtDeclarative
+        isQt5 = True
+        from PyQt5 import Qt
     except ImportError:
-        QtDeclarative = None
-    try:
-        import PyQt4.phonon as phonon
-        phonon = phonon.Phonon
-    except ImportError:
-        phonon = None
-    try:
-        from PyQt4 import Qsci
-    except ImportError:
-        Qsci = None
-    try:
-        import PyQt4.QtSvg as QtSvg
-    except ImportError:
-        QtSvg = None
-    try:
-        from PyQt4 import uic
-    except ImportError:
-        uic = None
-    try:
-        import PyQt4.QtWebKit as QtWebKitWidgets # Name change.
-    except ImportError:
-        QtWebKitWidgets = None
+        isQt5 = False
+        try:
+            from PyQt4 import Qt
+        except ImportError:
+            print('leoQt.py: can not import either PyQt4 or PyQt5.')
+            raise
+    if isQt5:
+        try:
+            from PyQt5 import QtCore
+            from PyQt5 import QtGui
+            from PyQt5 import QtWidgets
+            from PyQt5.QtCore import QUrl
+            QtConst = QtCore.Qt
+        except ImportError:
+            print('leoQt.py: can not fully import PyQt5.')
+    else:
+        try:
+            from PyQt4 import QtCore
+            from PyQt4 import QtGui
+            from PyQt4.QtCore import QUrl
+            QtConst = QtCore.Qt
+            QtWidgets = QtGui
+        except ImportError:
+            print('leoQt.py: can not fully import PyQt4.')
+    qt_version = QtCore.QT_VERSION_STR
+    if 0:
+        import leo.core.leoGlobals as g
+        isNewQt = g.CheckVersion(qt_version,'4.5.0')
+        # print(qt_version,isNewQt)
+    
+    # Define phonon,Qsci,QtSvg,QtWebKitWidgets,uic.
+    # These imports may fail without affecting the isQt5 constant.
+    if isQt5:
+        try:
+            import PyQt5.QtDeclarative as QtDeclarative
+        except ImportError:
+            QtDeclarative = None
+        try:
+            import PyQt5.phonon as phonon
+            phonon = phonon.Phonon
+        except ImportError:
+            phonon = None
+        try:
+            from PyQt5 import Qsci
+        except ImportError:
+            Qsci = None
+        try:
+            import PyQt5.QtSvg as QtSvg
+        except ImportError:
+            QtSvg = None
+        try:
+            from PyQt5 import uic
+        except ImportError:
+            uic = None
+        try:
+            import PyQt5.QtWebKitWidgets as QtWebKitWidgets
+        except ImportError:
+            QtWebKitWidgets = None
+    else:
+        try:
+            import PyQt4.QtDeclarative as QtDeclarative
+        except ImportError:
+            QtDeclarative = None
+        try:
+            import PyQt4.phonon as phonon
+            phonon = phonon.Phonon
+        except ImportError:
+            phonon = None
+        try:
+            from PyQt4 import Qsci
+        except ImportError:
+            Qsci = None
+        try:
+            import PyQt4.QtSvg as QtSvg
+        except ImportError:
+            QtSvg = None
+        try:
+            from PyQt4 import uic
+        except ImportError:
+            uic = None
+        try:
+            import PyQt4.QtWebKit as QtWebKitWidgets # Name change.
+        except ImportError:
+            QtWebKitWidgets = None
 #@-leo
