72d03f3791a22248091678d7bb05428af043e6cc
Edward K. Ream <edreamleo@gmail.com>
2015-04-02 13:45:06 -0500

    Fixed a pylint complaint in cursesGui.py.

--- leo/plugins/cursesGui.py
+++ leo/plugins/cursesGui.py
@@ -120,7 +120,8 @@ class textGui(leoGui.LeoGui):
     def runMainLoop(self):
         self.text_run()
     #@+node:ekr.20150107090324.16: *3* runOpenFileDialog
-    def runOpenFileDialog(self,title,filetypes,defaultextension,multiple=False,startpath=None):
+
+    def runOpenFileDialog(self,c,title,filetypes,defaultextension,multiple=False,startpath=None):
 
         initialdir = g.app.globalOpenDir or g.os_path_abspath(os.getcwd())
         ret = get_input("Open which %s file (from %s?) > " % (repr(filetypes), initialdir))
