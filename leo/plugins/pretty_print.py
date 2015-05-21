51a97ea680603c0e89d126fac4d40c6396d8335e
Edward K. Ream <edreamleo@gmail.com>
2015-05-21 13:19:19 -0500

    Modified pretty_print.py plugin to use leoBeautify.PythonTokenBeautifier.
    Leo build: 20150521131920

--- leo/plugins/pretty_print.py
+++ leo/plugins/pretty_print.py
@@ -2,27 +2,28 @@
 #@+node:ekr.20041021120118: * @file pretty_print.py
 '''Customizes pretty printing.
 
-The plugin creates a do-nothing subclass of the default pretty printer. To
+The plugin creates a do-nothing subclass of Leo's tokenizing pretty printer. To
 customize, simply override in this file the methods of the base prettyPrinter
-class in leoCommands.py. You would typically want to override putNormalToken or
+class in leoBeautify.py. You would typically want to override putNormalToken or
 its allies. Templates for these methods have been provided. You may, however,
 override any methods you like. You could even define your own class entirely,
 provided you implement the prettyPrintNode method.
 
 '''
 import leo.core.leoGlobals as g
-import leo.core.leoCommands as leoCommands
+import leo.core.leoBeautify as leoBeautify
 #@+others
 #@+node:ekr.20100128073941.5378: ** init
 def init():
     '''Return True if the plugin has loaded successfully.'''
-    ok = not g.app.unitTesting # Not for unit testing:  modifies core.
+    ok = not g.app.unitTesting # Not for unit testing: modifies core.
     if ok:
-        leoCommands.Commands.prettyPrinter = MyPrettyPrinter
+        # Huh?
+        # leoBeautify.prettyPrinter = MyPrettyPrinter
         g.plugin_signon(__name__)
     return ok
 #@+node:ekr.20041021120454: ** class MyPrettyPrinter
-class MyPrettyPrinter(leoCommands.Commands.PythonPrettyPrinter):
+class MyPrettyPrinter(leoBeautify.PythonTokenBeautifier):
 
     '''An example subclass of Leo's PrettyPrinter class.
 
@@ -33,10 +34,10 @@ class MyPrettyPrinter(leoCommands.Commands.PythonPrettyPrinter):
     #@+node:ekr.20041021123018: *3* myPrettyPrinter.__init__
     def __init__ (self,c):
         '''Ctor for MyPrettyPrinter class.'''
-        leoCommands.Commands.PythonPrettyPrinter.__init__(self,c)
+        leoBeautify.PythonTokenBeautifier.__init__(self,c)
             # Init the base class.
         self.tracing = False
-        # g.pr("Overriding class leoCommands.PythonPrettyPrinter")
+        # g.pr("Overriding class leoBeautifier.PythonTokenBeautifier")
     #@-others
 #@-others
 #@@language python
