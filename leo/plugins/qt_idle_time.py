8b441efd574ec4f5c0d56c04192989ae95f840c5
Edward K. Ream <edreamleo@gmail.com>
2016-04-13 07:55:46 -0500

    added disabled traces

--- leo/plugins/qt_idle_time.py
+++ leo/plugins/qt_idle_time.py
@@ -51,7 +51,7 @@ class IdleTime:
     #@+node:ekr.20140825042850.18406: *3* IdleTime.__init__
     def __init__(self, handler, delay=500, tag=None):
         '''ctor for IdleTime class.'''
-        # g.trace(tag)
+        # g.trace('(IdleTime)', tag)
         # For use by handlers...
         self.count = 0
             # The number of times handler has been called.
@@ -90,6 +90,7 @@ class IdleTime:
     #@+node:ekr.20140825042850.18407: *3* IdleTime.at_idle_time
     def at_idle_time(self):
         '''Call self.handler not more than once every self.delay msec.'''
+        # g.trace(self.tag)
         if g.app.killed:
             self.stop()
         elif self.enabled:
