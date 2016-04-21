a4d0fdd9b3a478c5e4840b3672c07047ceef448a
Edward K. Ream <edreamleo@gmail.com>
2016-04-21 09:39:49 -0500

    improved leo-bridge-test.py

--- leo/test/leo-bridge-test.py
+++ leo/test/leo-bridge-test.py
@@ -1,23 +1,49 @@
 '''A simple test bed for the leoBridge module.'''
 import os
 import sys
-abspath, join = os.path.abspath, os.path.join
-dir_ = abspath('.')
+
+# Leo files.
+files = [
+    'c:/leo.repo/leo-editor/leo/test/unitTest.leo',
+    'c:/leo.repo/leo-editor/leo/test/test.leo',
+    'c:/xyzzy.xxx',
+]
+
+# Switches...
+gui='nullGui'           # 'nullGui', 'qt',
+loadPlugins=False       # True: attempt to load plugins.
+readSettings=True       # True: read standard settings files.
+silent=True             # True: don't print signon messages.
+trace_sys_path = False  # True: trace imports here.
+verbose=True            # True: verbose output.
+
+# Import stuff...
+dir_ = os.path.abspath('.')
 if dir_ not in sys.path:
-    # print('append to sys.path: %s' % dir_)
+    if trace_sys_path: print('appending %s to sys.path' % dir_)
     sys.path.append(dir_)
-# import leo.core.leoQt as leoQt
-    # Required only if we want to use the qt gui.
+if trace_sys_path:
+    print('sys.path:...\n%s' % '\n'.join(sorted(sys.path)))
+    print('sys.argv: %s %s' % (len(sys.argv), '\n'.join(sys.argv)))
+if gui == 'qt':
+    # Do this here to bypass leoQt g.in_bridge logic.
+    import leo.core.leoQt as leoQt
 import leo.core.leoBridge as leoBridge
-# print('sys.path:...\n%s' % '\n'.join(sorted(sys.path)))
-# print('sys.argv: %s %s' % (len(sys.argv), '\n'.join(sys.argv)))
 
 controller = leoBridge.controller(
-    gui='nullGui',
-    # gui='qt', # not qttabs
-    loadPlugins=False,  # True: attempt to load plugins.
-    readSettings=False, # True: read standard settings files.
-    silent=True,      # True: don't print signon messages.
-    verbose=True)     # True: print informational messages.
+    gui=gui,
+    loadPlugins=loadPlugins,
+    readSettings=readSettings,
+    silent=silent,
+    verbose=verbose)
 g = controller.globals()
-# c = controller.openLeoFile(path)
+for path in files:
+    if os.path.exists(path):
+        c = controller.openLeoFile(path)
+        if c:
+            n = 0
+            for p in c.all_positions():
+                n += 1
+            print('%s has %s nodes' % (c.shortFileName(), n))
+    else:
+        print('file not found: %s' % path)
