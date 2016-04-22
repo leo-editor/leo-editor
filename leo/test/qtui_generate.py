5b4e548c8ef6a1f6b85599ea2ef28ff7d42a65b7
Edward K. Ream <edreamleo@gmail.com>
2016-04-22 10:49:32 -0500

    Removed some unused test files from leo/test, and move one test to leo/test/unittest. All tests pass.

--- leo/test/qtui_generate.py
+++
@@ -1,6 +0,0 @@
-import os
-os.chdir('../plugins')
-os.system('pyuic4 -o qt_main.py qt_main.ui')
-# os.system('pyuic4 -o qt_quicksearch.py qt_quicksearch.ui')
-    # 2014/10/24: qt_quicksearch.py now contain modified code,
-    # sot it's not wise to run pyuic4 on qt_quicksearch.ui
