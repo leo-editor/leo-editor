ee82029e82c63e95af53da8f592b1677457c1c03
Edward K. Ream <edreamleo@gmail.com>
2016-05-17 14:37:52 -0500

    The flake8 and pyflakes commands are now operational, and completely separate.

--- leo/core/leoApp.py
+++ leo/core/leoApp.py
@@ -6,9 +6,17 @@
 #@+node:ekr.20120219194520.10463: ** << imports >> (leoApp)
 import leo.core.leoGlobals as g
 import leo.core.leoExternalFiles as leoExternalFiles
+try:
+    import flake8
+except ImportError:
+    flake8 = None
 import glob
 import os
 import optparse
+try:
+    import pyflakes
+except ImportError:
+    pyflakes = None
 import string
 import sys
 import time
@@ -49,6 +57,123 @@ def toggle_idle_time_events(event):
         g.disableIdleTimeHook()
     else:
         g.enableIdleTimeHook()
+#@+node:ekr.20160517133001.1: *3* flake8 command
+@g.command('flake8')
+def flake8_command(event):
+    '''
+    Run flake8 on all nodes of the selected tree,
+    or the first @<file> node in an ancestor.
+    '''
+    #@+others
+    #@+node:ekr.20160517133049.1: *4* class Flake8Command
+    class Flake8Command(object):
+        '''A class to run flake8 on all Python @<file> nodes in c.p's tree.'''
+
+        def __init__(self, c, quiet=False):
+            '''ctor for Flake8Command class.'''
+            self.c = c
+            self.quiet = quiet
+            self.seen = [] # List of checked paths.
+
+        #@+others
+        #@+node:ekr.20160517133049.2: *5* flake8.check_all
+        def check_all(self, paths):
+            '''Run flake8 on all paths.'''
+            from flake8 import engine, main
+            style = engine.get_style_guide(
+                parse_argv=False,
+                config_file=self.get_flake8_config(),
+            )
+            if not style:
+                return
+            report = style.check_files(paths=paths)
+            # Set statistics here, instead of from the command line.
+            options = style.options
+            options.statistics = True
+            options.total_errors = True
+            # options.benchmark = True
+            main.print_report(report, style)
+        #@+node:ekr.20160517133049.3: *5* flake8.find
+        def find(self, p):
+            '''Return True and add p's path to self.seen if p is a Python @<file> node.'''
+            found = False
+            if p.isAnyAtFileNode():
+                aList = g.get_directives_dict_list(p)
+                path = c.scanAtPathDirectives(aList)
+                fn = p.anyAtFileNodeName()
+                if fn.endswith('.py'):
+                    fn = g.os_path_finalize_join(path, fn)
+                    if fn not in self.seen:
+                        self.seen.append(fn)
+                        found = True
+            return found
+        #@+node:ekr.20160517133049.4: *5* flake8.get_flake8_config
+        def get_flake8_config(self):
+            '''Return the path to the pylint configuration file.'''
+            trace = False and not g.unitTesting
+            join = g.os_path_finalize_join
+            table = (
+                g.app.homeDir,
+                join(g.app.homeDir, '.leo'),
+                join(g.app.loadDir, '..', '..', 'leo', 'test'),
+            )
+            for base in ('flake8', 'flake8.txt'):
+                for path in table:
+                    fn = g.os_path_abspath(join(path, base))
+                    if g.os_path_exists(fn):
+                        if trace: g.trace('found:', fn)
+                        return fn
+            if not g.unitTesting:
+                g.es_print('no flake8 configuration file found in\n%s' % (
+                    '\n'.join(table)))
+            return None
+        #@+node:ekr.20160517133049.5: *5* flake8.run
+        def run(self, p=None):
+            '''Run flake8 on all Python @<file> nodes in c.p's tree.'''
+            c = self.c
+            root = p or c.p
+            # Make sure Leo is on sys.path.
+            leo_path = g.os_path_finalize_join(g.app.loadDir, '..')
+            if leo_path not in sys.path:
+                sys.path.append(leo_path)
+            # Run flake8 on all Python @<file> nodes in root's tree.
+            t1 = time.clock()
+            found = False
+            for p in root.self_and_subtree():
+                found |= self.find(p)
+            # Look up the tree if no @<file> nodes were found.
+            if not found:
+                for p in root.parents():
+                    if self.find(p):
+                        found = True
+                        break
+            # If still not found, expand the search if root is a clone.
+            if not found:
+                isCloned = any([p.isCloned() for p in root.self_and_parents()])
+                # g.trace(isCloned,root.h)
+                if isCloned:
+                    for p in c.all_positions():
+                        if p.isAnyAtFileNode():
+                            isAncestor = any([z.v == root.v for z in p.self_and_subtree()])
+                            # g.trace(isAncestor,p.h)
+                            if isAncestor and self.find(p):
+                                break
+            paths = list(set(self.seen))
+            if paths:
+                self.check_all(paths)
+            g.es_print('flake8: %s file%s in %s' % (
+                len(paths), g.plural(paths), g.timeSince(t1)))
+        #@-others
+    #@-others
+        # define class Flake8Command.
+    c = event.get('c')
+    if c:
+        if c.isChanged():
+            c.save()
+        if flake8:
+            Flake8Command(c).run()
+        else:
+            g.es_print('can not import flake8')
 #@+node:ekr.20150514125218.4: *3* join-leo-irc
 @g.command('join-leo-irc')
 def join_leo_irc(event=None):
@@ -130,14 +255,6 @@ def pylint_command(event):
         def run(self):
             '''Run Pylint on all Python @<file> nodes in c.p's tree.'''
             c, root = self.c, self.c.p
-            try:
-                import time
-                from pylint import lint # NOQA
-                    # flake8: lint imported but not used.
-                # in pythonN/Lib/site-packages.
-            except ImportError:
-                g.warning('can not import pylint')
-                return
             rc_fn = self.get_rc_file()
             if not rc_fn:
                 return
@@ -201,7 +318,7 @@ def pylint_command(event):
 @g.command('pyflakes')
 def pyflakes_command(event):
     '''
-    Run pylint on all nodes of the selected tree,
+    Run pyflakes on all nodes of the selected tree,
     or the first @<file> node in an ancestor.
     '''
     #@+others
@@ -212,46 +329,30 @@ def pyflakes_command(event):
         def __init__(self, c):
             '''ctor for PyflakesCommand class.'''
             self.c = c
-            self.flake8_main = None
-            self.flake8_engine = None
-            self.pyflakes_api = None
-            self.reporter_reporter = None
             self.seen = [] # List of checked paths.
+
         #@+others
         #@+node:ekr.20160516072613.6: *5* pyflakes.check_all
         def check_all(self, paths):
             '''Run pyflakes on fn.'''
-            if self.flake8_main:
-                # Prefer flake8 to 
-                flake8_style = self.flake8_engine.get_style_guide(
-                    parse_argv=False,
-                    config_file=self.get_flake8_config(),
-                )
-                report = flake8_style.check_files(paths=paths)
-                # Set statistics here, instead of from the command line.
-                options = flake8_style.options
-                options.statistics = True
-                options.total_errors = True
-                # options.benchmark = True
-                self.flake8_main.print_report(report, flake8_style)
-            else:
-                for fn in paths:
-                    # Report the file name.
-                    sfn = g.shortFileName(fn)
-                    print('pyflakes: %s' % sfn)
-                    s = g.readFileIntoUnicodeString(fn, silent=False)
-                    if not s.strip():
-                        return
-                    reporter = self.pyflakes_reporter.Reporter(
-                        warningStream=sys.stderr,
-                        errorStream=sys.stderr)
-                    self.pyflakes_api.check(s, sfn, reporter)
+            from pyflakes import api, reporter
+            for fn in paths:
+                # Report the file name.
+                sfn = g.shortFileName(fn)
+                print('pyflakes: %s' % sfn)
+                s = g.readFileIntoUnicodeString(fn, silent=False)
+                if not s.strip():
+                    return
+                r = reporter.Reporter(
+                    errorStream=sys.stderr,
+                    warningStream=sys.stderr,
+                    )
+                api.check(s, sfn, r)
         #@+node:ekr.20160516072613.3: *5* pyflakes.find
         def find(self, p):
             '''Return True and add p's path to self.seen if p is a Python @<file> node.'''
             found = False
             if p.isAnyAtFileNode():
-                # Fix bug: https://github.com/leo-editor/leo-editor/issues/67
                 aList = g.get_directives_dict_list(p)
                 path = c.scanAtPathDirectives(aList)
                 fn = p.anyAtFileNodeName()
@@ -261,50 +362,11 @@ def pyflakes_command(event):
                         self.seen.append(fn)
                         found = True
             return found
-        #@+node:ekr.20160516111656.1: *5* pyflakes.get_flake8_config
-        def get_flake8_config(self):
-            '''Return the path to the pylint configuration file.'''
-            trace = False and not g.unitTesting
-            base = 'flake8.txt'
-            table = (
-                g.os_path_finalize_join(g.app.homeDir, base),
-                    # In ~/
-                g.os_path_finalize_join(g.app.homeDir, '.leo', base),
-                    # In ~/.leo
-                g.os_path_finalize_join(g.app.loadDir, '..', '..', 'leo', 'test', base),
-                    # In leo/test
-            )
-            for base in ('flake8', 'flake8.txt'):
-                for fn in table:
-                    fn = g.os_path_abspath(fn)
-                    if g.os_path_exists(fn):
-                        if trace: g.trace('found:', fn)
-                        return fn
-            g.es_print('no flake8 configuration file found in\n%s' % (
-                '\n'.join(table)))
-            return None
         #@+node:ekr.20160516072613.5: *5* pyflakes.run
         def run(self, p=None):
             '''Run Pyflakes on all Python @<file> nodes in c.p's tree.'''
             c = self.c
             root = p or c.p
-            try:
-                import flake8.main
-                import flake8.engine
-                self.flake8_engine = flake8.engine
-                self.flake8_main = flake8.main
-            except Exception:
-                flake8 = None
-            try:
-                import pyflakes.api
-                import pyflakes.reporter
-                self.pyflakes_api = pyflakes.api
-                self.pyflakes_reporter = pyflakes.reporter
-                # in pythonN/Lib/site-packages.
-            except ImportError:
-                if flake8 is None:
-                    g.warning('can not import flake8 or pyflakes')
-                    return
             # Make sure Leo is on sys.path.
             leo_path = g.os_path_finalize_join(g.app.loadDir, '..')
             if leo_path not in sys.path:
@@ -323,12 +385,10 @@ def pyflakes_command(event):
             # If still not found, expand the search if root is a clone.
             if not found:
                 isCloned = any([p.isCloned() for p in root.self_and_parents()])
-                # g.trace(isCloned,root.h)
                 if isCloned:
                     for p in c.all_positions():
                         if p.isAnyAtFileNode():
                             isAncestor = any([z.v == root.v for z in p.self_and_subtree()])
-                            # g.trace(isAncestor,p.h)
                             if isAncestor and self.find(p):
                                 break
             paths = list(set(self.seen))
@@ -343,7 +403,10 @@ def pyflakes_command(event):
     if c:
         if c.isChanged():
             c.save()
-        PyflakesCommand(c).run()
+        if pyflakes:
+            PyflakesCommand(c).run()
+        else:
+            g.es_print('can not import pyflakes')
 #@+node:ekr.20120209051836.10241: ** class LeoApp
 class LeoApp(object):
     """A class representing the Leo application itself.
@@ -1477,13 +1540,10 @@ class LeoApp(object):
             g.app.createDefaultGui(fileName='g.app.setLeoId', verbose=True)
         if g.app.gui is None: # Neither gui could be created: this should never happen.
             g.es_debug("Please enter LeoID (e.g. your username, 'johndoe'...)")
-            # pylint: disable=undefined-variable
-            # raw_input does not exist in Python 3.
-            if g.isPython3:
-                leoid = input('LeoID: ')
-            else:
-                leoid = raw_input('LeoID: ') # NOQA
-                    # flake8: undefined name 'raw_input'
+            import builtins
+            f = builtins.input if g.isPython3 else builtins.raw_input
+                # Use this to suppresss pyflakes complaint.
+            leoid = f('LeoID: ')
         else:
             leoid = g.app.gui.runAskLeoIDDialog()
         # Bug fix: 2/6/05: put result in g.app.leoID.
@@ -2357,8 +2417,9 @@ class LoadManager(object):
         import leo.core.leoSessions as leoSessions
         # Import leoIPython only if requested.  The import is quite slow.
         if g.app.useIpython:
-            import leo.core.leoIPython as leoIPython # NOQA
-                # flake8: LeoIPython imported but unused.
+            import leo.core.leoIPython as leoIPython
+                # This launches the IPython Qt Console.  It *is* required.
+            assert leoIPython # suppress pyflakes/flake8 warning.
         # Make sure we call the new leoPlugins.init top-level function.
         leoPlugins.init()
         # Force the user to set g.app.leoID.
