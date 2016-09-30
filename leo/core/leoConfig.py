d4a4655057a7b8802bd57329b77cc9c6e4aef0fc
Edward K. Ream <edreamleo@gmail.com>
2016-09-30 05:12:41 -0500

    fixed a crasher in SettingsFinder.find_setting

--- leo/core/leoConfig.py
+++ leo/core/leoConfig.py
@@ -2083,7 +2083,7 @@ class SettingsFinder(object):
         key = g.app.config.canonicalizeSettingName(setting)
         value = self.c.config.settingsDict.get(key)
         which = None
-        while value and value.val and value.val.startswith('@'):
+        while value and g.isString(value.val) and value.val.startswith('@'):
             msg = ("The relevant setting, '@{specific}', is using the value of "
             "a more general setting, '{general}'.  Would you like to edit the "
             "more specific setting, '@{specific}', or the more general setting, "
