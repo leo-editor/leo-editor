32718328b2e0176f011d6bc23f829f91690f0dfa
Edward K. Ream <edreamleo@gmail.com>
2016-10-10 15:24:22 -0500

    Fixed a number of problems with the javascript scanner. - Added state.push/pop to reset the scanner properly when parsing inner constructs. - No indentation is ever given to @others or section references when gen_clean is False.   This is correct: it guarantees a correct import. - Only one StateScanner object is ever created.

--- leo/plugins/importers/javascript.py
+++ leo/plugins/importers/javascript.py
@@ -10,9 +10,10 @@ new_scanner = True
 class Block:
     '''A class describing a block and its possible rescans.'''
 
-    def __init__(self, lines, simple, base=None):
+    def __init__(self, lines, state, simple=None):
         '''Ctor for the Block class.'''
-        self.base = base
+        assert simple in (True, False), g.callers()
+        self.base = state.get_base()
         self.children = []
         self.lines = lines
         self.headline = ''
@@ -59,27 +60,26 @@ class Block:
 #@+node:ekr.20161004092007.1: ** class ScanState
 class ScanState(object):
     '''A class to store and update scanning state.'''
-    
-    def __init__(self, base=None):
-        '''Ctor for ScanState class.'''
-        if base:
-            curlies, parens = base
-        else:
-            curlies, parens = 0, 0
-        self.base_curlies = curlies
-        self.base_parens = parens
-        self.context = '' # in ('/', '/*', '"', "'")
-        self.curlies = curlies
-        self.parens = parens
-        # self.stack = []
+    #@+others
+    #@+node:ekr.20161010140018.1: *3* state.ctor & repr
+    def __init__(self, c, root):
+        '''Ctor for the singleton ScanState class.'''
+        # Ivars for traces...
+        self.c = c
+        self.root = root
+        # Ivars representing the scan state...
+        self.base_curlies = self.curlies = 0
+        self.base_parens = self.parens = 0
+        self.context = '' # in ('/*', '"', "'") Comments and regex do *not* create states.
+        self.stack = []
 
     def __repr__(self):
-        return 'ScanState: %3s context: %2r' % (
-            '{' * self.curlies + '(' * self.parens, self.context)
+        return 'ScanState: base: %3r state: %3r context: %2r' % (
+            '{' * self.base_curlies + '(' * self.base_parens, 
+            '{' * self.curlies + '(' * self.parens,
+            self.context)
             
     __str__ = __repr__
-    
-    #@+others
     #@+node:ekr.20161004092056.1: *3* state.continues_block and starts_block
     def continues_block(self):
         '''Return True if the just-scanned lines should be placed in the inner block.'''
@@ -92,7 +92,19 @@ class ScanState(object):
     #@+node:ekr.20161007053002.1: *3* state.get_base
     def get_base (self):
         '''Return the present counts.'''
+        assert not self.context, repr(self.context)
         return self.curlies, self.parens
+    #@+node:ekr.20161006182212.1: *3* state.push & pop
+    def pop(self):
+        '''Restore the base state from the stack.'''
+        self.base_curlies, self.base_parens = self.stack.pop()
+        
+    def push(self):
+        '''Save the base state on the stack and enter a new base state.'''
+        self.stack.append((self.base_curlies, self.base_parens),)
+        self.base_curlies = self.curlies
+        self.base_parens = self.parens
+
     #@+node:ekr.20161007061524.1: *3* state.scan_block
     def scan_block(self, i, lines):
         '''Scan lines[i:]. Return (i, lines).'''
@@ -121,43 +133,37 @@ class ScanState(object):
     #@+node:ekr.20161004071532.1: *3* state.scan_line
     def scan_line(self, s):
         '''Update the scan state by scanning s.'''
-        trace = False and not g.unitTesting
+        trace = False and self.root.h.endswith('alt.js') and not g.unitTesting
+        # if s.strip().startswith('// 1.02 here arrived at'): g.pdb()
         i = 0
         while i < len(s):
             progress = i
-            ch = s[i]
+            ch, s2 = s[i], s[i:i+2]
             if self.context:
-                if self.context == '/':
-                    # A regex.
-                    if ch == '\\':
-                        i += 1
-                    elif ch == '/':
-                        self.context = ''
-                            # Regex modifiers won't affect the scan.
-                elif self.context == '/*' and s[i:i+2] == '*/':
+                if self.context == '/*' and s2 == '*/':
                     self.context = ''
                     i += 1
                 elif self.context == ch:
                     self.context = ''
                 else:
                     pass # Continue the present context
-            else:
-                if s[i:i+2] == '//':
-                    if trace: g.trace('found comment:', s.rstrip())
-                    break
-                elif ch == '/':
-                    self.context = '/' # A regex.
-                elif s[i:i+2] == '/*':
-                    self.context = '/*'
-                    i += 1
-                elif ch in ('"', "'"):
-                    self.context = ch
-                elif ch == '{': self.curlies += 1
-                elif ch == '}': self.curlies -= 1
-                elif ch == '(': self.parens += 1
-                elif ch == ')': self.parens -= 1
-                # elif ch == '[': self.squares += 1
-                # elif ch == ']': self.squares -= 1
+            elif s2 == '//':
+                break
+            # It's very hard to discover regex's.
+            # Instead, we'll just ignore \?
+            elif ch == '\\':
+                i += 2
+            elif s2 == '/*':
+                self.context = '/*'
+                i += 1
+            elif ch in ('"', "'"):
+                self.context = ch
+            elif ch == '{': self.curlies += 1
+            elif ch == '}': self.curlies -= 1
+            elif ch == '(': self.parens += 1
+            elif ch == ')': self.parens -= 1
+            # elif ch == '[': self.squares += 1
+            # elif ch == ']': self.squares -= 1
             i += 1
             assert progress < i
         if trace and s.strip().startswith('//') and self.continues_block():
@@ -308,8 +314,11 @@ class JavaScriptScanner(basescanner.BaseScanner):
     #@+node:ekr.20161007151845.1: *3* jss.rescan_block & helpers
     def rescan_block(self, parent_block, strip_lines=True):
         '''Rescan a non-simple block, possibly creating child blocks.'''
+        state = self.state
         trace = False and not g.unitTesting
-        if trace: g.trace('parent_block', len(parent_block.lines), parent_block.get_headline())
+        if trace: g.trace('len: %3s' % len(parent_block.lines), parent_block.get_headline())
+        # This is arbitrary, and so a bit confusing.
+        # However, it is about right in practice.
         if len(parent_block.lines) < 10:
             return
         if strip_lines:
@@ -322,8 +331,7 @@ class JavaScriptScanner(basescanner.BaseScanner):
             # This is the top-level block, when self.gen_refs is True.
             first_line, last_line = None, None
             lines = parent_block.lines
-        # Start the state with the proper base.
-        state = ScanState(base=parent_block.base)
+        state.push() # Set the bases to the present counts.
         blocks, block_lines = [], []
         i = 0
         while i < len(lines):
@@ -331,18 +339,23 @@ class JavaScriptScanner(basescanner.BaseScanner):
             line = lines[i]
             state.scan_line(line)
             if state.starts_block():
-                block_base = state.get_base()
-                if block_lines: blocks.append(Block(block_lines, simple=True))
+                base = state.get_base()
+                assert base is not None, 'rescan_block'
+                if block_lines:
+                    # Finish any previous block.
+                    blocks.append(Block(block_lines, state, simple=True))
+                # This is the *only* reparsing that is done.
                 i, block_lines = state.scan_block(i, lines)
-                blocks.append(Block(block_lines, simple=False, base=block_base))
+                blocks.append(Block(block_lines, state, simple=False))
                 block_lines = []
             else:
                 block_lines.append(line)
                 i += 1
             assert progress < i
+        state.pop() # Restore the bases.
         # End the lines.
         if block_lines:
-            blocks.append(Block(block_lines, simple=True))
+            blocks.append(Block(block_lines, state, simple=True))
         if self.gen_refs: 
             # Generate section references.
             self.make_ref_children(blocks, first_line, last_line, parent_block)
@@ -353,6 +366,7 @@ class JavaScriptScanner(basescanner.BaseScanner):
     def make_at_others_children(self, blocks, first_line, last_line, parent_block):
         '''Generate child blocks for all blocks using @others.'''
         trace = False and not g.unitTesting
+        state = self.state
         if not blocks:
             return
         if trace:
@@ -361,18 +375,20 @@ class JavaScriptScanner(basescanner.BaseScanner):
             for z in blocks:
                 print('%s %s' % (self.max_blocks_indent([z]), z))
         # Create child nodes only if there are enough lines.
+        # This is arbitrary, and so a bit confusing.
+        # However, it is about right in practice.
         n_lines = sum([len(z.lines) for z in blocks])
-        if n_lines < 20: # We want this to be a small number.
+        if n_lines < 10: # We want this to be a small number.
             return
-        max_indent = 4 if self.gen_clean else self.max_blocks_indent(blocks)
-            # This doesn't work when self.gen_clean is False.
+        max_indent = 4 if self.gen_clean else 0
+            # self.max_blocks_indent(blocks) doesn't necessarily work when gen_clean is False.
         parent_block.lines = [
             first_line,
             '%s@others\n' % (' ' * max_indent),
             last_line]
         children = []
         for block in blocks:
-            child_block = Block(block.lines,simple=block.simple)
+            child_block = Block(block.lines, state, simple=block.simple)
             if self.gen_clean:
                 child_undent = self.max_blocks_indent([child_block])
             else:
@@ -384,7 +400,10 @@ class JavaScriptScanner(basescanner.BaseScanner):
     #@+node:ekr.20161008091822.1: *4* jss.make_ref_children
     def make_ref_children(self, blocks, first_line, last_line, parent_block):
         '''Generate child blocks for all blocks using section references'''
-        # complex_blocks = [z for z in blocks if not z.simple]
+        trace = False and not g.unitTesting and self.root.h.endswith('alt.js')
+        state = self.state
+        if trace: g.trace('len: %3s base: %5s %s' % (
+            len(parent_block.lines), parent_block.base, parent_block.get_headline()))
         body, children = [], []
         for block in blocks:
             if block.simple:
@@ -392,7 +411,9 @@ class JavaScriptScanner(basescanner.BaseScanner):
                 # g.trace(block.get_headline())
                 body.extend(block.lines)
             else:
-                child_block = Block(block.lines,simple=False)
+                base = block.base
+                assert base is not None, 'make_ref_children'
+                child_block = Block(block.lines, state, simple=False)
                 children.append(child_block)
                 ref = self.ref_line(child_block)
                 child_block.headline = ref.strip()
@@ -403,7 +424,6 @@ class JavaScriptScanner(basescanner.BaseScanner):
                     body.append(' '*child_undent + ref)
                 else:
                     # Don't indent the ref, and don't unindent the children.
-                    # This works, because references are never indented.
                     body.append(ref)
         # Replace the block with the child blocks.
         parent_block.lines = []
@@ -433,17 +453,17 @@ class JavaScriptScanner(basescanner.BaseScanner):
         # pylint: disable=arguments-differ
             # parse_body not used.
         lines = g.splitLines(s1)
+        self.state = state = ScanState(self.c, self.root)
         if len(lines) < 20:
             if trace: g.trace('small file: %s' % parent.h)
             parent.b = '@language javascript\n' + ''.join(lines)
             return
         if self.gen_refs:
-            block = Block(lines, simple=False)
+            block = Block(lines, state, simple=False)
             self.rescan_block(block, strip_lines=False)
             parent.b = '@language javascript\n'
             self.put_block(block, parent, create_child=False)
         else:
-            state = ScanState()
             blocks = []
             block_lines = [] # The lines of the present block.
             # Find all top-level blocks.
@@ -453,10 +473,10 @@ class JavaScriptScanner(basescanner.BaseScanner):
                 line = lines[i]
                 state.scan_line(line)
                 if state.starts_block():
-                    block_base = state.get_base()
-                    if block_lines: blocks.append(Block(block_lines, simple=True))
+                    base = state.get_base()
+                    if block_lines: blocks.append(Block(block_lines, state, simple=True))
                     i, block_lines = state.scan_block(i, lines)
-                    blocks.append(Block(block_lines, simple=False, base=block_base))
+                    blocks.append(Block(block_lines, state, simple=False))
                     block_lines = []
                 else:
                     block_lines.append(line)
@@ -464,7 +484,7 @@ class JavaScriptScanner(basescanner.BaseScanner):
                 assert progress < i
             # End the blocks properly.
             if block_lines:
-                blocks.append(Block(block_lines, simple=True))
+                blocks.append(Block(block_lines, state, simple=True))
             if trace and trace_blocks:
                 self.dump_blocks(blocks, parent)
             # Rescan all the blocks, possibly creating more child blocks.
@@ -484,9 +504,9 @@ class JavaScriptScanner(basescanner.BaseScanner):
         if self.gen_refs:
             # Alas, the *actual* @auto write code refuses to write section references!!
             at.write(self.root,
-                    nosentinels=True, ### False,
-                    perfectImportFlag=False, ###True,
-                    scriptWrite=True, ### False,
+                    nosentinels=True,           # was False,
+                    perfectImportFlag=False,    # was True,
+                    scriptWrite=True,           # was False,
                     thinFile=True,
                     toString=True,
                 )
