1b9c80e186991a97886a797780c1af4bc56238a4
Edward K. Ream <edreamleo@gmail.com>
2016-10-18 13:33:31 -0500

    Renamed pre-commit to pre-commit.sh in leo/extensions. Added comments that the file name must be pre-commit, not pre-commit.py or .sh. The hook now works.

--- leo/extensions/hooks/pre-commit.py
+++ leo/extensions/hooks/pre-commit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-# Should be moved to leo-editor/.git/hooks/pre-commit.py
-print('===== pre-commit')
+# Should be moved to leo-editor/.git/hooks/pre-commit
+# The file must be named pre-commit, NOT pre-commit.py
+
+print('===== pre-commit ====')
 import leo.core.leoVersion as v
 v.create_commit_timestamp_json(after=True)
--- leo/extensions/hooks/pre-commit.sh
+++ leo/extensions/hooks/pre-commit.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# Move this file to leo-editor/.git/hooks/pre-commit.
+# The file must be named pre-commit, NOT pre-commit.sh
  
 # http://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommited-changes
  
