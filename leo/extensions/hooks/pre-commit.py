8bbf4b61d2d4c8ebdac84a7bd92543aa5cd6a8ff
Edward K. Ream <edreamleo@gmail.com>
2016-10-18 14:06:41 -0500

    Preparing for Leo 5.4b1

--- leo/extensions/hooks/pre-commit.py
+++ leo/extensions/hooks/pre-commit.py
@@ -3,6 +3,8 @@
 # Move this file to leo-editor/.git/hooks/pre-commit.
 # The file must be named pre-commit, NOT pre-commit.py
 
+# pylint: disable=invalid-name
+
 print('===== pre-commit ====')
 import leo.core.leoVersion as v
 v.create_commit_timestamp_json(after=True)
