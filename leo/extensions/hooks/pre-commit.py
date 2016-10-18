af9933e9f12ae45e980adc9a32c6608dc1a9efc5
Edward K. Ream <edreamleo@gmail.com>
2016-10-18 12:28:55 -0500

    test of pre-commit hook

--- leo/extensions/hooks/pre-commit.py
+++ leo/extensions/hooks/pre-commit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
 # Should be moved to leo-editor/.git/hooks/pre-commit.py
+print('===== pre-commit')
 import leo.core.leoVersion as v
 v.create_commit_timestamp_json(after=True)
