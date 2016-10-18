1239424af070e5017be7f1c7a3e75b89192d5658
Edward K. Ream <edreamleo@gmail.com>
2016-10-18 11:44:05 -0500

    added executable pre-commit.py

---
+++ leo/extensions/hooks/pre-commit.py
@@ -0,0 +1,8 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20161018111818.1: * @file ../extensions/hooks/pre-commit.py
+#!/usr/bin/env python
+
+# Should be moved to leo-editor/.git/hooks/pre-commit.py
+import leo.core.leoVersion as v
+v.create_commit_timestamp_json(after=True)
+#@-leo
