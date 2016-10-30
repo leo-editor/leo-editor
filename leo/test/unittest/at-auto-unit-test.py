393381bd1b975ea2d1b39fa3f0c7e9f65b6a54c2
Edward K. Ream <edreamleo@gmail.com>
2016-10-30 04:16:47 -0500

    updated at-auto-unit-test.py to make it easier to read
    Leo build:

--- leo/test/unittest/at-auto-unit-test.py
+++ leo/test/unittest/at-auto-unit-test.py
@@ -1,12 +1,12 @@
-class child1:
-    def child1_method1():
+class class1:
+    def class1_method1():
         pass
-    def child1_method2():
+    def class1_method2():
         pass
     # After @others in child1.
-class child2:
-    def child2_method1():
+class class2:
+    def class2_method1():
         pass
-    def child2_method2():
+    def class2_method2():
         pass
 # last line
