2bbeed0a035997bab94209237c236461a6c13c12
Edward K. Ream <edreamleo@gmail.com>
2016-11-17 17:44:13 -0600

    Removed vestiges of character-oriented importer from typescript importer.  All tests pass.
    Leo build:

--- leo/plugins/importers/typescript.py
+++ leo/plugins/importers/typescript.py
@@ -4,34 +4,19 @@
 import leo.plugins.importers.javascript as javascript
 JS_Importer = javascript.JS_Importer
 #@+others
-#@+node:ekr.20140723122936.18111: ** class TS_ImportController(JS_Importer)
+#@+node:ekr.20140723122936.18111: ** class TS_Importer(JS_Importer)
 class TS_Importer(JS_Importer):
     
     def __init__(self, importCommands, atAuto):
+        '''TS_Importer ctor.'''
         # Init the base class.
         JS_Importer.__init__(self,
             importCommands,
             atAuto=atAuto,
             language='typescript',
         )
-        # Overrides of ivars.
-        self.hasClasses = True
-        self.classTags = ['module', 'class', 'interface',]
-        self.functionTags = [
-            'constructor', 'enum', 'function',
-            'public', 'private', 'export',]
 
     #@+others
-    #@+node:ekr.20140723122936.18113: *3* V1: TS_ImportController.getSigId
-    def getSigId(self, ids):
-        '''Return the signature's id.
-
-        This is the last id of the ids list, or the id before extends anotherId.
-        '''
-        if len(ids) > 2 and ids[-2] == 'extends':
-            return ids[-3]
-        else:
-            return ids and ids[-1]
     #@-others
 #@-others
 importer_dict = {
