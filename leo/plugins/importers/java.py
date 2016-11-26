55e5372c973e3f8b6885ab345a8d2ac64bea5837
Edward K. Ream <edreamleo@gmail.com>
2016-11-26 16:35:03 -0600

    Rewrote the java importer.  All unit tests pass.
    Leo build:

--- leo/plugins/importers/java.py
+++ leo/plugins/importers/java.py
@@ -1,41 +1,81 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20140723122936.18143: * @file importers/java.py
-'''The @auto importer for Java.'''
-import leo.plugins.importers.basescanner as basescanner
+'''The @auto importer for the java language.'''
+import leo.plugins.importers.linescanner as linescanner
+Importer = linescanner.Importer
 #@+others
-#@+node:ekr.20140723122936.18046: ** class JavaScanner
-class JavaScanner(basescanner.BaseScanner):
-    #@+others
-    #@+node:ekr.20140723122936.18047: *3* JavaScanner.__init__
-    def __init__(self, importCommands, atAuto):
-        # Init the base class.
-        basescanner.BaseScanner.__init__(self, importCommands, atAuto=atAuto, language='java')
-        # Set the parser delims.
-        self.blockCommentDelim1 = '/*'
-        self.blockCommentDelim2 = '*/'
-        self.lineCommentDelim = '//'
-        self.lineCommentDelim2 = None
-        self.outerBlockDelim1 = '{'
-        self.classTags = ['class', 'interface']
-        self.functionTags = []
-        self.sigFailTokens = [';', '='] # Just like c.
-    #@+node:ekr.20140723122936.18048: *3* JavaScanner.getSigId
-    def getSigId(self, ids):
-        '''Return the signature's id.
+#@+node:ekr.20161126161824.2: ** class Java_Importer
+class Java_Importer(Importer):
+    '''The importer for the java lanuage.'''
 
-        By default, this is the last id in the ids list.'''
-        # Remove 'public' and 'private'
-        ids2 = [z for z in ids if z not in ('public', 'private', 'final',)]
-        # Remove 'extends' and everything after it.
-        ids = []
-        for z in ids2:
-            if z == 'extends': break
-            ids.append(z)
-        return ids and ids[-1]
+    def __init__(self, importCommands, atAuto):
+        '''Java_Importer.__init__'''
+        # Init the base class.
+        Importer.__init__(self,
+            importCommands,
+            atAuto = atAuto,
+            language = 'java',
+            state_class = Java_ScanState,
+            strict = False,
+        )
+        
+    #@+others
+    #@+node:ekr.20161126163014.1: *3* java_i.clean_headline
+    def clean_headline(self, headline):
+        '''Return the cleaned headline.'''
+        if headline.strip().endswith('{'):
+            return headline.strip('{').strip()
+        else:
+            return headline.strip()\
+
+        
+    #@-others
+#@+node:ekr.20161126161824.6: ** class class Java_ScanState
+class Java_ScanState:
+    '''A class representing the state of the java line-oriented scan.'''
+    
+    def __init__(self, d=None):
+        '''Java_ScanState.__init__'''
+        if d:
+            prev = d.get('prev')
+            self.context = prev.context
+            self.curlies = prev.curlies
+        else:
+            self.context = ''
+            self.curlies = 0
+
+    def __repr__(self):
+        '''Java_ScanState.__repr__'''
+        return "Java_ScanState context: %r curlies: %s" % (
+            self.context, self.curlies)
+
+    __str__ = __repr__
+
+    #@+others
+    #@+node:ekr.20161126161824.7: *3* java_state.level
+    def level(self):
+        '''Java_ScanState.level.'''
+        return self.curlies
+
+    #@+node:ekr.20161126161824.8: *3* java_state.update
+    def update(self, data):
+        '''
+        Java_ScanState.update
+
+        Update the state using the 6-tuple returned by v2_scan_line.
+        Return i = data[1]
+        '''
+        context, i, delta_c, delta_p, delta_s, bs_nl = data
+        # All ScanState classes must have a context ivar.
+        self.context = context
+        self.curlies += delta_c  
+        return i
     #@-others
 #@-others
 importer_dict = {
-    'class': JavaScanner,
-    'extensions': ['.java',],
+    'class': Java_Importer,
+    'extensions': ['.java'],
 }
+#@@language python
+#@@tabwidth -4
 #@-leo
--- leo/plugins/importers/php.py
+++ leo/plugins/importers/php.py
@@ -1,6 +1,7 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20140723122936.18148: * @file importers/php.py
 '''The @auto importer for PHP.'''
+import re
 import leo.core.leoGlobals as g
 import leo.plugins.importers.basescanner as basescanner
 import string
@@ -34,6 +35,43 @@ class PhpScanner(basescanner.BaseScanner):
             s.startswith('<?') and
             s[2: 3] in ('P', 'p', '=', '\n', '\r', ' ', '\t') and
             s.endswith('?>'))
+    #@+node:ekr.20161126161149.1: *3* skip_heredoc_string
+    def skip_heredoc_string(self, s, i):
+        #@+<< skip_heredoc docstrig >>
+        #@+node:ekr.20161126161323.1: *4* << skip_heredoc docstrig >>
+        #@@nocolor-node
+        '''
+        08-SEP-2002 DTHEIN:  added function skip_heredoc_string
+        A heredoc string in PHP looks like:
+
+          <<<EOS
+          This is my string.
+          It is mine. I own it.
+          No one else has it.
+          EOS
+
+        It begins with <<< plus a token (naming same as PHP variable names).
+        It ends with the token on a line by itself (must start in first position.
+        '''
+        #@-<< skip_heredoc docstrig >>
+        j = i
+        assert(g.match(s, i, "<<<"))
+        # pylint: disable=anomalous-backslash-in-string
+        m = re.match("\<\<\<([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)", s[i:])
+        if m is None:
+            i += 3
+            return i
+        # 14-SEP-2002 DTHEIN: needed to add \n to find word, not just string
+        delim = m.group(1) + '\n'
+        i = g.skip_line(s, i) # 14-SEP-2002 DTHEIN: look after \n, not before
+        n = len(s)
+        while i < n and not g.match(s, i, delim):
+            i = g.skip_line(s, i) # 14-SEP-2002 DTHEIN: move past \n
+        if i >= n:
+            g.scanError("Run on string: " + s[j: i])
+        elif g.match(s, i, delim):
+            i += len(delim)
+        return i
     #@+node:ekr.20140723122936.18086: *3* Overrides
     # Does not create @first/@last nodes
     #@+node:ekr.20140723122936.18087: *4* startsString skipString
