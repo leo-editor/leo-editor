8efa8b483997a99ef994c45e8976b89662852edd
Terry Brown <terry_n_brown@yahoo.com>
2017-01-21 14:38:24 -0600

    additions, need to use more widget classes
    can't hide a layout, so need (hideable) widgets for header, controls.
    
    Leo build: 20170121143824

--- leo/core/editpane/editpane.py
+++ leo/core/editpane/editpane.py
@@ -17,18 +17,27 @@ class LeoEditPane(QtWidgets.QWidget):
         :param outline c: outline to bind to
         """
         self.c = kwargs['c']
-        del kwargs['c']
+        p = kwargs.get('p', self.c.p)
+        mode = kwargs.get('mode', 'edit')
+        show_head = kwargs.get('show_head', True)
+        show_control = kwargs.get('show_control', True)
+
+        for arg in 'c', 'p', 'show_head', 'show_control', 'mode':
+            if arg in kwargs:
+                del kwargs[arg]
+
         QtWidgets.QWidget.__init__(self, *args, **kwargs)
-        
-        self._build_layout()
+
+        self._build_layout(mode=mode, show_head=show_head, show_control=show_control)
         self._register_handlers()
-        
+
         self.track   = self.cb_track.isChecked()
         self.update  = self.cb_update.isChecked()
         self.edit    = self.cb_edit.isChecked()
         self.split   = self.cb_split.isChecked()
         self.recurse = self.cb_recurse.isChecked()
 
+        self.new_position(p)
     def _add_checkbox(self, text, state_changed, checked=True, enabled=True):
         """
         _add_checkbox - helper to add a checkbox
@@ -46,14 +55,52 @@ class LeoEditPane(QtWidgets.QWidget):
         cbox.stateChanged.connect(state_changed)
         return cbox
 
-    def _build_layout(self):
+    def _after_select(self, tag, keywords):
+        """_after_select - after Leo selects another node
+
+        :param str tag: handler name ("select2")
+        :param dict keywords: c, new_p, old_p
+        :return: None
+        """
+
+        c = keywords['c']
+        if c != self.c:
+            return None
+
+        DBG("after select")
+
+        return None
+    def _before_select(self, tag, keywords):
+        """_before_select - before Leo selects another node
+
+        :param str tag: handler name ("select1")
+        :param dict keywords: c, new_p, old_p
+        :return: None
+        """
+
+        c = keywords['c']
+        if c != self.c:
+            return None
+
+        DBG("before select")
+
+        return None
+
+    def _register_handlers(self):
+        """_register_handlers - attach to Leo signals
+        """
+        DBG("\nregister handlers")
+        g.registerHandler('select1', self._before_select)
+        g.registerHandler('select2', self._after_select)
+
+    def _build_layout(self, mode='edit', show_head=True, show_control=True):
         """build_layout - build layout
         """
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.layout.setSpacing(0)
         self.setLayout(self.layout)
-        
+
         # header
         self.header_layout = QtWidgets.QHBoxLayout()
         self.layout.addLayout(self.header_layout)
@@ -63,8 +110,6 @@ class LeoEditPane(QtWidgets.QWidget):
         # controls
         self.control_layout = QtWidgets.QHBoxLayout()
         self.layout.addLayout(self.control_layout)
-        self.control_menu_button = QtWidgets.QPushButton("Menu", self)
-        self.control_layout.addWidget(self.control_menu_button)
         # checkboxes
         self.cb_track = self._add_checkbox("Track", self.change_track)
         self.cb_update = self._add_checkbox("Update", self.change_update)
@@ -75,6 +120,9 @@ class LeoEditPane(QtWidgets.QWidget):
         self.btn_render = QtWidgets.QPushButton("Render", self)
         self.control_layout.addWidget(self.btn_render)
         self.btn_render.clicked.connect(self.render)
+        # menu
+        self.control_menu_button = QtWidgets.QPushButton(u"More\u2026", self)
+        self.control_layout.addWidget(self.control_menu_button)
         # padding
         self.control_layout.addItem(
             QtWidgets.QSpacerItem(0, 0, hPolicy=QtWidgets.QSizePolicy.Expanding))
@@ -90,63 +138,53 @@ class LeoEditPane(QtWidgets.QWidget):
         # debug
         self.header.setText("test")
 
-    def close(self):
-        """close - clean up
-        """
-        do_close = QtWidgets.QWidget.close(self)
-        if do_close:
-            DBG("unregister handlers")
-            g.unregisterHandler('select1', self._before_select)
-            g.unregisterHandler('select2', self._after_select)
-        return do_close
-    def change_track(self, state):
-        self.track = bool(state)
-    def change_update(self, state):
-        self.update = bool(state)
     def change_edit(self, state):
         self.edit = bool(state)
+    def change_recurse(self, state):
+        self.recurse = bool(state)
     def change_split(self, state):
         self.split = bool(state)
         # always edit if split
         self.cb_edit.setEnabled(not state)
         if state:
             self.cb_edit.setChecked(True)
-    def change_recurse(self, state):
-        self.recurse = bool(state)
-    def _register_handlers(self):
-        """_register_handlers - attach to Leo signals
+    def change_track(self, state):
+        self.track = bool(state)
+    def change_update(self, state):
+        self.update = bool(state)
+    def close(self):
+        """close - clean up
         """
-        DBG("register handlers")
-        g.registerHandler('select1', self._before_select)
-        g.registerHandler('select2', self._after_select)
+        do_close = QtWidgets.QWidget.close(self)
+        if do_close:
+            DBG("unregister handlers\n")
+            g.unregisterHandler('select1', self._before_select)
+            g.unregisterHandler('select2', self._after_select)
+        return do_close
+    def new_position(self, p):
+        """new_position - update editor and view for new Leo position
 
-    def _before_select(self, tag, keywords):
-        """_before_select - before Leo selects another node
-
-        :param str tag: handler name ("select1")
-        :param dict keywords: c, new_p, old_p
-        :return: None
-        """
-
-        c = keywords['c']
-        if c != self.c:
-            return None
-
-        return None
-
-    def _after_select(self, tag, keywords):
-        """_after_select - after Leo selects another node
-
-        :param str tag: handler name ("select2")
-        :param dict keywords: c, new_p, old_p
-        :return: None
+        :param position p: the new position
         """
 
-        c = keywords['c']
-        if c != self.c:
-            return None
+        if self.edit or self.split:
+            self.new_position_edit(p)
+        if not self.edit or self.split:
+            self.new_position_view(p)
+    def new_position_edit(self, p):
+        """new_position_edit - update editor for new position
 
-        return None
+        :param position p: the new position
+        """
+
+        pass
+    def new_position_view(self, p):
+        """new_position_view - update viewer for new position
+
+        :param position p: the new position
+        """
+
+        pass
     def render(self, checked):
         pass
 
