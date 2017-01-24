bbafd3082ee3ae69e27e7dd2042842f0bef16685
Terry N. Brown <terry_n_brown@yahoo.com>
2017-01-24 16:53:35 -0600

    use button checkbox combos, add goto
    Leo build: 20170124165337

--- leo/core/editpane/editpane.py
+++ leo/core/editpane/editpane.py
@@ -11,7 +11,7 @@ def DBG(text):
 
     :param str text: text to print
     """
-    print("LEP: \033[33m%s\033[39m" % text)
+    print("LEP: %s" % text)
 class LeoEditPane(QtWidgets.QWidget):
     """
     Leo node body editor / viewer
@@ -57,6 +57,7 @@ class LeoEditPane(QtWidgets.QWidget):
         self.track   = self.cb_track.isChecked()
         self.update  = self.cb_update.isChecked()
         self.recurse = self.cb_recurse.isChecked()
+        self.goto    = self.cb_goto.isChecked()
 
         reload(plaintextedit)
         reload(plaintextview)
@@ -67,7 +68,8 @@ class LeoEditPane(QtWidgets.QWidget):
 
         self.set_mode(self.mode)
         self.new_position(p)
-    def _add_checkbox(self, text, state_changed, tooltip, checked=True, enabled=True):
+    def _add_checkbox(self, text, state_changed, tooltip, checked=True, 
+        enabled=True, button_label=True):
         """
         _add_checkbox - helper to add a checkbox
 
@@ -75,10 +77,19 @@ class LeoEditPane(QtWidgets.QWidget):
         :param function state_changed: callback for state_changed signal
         :param bool checked: initially checked?
         :param bool enabled: initially enabled?
+        :param bool button_label: label should be a button for single shot use
         :return: QCheckBox
         """
-        cbox = QtWidgets.QCheckBox(text, self)
+        cbox = QtWidgets.QCheckBox('' if button_label else text, self)
         self.control.layout().addWidget(cbox)
+        btn = None
+        if button_label:
+            btn = QtWidgets.QPushButton(text, self)
+            self.control.layout().addWidget(btn)
+            def cb(checked, cbox=cbox, state_changed=state_changed):
+                state_changed(cbox.isChecked(), one_shot=True)
+            btn.clicked.connect(cb)
+            btn.setToolTip(tooltip)
         cbox.setChecked(checked)
         cbox.setEnabled(enabled)
         cbox.stateChanged.connect(state_changed)
@@ -191,12 +202,15 @@ class LeoEditPane(QtWidgets.QWidget):
         # controls
         self.control = self._add_frame()
         # checkboxes
+        txt = ",\ncheck to do this always"
         self.cb_track = self._add_checkbox("Track", self.change_track,
-            "Track the node selected in the tree")
+            "Track the node selected in the tree"+txt)
+        self.cb_goto = self._add_checkbox("Goto", self.change_goto,
+            "Make the tree go to this node"+txt)
         self.cb_update = self._add_checkbox("Update", self.change_update,
-            "Update view to match changed node")
+            "Update view to match changed node"+txt)
         self.cb_recurse = self._add_checkbox("Recurse", self.change_recurse, 
-            "Recursive view", checked=recurse)
+            "Recursive view"+txt, checked=recurse)
         # mode menu
         btn = self.btn_mode = QtWidgets.QPushButton("Mode", self)
         self.control.layout().addWidget(btn)
@@ -206,10 +220,6 @@ class LeoEditPane(QtWidgets.QWidget):
         btn.clicked.connect(  # or left click
             lambda checked: self.mode_menu())
 
-        # render now button
-        self.btn_render = QtWidgets.QPushButton("Render", self)
-        self.control.layout().addWidget(self.btn_render)
-        self.btn_render.clicked.connect(self.render)
         # misc. menu
         self.control_menu_button = QtWidgets.QPushButton(u"More\u25BE", self)
         self.control.layout().addWidget(self.control_menu_button)
@@ -237,20 +247,27 @@ class LeoEditPane(QtWidgets.QWidget):
         # debug
         self.line_edit.setText("test")
 
-    def change_recurse(self, state):
-        self.recurse = bool(state)
+    def change_goto(self, state, one_shot=False):
+        self.goto = one_shot or bool(state)
         self.state_changed()
-    def change_track(self, state):
-        self.track = bool(state)
+        self.goto = bool(state)
+    def change_recurse(self, state, one_shot=False):
+        self.recurse = one_shot or bool(state)
+        self.state_changed()
+        self.recurse = bool(state)
+    def change_track(self, state, one_shot=False):
+        self.track = one_shot or bool(state)
         if self.track:
             p = self.c.p
             self.new_position(p)
-    def change_update(self, state):
-        self.update = bool(state)
+        self.track = one_shot
+    def change_update(self, state, one_shot=False):
+        self.update = one_shot or bool(state)
         if self.update:
             p = self.get_position()
             if p is not None:
                 self.new_position(p)
+        self.update = bool(state)
     def close(self):
         """close - clean up
         """
@@ -264,6 +281,11 @@ class LeoEditPane(QtWidgets.QWidget):
     def get_position(self):
         """get_position - get current position"""
         return self._find_gnx_node(self.gnx)
+    def goto_node(self):
+        """goto_node - goto node being edited / viewed"""
+        p = self.get_position()
+        if p and p != self.c.p:
+            self.c.selectPosition(p)
     def mode_menu(self):
         """build menu on Action button"""
         menu = QtWidgets.QMenu()
@@ -373,6 +395,9 @@ class LeoEditPane(QtWidgets.QWidget):
     def state_changed(self):
         """state_changed - control state has changed
         """
+
+        if self.goto and self.get_position() != self.c.p:
+            self.goto_node()
 
         if self.mode == 'edit':
             self.edit_frame.show()
