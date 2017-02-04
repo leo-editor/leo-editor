9616dd58ce294c1465e863a7ffb5d851655bd4cb
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2017-02-04 15:45:40 +0300

    two changes to rst and md writers: the rst underline characters is now more consistent with sphinx defaults and added a space between the # the title text in markdown writer

--- leo/plugins/importers/leo_rst.py
+++ leo/plugins/importers/leo_rst.py
@@ -9,7 +9,8 @@ import leo.core.leoGlobals as g
 import leo.plugins.importers.linescanner as linescanner
 Importer = linescanner.Importer
 # Used by writers.leo_rst as well as in this file.
-underlines = "!\"$%&'()*+,-./:;<=>?@[\\]^_`{|}~#"
+underlines_old = "!\"$%&'()*+,-./:;<=>?@[\\]^_`{|}~#"
+underlines = '*=-^~"+!\$%&(),./:;<>?@[\\]_`{|}#'
     # All valid rst underlines, with '#' *last*, so it is effectively reserved.
 #@+others
 #@+node:ekr.20161127192007.2: ** class Rst_Importer
