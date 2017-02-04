9a32e93a952905f32ebffe491d6c8651cdfe1027
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2017-02-04 18:03:01 +0300

    minor fix to remove redundant backslash in auto-rst underlines sequence which causes a pylint warning

--- leo/plugins/importers/leo_rst.py
+++ leo/plugins/importers/leo_rst.py
@@ -10,7 +10,7 @@ import leo.plugins.importers.linescanner as linescanner
 Importer = linescanner.Importer
 # Used by writers.leo_rst as well as in this file.
 underlines_old = "!\"$%&'()*+,-./:;<=>?@[\\]^_`{|}~#"
-underlines = '*=-^~"+!\$%&(),./:;<>?@[\\]_`{|}#'
+underlines = '*=-^~"+!$%&(),./:;<>?@[\\]_`{|}#'
     # All valid rst underlines, with '#' *last*, so it is effectively reserved.
 #@+others
 #@+node:ekr.20161127192007.2: ** class Rst_Importer
