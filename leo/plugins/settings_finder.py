leo-editor/leo-editor

commit eb52ec412d4d7032c30299c576fceafeef2ac162

Restored Terry's update of the dialog message.
Leo build: 20170313032423

leo/plugins/settings_finder.py
@@ -145,7 +145,7 @@ class SettingsFinder(object):
         key = g.app.config.canonicalizeSettingName(setting)
         value = self.c.config.settingsDict.get(key)
         which = None
-        while value and g.isString(value.val) and value.val.startswith('@'):
+        while value and g.isString(value.val) and value.val.startswith('g@'):
             msg = ("The relevant setting, '@{specific}', is using the value of "
             "a more general setting, '{general}'.  Would you like to edit the "
             "more specific setting, '@{specific}', or the more general setting, "
@@ -166,28 +166,26 @@ class SettingsFinder(object):
             g.os_path_realpath(value.path) == g.os_path_realpath(g.os_path_join(
             g.app.loadManager.computeGlobalConfigDir(), 'leoSettings.leo')
         )):
-            if 0: # Bypass the dialog.
-                unl = self.copy_to_my_settings(unl, which=2)
-            else: # Get the context from a dialog.
-                msg = ("The setting '@{specific}' is in the Leo global configuration "
-                "file 'leoSettings.leo'\nand should probably be copied to "
-                "'myLeoSettings.leo' before editing.\n"
-                "It may make more sense to copy a group or category of settings.\n\n"
-                "Please enter 1, 2, 3, or 4:\n"
-                "1. copy the one setting, '@{specific}'\n"
-                "2. copy the setting group, '{group}' (Recommended)\n"
-                "3. copy the setting whole category, '{category}'\n"
-                "4. edit the setting in 'leoSettings.leo' anyway\n"
-                ).format(specific=setting.lstrip('@'),
-                    group=unl.split('-->')[-2].split(':', 1)[0].replace('%20', ' '),
-                    category=unl.split('-->')[-3].split(':', 1)[0].replace('%20', ' '))
-                which = g.app.gui.runAskOkCancelNumberDialog(
-                    self.c, "Copy setting?", message=msg)
-                if which is None:
-                    return
-                which = int(which)
-                if which < 4:
-                    unl = self.copy_to_my_settings(unl, which)
+            msg = ("The setting '@{specific}' is in the Leo global configuration "
+            "file 'leoSettings.leo'\nand should be copied to "
+            "'myLeoSettings.leo' before editing.\n"
+            "It may make more sense to copy a group or category of settings.\n\n"
+            "Please enter 1, 2, 3, or 4:\n"
+            "1. just select the node in 'leoSettings.leo', I will decide how much\n"
+            "   to copy into 'myLeoSettings.leo' (Recommended).\n"
+            "2. copy the one setting, '@{specific}'\n"
+            "3. copy the setting group, '{group}'\n"
+            "4. copy the whole setting category, '{category}'\n"
+            ).format(specific=setting.lstrip('@'),
+                group=unl.split('-->')[-2].split(':', 1)[0].replace('%20', ' '),
+                category=unl.split('-->')[-3].split(':', 1)[0].replace('%20', ' '))
+            which = g.app.gui.runAskOkCancelNumberDialog(
+                self.c, "Copy setting?", message=msg)
+            if which is None:
+                return
+            which = int(which)
+            if which < 4:
+                unl = self.copy_to_my_settings(unl, which)
         if unl:
             g.handleUnl(unl, c=self.c)
     #@+node:tbrown.20150818161651.7: *3* sf.get_command

commit 1cd3ba4cebaa0b55f668f7f37d2bbd4858e5626a

undo breakage introduced in eb52ec4
Leo build: 20170313074319

leo/plugins/settings_finder.py
@@ -125,18 +125,17 @@ class SettingsFinder(object):
         if not c2:
             return '' # Fix 434.
         found, maxdepth, maxp = g.recursiveUNLFind(unl, c2)
-        if found:
-            if trace: g.trace('FOUND', unl)
-            dest = maxp.get_UNL()
-        else:
-            if trace: g.trace('CREATING', unl)
-            nd = settings.insertAsLastChild()
-            dest = nd.get_UNL()
-            self.copy_recursively(maxp, nd)
-            my_settings_c.redraw()
-            shortcutsDict, settingsDict = g.app.loadManager.createSettingsDicts(my_settings_c, False)
-            self.c.config.settingsDict.update(settingsDict)
-            my_settings_c.config.settingsDict.update(settingsDict)
+
+        if trace: g.trace('COPYING', unl)
+        nd = settings.insertAsLastChild()
+        dest = nd.get_UNL()
+        self.copy_recursively(maxp, nd)
+        my_settings_c.setChanged()
+        my_settings_c.redraw()
+        shortcutsDict, settingsDict = g.app.loadManager.createSettingsDicts(my_settings_c, False)
+        self.c.config.settingsDict.update(settingsDict)
+        my_settings_c.config.settingsDict.update(settingsDict)
+
         if trace: g.trace('-->'.join([dest] + tail))
         return '-->'.join([dest] + tail)
     #@+node:tbrown.20150818161651.6: *3* sf.find_setting
@@ -145,7 +144,7 @@ class SettingsFinder(object):
         key = g.app.config.canonicalizeSettingName(setting)
         value = self.c.config.settingsDict.get(key)
         which = None
-        while value and g.isString(value.val) and value.val.startswith('g@'):
+        while value and g.isString(value.val) and value.val.startswith('@'):
             msg = ("The relevant setting, '@{specific}', is using the value of "
             "a more general setting, '{general}'.  Would you like to edit the "
             "more specific setting, '@{specific}', or the more general setting, "
@@ -184,8 +183,8 @@ class SettingsFinder(object):
             if which is None:
                 return
             which = int(which)
-            if which < 4:
-                unl = self.copy_to_my_settings(unl, which)
+            if which > 1:
+                unl = self.copy_to_my_settings(unl, which-1)
         if unl:
             g.handleUnl(unl, c=self.c)
     #@+node:tbrown.20150818161651.7: *3* sf.get_command