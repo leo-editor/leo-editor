0e9bc088fb2edfcf86c1fa4e0382fc53670eff83
Edward K. Ream <edreamleo@gmail.com>
2017-04-19 11:20:06 -0500

    Added CursesFrame and CursesMenu classes, both with __getattr__ attributes. Startup now happens without a crash.
    Leo build: 20170419112007

--- leo/plugins/cursesGui2.py
+++ leo/plugins/cursesGui2.py
@@ -16,6 +16,32 @@ def init():
         s = "Can't install text gui: previous gui installed"
         g.es_print(s, color="red")
     return ok
+#@+node:ekr.20170419105852.1: ** class CursesFrame
+class CursesFrame:
+    
+    def __init__ (self, c, title):
+        self.c = c
+        self.d = {}
+        self.title = title
+        self.menu = CursesMenu(c)
+        
+    #@+others
+    #@+node:ekr.20170419111214.1: *3* CF.__getattr__
+    # https://docs.python.org/2/reference/datamodel.html#object.__getattr__
+    def __getattr__(self, name):
+        aList = self.d.get(name, [])
+        callers = g.callers(4)
+        if callers not in aList:
+            aList.append(callers)
+            self.d[name] = aList
+            g.trace('%30s' % ('CursesFrame.' + name), callers)
+        return g.NullObject()
+            # Or just raise AttributeError.
+    #@+node:ekr.20170419111305.1: *3* CF.getShortCut
+    def getShortCut(self, *args, **kwargs):
+        return None
+    #@-others
+    
 #@+node:ekr.20170419094731.1: ** class CursesGui
 class CursesGui(leoGui.LeoGui):
     '''A do-nothing curses gui template.'''
@@ -23,16 +49,46 @@ class CursesGui(leoGui.LeoGui):
     def __init__(self):
         self.d = {}
             # Keys are names, values of lists of g.callers values.
-
+            
+    #@+others
+    #@+node:ekr.20170419110330.1: *3* CG.__getattr__
     # https://docs.python.org/2/reference/datamodel.html#object.__getattr__
     def __getattr__(self, name):
         aList = self.d.get(name, [])
-        callers = g.callers(6)
+        callers = g.callers(4)
         if callers not in aList:
             aList.append(callers)
             self.d[name] = aList
-            g.trace(name, callers)
+            g.trace('%30s' % ('CursesGui.' + name), callers)
         return g.NullObject()
             # Or just raise AttributeError.
+    #@+node:ekr.20170419110052.1: *3* CG.createLeoFrame
+    def createLeoFrame(self, c, title):
+        
+        return CursesFrame(c, title)
+    #@+node:ekr.20170419111744.1: *3* CG.Focus...
+    def get_focus(self, *args, **keys):
+        return None
+    #@-others
+#@+node:ekr.20170419111515.1: ** class CursesMenu
+class CursesMenu:
+    
+    def __init__ (self, c):
+        self.c = c
+        self.d = {}
+        
+    #@+others
+    #@+node:ekr.20170419111555.1: *3* CM.__getattr__
+    # https://docs.python.org/2/reference/datamodel.html#object.__getattr__
+    def __getattr__(self, name):
+        aList = self.d.get(name, [])
+        callers = g.callers(4)
+        if callers not in aList:
+            aList.append(callers)
+            self.d[name] = aList
+            g.trace('%30s' % ('CursesMenu.' + name), callers)
+        return g.NullObject()
+            # Or just raise AttributeError.
+    #@-others
 #@-others
 #@-leo
