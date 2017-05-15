86759224d2b292193bca83a291789d8fb9fe65a6
Edward K. Ream <edreamleo@gmail.com>
2017-05-15 09:24:27 -0500

    Getting Things Done
    Leo build: 20170515092427

--- leo/plugins/at_produce.py
+++ leo/plugins/at_produce.py
@@ -132,7 +132,7 @@ def runList(c,aList):
                 p = subprocess.Popen(
                     command, 
                     # bufsize=bufsize,
-                    # close_fds=True,
+                    # close_fds=True, # Dubious to disable this.
                     stdin=PIPE, stdout=PIPE, stderr=PIPE,
                     shell=True,
                 )
