758da49797e82c8952bfc3c704f61a6ae619451c
Edward K. Ream <edreamleo@gmail.com>
2017-06-24 14:06:46 -0500

    Simplified the severe error message
    Leo build: 20170624140646

--- leo/core/leoCache.py
+++ leo/core/leoCache.py
@@ -235,10 +235,10 @@ class Cacher(object):
             child_v.tempRoots = set()
         child_v.tempRoots.add(fileName)
         if must_warn:
-            self.warning('Warning: out-of-synch nodes in different external files')
-            for fn in sorted(list(child_v.tempRoots)):
-                g.es_print(fn)
-            self.warning('Retaining version found in\n%s' % (fileName))
+            self.warning('Warning: out-of-synch node')
+            # for fn in sorted(list(child_v.tempRoots)):
+                # g.es_print(fn)
+            g.es_print('Retaining node in\n%s' % (fileName))
         if always_warn or must_warn:
             c.nodeConflictList.append(g.bunch(
                 tag='(cached)',
