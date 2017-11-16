01b82a0ae7e7b9eab46d5875d0ca6b5d24b672a5
Edward K. Ream <edreamleo@gmail.com>
2017-11-16 09:47:36 -0600

    Fixed pylint complaint about IPython test() method
    Leo build: 20171116094736

--- leo/core/leoIPython.py
+++ leo/core/leoIPython.py
@@ -216,23 +216,15 @@ class InternalIPKernel(object):
         if old_show:
             shell._showtraceback = old_show
     #@+node:ekr.20171115090205.1: *3* ileo.test
-    def test():
+    def test(self):
         
         from ipykernel.connect import connect_qtconsole
         from ipykernel.kernelapp import IPKernelApp
         
-        import leo.core.leoGlobals as g
-        assert g.isPython3
-        
-        def main():
-            # From pylab_kernel
-            kernelApp = IPKernelApp.instance()
-            # pylab is needed for Qt event loop integration.
-            args = ['python', '--pylab=qt', '--log-level=20']
-            kernelApp.initialize(args)
-            console = connect_qtconsole()
-            assert console
-            # g.trace(console)
+        kernelApp = IPKernelApp.instance()
+        args = ['python', '--pylab=qt', '--log-level=20']
+        kernelApp.initialize(args)
+        connect_qtconsole()
     #@-others
 #@+node:ekr.20130930062914.16002: ** class LeoNameSpace
 class LeoNameSpace(object):
