d83085bc0401b8d3f183151273e828f5d85ed402
matt wilkie <maphew@gmail.com>
2017-11-23 23:11:14 -0800

    version and platform conditional dependency fails (https://github.com/pypa/pip/issues/4886)
    Leo build: 20171123231114

--- setup.py
+++ setup.py
@@ -77,7 +77,8 @@ user_requires = [
     #'pyenchant', # spell check support ## no wheels for some platforms, e.g. amd64
     #'pyxml', # xml importing ## no pip package
     'PyQt5; python_version >= "3.0"',
-    'python-qt5; python_version < "3.0" and platform_system=="Windows"',
+    #'python-qt5; python_version < "3.0" and platform_system=="Windows"',
+		# disabled, pending "pip install from .whl fails conditional dependency check" https://github.com/pypa/pip/issues/4886
     ## missing: pyqt for Linux python 2.x (doesn't exist on PyPi)
     ]
 #@+node:maphew.20171120133437.1: ** Dev requirements
