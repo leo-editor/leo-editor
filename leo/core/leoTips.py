a77da5ab9315760306077f840066f47562ec6c8c
Edward K. Ream <edreamleo@gmail.com>
2018-01-21 11:04:52 -0600

    Added and/or edited all tips.  Improved make_tips script
    Leo build: 20180121110452

--- leo/core/leoTips.py
+++ leo/core/leoTips.py
@@ -43,33 +43,37 @@ class UserTip(object):
     '''A User Tip.'''
     
     def __init__(self, n, tags=None, text='', title=''):
-        self.n = n
-        self.tags = tags or []
-        self.title = title
+        self.n = n # Not used.
+        self.tags = tags or [] # Not used.
+        self.title = title.strip()
         self.text = text.strip()
         
     def __repr__(self):
-        # return 'User tip %s...\n\n%s\n\n' % (self.n, self.text.strip())
-        return self.text.strip() + '\n'
+        return '%s\n\n%s\n' % (self.title, self.text)
+
     __str__ = __repr__
 #@+node:ekr.20180121045646.1: ** make_tips
-def make_tips():
+def make_tips(c):
     '''
     A script to make entries in the global tips array.
     
-    Each entry will have the form:
+    Each printed entry has the form:
         
         UserTip(
             n=anInt
             tags=[list of tags],
-            text=aString,
             title=aString,
-        )
+            text="""
+            
+            aString
+            
+            """)
         
-    Run this script, then copy the result from the console to leoTips.py.
+    After running this script, copy the result from the console to the
+    <define tips> section of leoTips.py.
     '''
     import requests
-    base_url = 'https://api.github.com/repos/leo-editor/leo-editor/issues?labels=Tip&state='
+    url = 'https://api.github.com/repos/leo-editor/leo-editor/issues?labels=Tip&state='
     
     def get_tips(data):
         """get_tips - get tips from GitHub issues
@@ -78,23 +82,27 @@ def make_tips():
         """
         tips = []
         for issue in data:
-            if '\n' in issue['body']:
-                tip, tags = issue['body'].strip().rsplit('\n', 1)
-            else:
-                tip, tags = issue['body'].strip(), ''
-            if tags.lower().startswith('tags:'):
-                tags = [i.strip().strip('.') for i in tags[5:].split(',')]
+            body, n, title = issue['body'], issue['number'], issue['title']
+            lines = g.splitLines(body)
+            for i, s in enumerate(lines):
+                if s.strip().lower().startswith('tags:'):
+                    lines = lines[:i] + lines[i+1:]
+                    text = ''.join(lines).strip()
+                    s = s.strip().rstrip('.').strip()
+                    s = s[len('tags:'):].strip()
+                    tags = [z.strip() for z in s.split(',')]
+                    break
             else:
                 tags = []
-                tip = "%s\n%s" % (tip, tags)
-            if isinstance(tip, (tuple, list)):
-                tip = (z for z in tip if z and z.strip())
+                text = body.strip()
+            # text = text.strip().strip('[]').strip()
+            # g.trace('%s: text...\n%r\n' % (n, text.strip()))
             tips.append(
                 UserTip(
-                    n=issue['number'],
+                    n=n,
                     tags=tags,
-                    text=tip.strip(), ### tip.strip().strip('[]').strip(),
-                    title=issue['title'],
+                    text=text.strip(),
+                    title=title.strip(),
                 ))
         return tips
     
@@ -109,15 +117,14 @@ UserTip(
 %s
 
 """),
-
 '''
-    for kind in ('open',): ### 'open', 'closed':
-        URL = base_url+kind
-        data = requests.get(URL).json()
+    template = g.adjustTripleString(template, c.tab_width)
+    for kind in ('open',): # 'closed':
+        data = requests.get(url+kind).json()
         for tip in get_tips(data):
             tags = ["%s" % (z) for z in tip.tags or []]
             text = tip.text.replace('\n', '\\n')
-            title = tip.title.rstrip('Tip: ')
+            title = tip.title.lstrip('Tip: ').strip()
             print(template % (tip.n, tags, title, text))
 #@-others
 #@+<< define tips >>
@@ -131,25 +138,19 @@ UserTip(
     title="Use a universal shortcut for your scripts",
     text="""\
     
-You can have a personal shortcut to run script while
-developing it. For example: put `@key=Alt-4` in headline.
+You can have a personal shortcut to run script while developing it. For example: put `@key=Alt-4` in headline.
 
-If your script grows to several subnodes, you won't have to
-select top node every time you wish to run script. It would
-be enough to just press your universal shortcut.
+If your script grows to several subnodes, you won't have to select top node every time you wish to run script. It would be enough to just press your universal shortcut.
 
 """),
 
 UserTip(
     n=628,
     tags=['Scripting'],
-    title="Tip: Clearing the Log window",
+    title="Clearing the Log window",
     text="""\
     
-When developing scripts that use Log window to display
-results, it is sometimes useful to clear Log window by
-inserting the following two lines at the beginning of your
-script:
+When developing scripts that use Log window to display results, it is sometimes useful to clear Log window by inserting the following two lines at the beginning of your script:
 
     c.frame.log.selectTab('Log')
     c.frame.log.clearLog()
@@ -162,9 +163,7 @@ UserTip(
     title="Use section references sparingly",
     text="""\
 
-Within scripts, use section references only when code must
-be placed exactly. Here is a common pattern for @file nodes
-for python files:
+Within scripts, use section references only when code must be placed exactly. Here is a common pattern for @file nodes for python files:
 
     @first # -*- coding: utf-8 -*-
     %s
@@ -178,8 +177,7 @@ UserTip(
     title="The @button make-md-toc script in LeoDocs.leo",
     text="""\
 
-The @button make-md-toc script in LeoDocs.leo writes a markdown table of
-contents to the console.
+The @button make-md-toc script in LeoDocs.leo writes a markdown table of contents to the console.
 
 You can then copy the text from the console to your document.
 
@@ -190,14 +188,12 @@ The selected outline node should be an `@auto-md` node.
 UserTip(
     n=624,
     tags=['Settings', 'Scripting'],
-    title="Run pyflakes automatically",
+    title="The pyflakes command",
     text="""\
     
-pyflakes is a superb programming tool. It checks python files almost
-instantly.
+pyflakes is a superb programming tool. It checks python files almost instantly.
 
-These settings cause Leo to run pyflakes whenever saving a
-.py file and to raise a dialog if any errors are found:
+These settings cause Leo to run pyflakes whenever saving a .py file and to raise a dialog if any errors are found:
 
     @bool run-pyflakes-on-write = True
     @bool syntax-error-popup = True
@@ -209,7 +205,7 @@ See https://pypi.python.org/pypi/pyflakes.
 UserTip(
     n=623,
     tags=['Commands', 'Scripting'],
-    title="The beautify command and @nobeautify",
+    title="The beautify command & @nobeautify directive",
     text="""\
 
 The @nobeautify directive suppresses beautification of the node in which it appears.
@@ -219,13 +215,12 @@ The @nobeautify directive suppresses beautification of the node in which it appe
 UserTip(
     n=622,
     tags=['Command', 'Testing'],
-    title="Use Leo's pylint command",
+    title="The pylint command",
     text="""\
     
 Leo's pylint command runs pylint on all `@<file>` nodes in the selected trees.
 
-Pylint runs in the background, so you can continue to use
-Leo while pylint runs.
+Pylint runs in the background, so you can continue to use Leo while pylint runs.
 
 See: https://www.pylint.org/.
 
@@ -234,11 +229,10 @@ See: https://www.pylint.org/.
 UserTip(
     n=621,
     tags=['Tutorial', 'Commands'],
-    title="Leo's rst3 command converts outlines to documents",
+    title="The rst3 command",
     text="""\
     
-Leo's rst3 command converts an @rst tree to a document
-file.
+The rst3 command converts an @rst tree to a document file.
 
 See http://leoeditor.com/tutorial-rst3.html.
 
@@ -250,11 +244,9 @@ UserTip(
     title="Use abbreviations",
     text="""\
     
-Leo's abbreviations can correct spelling mistakes, expand to
-multiple lines or even trees of nodes.
+Leo's abbreviations can correct spelling mistakes, expand to multiple lines or even trees of nodes.
 
-Abbreviations can execute scripts and can prompt for values
-to be substituted within the abbreviation.
+Abbreviations can execute scripts and can prompt for values to be substituted within the abbreviation.
 
 See http://leoeditor.com/tutorial-pim.html#using-abbreviations-and-templates.
 
@@ -266,8 +258,7 @@ UserTip(
     title="Use @test nodes",
     text="""\
     
-@test nodes create unit tests. They automatically convert
-the body to a subclass of unittest.TestCase.
+@test nodes create unit tests. They automatically convert the body to a subclass of unittest.TestCase.
 
 Leo's run-* commands execute unit tests.
 
@@ -281,8 +272,7 @@ UserTip(
     title="Use @button nodes",
     text="""\
     
-@button nodes create commands. For example,
-`@button my-command` creates the `my-command` button and the `my-command` command.
+@button nodes create commands. For example, `@button my-command` creates the `my-command` button and the `my-command` command.
 
 Within `@button` scripts, c.p is the presently selected outline node.
 
@@ -293,24 +283,24 @@ As a result, @button nodes bring scripts to data.
 UserTip(
     n=617,
     tags=['Plugins'],
-    title="Learn Leo's most important plugins",
+    title="Leo's most important plugins",
     text="""\
     
 Become familiar with Leo's most important plugins:
     
 - bookmarks.py manages bookmarks.
-- contextmenu.py shows a context menu when you right-click a headline.
+- contextmenu.py shows a menu when when righ-clicking.
 - mod_scripting.py supports @button and @command nodes.
 - quicksearch.py adds a Nav tab for searching.
-- todo.py provides to-do list and simple project-management capabilities.
-- valuespace.py adds outline-oriented spreadsheet capabilities.
-- viewrendered.py creates the rendering pane and renders content in it.
+- todo.py handles to-do lists and is a project manager.
+- valuespace.py creates an outline-oriented spreadsheet.
+- viewrendered.py renders content in the rendering pane.
 
 """),
 
 UserTip(
     n=616,
-    tags=['Settigns',],
+    tags=['Settings',],
     title="Put personal settings myLeoSettings.leo",
     text="""\
     
@@ -319,6 +309,129 @@ Put your personal settings in myLeoSettings.leo, not leoSettings.leo.
 - The leo-settings-leo command opens leoSettings.leo.
 - The my-leo-settings-leo command opens myLeoSettings.leo.
 - Copy the desired settings nodes from leoSettings.leo to myLeoSettings.leo.
+
+"""),
+
+### New tips
+
+UserTip(
+    n=615,
+    tags=['Tutorial',],
+    title="Learn to use clones",
+    text="""
+    
+Clones are "live" copies of the node itself and all its descendants.
+
+See http://leoeditor.com/tutorial-pim.html#clones.
+
+"""),
+
+UserTip(
+    n=614,
+    tags=['Command', 'Tutorial'],
+    title="You don't have to remember command names",
+    text="""
+
+To execute a command, type `Alt-X` followed by the first few characters of command name, followed by `Tab`. The list of commands matching what you have typed appears.
+
+"""),
+
+UserTip(
+    n=612,
+    tags=['Commands', 'Power user', 'Scripting', 'Study'],
+    title="Use cff to gather nodes matching a pattern",
+    text="""
+
+The cff command (aka clone-find-flattened) prompts for a search pattern, then clones all matching nodes so they are the children of a new last top-level node.
+
+This is a great way to study code.
+
+"""),
+
+UserTip(
+    n=611,
+    tags=['Command', 'Power-User'],
+    title="Use cffm to gather outline nodes",
+    text="""
+
+The cff command (aka clone-find-flattened-marked) clones all marked nodes as a children of a new node, created as the last top-level node.
+
+Use this to gather nodes throughout an outline.
+
+"""),
+
+UserTip(
+    n=610,
+    tags=['Scripting', 'Debugging', 'Beginner'],
+    title="g.callers() returns a list of callers",
+    text="""
+    
+g.callers() returns the last n callers (default 4) callers of a function or method. The verbose option shows each caller on a separate line.  For example:
+    
+    g.trace(g.callers())
+
+You must [launch Leo from a console for this to work.
+See http://leoeditor.com/running.html#running-leo-from-a-console-window.
+
+"""),
+
+UserTip(
+    n=609,
+    tags=['Scripting', 'Debugging', 'Beginner'],
+    title="Use g.trace to debug scripts",
+    text="""
+    
+The g.trace function prints all its arguments to the console.
+
+It's great for seeing patterns in running code.
+
+You must [launch Leo from a console for this to work.
+See http://leoeditor.com/running.html#running-leo-from-a-console-window.
+
+"""),
+
+UserTip(
+    n=608,
+    tags=['Scripting', 'Debugging'],
+    title="Use g.pdb from the console",
+    text="""
+    
+g.pdb launches Python's pdb debugger, adapted for Leo.
+
+See https://docs.python.org/3/library/pdb.html.
+
+You must [launch Leo from a console for this to work.
+See http://leoeditor.com/running.html#running-leo-from-a-console-window.
+
+"""),
+
+UserTip(
+    n=607,
+    tags=['Commands', 'Find'],
+    title="The find-quick-selected command",
+    text="""
+
+The find-quick-selected (Ctrl-Shift-F) command finds all nodes containing the selected text.
+
+"""),
+
+UserTip(
+    n=606,
+    tags=['Commands', 'Scripting'],
+    title="The parse-body command",
+    text="""
+
+The parse-body command parses p.b (the body text of the selected node) into separate nodes.
+
+"""),
+
+UserTip(
+    n=605,
+    tags=['Commands', ],
+    title="The sort-siblings command",
+    text="""
+
+The sort-siblings (Alt-A) command sorts all the child nodes of their parent, or all top-level nodes.
 
 """),
 
