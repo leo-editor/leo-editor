47fd8c7468ce2e31a6db2421484303a74aec1163
Edward K. Ream <edreamleo@gmail.com>
2018-02-02 06:10:10 -0600

    Initial checkin of writers/treepad.py.  Initial tests pass.
    Leo build: 20180202061010

---
+++ leo/plugins/writers/treepad.py
@@ -0,0 +1,40 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20180202053206.1: * @file writers/treepad.py
+'''The @auto write code for TreePad (.hjt) files.'''
+import leo.core.leoGlobals as g
+import leo.plugins.writers.basewriter as basewriter
+#@+others
+#@+node:ekr.20180202053206.2: ** class TreePad_Writer
+class TreePad_Writer(basewriter.BaseWriter):
+    '''The writer class for TreePad (.hjt) files.'''
+    # No ctor.
+    #@+others
+    #@+node:ekr.20180202053206.3: *3* treepad_w.write
+    def write(self, root):
+        """Write the entire @auto tree."""
+        g.trace('=====', root.h)
+        self.put("<Treepad version 3.0>")
+        root_level = root.level()
+        for p in root.self_and_subtree():
+            self.put('dt=Text')
+            self.put('<node>')
+            h = 'Root' if p.v == root.v else p.h
+            self.put(h)
+            indent = p.level()-root_level
+            self.put(str(indent))
+            for s in g.splitLines(p.b):
+                if not g.isDirective(s):
+                    self.put(s)
+            self.put('<end node> 5P9i0s8y19Z')
+        root.setVisited()
+        return True
+    #@-others
+#@-others
+writer_dict = {
+    '@auto': [],
+    'class': TreePad_Writer,
+    'extensions': ['.hjt',],
+}
+#@@language python
+#@@tabwidth -4
+#@-leo
