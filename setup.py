9933e11dfab3dec89eaca251475f25c3c4b11361
matt wilkie <maphew@gmail.com>
2018-02-07 02:26:04 -0800

    warn when most recent tag can't be used for PyPi version number
    Leo build: 20180207022604

--- setup.py
+++ setup.py
@@ -35,13 +35,16 @@ def git_version(file):
             # tuple of major, minor, build, pre-release, patch
             # 5.6b2 --> 5.6-b2
     except ValueError:
-        print('*** Failed to parse Semantic Version from git tag')
+        print('''*** Failed to parse Semantic Version from git tag '{0}'.
+        Expecting tag name like '5.7b2', 'leo-4.9.12', 'v4.3' for releases.
+        This version can't be uploaded to PyPi.org.'''.format(tag))
+        version = tag
     if int(distance) > 0:
         version = '{}-dev{}'.format(version, distance)
     return version
 def clean_git_tag(tag):
-    '''Return only version number from tag name. Ignore unkown formats. Specific
-       to tags in Leo's repository.
+    '''Return only version number from tag name. Ignore unkown formats. 
+       Is specific to tags in Leo's repository.
             5.7b1          -->	5.7b1
             Leo-4-4-8-b1   -->	4-4-8-b1
             v5.3           -->	5.3
