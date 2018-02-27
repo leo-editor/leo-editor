09cb46ae8768409436abd3aba9a1086485bd8f88
Edward K. Ream <edreamleo@gmail.com>
2018-02-27 11:15:11 -0600

    Updated version to 5.8 devel
    Leo build: 20180227111512

--- leo/core/leoVersion.py
+++ leo/core/leoVersion.py
@@ -44,7 +44,7 @@ import leo.core.leoGlobals as g
 # 
 # The install_hooks.py script copies these two files to leo-editor/.git/hooks.
 #@-<< about install hooks >>
-version = "5.7 final"
+version = "5.8 devel"
 date, build = g.jsonCommitInfo()
 branch = g.gitBranchName()
 #@@language python
