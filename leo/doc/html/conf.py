b28737ad3f5b1452914cd296987d8e0706a14f98
Edward K. Ream <edreamleo@gmail.com>
2018-03-17 11:31:17 -0500

    Fixed searching on Leo's web site. #789.

--- leo/doc/html/conf.py
+++ leo/doc/html/conf.py
@@ -174,16 +174,19 @@ html_last_updated_fmt = '%b %d, %Y'
 html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
+
 ## html_sidebars = {'FAQ':'noSideBar.txt'} # raises TemplateNotFound exception.
 
-    # * localtoc.html – a fine-grained table of contents of the current document
-    # * globaltoc.html – a coarse-grained table of contents for the whole documentation set, collapsed
-    # * relations.html – two links to the previous and next documents
-    # * sourcelink.html – a link to the source of the current document, if enabled in html_show_sourcelink
-    # * searchbox.html – the “quick search” box
+# * localtoc.html – a fine-grained table of contents of the current document
+# * globaltoc.html – a coarse-grained table of contents for the whole documentation set, collapsed
+# * relations.html – two links to the previous and next documents
+# * sourcelink.html – a link to the source of the current document, if enabled in html_show_sourcelink
+# * searchbox.html – the “quick search” box
 
 html_sidebars = {
+
    '**': ['relations.html','searchbox.html',] 
+
    # 'localtoc.html',
    # 'globaltoc.html',
    # 'sourcelink.html',
