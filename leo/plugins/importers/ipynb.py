40eb34ac94d931a77cdad55c88ee1887e19dc10c
Edward K. Ream <edreamleo@gmail.com>
2018-04-08 11:29:50 -0500

    Reorg nodes, preparing to remove unused code

--- leo/plugins/importers/ipynb.py
+++ leo/plugins/importers/ipynb.py
@@ -36,23 +36,8 @@ class Import_IPYNB(object):
             # The root of the to-be-created outline.
 
     #@+others
-    #@+node:ekr.20160412101845.24: *3* ipynb.get_file_name
-    def get_file_name(self):
-        '''Open a dialog to write a Jupyter (.ipynb) file.'''
-        c = self.c
-        fn = g.app.gui.runSaveFileDialog(
-            c,
-            defaultextension=".ipynb",
-            filetypes=[
-                ("Jupyter files", "*.ipynb"),
-                ("All files", "*"),
-            ],
-            initialfile='',
-            title="Export To Jupyter File",
-        )
-        c.bringToFront()
-        return fn
-    #@+node:ekr.20160412101537.14: *3* ipynb.import_file & helpers
+    #@+node:ekr.20180408112531.1: *3* ipynb.Entries
+    #@+node:ekr.20160412101537.14: *4* ipynb.import_file
     def import_file(self, fn, root):
         '''
         Import the given .ipynb file.
@@ -75,7 +60,43 @@ class Import_IPYNB(object):
         self.indent_cells()
         c.selectPosition(self.root)
         c.redraw()
-    #@+node:ekr.20160412101537.12: *4* ipynb.do_cell & helpers
+    #@+node:ekr.20160412103110.1: *4* ipynb.run
+    def run(self, s, parent, parse_body=False):
+        '''
+        @auto entry point. Called by code in leoImport.py.
+        '''
+        c = self.c
+        fn = parent.atAutoNodeName()
+        if c and fn:
+            changed = c.isChanged()
+            self.import_file(fn, parent)
+            # Similar to Importer.run.
+            parent.b = (
+                '@nocolor-node\n\n' +
+                'Note: This node\'s body text is ignored when writing this file.\n\n' +
+                'The @others directive is not required\n'
+            )
+            for p in parent.self_and_subtree():
+                p.clearDirty()
+            c.setChanged(changed)
+        elif not c or not fn:
+            g.trace('can not happen', c, fn)
+    #@+node:ekr.20180408112600.1: *3* ipynb.JSON handlers
+    #@+node:ekr.20160412101537.10: *4* ipynb.check_header
+    def check_header(self, m):
+        '''Return (n, name) or (None, None) on error.'''
+        val = (None, None)
+        if m:
+            n1, name, n2 = m.group(1), m.group(2), m.group(3)
+            try:
+                if int(n1) == int(n2):
+                    val = int(n1), name
+            except Exception:
+                pass
+            if val == (None, None):
+                g.trace('malformed header:', m.group(0))
+        return val
+    #@+node:ekr.20160412101537.12: *4* ipynb.do_cell
     def do_cell(self, cell, n):
 
         trace = False and not g.unitTesting
@@ -99,7 +120,28 @@ class Import_IPYNB(object):
                     self.do_source(key, val)
                 del cell[key]
         self.set_ua(self.parent, 'cell', cell)
-    #@+node:ekr.20160412101537.9: *5* ipynb.do_source & helpers
+    #@+node:ekr.20160412101537.11: *4* ipynb.do_markdown_cell
+    def do_markdown_cell(self, p, s):
+        '''Split the markdown cell p if it contains one or more html headers.'''
+        if not s.strip():
+            return
+        lines = g.splitLines(s)
+        for s in lines:
+            m = self.re_header.search(s)
+            n, name = self.check_header(m)
+            if n is not None:
+                p.h = '<h%s> %s </h%s>' % (n, name.strip(), n)
+                break
+        p.b = '@language md\n\n' + ''.join(lines).lstrip()
+    #@+node:ekr.20160412101537.13: *4* ipynb.do_prefix
+    def do_prefix(self, d):
+        '''Handle everything except the 'cells' attribute.'''
+        if d:
+            self.cells = d.get('cells',[])
+            if self.cells:
+                del d['cells']
+            self.set_ua(self.root, 'prefix', d)
+    #@+node:ekr.20160412101537.9: *4* ipynb.do_source
     def do_source(self, key, val):
         '''Set the cell's body text, or create a 'source' node.'''
         assert key == 'source', (key, val)
@@ -116,41 +158,17 @@ class Import_IPYNB(object):
             # Do create a new node.
             p = self.new_node('# list:%s' % key)
             p.b = val
-    #@+node:ekr.20160412101537.10: *6* ipynb.check_header
-    def check_header(self, m):
-        '''Return (n, name) or (None, None) on error.'''
-        val = (None, None)
+    #@+node:ekr.20160412101537.19: *4* ipynb.get_code_language (to be removed)
+    def get_code_language(self, d):
+        '''Return the language specified by the top-level metadata.'''
+        name = None
+        m = d.get('metadata')
         if m:
-            n1, name, n2 = m.group(1), m.group(2), m.group(3)
-            try:
-                if int(n1) == int(n2):
-                    val = int(n1), name
-            except Exception:
-                pass
-            if val == (None, None):
-                g.trace('malformed header:', m.group(0))
-        return val
-    #@+node:ekr.20160412101537.11: *6* ipynb.do_markdown_cell
-    def do_markdown_cell(self, p, s):
-        '''Split the markdown cell p if it contains one or more html headers.'''
-        if not s.strip():
-            return
-        lines = g.splitLines(s)
-        for s in lines:
-            m = self.re_header.search(s)
-            n, name = self.check_header(m)
-            if n is not None:
-                p.h = '<h%s> %s </h%s>' % (n, name.strip(), n)
-                break
-        p.b = '@language md\n\n' + ''.join(lines).lstrip()
-    #@+node:ekr.20160412101537.23: *6* ipynb.new_node
-    def new_node(self, h):
-
-        parent = self.parent or self.root
-        p = parent.insertAsLastChild()
-        p.h = h
-        return p
-    #@+node:ekr.20160412101537.22: *5* ipynb.is_empty_code
+            info = m.get('language_info')
+            if info:
+                name = info.get('name')
+        return name
+    #@+node:ekr.20160412101537.22: *4* ipynb.is_empty_code
     def is_empty_code(self, cell):
         '''Return True if cell is an empty code cell.'''
         if cell.get('cell_type') == 'code':
@@ -163,25 +181,31 @@ class Import_IPYNB(object):
             # g.trace(len(source), self.parent.h, sorted(cell))
             return not source and not keys and not outputs
         return False
-    #@+node:ekr.20160412101537.13: *4* ipynb.do_prefix
-    def do_prefix(self, d):
-        '''Handle everything except the 'cells' attribute.'''
-        if d:
-            self.cells = d.get('cells',[])
-            if self.cells:
-                del d['cells']
-            self.set_ua(self.root, 'prefix', d)
-    #@+node:ekr.20160412101537.19: *4* ipynb.get_code_language
-    def get_code_language(self, d):
-        '''Return the language specified by the top-level metadata.'''
-        name = None
-        m = d.get('metadata')
-        if m:
-            info = m.get('language_info')
-            if info:
-                name = info.get('name')
-        return name
-    #@+node:ekr.20160412101537.15: *4* ipynb.indent_cells & helper
+    #@+node:ekr.20160412101537.23: *4* ipynb.new_node
+    def new_node(self, h):
+
+        parent = self.parent or self.root
+        p = parent.insertAsLastChild()
+        p.h = h
+        return p
+    #@+node:ekr.20180408112636.1: *3* ipynb.Utils
+    #@+node:ekr.20160412101845.24: *4* ipynb.get_file_name
+    def get_file_name(self):
+        '''Open a dialog to write a Jupyter (.ipynb) file.'''
+        c = self.c
+        fn = g.app.gui.runSaveFileDialog(
+            c,
+            defaultextension=".ipynb",
+            filetypes=[
+                ("Jupyter files", "*.ipynb"),
+                ("All files", "*"),
+            ],
+            initialfile='',
+            title="Export To Jupyter File",
+        )
+        c.bringToFront()
+        return fn
+    #@+node:ekr.20160412101537.15: *4* ipynb.indent_cells
     def indent_cells(self):
         '''
         Indent md nodes in self.root.children().
@@ -202,7 +226,7 @@ class Import_IPYNB(object):
             stack = self.move_node(n, p, stack)
             p.moveToNodeAfterTree()
             # g.trace('=====', p and p.h)
-    #@+node:ekr.20160412101537.16: *5* ipynb.move_node
+    #@+node:ekr.20160412101537.16: *4* ipynb.move_node
     def move_node(self, n, p, stack):
         '''Move node to level n'''
         # Cut back the stack so that p will be at level n (if possible).
@@ -233,7 +257,7 @@ class Import_IPYNB(object):
         d2 [key] = val
         d ['ipynb'] = d2
         p.v.u = d
-    #@+node:ekr.20160412101537.24: *3* ipynb.parse
+    #@+node:ekr.20160412101537.24: *4* ipynb.parse
     def parse(self, fn):
         '''Parse the file, which should be JSON format.'''
         if g.os_path_exists(fn):
@@ -251,27 +275,6 @@ class Import_IPYNB(object):
         else:
             g.es_print('not found', fn)
             return None
-    #@+node:ekr.20160412103110.1: *3* ipynb.run
-    def run(self, s, parent, parse_body=False):
-        '''
-        @auto entry point. Called by code in leoImport.py.
-        '''
-        c = self.c
-        fn = parent.atAutoNodeName()
-        if c and fn:
-            changed = c.isChanged()
-            self.import_file(fn, parent)
-            # Similar to Importer.run.
-            parent.b = (
-                '@nocolor-node\n\n' +
-                'Note: This node\'s body text is ignored when writing this file.\n\n' +
-                'The @others directive is not required\n'
-            )
-            for p in parent.self_and_subtree():
-                p.clearDirty()
-            c.setChanged(changed)
-        elif not c or not fn:
-            g.trace('can not happen', c, fn)
     #@-others
 #@-others
 importer_dict = {
