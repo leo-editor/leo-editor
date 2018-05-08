0e182e0ea69b277890b37d21b5c39357faebbbde
Edward K. Ream <edreamleo@gmail.com>
2018-05-08 08:24:27 -0500

    Restored non-trivial tracing logic in pylab_kernel

--- leo/core/leoIPython.py
+++ leo/core/leoIPython.py
@@ -173,6 +173,8 @@ class InternalIPKernel(object):
     #@+node:ekr.20130930062914.15992: *3* ileo.pylab_kernel
     def pylab_kernel(self, gui):
         '''Launch an IPython kernel with pylab support for the gui.'''
+        trace = False and not g.unitTesting
+            # Increased logging.
         self.kernelApp = kernelApp = IPKernelApp.instance()
             # IPKernalApp is a singleton class.
             # Return the singleton instance, creating it if necessary.
@@ -184,6 +186,13 @@ class InternalIPKernel(object):
             args = ['python', '--pylab']
                 # Fails
                 # args = ['python', '--pylab=%s' % (gui)]
+            if trace:
+                args.append('--log-level=20')
+                    # Higher is *quieter*
+                # args.append('--debug')
+                # Produces a verbose IPython log.
+                #'--log-level=10'
+                # '--pdb', # User-level debugging
             try:
                 # self.pdb()
                 kernelApp.initialize(args)
