e5858efba77a2d5d5d9d3ae2a509f3eb896bd0ff
Edward K. Ream <edreamleo@gmail.com>
2018-05-12 05:29:37 -0500

    Removed traces from completer plugins

--- leo/plugins/ctagscompleter.py
+++ leo/plugins/ctagscompleter.py
@@ -85,7 +85,6 @@ def read_tags_file():
     '''Return the lines of ~/.leo/tags.
     Return [] on error.'''
 
-    trace = False ; verbose = True
     tagsFileName = os.path.expanduser('~/.leo/tags')
     if not os.path.exists(tagsFileName):
         return [] # EKR: 11/18/2009
@@ -95,12 +94,6 @@ def read_tags_file():
         f = open(tagsFileName)
         tags = f.read()
         lines = g.splitLines(tags)
-        if trace:
-            print('ctagscomplter.py: ~/.leo/tags has %s lines' % (
-                len(lines)))
-            if verbose:
-                for z in lines[:30]:
-                    print(repr(z))
         return lines
     except IOError:
         return []
@@ -186,27 +179,16 @@ class CtagsController(object):
 
         '''Return a list of all items starting with prefix.'''
 
-        trace = True ; verbose = False
         global tagLines
 
         if keep_tag_lines:
             # Use saved lines. Split at first whitespace.
             hits = [z.split(None,1) for z in tagLines if z.startswith(prefix)]
-            if trace:
-                for z in tagLines:
-                    if z.startswith(prefix):
-                        aList = z.split('\t')
-                        print(aList[0],g.shortFileName(aList[1]))
-                        print(aList[2:],'\n')
         else:
             # Open the file in a separate process, then use grep to match lines.
             # This will be slower, but grep returns very few lines.
             hits = (z.split(None) for z in os.popen('grep "^%s" ~/.leo/tags' % prefix))
 
-        if trace:
-            g.trace('%s hits' % len(hits))
-            if verbose:
-                for z in hits: print(z)
 
         desc = []
         for h in hits:
