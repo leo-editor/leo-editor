46018fdd04fba7953a944252c89bb9b68625aacc
Edward K. Ream <edreamleo@gmail.com>
2018-07-08 06:29:29 -0500

    More cleanups

--- leo/core/leoFileCommands.py
+++ leo/core/leoFileCommands.py
@@ -190,8 +190,8 @@ class FastRead (object):
                 'height': int(height),
                 'width': int(width),
             }
-        # Return reasonable defaults.
         else:
+            # Use reasonable defaults.
             d = {'top': 50, 'left': 50, 'height': 500, 'width': 800}
         d ['r1'] = float(c.db.get('body_outline_ratio', '0.5'))
         d ['r2'] = float(c.db.get('body_secondary_ratio', '0.5'))
@@ -209,18 +209,6 @@ class FastRead (object):
                 if key != 'tx':
                     gnx2ua [gnx][key] = self.resolveUa(key, val)
         return gnx2body, gnx2ua
-        
-        # From handleTnodeSaxAttributes:
-        
-            # d = sax_node.tnodeAttributes
-            # aDict = {}
-            # for key in d:
-                # val = g.toUnicode(d.get(key))
-                # val2 = self.getSaxUa(key, val)
-                # aDict[key] = val2
-            # if aDict:
-                # v.unknownAttributes = aDict
-
     #@+node:ekr.20180602062323.9: *5* fast.scanVnodes & helper
     def scanVnodes(self, gnx2body, gnx2vnode, gnx2ua, v_elements):
         
