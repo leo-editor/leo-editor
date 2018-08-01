2b4858432f45f0771ff322047e9a75bb7f78f541
Edward K. Ream <edreamleo@gmail.com>
2018-08-01 05:16:17 -0500

    Removed disabled code

--- leo/core/leoKeys.py
+++ leo/core/leoKeys.py
@@ -1671,8 +1671,6 @@ class KeyHandlerClass(object):
             # A list of commands whose bindings have been set to None in the local file.
         self.replace_meta_with_alt = False
             # True: (Mac only) swap Meta and Alt keys.
-        ### self.swap_mac_keys = False
-            # True: (Mac only) swap Command and Control keys.
         self.w = None
             # Note: will be None for NullGui.
         # Generalize...
@@ -1984,7 +1982,6 @@ class KeyHandlerClass(object):
         self.minibuffer_warning_color = getColor('minibuffer_warning_color') or 'lightgrey'
         self.minibuffer_error_color = getColor('minibuffer_error_color') or 'red'
         self.replace_meta_with_alt = getBool('replace_meta_with_alt')
-        ### self.swap_mac_keys = getBool('swap_mac_keys')
         self.warn_about_redefined_shortcuts = getBool('warn_about_redefined_shortcuts')
         # Has to be disabled (default) for AltGr support on Windows
         self.enable_alt_ctrl_bindings = c.config.getBool('enable_alt_ctrl_bindings')
