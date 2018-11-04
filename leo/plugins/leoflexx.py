1ffb3075c7cafa88a8926839954673dbfba48f69
Edward K. Ream <edreamleo@gmail.com>
2018-11-04 09:16:16 -0600

    Holy cow: added ace editor in the body pane

--- leo/plugins/leoflexx.py
+++ leo/plugins/leoflexx.py
@@ -76,8 +76,35 @@ class LeoTree(flx.Widget):
             self.label.set_html(text + '<br />' + self.label.html)
     #@-others
 #@+node:ekr.20181104082144.1: ** class LeoBody
+base_url = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.6/'
+flx.assets.associate_asset(__name__, base_url + 'ace.js')
+flx.assets.associate_asset(__name__, base_url + 'mode-python.js')
+flx.assets.associate_asset(__name__, base_url + 'theme-solarized_dark.js')
+
 class LeoBody(flx.Widget):
-    pass
+    
+    """ A CodeEditor widget based on Ace.
+    """
+
+    CSS = """
+    .flx-CodeEditor > .ace {
+        width: 100%;
+        height: 100%;
+    }
+    """
+
+    def init(self):
+        global window
+        # https://ace.c9.io/#nav=api
+        self.ace = window.ace.edit(self.node, "editor")
+        self.ace.setValue("import os\n\ndirs = os.walk")
+        self.ace.navigateFileEnd()  # otherwise all lines highlighted
+        self.ace.setTheme("ace/theme/solarized_dark")
+        self.ace.getSession().setMode("ace/mode/python")
+
+    @flx.reaction('size')
+    def __on_size(self, *events):
+        self.ace.resize()
 #@+node:ekr.20181104082149.1: ** class LeoLog
 class LeoLog(flx.Widget):
     pass
