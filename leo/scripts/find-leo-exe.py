4edbad707a61adf390b795ed4c20794459c11d92
matt wilkie <maphew@gmail.com>
2019-02-27 23:09:23 -0800

    works from Leo GUI, --script, and straight python :)
    ```
    matt@ENV-SURFACE-MHW C:\Users\mattw\code
    $ c:\apps\miniconda3\envs\leo-dev\python leo-editor\leo\scripts\win-register-leo.leox
    --- Running register-leo Leo script
    Attempting to register leo filetype with Windows...
      Python: c:\apps\miniconda3\envs\leo-dev\pythonw.exe
      Leo: c:\apps\miniconda3\envs\leo-dev\Scripts\leo.exe
      Found: c:\users\mattw\code\leo-editor\leo\Icons\LeoDoc.ico
      Registered!
      .leo, .leox files associated as 'Leo.File'
    --- end of script
    
    matt@ENV-SURFACE-MHW C:\Users\mattw\code
    $ c:\apps\miniconda3\envs\leo-dev\python leo-editor\leo\scripts\win-unregister-leo.leox
    --- Running unregister-leo Leo script
      Attempting to un-register leo filetype with Windows...
      Leo file type has been removed from registry
      .leo, .leox file associations removed from registry
    --- end of script
    
    matt@ENV-SURFACE-MHW C:\Users\mattw\code
    $ c:\apps\miniconda3\envs\leo-dev\Scripts\leo-m.exe --script=..\scripts\win-register-leo.leox
    
    Leo 5.8.1-b2 devel, build 20190222005157, Fri Feb 22 00:51:57 PST 2019
    --- Running register-leo Leo script
    Attempting to register leo filetype with Windows...
      Python: c:\apps\miniconda3\envs\leo-dev\pythonw.exe
      Leo: c:\apps\miniconda3\envs\leo-dev\Scripts\leo.exe
      Found: c:\users\mattw\code\leo-editor\leo\Icons\LeoDoc.ico
      Registered!
      .leo, .leox files associated as 'Leo.File'
    --- end of script
    ```
    
    Leo build: 20190227230924

--- leo/scripts/find-leo-exe.py
+++ leo/scripts/find-leo-exe.py
@@ -4,11 +4,16 @@ print(sys.executable)   # python.exe, pythonw.exe
 print(sys.exec_prefix)  # python.exe's directory
 
 # Leo's library path (ie: ./leo-editor/leo or PYTHONHOME/Lib/site-packages/leo)
-# We don't use leoGlobals.computeLoadDir() because it doesn't work 
-# without instantiating g.app first, and that's too much for just this
 import leo
 leolibpath = leo.__path__
 print(leolibpath)
+
+## We don't use leoGlobals.computeLoadDir() because it doesn't work 
+## without instantiating g.app first, and that's too much for just this
+# import leo.core.runLeo
+# import leo.core.leoGlobals as g
+# g.es_print(g.computeLeoDir())
+
 
 # Leo's launch exe, if in PATH. Py3 only, cross platform
 import shutil
