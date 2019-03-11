18ea4ef12bcb8ffb3d00ed1cdfce740c423376a0
matt wilkie <maphew@gmail.com>
2019-03-10 21:03:15 -0700

    move Win-only scripts into own folder; Broken unregister.
    re-org scripts.leo to match the move.
    
    Half-baked attempt to combine elevate and unregister-leo.leox into a
    single file. Still much to do.
    
    Leo build: 20190310210315

--- leo/scripts/win/elevate-and-register.py
+++ leo/scripts/win/elevate-and-register.py
@@ -5,10 +5,10 @@ import leo
 import leo.core.leoGlobals as g
 import subprocess
 
-sc_path = os.path.realpath(os.path.join(leo.__path__[0], 'scripts'))
+sc_path = os.path.realpath(os.path.join(leo.__path__[0], 'scripts/win'))
 elevate = os.path.join(sc_path, 'elevate.py')
-register = os.path.join(sc_path, 'win-register-leo.leox')
-unregister = os.path.join(sc_path, 'win-unregister-leo.leox')
+register = os.path.join(sc_path, 'register-leo.leox')
+unregister = os.path.join(sc_path, 'unregister-leo.leox')
 
 g.es_print('--- Running elevate and register')
 result = subprocess.run(['python', elevate, register], stdout=subprocess.PIPE)
