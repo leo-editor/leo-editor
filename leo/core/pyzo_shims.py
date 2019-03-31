b4e4165f4bc423788e314d4d517a1a3b25a164db
Edward K. Ream <edreamleo@gmail.com>
2019-03-31 08:29:20 -0500

    Begin transition to new settings in ConfigShim

--- leo/core/pyzo_shims.py
+++ leo/core/pyzo_shims.py
@@ -60,117 +60,126 @@ except ImportError:
     
 from pyzo.util.zon import Dict
 
+# config_base = _dict if new_config else Dict
+
 config_base = _dict if new_config else Dict
+    # The old code needs _dict, not dict.
 
 class ConfigShim(config_base):
     
     if new_config:
-        #@+<< define settings >>
-        #@+node:ekr.20190331053237.1: *3* << define settings >>
-        # ^(\s*)(\w+)\:
+        #@+<< define bunch settings >>
+        #@+node:ekr.20190331082353.1: *3* << define bunch settings >>
+        # '(\w+)': With: \1 = 
 
-        advanced = {
-            'autoCompDelay':200,
-            'fileExtensionsToLoadFromDir':'py,pyw,pyx,txt,bat',
-            'find_autoHide_timeout':10,
-            'homeAndEndWorkOnDisplayedLine':0,
-            'shellMaxLines':10000,
-            'titleText':'{fileName} ({fullPath}) - Interactive Editor for Python',
-        }
-        settings = {
-            'allowFloatingShell':0,
-            'autoCallTip':1,
-            'autoClose_Brackets':1,
-            'autoClose_Quotes':1,
-            'autoComplete':1,
-            'autoComplete_acceptKeys':'Tab',
-            'autoComplete_caseSensitive':0,
-            'autoComplete_fillups':'\n',
-            'autoComplete_keywords':1,
-            'autoIndent':1,
-            'changeDirOnFileExec':0,
-            'defaultIndentUsingSpaces':1,
-            'defaultIndentWidth':4,
-            'defaultLineEndings':'CRLF',
-            'defaultStyle':'python',
-            'justificationWidth':70,
-            'language':'English (US)',
-            'removeTrailingWhitespaceWhenSaving':0,
-        }
-        shellConfigs2 = [{
-            'argv':'',
-            'environ':'',
-            'exe':'c:\\anaconda3\\python.exe',
-            'gui':'auto',
-            'ipython':'yes',
-            'name':'Python',
-            'projectPath':'',
-            'pythonPath':'',
-            'scriptFile':'',
-            'startDir':'',
-            'startupScript':'',
-        }]
-        shortcuts2 = {
-            #@+<< define shortcuts2 >>
-            #@+node:ekr.20190331053528.1: *4* << define shortcuts2 >>
-            'edit__comment':'Ctrl+R,',
-            'edit__copy':'Ctrl+C,Ctrl+Insert',
-            'edit__cut':'Ctrl+X,Shift+Delete',
-            'edit__dedent':'Shift+Tab,',
-            'edit__delete_line':'Ctrl+D,',
-            'edit__duplicate_line':'Ctrl+Shift+D,',
-            'edit__find_next':'Ctrl+G,F3',
-            'edit__find_or_replace':'Ctrl+F,',
-            'edit__find_previous':'Ctrl+Shift+G,Shift+F3',
-            'edit__find_selection':'Ctrl+F3,',
-            'edit__find_selection_backward':'Ctrl+Shift+F3,',
-            'edit__indent':'Tab,',
-            'edit__justify_commentdocstring':'Ctrl+J,',
-            'edit__paste':'Ctrl+V,Shift+Insert',
-            'edit__paste_and_select':'Ctrl+Shift+V',
-            'edit__redo':'Ctrl+Y,',
-            'edit__select_all':'Ctrl+A,',
-            'edit__toggle_breakpoint':'Ctrl+B,',
-            'edit__uncomment':'Ctrl+T,',
-            'edit__undo':'Ctrl+Z,',
-            'file__close':'Ctrl+W,',
-            'file__new':'Ctrl+N,',
-            'file__open':'Ctrl+O,',
-            'file__save':'Ctrl+S,',
-            'run__execute_cell':'Ctrl+Return,Ctrl+Enter',
-            'run__execute_cell_and_advance':'Ctrl+Shift+Return,Ctrl+Shift+Enter',
-            'run__execute_file':'Ctrl+E,F5',
-            'run__execute_main_file':'Ctrl+M,F6',
-            'run__execute_selection':'Alt+Return,F9',
-            'run__execute_selection_and_advance':'Shift+F9,Shift+Alt+Return',
-            'run__run_file_as_script':'Ctrl+Shift+E,Ctrl+F5',
-            'run__run_main_file_as_script':'Ctrl+Shift+M,Ctrl+F6',
-            'shell__clear_screen':'Ctrl+L,',
-            'shell__close':'Alt+K,',
-            'shell__create_shell_1_':'Ctrl+1,',
-            'shell__create_shell_2_':'Ctrl+2,',
-            'shell__create_shell_3_':'Ctrl+3,',
-            'shell__create_shell_4_':'Ctrl+4,',
-            'shell__create_shell_5_':'Ctrl+5,',
-            'shell__create_shell_6_':'Ctrl+6,',
-            'shell__create_shell_7_':'Ctrl+7,',
-            'shell__create_shell_8_':'Ctrl+8,',
-            'shell__interrupt':'Ctrl+I,Meta+C',
-            'shell__postmortem_debug_from_last_traceback':'Ctrl+P,',
-            'shell__restart':'Ctrl+K,',
-            'shell__terminate':'Ctrl+Shift+K,',
-            'view__select_editor':'Ctrl+9,F2',
-            'view__select_previous_file':'Ctrl+Tab,',
-            'view__select_shell':'Ctrl+0,F1',
-            'view__zooming__zoom_in':'Ctrl+=,Ctrl++',
-            'view__zooming__zoom_out':'Ctrl+-,',
-            'view__zooming__zoom_reset':'Ctrl+\\,'
-            #@-<< define shortcuts2 >>
-        }
-        state = {
-            'editorState2':[
+        advanced = g.Bunch(
+            autoCompDelay=200,
+            fileExtensionsToLoadFromDir='py,pyw,pyx,txt,bat',
+            find_autoHide_timeout=10,
+            homeAndEndWorkOnDisplayedLine=0,
+            shellMaxLines=10000,
+            titleText='{fileName} ({fullPath}) - Interactive Editor for Python',
+        )
+
+        settings = g.Bunch(   
+            allowFloatingShell=0,
+            autoCallTip=1,
+            autoClose_Brackets=1,
+            autoClose_Quotes=1,
+            autoComplete=1,
+            autoComplete_acceptKeys='Tab',
+            autoComplete_caseSensitive=0,
+            autoComplete_fillups='\n',
+            autoComplete_keywords=1,
+            autoIndent=1,
+            changeDirOnFileExec=0,
+            defaultIndentUsingSpaces=1,
+            defaultIndentWidth=4,
+            defaultLineEndings='CRLF',
+            defaultStyle='python',
+            justificationWidth=70,
+            language='English (US)',
+            removeTrailingWhitespaceWhenSaving=0,
+        )
+
+        shellConfigs2 = [
+            g.Bunch(
+                argv = '',
+                environ = '',
+                exe = 'c:\\anaconda3\\python.exe',
+                gui = 'auto',
+                ipython = 'yes',
+                name = 'Python',
+                projectPath = '',
+                pythonPath = '',
+                scriptFile = '',
+                startDir = '',
+                startupScript = '',
+                )
+        ]
+
+        shortcuts2 = g.bunch(
+            #@+<< define bunch shortcuts2 >>
+            #@+node:ekr.20190331082549.1: *4* << define bunch shortcuts2 >>
+            edit__comment = 'Ctrl+R,',
+            edit__copy = 'Ctrl+C,Ctrl+Insert',
+            edit__cut = 'Ctrl+X,Shift+Delete',
+            edit__dedent = 'Shift+Tab,',
+            edit__delete_line = 'Ctrl+D,',
+            edit__duplicate_line = 'Ctrl+Shift+D,',
+            edit__find_next = 'Ctrl+G,F3',
+            edit__find_or_replace = 'Ctrl+F,',
+            edit__find_previous = 'Ctrl+Shift+G,Shift+F3',
+            edit__find_selection = 'Ctrl+F3,',
+            edit__find_selection_backward = 'Ctrl+Shift+F3,',
+            edit__indent = 'Tab,',
+            edit__justify_commentdocstring = 'Ctrl+J,',
+            edit__paste = 'Ctrl+V,Shift+Insert',
+            edit__paste_and_select = 'Ctrl+Shift+V',
+            edit__redo = 'Ctrl+Y,',
+            edit__select_all = 'Ctrl+A,',
+            edit__toggle_breakpoint = 'Ctrl+B,',
+            edit__uncomment = 'Ctrl+T,',
+            edit__undo = 'Ctrl+Z,',
+            file__close = 'Ctrl+W,',
+            file__new = 'Ctrl+N,',
+            file__open = 'Ctrl+O,',
+            file__save = 'Ctrl+S,',
+            run__execute_cell = 'Ctrl+Return,Ctrl+Enter',
+            run__execute_cell_and_advance = 'Ctrl+Shift+Return,Ctrl+Shift+Enter',
+            run__execute_file = 'Ctrl+E,F5',
+            run__execute_main_file = 'Ctrl+M,F6',
+            run__execute_selection = 'Alt+Return,F9',
+            run__execute_selection_and_advance = 'Shift+F9,Shift+Alt+Return',
+            run__run_file_as_script = 'Ctrl+Shift+E,Ctrl+F5',
+            run__run_main_file_as_script = 'Ctrl+Shift+M,Ctrl+F6',
+            shell__clear_screen = 'Ctrl+L,',
+            shell__close = 'Alt+K,',
+            shell__create_shell_1_ = 'Ctrl+1,',
+            shell__create_shell_2_ = 'Ctrl+2,',
+            shell__create_shell_3_ = 'Ctrl+3,',
+            shell__create_shell_4_ = 'Ctrl+4,',
+            shell__create_shell_5_ = 'Ctrl+5,',
+            shell__create_shell_6_ = 'Ctrl+6,',
+            shell__create_shell_7_ = 'Ctrl+7,',
+            shell__create_shell_8_ = 'Ctrl+8,',
+            shell__interrupt = 'Ctrl+I,Meta+C',
+            shell__postmortem_debug_from_last_traceback = 'Ctrl+P,',
+            shell__restart = 'Ctrl+K,',
+            shell__terminate = 'Ctrl+Shift+K,',
+            view__select_editor = 'Ctrl+9,F2',
+            view__select_previous_file = 'Ctrl+Tab,',
+            view__select_shell = 'Ctrl+0,F1',
+            view__zooming__zoom_in = 'Ctrl+=,Ctrl++',
+            view__zooming__zoom_out = 'Ctrl+-,',
+            view__zooming__zoom_reset = 'Ctrl+\\,'
+            #@-<< define bunch shortcuts2 >>
+        )
+
+        state = g.Bunch(
+            editorState2=[
                 #@+<< define editorState2 >>
-                #@+node:ekr.20190331053634.1: *4* << define editorState2 >>
+                #@+node:ekr.20190331082353.2: *4* << define editorState2 >>
                 [
                     'C:\\apps\\pyzo\\source\\pyzo\\codeeditor\\highlighter.py',
                     3279,
@@ -191,89 +200,92 @@ class ConfigShim(config_base):
                 ]
                 #@-<< define editorState2 >>
             ],
-            'find_autoHide':1,
-            'find_matchCase':0,
-            'find_regExp':0,
-            'find_show':0,
-            'find_wholeWord':1,
-            'loadedTools':['pyzofilebrowser', 'pyzologger', 'pyzosourcestructure'],
-            'newUser':1,
-            'windowGeometry':'AdnQywACAAAAAAGjAAAA2AAABv0AAANWAAABqwAAAPcAAAb1AAADTgAAAAAAAAAAB4A=\n',
-            'windowState':[
-                'AAAA/wAAAAD9AAAAAgAAAAAAAACeAAACRPwCAAAAAfwAAAAUAAACRAAAAYgA/////AIAAAAC+wAA\n',
-                'AB4AcAB5AHoAbwBmAGkAbABlAGIAcgBvAHcAcwBlAHIBAAAAFAAAAWEAAAEIAP////sAAAAmAHAA\n',
-                'eQB6AG8AcwBvAHUAcgBjAGUAcwB0AHIAdQBjAHQAdQByAGUBAAABewAAAN0AAAB6AP///wAAAAEA\n',
-                'AAGEAAACRPwCAAAABvsAAAAMAHMAaABlAGwAbABzAQAAABQAAAD/AAAAcwD////7AAAAFABwAHkA\n',
-                'egBvAGwAbwBnAGcAZQByAQAAARkAAAE/AAAAWQD////7AAAAGgBwAHkAegBvAHcAbwByAGsAcwBw\n',
-                'AGEAYwBlAAAAAT4AAAEaAAAAAAAAAAD7AAAAJgBwAHkAegBvAGkAbgB0AGUAcgBhAGMAdABpAHYA\n',
-                'ZQBoAGUAbABwAAAAAnkAAAB6AAAAAAAAAAD7AAAAIgBwAHkAegBvAGgAaQBzAHQAbwByAHkAdgBp\n',
-                'AGUAdwBlAHIAAAACGgAAAVsAAAAAAAAAAPsAAAAcAHAAeQB6AG8AdwBlAGIAYgByAG8AdwBzAGUA\n',
+            find_autoHide=1,
+            find_matchCase=0,
+            find_regExp=0,
+            find_show=0,
+            find_wholeWord=1,
+            loadedTools=['pyzofilebrowser', 'pyzologger', 'pyzosourcestructure'],
+            newUser=1,
+            windowGeometry='AdnQywACAAAAAAGjAAAA2AAABv0AAANWAAABqwAAAPcAAAb1AAADTgAAAAAAAAAAB4A=\n',
+            windowState=(
+                'AAAA/wAAAAD9AAAAAgAAAAAAAACeAAACRPwCAAAAAfwAAAAUAAACRAAAAYgA/////AIAAAAC+wAA\n'
+                'AB4AcAB5AHoAbwBmAGkAbABlAGIAcgBvAHcAcwBlAHIBAAAAFAAAAWEAAAEIAP////sAAAAmAHAA\n'
+                'eQB6AG8AcwBvAHUAcgBjAGUAcwB0AHIAdQBjAHQAdQByAGUBAAABewAAAN0AAAB6AP///wAAAAEA\n'
+                'AAGEAAACRPwCAAAABvsAAAAMAHMAaABlAGwAbABzAQAAABQAAAD/AAAAcwD////7AAAAFABwAHkA\n'
+                'egBvAGwAbwBnAGcAZQByAQAAARkAAAE/AAAAWQD////7AAAAGgBwAHkAegBvAHcAbwByAGsAcwBw\n'
+                'AGEAYwBlAAAAAT4AAAEaAAAAAAAAAAD7AAAAJgBwAHkAegBvAGkAbgB0AGUAcgBhAGMAdABpAHYA\n'
+                'ZQBoAGUAbABwAAAAAnkAAAB6AAAAAAAAAAD7AAAAIgBwAHkAegBvAGgAaQBzAHQAbwByAHkAdgBp\n'
+                'AGUAdwBlAHIAAAACGgAAAVsAAAAAAAAAAPsAAAAcAHAAeQB6AG8AdwBlAGIAYgByAG8AdwBzAGUA\n'
                 'cgAAAAKwAAAAxQAAAAAAAAAAAAADHQAAAkQAAAAEAAAABAAAAAgAAAAI/AAAAAA=\n'
-            ]
-        }
-        tools = {
-            'pyzofilebrowser':{},
-            'pyzofilebrowser2':{
-                'expandedDirs':['c:\\apps\\pyzo\\source\\pyzo'],
-                'nameFilter':'!*.pyc',
-                'path':'c:\\apps\\pyzo\\source\\pyzo',
-                'searchMatchCase':0,
-                'searchRegExp':0,
-                'searchSubDirs':1,
-                'starredDirs':[{
-                    'addToPythonpath':0,
-                    'name':'Pyzo sources',
-                    'path':'c:\\apps\\pyzo\\source'
-                }]
-            },
-            'pyzohistoryviewer':{},
-            'pyzointeractivehelp':{
-                'fontSize':14,
-                'noNewlines':1,
-                'smartNewlines':1
-            },
-            'pyzologger':{},
-            'pyzosourcestructure':{
-                'level':1,
-                'showTypes':['class', 'def', 'cell', 'todo'],
-            },
-            'pyzowebbrowser':{
-                'bookMarks':[
+            )
+        )
+
+        tools = g.Bunch(
+            pyzofilebrowser = g.Bunch(),
+            pyzofilebrowser2 = g.Bunch(
+                expandedDirs = ['c:\\apps\\pyzo\\source\\pyzo'],
+                nameFilter = '!*.pyc',
+                path = 'c:\\apps\\pyzo\\source\\pyzo',
+                searchMatchCase = 0,
+                searchRegExp = 0,
+                searchSubDirs = 1,
+                starredDirs =[
+                    g.Bunch(
+                        addToPythonpath = 0,
+                        name = 'Pyzo sources',
+                        path = 'c:\\apps\\pyzo\\source',
+                    )
+                ]
+            ),
+            pyzohistoryviewer = g.Bunch(),
+            pyzointeractivehelp = g.Bunch(
+                fontSize = 14,
+                noNewlines = 1,
+                smartNewlines = 1
+            ),
+            pyzologger = g.Bunch(),
+            pyzosourcestructure = g.Bunch(
+                level = 1,
+                showTypes = ['class', 'def', 'cell', 'todo'],
+            ),
+            pyzowebbrowser = g.Bunch(
+                bookMarks = [
                     'docs.python.org',
                     'scipy.org',
                     'doc.qt.nokia.com/4.5/',
                     'pyzo.org',
                 ],
-                'zoomFactor':1.0,
-            },
-            'pyzoworkspace':{
-                'hideTypes':[],
-                'typeTranslation':{
-                    'builtin_function_or_method':'function',
-                    'method':'function'
-                }
-            }
-        }
-            
-        view = {
-            'autoComplete_popupSize':[300, 100],
-            'codeFolding':0,
-            'doBraceMatch':1,
-            'edgeColumn':80,
-            'fontname':'DejaVu Sans Mono',
-            'highlightCurrentLine':1,
-            'highlightMatchingBracket':1,
-            'qtstyle':'fusion',
-            'showIndentationGuides':1,
-            'showLineEndings':0,
-            'showStatusbar':0,
-            'showWhitespace':0,
-            'showWrapSymbols':0,
-            'tabWidth':4,
-            'wrap':1,
-            'zoom':2
-        }
-        #@-<< define settings >>
+                zoomFactor = 1.0,
+            ),
+            pyzoworkspace = g.Bunch(
+                hideTypes = [],
+                typeTranslation = g.Bunch(
+                    builtin_function_or_method = 'function',
+                    method = 'function'
+                )
+            )
+        ) 
+
+        view = g.Bunch(
+            autoComplete_popupSize=[300, 100],
+            codeFolding=0,
+            doBraceMatch=1,
+            edgeColumn=80,
+            fontname='DejaVu Sans Mono',
+            highlightCurrentLine=1,
+            highlightMatchingBracket=1,
+            qtstyle='fusion',
+            showIndentationGuides=1,
+            showLineEndings=0,
+            showStatusbar=0,
+            showWhitespace=0,
+            showWrapSymbols=0,
+            tabWidth=4,
+            wrap=1,
+            zoom=2
+        )
+        #@-<< define bunch settings >>
         #@+<< new config methods >>
         #@+node:ekr.20190331052308.1: *3* << new config methods >>
         #@+others
