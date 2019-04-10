c09b0e76ec6638d856cb8e0c2f54c8df8f6f0126
Edward K. Ream <edreamleo@gmail.com>
2019-04-10 17:35:36 -0500

    Added pyzo_support plugin. It gets through pyzo startup, with traces and --pyzo enabled.

---
+++ leo/plugins/pyzo_support.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#@+leo-ver=5-thin
+#@+node:ekr.20190410171646.1: * @file pyzo_support.py
+#@@first
+'''
+pyzo_support.py: Allow access to pyzo features within Leo.
+
+This plugin is active only if pyzo modules import successfully.
+'''
+
+#@+<< imports >>
+#@+node:ekr.20190410172058.1: ** << imports >> pyzo support
+import leo.core.leoGlobals as g
+
+#
+# Automatically add --pyzo to sys.argv.
+import sys
+if '--pyzo' not in sys.argv:
+    sys.argv.append('--pyzo')
+#
+# See whether pyzo can be imported.
+try:
+    #
+    # For now, enable all traces.
+    g.pyzo = True
+    g.pyzo_trace = True
+    g.pyzo_trace_imports = True
+    # Be explicit about where everything comes from...
+    import pyzo
+    import pyzo.core.editor
+    import pyzo.core.main
+    import pyzo.core.splash
+    import pyzo.util
+except ImportError:
+    pyzo = None
+    g.pyzo = False
+    g.pyzo_trace = False
+    g.pyzo_trace_imports = False
+#@-<< imports >>
+#@+others
+#@+node:ekr.20190410171905.1: ** init
+def init():
+    '''Return True if the plugin has loaded successfully.'''
+    if g.app.gui.guiName() != "qt":
+        print('pyzo_support plugin requires Qt gui')
+        return False
+    if not pyzo:
+        print('pyzo_support plugin requires pyzo')
+        return False
+    g.trace('pyzo_support.init: pyzo imported successfully', pyzo)
+    # g.registerHandler('after-create-leo-frame',onCreate)
+    g.plugin_signon(__name__)
+    return True
+#@-others
+#@-leo
