add35de668c2339fb9f6bb39653259a0635ce371
Edward K. Ream <edreamleo@gmail.com>
2019-04-10 20:44:07 -0500

    Cleaned up init code.  Added is_pyzo_loaded. Using the ToolManager class is probably required.

--- leo/plugins/pyzo_support.py
+++ leo/plugins/pyzo_support.py
@@ -7,56 +7,61 @@ pyzo_support.py: Allow access to pyzo features within Leo.
 
 This plugin is active only if pyzo modules import successfully.
 '''
-
 # To do:
 # 1. Support pyzo file browser.
 # 2. Support pyzo shell & debugger.
-
-#@+<< imports >>
-#@+node:ekr.20190410172058.1: ** << imports >> pyzo support
-import leo.core.leoGlobals as g
-#
-# Automatically add --pyzo to sys.argv.
-# This is a convention used in my personal copy of pyzo.
 import sys
+import leo.core.leoGlobals as g
+#@+<< add --pyzo to sys.argv >>
+#@+node:ekr.20190410200815.1: ** << add --pyzo to sys.argv >>
+#
+# This is a convention used in my personal copy of pyzo.
+
 if '--pyzo' not in sys.argv:
     sys.argv.append('--pyzo')
-#
-# See whether pyzo can be imported.
-try:
-    if 1:
-        g.pyzo = False
-        g.pyzo_trace = False
-        g.pyzo_trace_imports = False
-    else:
-        g.pyzo = True
-        g.pyzo_trace = True
-        g.pyzo_trace_imports = True
-    # Be explicit about where everything comes from...
-    import pyzo
-    # import pyzo.core.editor
-    # import pyzo.core.main
-    # import pyzo.core.splash
-    # import pyzo.util
-except ImportError:
-    pyzo = None
-    g.pyzo = False
-    g.pyzo_trace = False
-    g.pyzo_trace_imports = False
-#@-<< imports >>
+#@-<< add --pyzo to sys.argv >>
+#@+<< set default traces >>
+#@+node:ekr.20190410200749.1: ** << set default traces >>
+g.pyzo = True
+g.pyzo_trace = True
+g.pyzo_trace_imports = True
+#@-<< set default traces >>
 #@+others
-#@+node:ekr.20190410171905.1: ** init
+#@+node:ekr.20190410171905.1: ** function: init
 def init():
     '''Return True if the plugin has loaded successfully.'''
     if g.app.gui.guiName() != "qt":
         print('pyzo_support plugin requires Qt gui')
         return False
-    if not pyzo:
+    if not is_pyzo_loaded():
         print('pyzo_support plugin requires pyzo')
         return False
-    # g.trace('pyzo_support.init: pyzo imported successfully', pyzo)
-    # g.registerHandler('after-create-leo-frame',onCreate)
     g.plugin_signon(__name__)
+    # g.registerHandler('after-create-leo-frame',onCreate)
     return True
+#@+node:ekr.20190410200453.1: ** function: is_pyzo_loaded
+def is_pyzo_loaded():
+    '''
+    Return True if pyzo appears to be loaded,
+    using the "lightest" possible imports.
+    '''
+    try:
+        import pyzo
+        assert pyzo
+        from pyzo.tools import ToolManager
+        assert ToolManager
+        # Be explicit about where everything comes from...
+            # import pyzo
+            # import pyzo.core.editor
+            # import pyzo.core.main
+            # import pyzo.core.splash
+            # import pyzo.util
+        return True
+    except Exception:
+        g.es_exception()
+        g.pyzo = False
+        g.pyzo_trace = False
+        g.pyzo_trace_imports = False
+        return False
 #@-others
 #@-leo
