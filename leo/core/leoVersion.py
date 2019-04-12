761c72305850fa9a25f576bce0bff3aa49ef6679
Edward K. Ream <edreamleo@gmail.com>
2019-04-12 10:37:51 -0500

    Version is now 5.9-final-devel

--- leo/core/leoVersion.py
+++ leo/core/leoVersion.py
@@ -49,7 +49,7 @@ import leo.core.leoGlobals as g
 # 
 # The install_hooks.py script copies these two files to leo-editor/.git/hooks.
 #@-<< about install hooks >>
-version = "5.9-b1"
+version = "5.9-final-devel"
 date, build = g.jsonCommitInfo()
 branch = g.gitBranchName()
 #@@language python
