aa77618e04993ee4e0ace0fa30568e462e53b392
Edward K. Ream <edreamleo@gmail.com>
2019-04-30 06:48:54 -0500

    leoVersion.py now contains just two static constants
    Leo build: 20190430064854

--- leo/core/leoVersion.py
+++ leo/core/leoVersion.py
@@ -3,14 +3,10 @@
 '''
 A module holding the following version-related info:
 
-leoVersion.branch:      The git branch name, or ''.
-leoVersion.build:       The timestamp field from commit_timestamp.json.
-leoVersion.date:        The asctime field from commit_timestamp.json.
 leoVersion.static_date: The date of official releases.
                         Also used when the git repo is not available.
-leoVersion.version:     Leo's version number, set below.
+leoVersion.version:     Leo's version number.
 '''
-import leo.core.leoGlobals as g
 #@+<< version dates >>
 #@+node:ekr.20141117073519.12: ** << version dates >>
 #@@nocolor-node
@@ -40,25 +36,7 @@ import leo.core.leoGlobals as g
 #@-<< version dates >>
 version = '6.0-devel'
 static_date = 'April 29, 2019'
-#@+others
-#@+node:ekr.20190429093839.1: ** compute_module_vars (leoVersion.py)
-def compute_module_vars(static_date):
-    '''
-    Return (date, branch, build), using git if possible.
-    
-    Fall back to (static_date, '', '') when git is not available.
-    '''
-    author, build, date = g.getGitVersion()
-    branch = g.gitBranchName()
-    return author, date or static_date, branch, build
-        # alpha order.
-#@-others
-author, date, branch, build = compute_module_vars(static_date)
-if 0:
-    print('AUTHOR', author)
-    print('DATE  ', date)
-    print('BRANCH', branch)
-    print('BUILD ', build)
+
 #@@language python
 #@@tabwidth -4
 #@-leo
