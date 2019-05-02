922b5059620a827258dc23257220d8b461d8326b
maphew <maphew@gmail.com>
2019-05-02 10:47:49 -0700

    we don't need grep if `--quiet` is _before_ the command
    thank you @tuomur
    https://stackoverflow.com/questions/21012855/how-do-i-make-python-setup-py-test-q-quieter

--- leo/scripts/win/build-leo.bat
+++ leo/scripts/win/build-leo.bat
@@ -1,6 +1,10 @@
 @:: Build source distribution and wheel
 @:: typically used for deploying to PyPi.org and installing Leo with Pip
-pushd %~dp0..\..
-python setup.py sdist bdist_wheel |findstr /v "^creating ^adding ^copying ^creating ^byte-compiling"
+@set _p=%prompt%
+@set prompt=$H
+@echo This can take a 5 or more minutes, be patient with no screen output.
+pushd %~dp0..\..\..
+rem python setup.py --quiet sdist bdist_wheel
 start dist
 popd
+@set prompt=%_p%
