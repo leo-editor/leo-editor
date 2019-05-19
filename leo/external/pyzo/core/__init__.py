0f6643300ae430d750b583522aa6391d3f664def
Edward K. Ream <edreamleo@gmail.com>
2019-05-18 19:03:34 -0500

    Removed entire leo/external/pyzo folder: Leo isn't going to use the Pyzo File Browser without pyzo itself.

--- leo/external/pyzo/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2016, the Pyzo development team
-#
-# Pyzo is distributed under the terms of the (new) BSD License.
-# The full license can be found in 'license.txt'.
-
-""" Package core - the core of Pyzo.
-"""
