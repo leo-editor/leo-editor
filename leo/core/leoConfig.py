a7a2f0441114b2c5cf39180f87f539d26aa11dfd
Edward K. Ream <edreamleo@gmail.com>
2019-08-31 04:55:46 -0500

    Create the start of inner outline structure

--- leo/core/leoConfig.py
+++ leo/core/leoConfig.py
@@ -926,11 +926,16 @@ class ActiveSettingsOutline:
         self.leo_settings_path = lm.computeLeoSettingsPath()
         self.my_settings_path = lm.computeMyLeoSettingsPath()
         self.leo_settings_c = lm.openSettingsFile(self.leo_settings_path)
-        self.my_settings_c = lm.openSettingsFile(self.leo_settings_path)
+        self.my_settings_c = lm.openSettingsFile(self.my_settings_path)
         #
         # This must be done *after* reading myLeoSettigns.leo.
         self.theme_path = lm.computeThemeFilePath()
         self.theme_c = lm.openSettingsFile(self.theme_path) if self.theme_path else None
+        #
+        # Create the list of hidden commanders. Order matters.
+        self.hidden_commanders = [self.leo_settings_c, self.my_settings_c]
+        if self.theme_c:
+            self.hidden_commanders.append(self.theme_c)
     #@+node:ekr.20190831035231.1: *3* aso.create_commander
     def create_commander(self):
         """Create a new commander, shown in a new tab"""
@@ -968,7 +973,27 @@ class ActiveSettingsOutline:
         c.frame.createFirstTreeNode()
         root = c.rootPosition()
         root.h = 'Active settings for %s' % self.c.shortFileName()
+        # Create all the inner settings outlines.
+        commanders = self.hidden_commanders + [self.c]
+        for commander in reversed(commanders):
+            p = root.insertAfter()
+            p.h = g.shortFileName(commander.fileName())
+            self.create_inner_outline(commander, p)
+        # Clean all dirty/changed bits, so closing this outline won't prompt for a save.
+        for p in c.all_positions():
+            p.v.clearDirty()
+        c.setChanged(changedFlag=False, redrawFlag=False)
         c.redraw()
+    #@+node:ekr.20190831044130.1: *3* aso.create_inner_outline
+    def create_inner_outline(self, hidden_c, root):
+        """
+        Create the outline for the given hidden commander, as descendants of root.
+        """
+        # Find the settings tree
+        active = root.insertAsLastChild()
+        active.h = 'active settings'
+        inactive = root.insertAsLastChild()
+        inactive.h = 'inactive settings'
     #@-others
 #@+node:ekr.20041119203941: ** class GlobalConfigManager
 class GlobalConfigManager:
