a168b145bc1055191fb86486b77bdc1938b81524
Edward K. Ream <edreamleo@gmail.com>
2019-10-07 05:46:39 -0500

    Rewrite the AsciiDoctorCommands class.  Added adoc-with-preview and pandoc-with-preview.

--- leo/core/leoAsciiDoctor.py
+++ leo/core/leoAsciiDoctor.py
@@ -5,23 +5,28 @@
 """Supports AsciiDoctor by defining the adoc command."""
 #@+<< leoAsciiDoctor imports >>
 #@+node:ekr.20190515070742.3: ** << leoAsciiDoctor imports >>
+from distutils.spawn import find_executable
 import io
 StringIO = io.StringIO
+import os
 import re
 import time
 import leo.core.leoGlobals as g
 
 #@-<< leoAsciiDoctor imports >>
+asciidoctor_exec = find_executable('asciidoctor')
+asciidoc3_exec = find_executable('asciidoc3')
+pandoc_exec = find_executable('pandoc')
 #@+others
-#@+node:ekr.20191006153522.1: ** commands
-#@+node:ekr.20190515070742.22: *3* adoc_command: @cmd('adoc')
+#@+node:ekr.20191006153522.1: ** adoc & pandoc commands
+#@+node:ekr.20190515070742.22: *3* @cmd('adoc') & @cmd('adoc-with-preview')
 @g.command('adoc')
 def adoc_command(event=None, verbose=True):
     #@+<< adoc command docstring >>
     #@+node:ekr.20190515115100.1: *4* << adoc command docstring >>
     """
     The adoc command writes all @adoc nodes in the selected tree to the
-    files given in each @@doc node. If no @adoc nodes are found, the
+    files given in each @doc node. If no @adoc nodes are found, the
     command looks up the tree.
 
     Each @adoc node should have the form: `@adoc x.adoc`. Relative file names
@@ -74,8 +79,16 @@ def adoc_command(event=None, verbose=True):
     c = event and event.get('c')
     if not c:
         return None
-    return c.asciiDoctorCommands.adoc_command(event, verbose=verbose)
-#@+node:ekr.20191006153411.1: *3* pandoc_command: @cmd('pandoc')
+    return c.asciiDoctorCommands.adoc_command(event, preview=False, verbose=verbose)
+    
+@g.command('adoc-with-preview')
+def adoc_with_preview_command(event=None, verbose=True):
+    """Run the adoc command, then show the result in the browser."""
+    c = event and event.get('c')
+    if not c:
+        return None
+    return c.asciiDoctorCommands.adoc_command(event, preview=True, verbose=verbose)
+#@+node:ekr.20191006153411.1: *3* @cmd('pandoc') & @cmd('pandoc-with-preview')
 @g.command('pandoc')
 def pandoc_command(event, verbose=True):
     #@+<< pandoc command docstring >>
@@ -136,58 +149,27 @@ def pandoc_command(event, verbose=True):
     if not c:
         return None
     return c.asciiDoctorCommands.pandoc_command(event, verbose=verbose)
+
+@g.command('pandoc-with-preview')
+def pandoc_with_preview_command(event=None, verbose=True):
+    """Run the pandoc command, then show the result in the browser."""
+    c = event and event.get('c')
+    if not c:
+        return None
+    return c.asciiDoctorCommands.pandoc_command(event, preview=True, verbose=verbose)
 #@+node:ekr.20191006154236.1: ** class AsciiDoctorCommands
 class AsciiDoctorCommands:
     """A class to write AsiiDoctor markup in Leo outlines."""
     
     def __init__(self, c):
         self.c = c
-        self.base_directory = None
         self.kind = None # 'adoc' or 'pandoc'
         self.level_offset = 0
         self.root_level = 0
 
     #@+others
-    #@+node:ekr.20191006155051.1: *3* adoc.commands
-    def adoc_command(self, event=None, verbose=True):
-        return self.command_helper(event, kind='adoc', verbose=verbose)
-        
-    def pandoc_command(self, event=None, verbose=True):
-        return self.command_helper(event, kind='pandoc', verbose=verbose)
-
-    #@+node:ekr.20191006153233.1: *3* adoc.command_helper
-    def command_helper(self, event, kind, verbose):
-
-        def predicate(p):
-            return self.ad_filename(p)
-
-        # Find all roots.
-        t1 = time.time()
-        c = self.c
-        self.kind = kind
-        p = event.p if event and hasattr(event, 'p') else c.p
-        if event and hasattr(event, 'base_directory'):
-            self.base_directory = event.base_directory
-        else:
-            directory = c.config.getString('adoc-base-directory')
-            self.base_directory = directory or c.frame.openDirectory
-        roots = g.findRootsWithPredicate(c, p, predicate=predicate)
-        if not roots:
-            g.warning('No @ascii-doctor nodes in', p.h)
-            return []
-        # Write each root.
-        paths = []
-        for p in roots:
-            path = self.write_root(p)
-            if path:
-                paths.append(path)
-        t2 = time.time()
-        if verbose:
-            g.es_print('adoc: wrote %s file%s in %4.2f sec.' % (
-                len(paths), g.plural(len(paths)), t2 - t1))
-        return paths
     #@+node:ekr.20190515084219.1: *3* adoc.ad_filename
-    adoc_pattern = re.compile(r'^@(adoc|ascii-doctor)')
+    adoc_pattern = re.compile(r'^@(adoc|asciidoctor)')
 
     def ad_filename(self, p):
         """Return the filename of the @adoc or @pandoc node, or None."""
@@ -203,46 +185,95 @@ class AsciiDoctorCommands:
         if g.match_word(h, 0, prefix):
             return h[len(prefix):].strip()
         return None
-    #@+node:ekr.20190515091706.1: *3* adoc.open_file & helper
-    def open_file(self, fn):
-        """Open the file, returning (fn, f)"""
-        fn = g.os_path_finalize_join(self.base_directory, fn)
-        if not self.create_directory(fn):
-            return fn, None
-        try:
-            return fn, open(fn, 'w', encoding='utf-8', errors='replace')
-        except Exception:
-            g.es_print('can not open: %r' % fn)
-            g.es_exception()
-            return fn, None
-    #@+node:ekr.20190515070742.79: *4* adoc.create_directory
-    def create_directory(self, fn):
-        """
-        Create the directory for fn if
-        a) it doesn't exist and
-        b) the user options allow it.
+    #@+node:ekr.20191006153233.1: *3* adoc.command_helper & helper
+    def command_helper(self, event, kind, preview, verbose):
 
-        Return True if the directory existed or was made.
-        """
+        def predicate(p):
+            return self.ad_filename(p)
+
+        # Find all roots.
+        t1 = time.time()
         c = self.c
-        theDir, junk = g.os_path_split(fn)
-        theDir = g.os_path_finalize(theDir) # #1341.
-        if g.os_path_exists(theDir):
-            return True
-        ok = g.makeAllNonExistentDirectories(theDir, c=c, force=False)
-        if not ok:
-            g.error('did not create:', theDir)
-        return ok
+        self.kind = kind
+        p = event.p if event and hasattr(event, 'p') else c.p
+        roots = g.findRootsWithPredicate(c, p, predicate=predicate)
+        if not roots:
+            g.warning('No @adoc nodes in', p.h)
+            return []
+        # Write each root to a file.
+        i_paths = []
+        for p in roots:
+            try:
+                i_path = self.ad_filename(p)
+                g.trace('i_path', i_path)
+                with open(i_path, 'w', encoding='utf-8', errors='replace') as self.output_file:
+                    self.write_root(p)
+                    i_paths.append(i_path)
+            except IOError:
+                g.es_print(f"Can not open {i_path!r}")
+            except Exception:
+                g.es_print('Unexpected exception')
+                g.es_exception()
+        # Convert each file to html.
+        o_paths = []
+        for i_path in i_paths:
+            o_path = self.compute_opath(i_path)
+            o_paths.append(o_path)
+            if kind == 'adoc':
+                self.run_asciidoctor(i_path, o_path)
+            else:
+                self.run_pandoc(i_path, o_path)
+        if preview:
+            # open .html files in the default browser.
+            g.execute_shell_commands(o_paths)
+        t2 = time.time()
+        if verbose:
+            g.es_print('adoc: wrote %s file%s in %4.2f sec.' % (
+                len(i_paths), g.plural(len(i_paths)), t2 - t1))
+        return i_paths
+    #@+node:ekr.20191007053522.1: *4* adoc.compute_opath
+    def compute_opath(self, i_path):
+        """
+        A workaround to an apparent asciidoctor glitch.
+        For some reason asciidoctor doesn't handle the .adoc.html extention well.
+        """
+        if self.kind == 'adoc':
+            return os.path.join(os.path.dirname(i_path), '.html')
+        return i_path + '.html'
+    #@+node:ekr.20191006155051.1: *3* adoc.commands
+    def adoc_command(self, event=None, preview=False, verbose=True):
+        return self.command_helper(event, kind='adoc', preview=preview, verbose=verbose)
+        
+    def pandoc_command(self, event=None, preview=False, verbose=True):
+        return self.command_helper(event, kind='pandoc', preview=preview, verbose=verbose)
+
+    #@+node:ekr.20191007043110.1: *3* adoc.run_asciidoctor
+    def run_asciidoctor(self, i_path, o_path):
+        """
+        Process the input file given by i_path with asciidoctor or asciidoc3.
+        """
+        global asciidoctor_exec, asciidoc3_exec
+        assert asciidoctor_exec or asciidoc3_exec, g.callers()
+        #
+        # Call the external program to write the output file.
+        prog = 'asciidoctor' if asciidoctor_exec else 'asciidoc3'
+        command = f"{prog} {i_path} -o {o_path} -b html5"
+            # The -e option deletes css.
+        g.execute_shell_commands(command)
+    #@+node:ekr.20191007043043.1: *3* adoc.run_pandoc
+    def run_pandoc(self, i_path, o_path):
+        """
+         Process the input file given by i_path with pandoc.
+        """
+        global pandoc_exec
+        assert pandoc_exec, g.callers()
+        # Call pandoc to write the output file.
+        command = f"pandoc {i_path} -t html5 -o {o_path}"
+            # --quiet does no harm.
+        g.execute_shell_commands(command)
     #@+node:ekr.20190515070742.24: *3* adoc.write_root & helpers
     def write_root(self, root):
-        """Process all nodes in an @ad tree."""
-        fn =  self.ad_filename(root)
-        if not fn:
-            g.es_print('Can not happen: not a @ad node: %r' % root.h)
-            return None
-        path, self.output_file = self.open_file(fn)
-        if not self.output_file:
-            return None
+        """Process all nodes in an @adoc tree to self.output_file"""
         # Write only the body of the root.
         self.write_body(root)
         # Write all nodes of the tree, except ignored nodes.
@@ -262,8 +293,6 @@ class AsciiDoctorCommands:
                 self.write_headline(p)
             self.write_body(p)
             p.moveToThreadNext()
-        self.output_file.close()
-        return path
     #@+node:ekr.20190515114836.1: *4* adoc.compute_level_offset
     title_pattern = re.compile(r'^= ')
 
