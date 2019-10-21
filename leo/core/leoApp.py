fa4b403ad1b87dce5fa5cc6928c708fad4cefab5
Edward K. Ream <edreamleo@gmail.com>
2019-10-21 13:31:41 -0500

    Completed #1416

--- leo/core/leoApp.py
+++ leo/core/leoApp.py
@@ -2566,7 +2566,7 @@ class LoadManager:
         # Clear g.app.initing _before_ creating commanders.
         lm = self
         g.app.initing = False # "idle" hooks may now call g.app.forceShutdown.
-        # Create the main frame.  Show it and all queued messages.
+        # Create the main frame.Show it and all queued messages.
         c = c1 = fn = None
         if lm.files:
             try: # #1403.
@@ -2574,20 +2574,20 @@ class LoadManager:
                     lm.more_cmdline_files = n < len(lm.files) - 1
                     c = lm.loadLocalFile(fn, gui=g.app.gui, old_c=None)
                         # Returns None if the file is open in another instance of Leo.
+                    if c and not c1: # #1416:
+                        c1 = c
             except Exception:
                 g.es_print(f"Unexpected exception reading {fn!r}")
                 g.es_exception()
                 c = None
-            if c and not c1:
-                c1 = c
+        # Load (and save later) a session *only* if the command line contains no files.
         g.app.loaded_session = not lm.files
-            # Load (and save later) a session only no files were given on the command line.
         if g.app.sessionManager and g.app.loaded_session:
             try: # #1403.
                 aList = g.app.sessionManager.load_snapshot()
                 if aList:
                     g.app.sessionManager.load_session(c1, aList)
-                    # tag:#659.
+                    # #659.
                     if g.app.windowList:
                         c = c1 = g.app.windowList[0].c
                     else:
@@ -2604,24 +2604,19 @@ class LoadManager:
             except Exception:
                 g.es_print('unexpected exception reading empty workbook')
                 g.es_exception()
-        # Fix bug #199.
+        # #199.
         g.app.runAlreadyOpenDialog(c1)
         # Put the focus in the first-opened file.
         fileName = lm.files[0] if lm.files else None
         c = c1
         # For qt gui, select the first-loaded tab.
-        if g.app.use_global_docks:
-            pass
-        else:
-            if hasattr(g.app.gui, 'frameFactory'):
-                factory = g.app.gui.frameFactory
-                if factory and hasattr(factory, 'setTabForCommander'):
-                    factory.setTabForCommander(c)
+        if hasattr(g.app.gui, 'frameFactory'):
+            factory = g.app.gui.frameFactory
+            if factory and hasattr(factory, 'setTabForCommander'):
+                factory.setTabForCommander(c)
         if not c:
             return False # Force an immediate exit.
-        # Fix bug 844953: tell Unity which menu to use.
-            # if c: c.enableMenuBar()
-        # Do the final inits.
+        # Final inits...
         g.app.logInited = True
         g.app.initComplete = True
         if c:
