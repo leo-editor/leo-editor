1667e84609b3098f9ced830f4599d40d4e8c66b7
Edward K. Ream <edreamleo@gmail.com>
2019-12-18 17:54:47 -0600

    toggle-angle-brackets calls s.setChanged()

--- leo/commands/commanderEditCommands.py
+++ leo/commands/commanderEditCommands.py
@@ -879,6 +879,7 @@ def toggleAngleBrackets(self, event=None):
         s = g.angleBrackets(' ' + s + ' ')
     p.setHeadString(s)
     p.setDirty()  # #1449.
+    c.setChanged()  # #1449.
     c.redrawAndEdit(p, selectAll=True)
 #@+node:ekr.20171123135625.49: ** c_ec.unformatParagraph & helper
 @g.commander_command('unformat-paragraph')
