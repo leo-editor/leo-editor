9b3ba22608451a86298d3b7c403c0e54419c0e7b
Edward K. Ream <edreamleo@gmail.com>
2020-02-02 06:17:56 -0600

    Delete beautify_tree

--- leo/core/leoAst.py
+++ leo/core/leoAst.py
@@ -266,14 +266,6 @@ class LeoGlobals: # pragma: no cover
 #@+node:ekr.20160521104628.1: **  leoAst.py: top-level
 if 1: # pragma: no cover
     #@+others
-    #@+node:ekr.20200131132814.1: *3* command: beatify_tree (To do)
-    def beautify_tree(event):
-        """Called from at.autoBeautify."""
-        c = event.get('c')
-        p = event.get('p')
-        if not c or not p:
-            return
-        g.es('@bool beautify-autobeautify is not yet supported')
     #@+node:ekr.20200107114409.1: *3* functions: reading & writing files
     #@+node:ekr.20200106171502.1: *4* function: get_encoding_directive
     encoding_pattern = re.compile(r'^[ \t\f]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)')
