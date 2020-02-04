ce5db5383d43a3695e17024dbc02170222fe8b87
Edward K. Ream <edreamleo@gmail.com>
2020-02-04 09:13:19 -0600

    Fix #1453 by returning default path as a last resort

--- leo/commands/spellCommands.py
+++ leo/commands/spellCommands.py
@@ -71,20 +71,21 @@ class BaseSpellWrapper:
     def find_user_dict(self):
         """Return the full path to the local dictionary."""
         c = self.c
+        join = g.os_path_finalize_join
         table = (
             c.config.getString('enchant-local-dictionary'),
                 # Settings first.
-            g.os_path_finalize_join(g.app.homeDir, '.leo', 'spellpyx.txt'),
+            join(g.app.homeDir, '.leo', 'spellpyx.txt'),
                 # #108: then the .leo directory.
-            g.os_path_finalize_join(g.app.loadDir, "..", "plugins", 'spellpyx.txt'),
+            join(g.app.loadDir, "..", "plugins", 'spellpyx.txt'),
                 # The plugins directory as a last resort.
         )
         for path in table:
             if g.os_path_exists(path):
                 return path
-        #
-        g.es_print('Do spellpyx.txt file found')
-        return None
+        g.es_print('Creating ~/.leo/spellpyx.txt')
+        # #1453: Return the default path.
+        return join(g.app.homeDir, '.leo', 'spellpyx.txt')
     #@+node:ekr.20150514063305.515: *3* spell.ignore
     def ignore(self, word):
         
