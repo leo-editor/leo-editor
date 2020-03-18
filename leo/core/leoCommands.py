ba21c2f27d463675c3b728390a3c2c396e52736e
vitalije <vitalije@kviziracija.net>
2020-03-18 16:20:39 +0100

    Fixed #1541 new method c.undoableDeletePositions(aList)

--- leo/core/leoCommands.py
+++ leo/core/leoCommands.py
@@ -3876,6 +3876,8 @@ class Commands:
         Delete all vnodes corresponding to the positions in aList.
 
         This is *very* tricky code. The theory of operation section explains why.
+        
+        returns the undo data
         """
         #@+<< theory of operation >>
         #@+node:ekr.20150312080344.8: *5* << theory of operation >> (deletePositionsInList)
@@ -4003,11 +4005,48 @@ class Commands:
             return p._childIndex, parent_v
 
         links_to_be_cut = sorted(set(map(p2link, aList)), key=lambda x:-x[0])
+        undodata = []
         for i, v in links_to_be_cut:
             ch = v.children.pop(i)
             ch.parents.remove(v)
+            undodata.append((v.gnx, i, ch.gnx))
         if redraw:
+            if not c.positionExists(c.p):
+                c.setCurrentPosition(c.rootPosition())
             c.redraw()
+        return undodata
+
+    #@+node:vitalije.20200318161844.1: *4* c.undoableDeletePositions
+    def undoableDeletePositions(self, aList):
+        """
+        Deletes all vnodes corresponding to the positions in aList,
+        and make changes undoable.
+        """
+        c = self
+        u = c.undoer
+        data = c.deletePositionsInList(aList)
+        gnx2v = c.fileCommands.gnxDict
+        def undo():
+            for pgnx, i, chgnx in reversed(u.getBead(u.bead).data):
+                v = gnx2v[pgnx]
+                ch = gnx2v[chgnx]
+                v.children.insert(i, ch)
+                ch.parents.append(v)
+            if not c.positionExists(c.p):
+                c.setCurrentPosition(c.rootPosition())
+        def redo():
+            for pgnx, i, chgnx in u.getBead(u.bead + 1).data:
+                v = gnx2v[pgnx]
+                ch = v.children.pop(i)
+                ch.parents.remove(v)
+            if not c.positionExists(c.p):
+                c.setCurrentPosition(c.rootPosition())
+        u.pushBead(g.Bunch(
+            data=data,
+            undoType='delete nodes',
+            undoHelper=undo,
+            redoHelper=redo,
+        ))
     #@+node:ekr.20091211111443.6265: *4* c.doBatchOperations & helpers
     def doBatchOperations(self, aList=None):
         # Validate aList and create the parents dict
