ba660be9e3ba14fee761fbbd45e39981dbe8fffc
Edward K. Ream <edreamleo@gmail.com>
2020-03-19 05:34:52 -0500

    Update version to 6.2-b1

--- leo/core/leoVersion.py
+++ leo/core/leoVersion.py
@@ -33,9 +33,10 @@ leoVersion.version:     Leo's version number.
 # 5.9:    May 5, 2019.
 # 6.0:    August 2, 2019.
 # 6.1:    November 8, 2019.
+# 6.2-b1  March 19, 2020.
 #@-<< version dates >>
-version = '6.3-devel'
-static_date = 'March 13, 2020'
+version = '6.2-b1'
+static_date = 'March 19, 2020'
 
 #@@language python
 #@@tabwidth -4
