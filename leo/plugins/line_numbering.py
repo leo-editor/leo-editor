c7967151cc6f76cb8ea9237269d4f1c414ee6511
X Caminhante <xcaminhante@i2pmail.org>
2020-07-29 18:10:56 +0000

    Fixed active_path binary file opening and a line_numbering issue

--- leo/plugins/line_numbering.py
+++ leo/plugins/line_numbering.py
@@ -236,11 +236,11 @@ def universal_line_numbers(root, target_p, delim_st, delim_en):
             return (0, n-st) if delim_st else (0, n-st)
         #@+node:vitalije.20170726193933.1: *4* doc part
         if doc_pattern.match(line):
-            if delim_en: return 0, 2
-            return 1,1
+            if delim_st: return 0, 1
+            return 0, 0
         #@+node:vitalije.20170726193941.1: *4* code part
         if code_pattern.match(line):
-            if delim_en: return 1, 2
+            # if delim_en: return 1, 2
             if delim_st: return 0, 1
             return 0, 0
         #@-others
