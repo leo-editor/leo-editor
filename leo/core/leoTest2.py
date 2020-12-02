e7bf1402012b03b81bd0718d2e33aed4b125c633
Edward K. Ream <edreamleo@gmail.com>
2020-12-02 08:56:45 -0600

    Create ConvertTests and ConvertEditCommandsTests classes. Modify convert_leoEditCommands_tests

--- leo/core/leoTest2.py
+++ leo/core/leoTest2.py
@@ -42,98 +42,35 @@ import unittest
 #@+node:ekr.20201130074836.1: *3* function: convert_leoEditCommands_tests
 def convert_leoEditCommands_tests(c, root, target):
     """
-    Convert old-style tests to new-style tests.
+    Convert @test nodes to new-style tests.
     
-    root: A node containing (a copy of) tests from unitTest.leo.
+    root:   A node containing (a copy of) tests from unitTest.leo.
     target: A node whose children will be the resulting tests.
             These nodes can then be copied to be children of a test class.
     """
-    #@+others
-    #@+node:ekr.20201130075024.2: *4* body
-    def body(after_p, after_sel, before_p, before_sel, command_name):
-        """Return the body of the test"""
-        real_command_name = command_name.split(' ')[0]
-        sel11, sel12 = before_sel.split(',')
-        sel21, sel22 = after_sel.split(',')
-        delim = "'''" if '"""' in before_p.b else '"""'
-        return (
-            f"def test_{function_name(command_name)}(self):\n"
-            f'    """Test case for {command_name}"""\n'
-            f'    before_b = {delim}\\\n'
-            f"{before_p.b}"
-            f'{delim}\n'
-            f'    after_b = {delim}\\\n'
-            f"{after_p.b}"
-            f'{delim}\n'
-            f"    self.run_test(\n"
-            f"        before_b=before_b,\n"
-            f"        after_b=after_b,\n"
-            f'        before_sel=("{sel11}", "{sel12}"),\n'
-            f'        after_sel=("{sel21}", "{sel22}"),\n'
-            f'        command_name="{real_command_name}",\n'
-            f"    )\n"
-        )
-    #@+node:ekr.20201130075024.3: *4* class_name (not used)
-    def class_name(command_name):
-        """Convert the command name to a class name."""
-        result = []
-        parts = command_name.split('-')
-        for part in parts:
-            s = part.replace('(','').replace(')','')
-            inner_parts = s.split(' ')
-            result.append(''.join([z.capitalize() for z in inner_parts]))
-        return ''.join(result)
-    #@+node:ekr.20201130075024.4: *4* convert
-    def convert(p):
-        after_p, before_p = None, None
-        after_sel, before_sel = None, None
-        assert p.h.startswith('@test')
-        command_name = p.h[len('@test'):].strip()
-        for child in p.children():
-            if child.h.startswith('after'):
-                after_p = child.copy()
-                after_sel= child.h[len('after'):].strip()
-                after_sel = after_sel.replace('sel=','').strip()
-            elif child.h.startswith('before'):
-                before_p = child.copy()
-                before_sel = child.h[len('before'):].strip()
-                before_sel = before_sel.replace('sel=','').strip()
-        assert before_p and after_p
-        assert before_sel and after_sel
-        new_child = target.insertAsLastChild()
-        new_child.h = command_name
-        new_child.b = body(after_p, after_sel, before_p, before_sel, command_name)
-            
-    #@+node:ekr.20201130075024.5: *4* function_name
-    def function_name(command_name):
-        """Convert a command name into a test function."""
-        result = []
-        parts = command_name.split('-')
-        for part in parts:
-            s = part.replace('(','').replace(')','')
-            inner_parts = s.split(' ')
-            result.append('_'.join(inner_parts))
-        return '_'.join(result)
-    #@-others
+    if not root or not target:
+        g.es_print('Error: root and target nodes must be top-level nodes.', color='red')
+        return
+    # Be safe.
+    if target.hasChildren():
+        g.es_print('Please delete children of ', target.h, color='red')
+        return
+    converter = ConvertEditCommandsTests()
     count = 0
-    if root and target:
-        target.deleteAllChildren()
-        for p in root.subtree():
-            if p.h.startswith('@test') and 'runEditCommandTest' in p.b:
-                convert(p)
-                count += 1
-        c.redraw()
-        print(f"converted {count} @test nodes")
-    else:
-        print('Error: root and target nodes must be top-level nodes.')
+    for p in root.subtree():
+        if p.h.startswith('@test') and 'runEditCommandTest' in p.b:
+            converter.convert(p, target)
+            count += 1
+    c.redraw()
+    g.es_print(f"converted {count} @test nodes", color='blue')
 #@+node:ekr.20201130195111.1: *3* function: create_app
 def create_app():
     """
     Create the Leo application, g.app, the Gui, g.app.gui, and a commander.
     
-    Return the commander.
-    
     This method is expensive (about 1 sec) only the first time it is called.
+    
+    Thereafter, recreating g.app, g.app.gui, and new commands is fast.
     """
     # Similar to leoBridge.py
     # print('CommanderTest.setUp')
@@ -240,10 +177,95 @@ def pytest_main(path, module):
             path,
         ]
         pytest.main(args=pycov_args)
-#@+node:ekr.20201130215055.1: *3* function: run_all_tests
-def run_all_tests():
-    pass
+#@+node:ekr.20201202083003.1: ** class ConvertTests
+class ConvertTests:
+    """
+    A class that converts @test nodes to proper unit tests.
     
+    Subclasses specialize the convert method.
+    """
+    #@+others
+    #@+node:ekr.20201130075024.2: *3* ConvertTests.body
+    def body(self, after_p, after_sel, before_p, before_sel, command_name):
+        """Return the body of the test"""
+        real_command_name = command_name.split(' ')[0]
+        sel11, sel12 = before_sel.split(',')
+        sel21, sel22 = after_sel.split(',')
+        delim = "'''" if '"""' in before_p.b else '"""'
+        return (
+            f"def test_{self.function_name(command_name)}(self):\n"
+            f'    """Test case for {command_name}"""\n'
+            f'    before_b = {delim}\\\n'
+            f"{before_p.b}"
+            f'{delim}\n'
+            f'    after_b = {delim}\\\n'
+            f"{after_p.b}"
+            f'{delim}\n'
+            f"    self.run_test(\n"
+            f"        before_b=before_b,\n"
+            f"        after_b=after_b,\n"
+            f'        before_sel=("{sel11}", "{sel12}"),\n'
+            f'        after_sel=("{sel21}", "{sel22}"),\n'
+            f'        command_name="{real_command_name}",\n'
+            f"    )\n"
+        )
+    #@+node:ekr.20201130075024.3: *3* ConvertTests.class_name
+    def class_name(self, command_name):
+        """Convert the command name to a class name."""
+        # This method is not used.
+        result = []
+        parts = command_name.split('-')
+        for part in parts:
+            s = part.replace('(','').replace(')','')
+            inner_parts = s.split(' ')
+            result.append(''.join([z.capitalize() for z in inner_parts]))
+        return ''.join(result)
+    #@+node:ekr.20201202083708.1: *3* ConvertTests.convert
+    def convert(self, p, target):
+        """
+        Convert one @test node, creating a new node as the last child of target.
+        
+        Must be overridden in subclasses.
+        """
+        g.trace('Must be overridden')
+    #@+node:ekr.20201130075024.5: *3* ConvertTests.function_name
+    def function_name(self, command_name):
+        """Convert a command name into a test function."""
+        result = []
+        parts = command_name.split('-')
+        for part in parts:
+            s = part.replace('(','').replace(')','')
+            inner_parts = s.split(' ')
+            result.append('_'.join(inner_parts))
+        return '_'.join(result)
+    #@-others
+#@+node:ekr.20201202083553.1: ** class ConvertEditCommandsTests (ConvertTests)
+class ConvertEditCommandsTests (ConvertTests):
+    
+    #@+others
+    #@+node:ekr.20201130075024.4: *3* ConvertTests.convert
+    def convert(self, p, target):
+        """Convert one @test node, creating a new node."""
+        after_p, before_p = None, None
+        after_sel, before_sel = None, None
+        assert p.h.startswith('@test')
+        command_name = p.h[len('@test'):].strip()
+        for child in p.children():
+            if child.h.startswith('after'):
+                after_p = child.copy()
+                after_sel= child.h[len('after'):].strip()
+                after_sel = after_sel.replace('sel=','').strip()
+            elif child.h.startswith('before'):
+                before_p = child.copy()
+                before_sel = child.h[len('before'):].strip()
+                before_sel = before_sel.replace('sel=','').strip()
+        assert before_p and after_p
+        assert before_sel and after_sel
+        new_child = target.insertAsLastChild()
+        new_child.h = command_name
+        new_child.b = self.body(after_p, after_sel, before_p, before_sel, command_name)
+            
+    #@-others
 #@+node:ekr.20201201085828.1: ** class RunAllLeoTests(unittest.TestCase)
 class RunAllLeoTests(unittest.TestCase):
     
