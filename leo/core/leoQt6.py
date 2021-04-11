9c8d25b236ff44278c0bd2f665050211a8a357b0
Edward K. Ream <edreamleo@gmail.com>
2021-04-11 03:29:08 -0500

    Remove all optional imports from leoQt6.py

--- leo/core/leoQt6.py
+++ leo/core/leoQt6.py
@@ -1,8 +1,12 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20210407011013.1: * @file leoQt6.py
-"""Import wrapper for pyQt6"""
-# pylint: disable=c-extension-no-member,import-error,no-name-in-module,unused-import
+"""
+Import wrapper for pyQt6.
 
+For Qt6, plugins are responsible for loading all optional modules.
+
+"""
+# pylint: disable=unused-import,no-name-in-module,c-extension-no-member
 #
 # Required imports
 from PyQt6 import Qt
@@ -11,56 +15,9 @@ from PyQt6 import QtGui
 from PyQt6 import QtWidgets
 from PyQt6.QtCore import QUrl
 from PyQt6.QtCore import pyqtSignal as Signal
+#
+# Standard abbreviations.
 QtConst = QtCore.Qt
 printsupport = Qt.printsupport
 qt_version = QtCore.QT_VERSION_STR
-
-# Optional imports...
-#
-try:
-    import PyQt6.QtDeclarative as QtDeclarative
-except ImportError:
-    QtDeclarative = None
-try:
-    import PyQt6.phonon as phonon
-    phonon = phonon.Phonon
-except ImportError:
-    phonon = None
-try:
-    from PyQt6 import QtMultimedia
-except ImportError:
-    QtMultimedia = None
-try:
-    from PyQt6 import Qsci
-except ImportError:
-    Qsci = None
-try:
-    import PyQt6.QtSvg as QtSvg
-except ImportError:
-    QtSvg = None
-try:
-    from PyQt6 import uic
-except ImportError:
-    uic = None
-try:
-    from PyQt6 import QtWebKit
-except ImportError:
-    # 2016/07/13: Reinhard: Support pyqt 5.6...
-    try:
-        from PyQt6 import QtWebEngineCore as QtWebKit
-    except ImportError:
-        QtWebKit = None
-try:
-    import PyQt6.QtWebKitWidgets as QtWebKitWidgets
-except ImportError:
-    try:
-        # https://groups.google.com/d/msg/leo-editor/J_wVIzqQzXg/KmXMxJSAAQAJ
-        # Reinhard: Support pyqt 5.6...
-        # used by viewrendered(2|3).py, bigdash.py, richtext.py.
-        import PyQt6.QtWebEngineWidgets as QtWebKitWidgets
-        QtWebKitWidgets.QWebView = QtWebKitWidgets.QWebEngineView
-        QtWebKit.QWebSettings = QtWebKitWidgets.QWebEngineSettings
-        QtWebKitWidgets.QWebPage = QtWebKitWidgets.QWebEnginePage
-    except ImportError:
-        QtWebKitWidgets = None
 #@-leo
