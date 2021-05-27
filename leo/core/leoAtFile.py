919fb5e7aa2dc6fbaa290999fa351bf33f124876
Edward K. Ream <edreamleo@gmail.com>
2021-05-27 06:10:41 -0500

    Remove legacy code, including newly-unused vars

--- leo/core/leoAtFile.py
+++ leo/core/leoAtFile.py
@@ -158,7 +158,6 @@ class AtFile:
         Compute default values of all write-related ivars.
         Return the finalized name of the output file.
         """
-        NEW = True
         at, c = self, self.c
         assert root
         self.initCommonIvars()
@@ -179,12 +178,7 @@ class AtFile:
             # For at.putBody only.
         at.outputList = []
             # For stream output.
-        if NEW:
-            pass
-        else:
-            at.targetFileName = targetFileName = root.anyAtFileNodeName() or ''  # #1914.
-            # For at.writeError only.
-        d = at.scanAllDirectives(root)
+        at.scanAllDirectives(root)
             # Sets the following ivars:
                 # at.encoding
                 # at.explicitLineEnding
@@ -194,7 +188,6 @@ class AtFile:
                 # at.tab_width
         #
         # Overrides of at.scanAllDirectives...
-        defaultDirectory = d.get('path')  # #1914
         if at.language == 'python':
             # Encoding directive overrides everything else.
             encoding = g.getPythonEncodingFromString(root.b)
@@ -206,40 +199,20 @@ class AtFile:
             if hasattr(at.root.v, 'tnodeList'):
                 delattr(at.root.v, 'tnodeList')
             at.root.v._p_changed = True
-        if NEW:
-            at.targetFileName = targetFileName = g.os_path_realpath(g.fullPath(c, root))
-            root_dir = g.os_path_dirname(targetFileName)
-            make_dirs = c and c.config and c.config.create_nonexistent_directories
-            if make_dirs and root_dir:
-                ok = g.makeAllNonExistentDirectories(root_dir)
-                if not ok:
-                    g.error(f"Did not create directory: {root_dir}")
-                    return None
-            return targetFileName
-        ### Legacy
         #
-        # Return the finalized file name.
-        # #1341 and #1450.
+        # #1907: Compute the file name and create directories as needed.
+        targetFileName = g.os_path_realpath(g.fullPath(c, root))
+        root_dir = g.os_path_dirname(targetFileName)
         make_dirs = c and c.config and c.config.create_nonexistent_directories
-        if defaultDirectory:
-            defaultDirectory = c.expand_path_expression(defaultDirectory)
-            if make_dirs:
-                ok = g.makeAllNonExistentDirectories(defaultDirectory)
-                if not ok:
-                    g.error(f"Did not create default directory: {defaultDirectory}")
-                    return None
-        # #1341 and #1450.
-        targetFileName = c.expand_path_expression(targetFileName)
-        if targetFileName:
-            theDir = g.os_path_dirname(targetFileName)
-            if theDir and make_dirs:
-                ok = g.makeAllNonExistentDirectories(theDir)
-                if not ok:
-                    g.trace(f"Did not create {theDir} for {targetFileName}")
-                    return None
-        # #1341.
-        return g.os_path_realpath(
-            g.os_path_finalize_join(defaultDirectory, targetFileName))
+        if make_dirs and root_dir:
+            ok = g.makeAllNonExistentDirectories(root_dir)
+            if not ok:
+                g.error(f"Did not create directory: {root_dir}")
+                return None
+                
+        at.targetFileName = targetFileName
+            # For at.writeError only.
+        return targetFileName
     #@+node:ekr.20041005105605.17: *3* at.Reading
     #@+node:ekr.20041005105605.18: *4* at.Reading (top level)
     #@+node:ekr.20070919133659: *5* at.checkExternalFile
