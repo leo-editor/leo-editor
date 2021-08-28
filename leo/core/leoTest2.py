49e6158c9c55162bae5b19b37c0c6fc366551a85
Edward K. Ream <edreamleo@gmail.com>
2021-08-28 11:55:56 -0500

    Start work on unit tests for leoNodes.py

--- leo/core/leoTest2.py
+++ leo/core/leoTest2.py
@@ -12,70 +12,11 @@ tests in leo/unittest. Eventually these classes will move to scripts.leo.
 """
 
 import sys
-import time
+import textwrap
+import unittest
 
 #@+others
 #@+node:ekr.20201129132455.1: ** Top-level functions...
-#@+node:ekr.20201129195238.1: *3* function: compareOutlines
-def compareOutlines(root1, root2, compareHeadlines=True, tag='', report=True):
-    """
-    Compares two outlines, making sure that their topologies, content and
-    join lists are equivalent
-    """
-    p2 = root2.copy()
-    ok = True
-    p1 = None
-    for p1 in root1.self_and_subtree():
-        b1 = p1.b
-        b2 = p2.b
-        if p1.h.endswith('@nonl') and b1.endswith('\n'):
-            b1 = b1[:-1]
-        if p2.h.endswith('@nonl') and b2.endswith('\n'):
-            b2 = b2[:-1]
-        ok = (
-            p1 and p2
-            and p1.numberOfChildren() == p2.numberOfChildren()
-            and (not compareHeadlines or (p1.h == p2.h))
-            and b1 == b2
-            and p1.isCloned() == p2.isCloned()
-        )
-        if not ok: break
-        p2.moveToThreadNext()
-    if report and not ok:
-        print('\ncompareOutlines failed: tag:', (tag or ''))
-        print('p1.h:', p1 and p1.h or '<no p1>')
-        print('p2.h:', p2 and p2.h or '<no p2>')
-        print(f"p1.numberOfChildren(): {p1.numberOfChildren()}")
-        print(f"p2.numberOfChildren(): {p2.numberOfChildren()}")
-        if b1 != b2:
-            showTwoBodies(p1.h, p1.b, p2.b)
-        if p1.isCloned() != p2.isCloned():
-            print('p1.isCloned() == p2.isCloned()')
-    return ok
-#@+node:ekr.20201130074836.1: *3* function: convert_leoEditCommands_tests
-def convert_leoEditCommands_tests(c, root, target):
-    """
-    Convert @test nodes to new-style tests.
-    
-    root:   A node containing (a copy of) tests from unitTest.leo.
-    target: A node whose children will be the resulting tests.
-            These nodes can then be copied to be children of a test class.
-    """
-    if not root or not target:
-        print('Error: root and target nodes must be top-level nodes.')
-        return
-    # Be safe.
-    if target.hasChildren():
-        print('Please delete children of ', target.h)
-        return
-    converter = ConvertEditCommandsTests()
-    count = 0
-    for p in root.subtree():
-        if p.h.startswith('@test') and 'runEditCommandTest' in p.b:
-            converter.convert(p, target)
-            count += 1
-    c.redraw()
-    print(f"converted {count} @test nodes")
 #@+node:ekr.20201130195111.1: *3* function: create_app
 def create_app():
     """
@@ -117,6 +58,40 @@ def create_app():
             # f"commander: {(t4-t2):.3f}\n"
             # f"    total: {(t4-t1):.3f}\n")
     return c
+#@+node:ekr.20210828111901.1: *3* function: create_outline
+def create_outline(c, s):
+    """
+    Replace all nodes of c with the nodes formed by string s, copied to the clipboard.
+    """
+    s2 = textwrap.dedent(s.strip() + '\n')
+    # g.printObj(g.splitLines(s2))
+    p = c.fileCommands.getLeoOutlineFromClipboard(s2)
+    unittest.TestCase().assertTrue(p)
+    return c
+#@+node:ekr.20201130074836.1: *3* function: convert_leoEditCommands_tests
+def convert_leoEditCommands_tests(c, root, target):
+    """
+    Convert @test nodes to new-style tests.
+    
+    root:   A node containing (a copy of) tests from unitTest.leo.
+    target: A node whose children will be the resulting tests.
+            These nodes can then be copied to be children of a test class.
+    """
+    if not root or not target:
+        print('Error: root and target nodes must be top-level nodes.')
+        return
+    # Be safe.
+    if target.hasChildren():
+        print('Please delete children of ', target.h)
+        return
+    converter = ConvertEditCommandsTests()
+    count = 0
+    for p in root.subtree():
+        if p.h.startswith('@test') and 'runEditCommandTest' in p.b:
+            converter.convert(p, target)
+            count += 1
+    c.redraw()
+    print(f"converted {count} @test nodes")
 #@+node:ekr.20201201144934.1: *3* function: dump_leo_modules
 def dump_leo_modules():
 
@@ -133,53 +108,6 @@ def dump_leo_modules():
     print(f"\n{len(plugins)} leo.plugins modules...\n")
     for key in sorted(plugins):
         print(key)
-#@+node:ekr.20201129133424.6: *3* function: expected_got
-def expected_got(expected, got):
-    """Return a message, mostly for unit tests."""
-    from leo.core import leoGlobals as g
-    #
-    # Let block.
-    e_lines = g.splitLines(expected)
-    g_lines = g.splitLines(got)
-    #
-    # Print all the lines.
-    result = ['\n']
-    result.append('Expected:\n')
-    for i, z in enumerate(e_lines):
-        result.extend(f"{i:3} {z!r}\n")
-    result.append('Got:\n')
-    for i, z in enumerate(g_lines):
-        result.extend(f"{i:3} {z!r}\n")
-    #
-    # Report first mismatched line.
-    for i, data in enumerate(zip(e_lines, g_lines)):
-        e_line, g_line = data
-        if e_line != g_line:
-            result.append(f"\nFirst mismatch at line {i}\n")
-            result.append(f"expected: {i:3} {e_line!r}\n")
-            result.append(f"     got: {i:3} {g_line!r}\n")
-            break
-    else:
-        result.append('\nDifferent lengths\n')
-    return ''.join(result)
-#@+node:ekr.20201129133502.1: *3* function: get_time
-def get_time():
-    return time.process_time()
-#@+node:ekr.20201129204348.1: *3* function: showTwoBodies
-def showTwoBodies(t, b1, b2):
-    print('\n', '-' * 20)
-    print(f"expected for {t}...")
-    for line in splitLines(b1):
-        print(f"{len(line):3d}", repr(line))
-    print('-' * 20)
-    print(f"result for {t}...")
-    for line in splitLines(b2):
-        print(f"{len(line):3d}", repr(line))
-    print('-' * 20)
-#@+node:ekr.20201203081125.1: *3* function: splitLines
-def splitLines(s):
-    """Same as g.splitLines(s)"""
-    return s.splitlines(True) if s else []
 #@+node:ekr.20201202083003.1: ** class ConvertTests
 class ConvertTests:
     """
--- leo/unittests/core/test_leoNodes.py
+++ leo/unittests/core/test_leoNodes.py
@@ -3,19 +3,21 @@
 #@+node:ekr.20201203042030.1: * @file ../unittests/core/test_leoNodes.py
 #@@first
 """Tests for leo.core.leoNodes"""
-
 import unittest
 from leo.core import leoGlobals as g
 from leo.core import leoTest2
 
-class LeoNodesTest(unittest.TestCase):
+#@+others
+#@+node:ekr.20210828112210.1: ** class NodesTest
+class NodesTest(unittest.TestCase):
     """Unit tests for leo/core/leoNodes.py."""
     #@+others
-    #@+node:ekr.20201203042409.2: ** LeoNodesTest.run_test
+    #@+node:ekr.20210828080615.1: *3* NodesTest: setUp, tearDown...
+    #@+node:ekr.20201203042409.2: *4* NodesTest.run_test
     def run_test(self):
         c = self.c
         assert c
-    #@+node:ekr.20201203042409.3: ** LeoNodesTest.setUp & tearDown
+    #@+node:ekr.20201203042409.3: *4* NodesTest.setUp & tearDown
     def setUp(self):
         """Create the nodes in the commander."""
         # Create a new commander for each test.
@@ -26,13 +28,158 @@ class LeoNodesTest(unittest.TestCase):
 
     def tearDown(self):
         self.c = None
-    #@+node:ekr.20201203042409.4: ** LeoNodesTest.setUpClass
+    #@+node:ekr.20201203042409.4: *4* NodesTest.setUpClass
     @classmethod
     def setUpClass(cls):
         leoTest2.create_app()
-    #@+node:ekr.20201203042645.1: ** LeoNodesTest: test cases
-    #@+node:ekr.20201203042550.1: *3* test_test
+    #@+node:ekr.20201203042550.1: *3* NodesTest.test_test
     def test_test(self):
         self.run_test()
+    #@+node:ekr.20210828075915.1: *3* NodesTest.test_all_nodes_coverage
+    def test_all_nodes_coverage(self):
+        # @test c iters: <coverage tests>
+        g.cls()
+        c = self.c
+        #@+<< define s >>
+        #@+node:ekr.20210828113536.1: *4* << define s >>
+        # All inner ''' must be escaped to, say, '\''.
+        s = '''\
+        <?xml version="1.0" encoding="utf-8"?>
+        <!-- Created by Leo: http://leoeditor.com/leo_toc.html -->
+        <leo_file xmlns:leo="http://leoeditor.com/namespaces/leo-python-editor/1.1" >
+        <leo_header file_format="2"/>
+        <vnodes>
+        <v t="ekr.20210828112210.1"><vh>class NodesTest</vh>
+        <v t="ekr.20210828080615.1"><vh>NodesTest: setUp, tearDown...</vh>
+        <v t="ekr.20201203042409.2"><vh>NodesTest.run_test</vh></v>
+        <v t="ekr.20201203042409.3"><vh>NodesTest.setUp &amp; tearDown</vh></v>
+        <v t="ekr.20201203042409.4"><vh>NodesTest.setUpClass</vh></v>
+        </v>
+        <v t="ekr.20201203042550.1"><vh>NodesTest.test_test</vh></v>
+        <v t="ekr.20210828075915.1"><vh>NodesTest.test_all_nodes_coverage</vh>
+        <v t="ekr.20210828113536.1"><vh>&lt;&lt; define s &gt;&gt;</vh></v>
+        </v>
+        </v>
+        </vnodes>
+        <tnodes>
+        <t tx="ekr.20201203042409.2">def run_test(self):
+            c = self.c
+            assert c
+        </t>
+        <t tx="ekr.20201203042409.3">def setUp(self):
+            """Create the nodes in the commander."""
+            # Create a new commander for each test.
+            # This is fast, because setUpClass has done all the imports.
+            from leo.core import leoCommands
+            self.c = c = leoCommands.Commands(fileName=None, gui=g.app.gui)
+            c.selectPosition(c.rootPosition())
+
+        def tearDown(self):
+            self.c = None
+        </t>
+        <t tx="ekr.20201203042409.4">@classmethod
+        def setUpClass(cls):
+            leoTest2.create_app()
+        </t>
+        <t tx="ekr.20201203042550.1">def test_test(self):
+            self.run_test()
+        </t>
+        <t tx="ekr.20210828075915.1">def test_all_nodes_coverage(self):
+            # @test c iters: &lt;coverage tests&gt;
+            g.cls()
+            c = self.c
+            &lt;&lt; define s &gt;&gt;
+            leoTest2.create_outline(c, s)
+            v1 = [p.v for p in c.all_positions()]
+            v2 = [v for v in c.all_nodes()]
+            g.printObj([z.h for z in c.all_positions()])
+            for v in v2:
+                self.assertTrue(v in v1)
+            for v in v1:
+                self.assertTrue(v in v2)
+        </t>
+        <t tx="ekr.20210828080615.1"></t>
+        <t tx="ekr.20210828112210.1">class NodesTest(unittest.TestCase):
+            """Unit tests for leo/core/leoNodes.py."""
+            #@+others
+            #@-others
+        </t>
+        <t tx="ekr.20210828113536.1">s = '\''\
+        &lt;?xml version="1.0" encoding="utf-8"?&gt;
+        &lt;!-- Created by Leo: http://leoeditor.com/leo_toc.html --&gt;
+        &lt;leo_file xmlns:leo="http://leoeditor.com/namespaces/leo-python-editor/1.1" &gt;
+        &lt;leo_header file_format="2"/&gt;
+        &lt;vnodes&gt;
+        &lt;v t="ekr.20031218072017.1551"&gt;&lt;vh&gt;c_oc.pasteOutline&lt;/vh&gt;&lt;/v&gt;
+        &lt;/vnodes&gt;
+        &lt;tnodes&gt;
+        &lt;t tx="ekr.20031218072017.1551"&gt;@g.commander_command('paste-node')
+        def pasteOutline(self,
+        event=None,
+        redrawFlag=True,
+        s=None,
+        undoFlag=True
+        ):
+        """
+        Paste an outline into the present outline from the clipboard.
+        Nodes do *not* retain their original identify.
+        """
+        c = self
+        if s is None:
+            s = g.app.gui.getTextFromClipboard()
+        c.endEditing()
+        if not s or not c.canPasteOutline(s):
+            return None  # This should never happen.
+        isLeo = g.match(s, 0, g.app.prolog_prefix_string)
+        if not isLeo:
+            return None
+        # Get *position* to be pasted.
+        pasted = c.fileCommands.getLeoOutlineFromClipboard(s)
+        if not pasted:
+            # Leo no longer supports MORE outlines. Use import-MORE-files instead.
+            return None
+        # Validate.
+        c.validateOutline()
+        c.checkOutline()
+        # Handle the "before" data for undo.
+        if undoFlag:
+            undoData = c.undoer.beforeInsertNode(c.p,
+                pasteAsClone=False,
+                copiedBunchList=[],
+            )
+        # Paste the node into the outline.
+        c.selectPosition(pasted)
+        pasted.setDirty()
+        c.setChanged(redrawFlag=redrawFlag)
+            # Prevent flash when fixing #387.
+        back = pasted.back()
+        if back and back.hasChildren() and back.isExpanded():
+            pasted.moveToNthChildOf(back, 0)
+        # Finish the command.
+        if undoFlag:
+            c.undoer.afterInsertNode(pasted, 'Paste Node', undoData)
+        if redrawFlag:
+            c.redraw(pasted)
+            c.recolor()
+        return pasted
+        &lt;/t&gt;
+        &lt;/tnodes&gt;
+        &lt;/leo_file&gt;
+        '\''
+        </t>
+        </tnodes>
+        </leo_file>
+        '''
+        #@-<< define s >>
+        leoTest2.create_outline(c, s)
+        v1 = [p.v for p in c.all_positions()]
+        v2 = [v for v in c.all_nodes()]
+        # g.printObj([z.h for z in c.all_positions()])
+        for v in v2:
+            self.assertTrue(v in v1)
+        for v in v1:
+            self.assertTrue(v in v2)
     #@-others
+#@-others
+
 #@-leo
