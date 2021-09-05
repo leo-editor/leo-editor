354ea13943ef18defd1c94b15723b82abae945f3
Edward K. Ream <edreamleo@gmail.com>
2021-09-05 06:43:48 -0500

    Rename nodes

--- leo/unittests/core/test_leoAtFile.py
+++ leo/unittests/core/test_leoAtFile.py
@@ -44,6 +44,192 @@ class TestAtFile(LeoUnitTest):
 
         if not g.unitTesting: # A hand test of at.syntaxError
             at.checkPythonSyntax(p,s2)
+    #@+node:ekr.20210905052021.19: *3* TestAtFile.test_at_directiveKind4
+    def test_at_directiveKind4(self):
+        c = self.c
+        at = c.atFileCommands
+        at.language = 'python' # Usually set by atFile read/write logic.
+        table = [
+            ('@=', 0, at.noDirective),
+            ('@', 0, at.atDirective),
+            ('@ ', 0, at.atDirective),
+            ('@\t', 0, at.atDirective),
+            ('@\n', 0, at.atDirective),
+            ('@all', 0, at.allDirective),
+            ('    @all', 4, at.allDirective),
+            ("@c",0, at.cDirective),
+            ("@code",0, at.codeDirective),
+            ("@doc",0, at.docDirective),
+            ("@end_raw", 0, at.endRawDirective),
+            ('@others', 0, at.othersDirective),
+            ('    @others', 4, at.othersDirective),
+            ("@raw", 0, at.rawDirective),
+        ]
+        for name in g.globalDirectiveList:
+            # Note: entries in g.globalDirectiveList do not start with '@'
+            if name not in ('all','c','code','doc','end_raw','others','raw',):
+                table.append(('@' + name,0,at.miscDirective),)
+
+        for s,i,expected in table:
+            result = at.directiveKind4(s,i)
+            assert result == expected, '%d %s result: %s expected: %s' % (
+                i,repr(s),at.sentinelName(result),at.sentinelName(expected))
+    #@+node:ekr.20210905052021.20: *3* TestAtFile.test_at_directiveKind4_new
+    def test_at_directiveKind4_new(self):
+        c = self.c
+        at = c.atFileCommands
+        at.language = 'python' # Usually set by atFile read/write logic.
+        table = (
+            (at.othersDirective, '@others'),
+            (at.othersDirective, '@others\n'),
+            (at.othersDirective, '    @others'),
+            (at.miscDirective,   '@tabwidth -4'),
+            (at.miscDirective,   '@tabwidth -4\n'),
+            (at.miscDirective,   '@encoding'),
+            (at.noDirective,     '@encoding.setter'),
+            (at.noDirective,     '@encoding("abc")'),
+            (at.noDirective,     'encoding = "abc"'),
+            (at.noDirective,     '@directive'), # A crucial new test.
+        )
+        for expected, s in table:
+            result = at.directiveKind4(s, 0)
+            assert expected == result, (expected, result, repr(s))
+    #@+node:ekr.20210905052021.21: *3* TestAtFile.test_at_get_setPathUa
+    def test_at_get_setPathUa(self):
+        c = self.c
+        p = c.p
+        at = c.atFileCommands
+        at.setPathUa(p, 'abc')
+        d = p.v.tempAttributes
+        d2 = d.get('read-path')
+        val1 = d2.get('path')
+        val2 = at.getPathUa(p)
+
+        table = (
+            ('d2.get',val1),
+            ('at.getPathUa',val2),
+        )
+        for kind,val in table:
+            assert val == 'abc','kind %s expected %s got %s' % (
+                kind,'abc',val)
+    #@+node:ekr.20210905052021.23: *3* TestAtFile.test_at_parseLeoSentinel
+    def test_at_parseLeoSentinel(self):
+        c = self.c
+        at=c.atFileCommands # self is a dummy argument.
+        table = (
+            # start, end, new_df, isThin, encoding
+            # pre 4.2 formats...
+            ('#',   '',   False,  True, 'utf-8', '#@+leo-thin-encoding=utf-8.'),
+            ('#',   '',   False,  False,'utf-8', '#@+leo-encoding=utf-8.'),
+            # 4.2 formats...
+            ('#',   '',   True,   True, 'utf-8',  '#@+leo-ver=4-thin-encoding=utf-8,.'),
+            ('/*',  '*/', True,   True, 'utf-8',  r'\*@+leo-ver=5-thin-encoding=utf-8,.*/'),
+            ('#',   '',   True,   True, 'utf-8',  '#@+leo-ver=5-thin'),
+            ('#',   '',   True,   True, 'utf-16', '#@+leo-ver=5-thin-encoding=utf-16,.'),
+        )
+        try:
+            for start, end, new_df, isThin, encoding, s in table:
+                valid, new_df2, start2, end2, isThin2 = at.parseLeoSentinel(s)
+                # g.trace('start',start,'end',repr(end),'len(s)',len(s))
+                assert valid, s
+                assert new_df == new_df2, s
+                assert isThin == isThin2, s
+                assert end == end2, (end, end2, s)
+                assert at.encoding == encoding, s
+        finally:
+            at.encoding = 'utf-8'
+    #@+node:ekr.20210905052021.24: *3* TestAtFile.test_at_remove
+    def test_at_remove(self):
+        c = self.c
+        import os
+
+        at = c.atFileCommands
+        exists = g.os_path_exists
+
+        path = g.os_path_join(g.app.testDir,'xyzzy')
+        if exists(path):
+            os.remove(path)
+
+        assert not exists(path)
+        assert not at.remove(path)
+
+        f = open(path,'w')
+        f.write('test')
+        f.close()
+
+        assert exists(path)
+        assert at.remove(path)
+        assert not exists(path)
+    #@+node:ekr.20210905052021.25: *3* TestAtFile.test_at_replaceFile_different_contents
+    def test_at_replaceFile_different_contents(self):
+        c = self.c
+        at = c.atFileCommands
+        # Duplicate init logic...
+        at.initCommonIvars()
+        at.scanAllDirectives(c.p) 
+        encoding = 'utf-8'
+        try:
+            # https://stackoverflow.com/questions/23212435
+            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
+            fn = f.name
+            contents = 'test contents'
+            val = at.replaceFile(contents, encoding, fn, at.root)
+            assert val, val
+        finally:
+            f.close()
+            os.unlink(f.name)
+    #@+node:ekr.20210905052021.26: *3* TestAtFile.test_at_replaceFile_no_target_file
+    def test_at_replaceFile_no_target_file(self):
+        c = self.c
+        at = c.atFileCommands
+        # Duplicate init logic...
+        at.initCommonIvars()
+        at.scanAllDirectives(c.p) 
+        encoding = 'utf-8'
+        at.outputFileName = None  # The point of this test, but I'm not sure it matters.
+        try:
+            # https://stackoverflow.com/questions/23212435
+            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
+            fn = f.name
+            contents = 'test contents'
+            val = at.replaceFile(contents, encoding, fn, at.root)
+            assert val, val
+        finally:
+            f.close()
+            os.unlink(f.name)
+    #@+node:ekr.20210905052021.27: *3* TestAtFile.test_at_replaceFile_same_contents
+    def test_at_replaceFile_same_contents(self):
+        c = self.c
+        at = c.atFileCommands
+        # Duplicate init logic...
+        at.initCommonIvars()
+        at.scanAllDirectives(c.p) 
+        encoding = 'utf-8'
+        try:
+            # https://stackoverflow.com/questions/23212435
+            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
+            fn = f.name
+            contents = 'test contents'
+            f.write(contents)
+            f.flush()
+            val = at.replaceFile(contents, encoding, fn, at.root)
+            assert not val, val
+        finally:
+            f.close()
+            os.unlink(f.name)
+    #@+node:ekr.20210905052021.28: *3* TestAtFile.test_at_scanAllDirectives
+    def test_at_scanAllDirectives(self):
+        c = self.c
+        d = c.atFileCommands.scanAllDirectives(c.p)
+        # These are the commander defaults, without any settings.
+        self.assertEqual(d.get('language'), 'python')
+        self.assertEqual(d.get('tabwidth'), -4)
+        self.assertEqual(d.get('pagewidth'), 132)
+    #@+node:ekr.20210905052021.29: *3* TestAtFile.test_at_scanAllDirectives_minimal_
+    def test_at_scanAllDirectives_minimal_(self):
+        c = self.c
+        d = c.atFileCommands.scanAllDirectives(c.p)
+        assert d
     #@+node:ekr.20210901140645.14: *3* TestAtFile.test_at_tabNannyNode
     def test_at_tabNannyNode(self):
         c, p = self.c, self.c.p
@@ -110,194 +296,7 @@ class TestAtFile(LeoUnitTest):
             child.b = '@language python\n# test #1889'
             path = g.fullPath(c, child)
             assert '~' not in path, repr(path)
-    #@+node:ekr.20210905052021.1: *3* Converted nodes
-    #@+node:ekr.20210905052021.19: *4* TestXXX.test_at_directiveKind4
-    def test_at_directiveKind4(self):
-        c = self.c
-        at = c.atFileCommands
-        at.language = 'python' # Usually set by atFile read/write logic.
-        table = [
-            ('@=', 0, at.noDirective),
-            ('@', 0, at.atDirective),
-            ('@ ', 0, at.atDirective),
-            ('@\t', 0, at.atDirective),
-            ('@\n', 0, at.atDirective),
-            ('@all', 0, at.allDirective),
-            ('    @all', 4, at.allDirective),
-            ("@c",0, at.cDirective),
-            ("@code",0, at.codeDirective),
-            ("@doc",0, at.docDirective),
-            ("@end_raw", 0, at.endRawDirective),
-            ('@others', 0, at.othersDirective),
-            ('    @others', 4, at.othersDirective),
-            ("@raw", 0, at.rawDirective),
-        ]
-        for name in g.globalDirectiveList:
-            # Note: entries in g.globalDirectiveList do not start with '@'
-            if name not in ('all','c','code','doc','end_raw','others','raw',):
-                table.append(('@' + name,0,at.miscDirective),)
-
-        for s,i,expected in table:
-            result = at.directiveKind4(s,i)
-            assert result == expected, '%d %s result: %s expected: %s' % (
-                i,repr(s),at.sentinelName(result),at.sentinelName(expected))
-    #@+node:ekr.20210905052021.20: *4* TestXXX.test_at_directiveKind4_new
-    def test_at_directiveKind4_new(self):
-        c = self.c
-        at = c.atFileCommands
-        at.language = 'python' # Usually set by atFile read/write logic.
-        table = (
-            (at.othersDirective, '@others'),
-            (at.othersDirective, '@others\n'),
-            (at.othersDirective, '    @others'),
-            (at.miscDirective,   '@tabwidth -4'),
-            (at.miscDirective,   '@tabwidth -4\n'),
-            (at.miscDirective,   '@encoding'),
-            (at.noDirective,     '@encoding.setter'),
-            (at.noDirective,     '@encoding("abc")'),
-            (at.noDirective,     'encoding = "abc"'),
-            (at.noDirective,     '@directive'), # A crucial new test.
-        )
-        for expected, s in table:
-            result = at.directiveKind4(s, 0)
-            assert expected == result, (expected, result, repr(s))
-    #@+node:ekr.20210905052021.21: *4* TestXXX.test_at_get_setPathUa
-    def test_at_get_setPathUa(self):
-        c = self.c
-        p = c.p
-        at = c.atFileCommands
-        at.setPathUa(p, 'abc')
-        d = p.v.tempAttributes
-        d2 = d.get('read-path')
-        val1 = d2.get('path')
-        val2 = at.getPathUa(p)
-
-        table = (
-            ('d2.get',val1),
-            ('at.getPathUa',val2),
-        )
-        for kind,val in table:
-            assert val == 'abc','kind %s expected %s got %s' % (
-                kind,'abc',val)
-    #@+node:ekr.20210905052021.23: *4* TestXXX.test_at_parseLeoSentinel
-    def test_at_parseLeoSentinel(self):
-        c = self.c
-        at=c.atFileCommands # self is a dummy argument.
-        table = (
-            # start, end, new_df, isThin, encoding
-            # pre 4.2 formats...
-            ('#',   '',   False,  True, 'utf-8', '#@+leo-thin-encoding=utf-8.'),
-            ('#',   '',   False,  False,'utf-8', '#@+leo-encoding=utf-8.'),
-            # 4.2 formats...
-            ('#',   '',   True,   True, 'utf-8',  '#@+leo-ver=4-thin-encoding=utf-8,.'),
-            ('/*',  '*/', True,   True, 'utf-8',  r'\*@+leo-ver=5-thin-encoding=utf-8,.*/'),
-            ('#',   '',   True,   True, 'utf-8',  '#@+leo-ver=5-thin'),
-            ('#',   '',   True,   True, 'utf-16', '#@+leo-ver=5-thin-encoding=utf-16,.'),
-        )
-        try:
-            for start, end, new_df, isThin, encoding, s in table:
-                valid, new_df2, start2, end2, isThin2 = at.parseLeoSentinel(s)
-                # g.trace('start',start,'end',repr(end),'len(s)',len(s))
-                assert valid, s
-                assert new_df == new_df2, s
-                assert isThin == isThin2, s
-                assert end == end2, (end, end2, s)
-                assert at.encoding == encoding, s
-        finally:
-            at.encoding = 'utf-8'
-    #@+node:ekr.20210905052021.24: *4* TestXXX.test_at_remove
-    def test_at_remove(self):
-        c = self.c
-        import os
-
-        at = c.atFileCommands
-        exists = g.os_path_exists
-
-        path = g.os_path_join(g.app.testDir,'xyzzy')
-        if exists(path):
-            os.remove(path)
-
-        assert not exists(path)
-        assert not at.remove(path)
-
-        f = open(path,'w')
-        f.write('test')
-        f.close()
-
-        assert exists(path)
-        assert at.remove(path)
-        assert not exists(path)
-    #@+node:ekr.20210905052021.25: *4* TestXXX.test_at_replaceFile_different_contents
-    def test_at_replaceFile_different_contents(self):
-        c = self.c
-        at = c.atFileCommands
-        # Duplicate init logic...
-        at.initCommonIvars()
-        at.scanAllDirectives(c.p) 
-        encoding = 'utf-8'
-        try:
-            # https://stackoverflow.com/questions/23212435
-            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
-            fn = f.name
-            contents = 'test contents'
-            val = at.replaceFile(contents, encoding, fn, at.root)
-            assert val, val
-        finally:
-            f.close()
-            os.unlink(f.name)
-    #@+node:ekr.20210905052021.26: *4* TestXXX.test_at_replaceFile_no_target_file
-    def test_at_replaceFile_no_target_file(self):
-        c = self.c
-        at = c.atFileCommands
-        # Duplicate init logic...
-        at.initCommonIvars()
-        at.scanAllDirectives(c.p) 
-        encoding = 'utf-8'
-        at.outputFileName = None  # The point of this test, but I'm not sure it matters.
-        try:
-            # https://stackoverflow.com/questions/23212435
-            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
-            fn = f.name
-            contents = 'test contents'
-            val = at.replaceFile(contents, encoding, fn, at.root)
-            assert val, val
-        finally:
-            f.close()
-            os.unlink(f.name)
-    #@+node:ekr.20210905052021.27: *4* TestXXX.test_at_replaceFile_same_contents
-    def test_at_replaceFile_same_contents(self):
-        c = self.c
-        at = c.atFileCommands
-        # Duplicate init logic...
-        at.initCommonIvars()
-        at.scanAllDirectives(c.p) 
-        encoding = 'utf-8'
-        try:
-            # https://stackoverflow.com/questions/23212435
-            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
-            fn = f.name
-            contents = 'test contents'
-            f.write(contents)
-            f.flush()
-            val = at.replaceFile(contents, encoding, fn, at.root)
-            assert not val, val
-        finally:
-            f.close()
-            os.unlink(f.name)
-    #@+node:ekr.20210905052021.28: *4* TestXXX.test_at_scanAllDirectives
-    def test_at_scanAllDirectives(self):
-        c = self.c
-        d = c.atFileCommands.scanAllDirectives(c.p)
-        # These are the commander defaults, without any settings.
-        self.assertEqual(d.get('language'), 'python')
-        self.assertEqual(d.get('tabwidth'), -4)
-        self.assertEqual(d.get('pagewidth'), 132)
-    #@+node:ekr.20210905052021.29: *4* TestXXX.test_at_scanAllDirectives_minimal_
-    def test_at_scanAllDirectives_minimal_(self):
-        c = self.c
-        d = c.atFileCommands.scanAllDirectives(c.p)
-        assert d
-    #@+node:ekr.20210905052021.32: *4* TestXXX.test_fast_readWithElementTree
+    #@+node:ekr.20210905052021.32: *3* TestAtFile.test_fast_readWithElementTree
     def test_fast_readWithElementTree(self):
         c = self.c
         """This code tests the translation table and associated logic."""
