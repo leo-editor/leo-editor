79b6975fa27a13d73d65e485c23d8bde5f65e91a
Edward K. Ream <edreamleo@gmail.com>
2021-09-09 10:02:43 -0500

    Complete another test

--- leo/unittests/core/test_leoPersistence.py
+++ leo/unittests/core/test_leoPersistence.py
@@ -111,6 +111,21 @@ class TestPersistence(LeoUnitTest):
         for unl, expected in table:
             result = pd.find_position_for_relative_unl(parent, unl)
             self.assertEqual(result, expected, msg=unl)
+    #@+node:ekr.20210908172651.19: *3* TestXXX.test_pd_find_representative_node
+    def test_pd_find_representative_node(self):
+        pd = self.c.persistenceController
+        root = self.root_p
+        root.h = '@auto root'
+        inner_clone = root.insertAsLastChild()
+        inner_clone.h = 'clone'
+        outer_clone = inner_clone.clone()
+        outer_clone.moveAfter(root)
+        rep = pd.find_representative_node(root, inner_clone)
+        assert rep
+        self.assertEqual(rep, outer_clone)
+    #@+node:ekr.20210908172651.20: *4* root
+    #@+node:ekr.20210908172651.21: *5* clone
+    #@+node:ekr.20210908172651.22: *4* clone
     #@-others
 #@-others
 
