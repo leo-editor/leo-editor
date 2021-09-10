0257315ff5440091bfff4e396d3000fd95956f0e
Edward K. Ream <edreamleo@gmail.com>
2021-09-09 20:48:54 -0500

    Add the last leoApp tests

--- leo/unittests/core/test_leoApp.py
+++ leo/unittests/core/test_leoApp.py
@@ -3,6 +3,8 @@
 #@+node:ekr.20210901170451.1: * @file ../unittests/core/test_leoApp.py
 #@@first
 """Tests of leoApp.py"""
+import os
+import zipfile
 from leo.core import leoGlobals as g
 from leo.core.leoTest2 import LeoUnitTest
 #@+others
@@ -47,6 +49,39 @@ class TestApp(LeoUnitTest):
         for ivar in ivars:
             self.assertTrue(hasattr(g.app, ivar))
             
+    #@+node:ekr.20210909194336.2: *3* TestApp.test_consistency_of_leoApp_tables
+    def test_consistency_of_leoApp_tables(self):
+        delims_d = g.app.language_delims_dict
+        lang_d = g.app.language_extension_dict
+        ext_d = g.app.extension_dict
+        for lang in lang_d:
+            ext = lang_d.get(lang)
+            assert lang in delims_d,'fail 1: %s' % lang
+            assert ext in ext_d,    'fail 2: %s' % ext
+        for ext in ext_d:
+            lang = ext_d.get(ext)
+            assert lang in lang_d,  'fail 3: %s' % lang
+    #@+node:ekr.20210909194336.3: *3* TestApp.test_lm_openAnyLeoFile
+    def test_lm_openAnyLeoFile(self):
+        lm = g.app.loadManager
+        # Create a zip file for testing.
+        s = 'this is a test file'
+        testDir = g.os_path_join(g.app.loadDir,'..','test')
+        assert g.os_path_exists(testDir), testDir
+        path = g.os_path_finalize_join(testDir,'testzip.zip')
+        with zipfile.ZipFile(path,'w') as f:
+            f.writestr('leo-zip-file',s)
+        # Open the file, and get the contents.
+        with lm.openAnyLeoFile(path) as f:
+            s2 = f.read()
+        self.assertEqual(s, s2)
+    #@+node:ekr.20210909194336.4: *3* TestApp.test_rfm_writeRecentFilesFileHelper
+    def test_rfm_writeRecentFilesFileHelper(self):
+        fn ='ффф.leo'
+        g.app.recentFilesManager.writeRecentFilesFileHelper(fn)
+        assert g.os_path_exists(fn), fn
+        os.remove(fn)
+        assert not g.os_path_exists(fn), fn
     #@-others
 #@-others
 #@-leo
