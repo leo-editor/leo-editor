0deb46e25b102333806d666632a39b730a88f7f7
Edward K. Ream <edreamleo@gmail.com>
2021-09-18 06:54:40 -0500

    Clean leoApp.py

--- leo/core/leoApp.py
+++ leo/core/leoApp.py
@@ -461,7 +461,7 @@ class LeoApp:
             "jmk":      "jmk",
             "js":       "javascript", # For javascript import test.
             "jsp":      "javaserverpage",
-            "json":     "json", 
+            "json":     "json",
             # "jsp":      "jsp",
             "ksh":      "kshell",
             "kv":       "kivy", # PeckJ 2014/05/05
@@ -685,7 +685,7 @@ class LeoApp:
             "objectrexx"         : "-- /* */",
             "occam"              : "--",
             "omnimark"           : ";",
-            "pandoc"             : "<!-- -->", 
+            "pandoc"             : "<!-- -->",
             "pascal"             : "// { }",
             "perl"               : "#",
             "perlpod"            : "# __=pod__ __=cut__", # 9/25/02: The perlpod hack.
@@ -836,7 +836,7 @@ class LeoApp:
             "javaserverpage": "jsp", # EKR: 2011/11/25
             "jhtml"         : "jhtml",
             "jmk"           : "jmk",
-            "json"          : "json", 
+            "json"          : "json",
             "jsp"           : "jsp",
             "jupyter"       : "ipynb",
             "kivy"          : "kv", # PeckJ 2014/05/05
@@ -1607,7 +1607,7 @@ class LeoApp:
     def makeAllBindings(self):
         """
         LeoApp.makeAllBindings:
-            
+
         Call c.k.makeAllBindings for all open commanders c.
         """
         app = self
@@ -1885,12 +1885,12 @@ class LoadManager:
     def computeThemeFilePath(self):
         """
         Return the absolute path to the theme .leo file, resolved using the search order for themes.
-        
+
         1. Use the --theme command-line option if it exists.
-        
+
         2. Otherwise, preload the first .leo file.
            Load the file given by @string theme-name setting.
-           
+
         3. Finally, look up the @string theme-name in the already-loaded, myLeoSettings.leo.
            Load the file if setting exists.  Otherwise return None.
         """
@@ -1951,7 +1951,7 @@ class LoadManager:
     def computeWorkbookFileName(self):
         """
         Return full path to the workbook.
-        
+
         Return None if testing, or in batch mode, or if the containing
         directory does not exist.
         """
@@ -2108,8 +2108,8 @@ class LoadManager:
                     f"--trace-binding: {c.shortFileName()} "
                     f"sets {binding} to None")
             elif localFlag and binding in c.commandsDict:
-                 d = c.k.computeInverseBindingDict()
-                 g.trace(
+                d = c.k.computeInverseBindingDict()
+                g.trace(
                     f"--trace-binding: {c.shortFileName():20} "
                     f"binds {binding} to {d.get(binding) or []}")
             else:
@@ -2243,7 +2243,7 @@ class LoadManager:
     def readGlobalSettingsFiles(self):
         """
         Read leoSettings.leo and myLeoSettings.leo using a null gui.
-        
+
         New in Leo 6.1: this sets ivars for the ActiveSettingsOutline class.
         """
         trace = 'themes' in g.app.debug
@@ -2805,9 +2805,9 @@ class LoadManager:
     #@@nobeautify
 
     def addOptionsToParser(self, parser, trace_m):
-        
+
         add = parser.add_option
-        
+
         def add_bool(option, help, dest=None):
             add(option, action='store_true', dest=dest, help=help)
 
@@ -3387,7 +3387,7 @@ class RecentFilesManager:
     def cleanRecentFiles(self, c):
         """
         Remove items from the recent files list that no longer exist.
-        
+
         This almost never does anything because Leo's startup logic removes
         nonexistent files from the recent files list.
         """
@@ -3737,10 +3737,10 @@ def demangle_recent_files_command(event):
     Path demangling potentially alters the paths in the recent files list
     according to find/replace patterns in the @data path-demangle setting.
     For example:
-        
+
         REPLACE: .gnome-desktop
         WITH: My Desktop
-        
+
     The default setting specifies no patterns.
     """
     c = event and event.get('c')
