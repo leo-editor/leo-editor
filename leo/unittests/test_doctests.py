2496a8eadc1d62079db3f610aab63cd726035834
Edward K. Ream <edreamleo@gmail.com>
2021-09-26 10:09:03 -0500

    test_all_doctests works

--- leo/unittests/test_doctests.py
+++ leo/unittests/test_doctests.py
@@ -2,17 +2,24 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20210926044012.1: * @file ../unittests/test_doctests.py
 #@@first
-"""Run all doctests in Leo's core."""
+"""Run all doctests."""
 import doctest
+import glob
+import os
 import unittest
 from leo.core import leoGlobals as g
-import leo
 
-# The doctest finder should find the doctests in this function.
-#@+<< define factorial >>
-#@+node:ekr.20210926053601.1: ** << define factorial >>
+unittest_dir = os.path.dirname(__file__)
+leo_dir = os.path.abspath(os.path.join(unittest_dir, '..'))
+
+#@+others  # Define a function containing a doctest.
+#@+node:ekr.20210926053601.1: ** factorial (test_dectests.py)
 def factorial(n):
+    # Modified from https://docs.python.org/3/library/doctest.html
+    # Must import factorial. See: stackoverflow.com/questions/65066002
     """Return the factorial of n, an exact integer >= 0.
+    
+    >>> from leo.unittests.test_doctests import factorial
 
     >>> [factorial(n) for n in range(6)]
     [1, 1, 2, 6, 24, 120]
@@ -36,7 +43,8 @@ def factorial(n):
     Traceback (most recent call last):
         ...
     OverflowError: n too large
-    """
+
+    """ # Blank line above is required.
 
     import math
     if not n >= 0:
@@ -51,11 +59,34 @@ def factorial(n):
         result *= factor
         factor += 1
     return result
-#@-<< define factorial >>
+#@-others
 
-class TestDocTests(unittest.TestCase):  # Not a subclass of leoTest2.LeoUnitTest.
+class TestDocTests(unittest.TestCase):  # No need to be a subclass of leoTest2.LeoUnitTest.
 
     def test_all_doctests(self):
-        finder = doctest.DocTestFinder()
-        g.printObj(finder.find(leo), tag='doctests')
+        fails_list = []  # List of files with failing doctests.
+        files_list = []  # List of files containing a doctest.
+        n = 0  # Total doctests found
+        for module in ('core', 'plugins', 'unittests'):
+            module_path = os.path.join(leo_dir, module)
+            self.assertTrue(os.path.exists(module_path), msg=repr(module_path))
+            path = os.path.join(module_path, '**', '*.py')
+            files = glob.glob(path, recursive=True)
+            files = [z for z in files if not z.endswith('__init__.py')]
+            for f in files:
+                # Exclude two problematic files.
+                if 'dtest.py' in f or 'javascript.py' in f:
+                    continue
+                fails, count = doctest.testfile(f)
+                n += count
+                if count:
+                    files_list.append(f)
+                if fails:
+                    fails_list.append(f)
+                    print(f"{fails} failures in {g.shortFileName(f)}")
+            self.assertEqual(fails_list, [])
+        if 0:
+            g.trace(f"{n} doctests found in {len(files_list)} file{g.plural(len(files_list))}")
+            g.printObj(files_list, tag="files containing any doctest")
+            g.printObj(fails_list, tag="files containing a failed doctest")
 #@-leo
