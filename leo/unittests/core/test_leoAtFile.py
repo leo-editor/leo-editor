d39b1113c452e4a3519a4bbfd23c22927453f191
Edward K. Ream <edreamleo@gmail.com>
2021-11-03 09:18:00 -0500

    Suppress pylint complaint

--- leo/unittests/core/test_leoAtFile.py
+++ leo/unittests/core/test_leoAtFile.py
@@ -670,7 +670,8 @@ class TestFastAtRead(LeoUnitTest):
         root.h = h # To match contents.
         #@+<< define contents >>
         #@+node:ekr.20211103080718.2: *4* << define contents >>
-        contents = textwrap.dedent(fr'''\
+        # pylint: disable=anomalous-backslash-in-string
+        contents = textwrap.dedent(f'''\
             ATq@@+leo-ver=5-thin@>
             ATq@@+node:ekr.20211103080829.1: * @{h}@>
             ATq@@@@language cweb@>
