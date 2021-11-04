c67d84446e3168adff7b719b36af40f831d6956a
Edward K. Ream <edreamleo@gmail.com>
2021-11-04 15:44:50 -0500

    Add test_validInAtOthers

--- leo/unittests/core/test_leoAtFile.py
+++ leo/unittests/core/test_leoAtFile.py
@@ -31,6 +31,65 @@ class TestAtFile(LeoUnitTest):
             silent=True,
             verbose=False,
         )
+    #@+node:ekr.20210905052021.28: *3* TestAtFile.test_at_scanAllDirectives
+    def test_at_scanAllDirectives(self):
+        
+        at, c = self.at, self.c
+        d = at.scanAllDirectives(c.p)
+        # These are the commander defaults, without any settings.
+        self.assertEqual(d.get('language'), 'python')
+        self.assertEqual(d.get('tabwidth'), -4)
+        self.assertEqual(d.get('pagewidth'), 132)
+    #@+node:ekr.20210905052021.29: *3* TestAtFile.test_at_scanAllDirectives_minimal_
+    def test_at_scanAllDirectives_minimal_(self):
+        
+        at, c = self.at, self.c
+        d = at.scanAllDirectives(c.p)
+        d = c.atFileCommands.scanAllDirectives(c.p)
+        assert d
+    #@+node:ekr.20200204094139.1: *3* TestAtFile.test_bug_1469
+    def test_bug_1469(self):
+        # Test #1469: saves renaming an external file
+        # Create a new outline with @file node and save it
+        bridge = self.bridge()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            filename = f"{temp_dir}{os.sep}test_file.leo"
+            c = bridge.openLeoFile(filename)
+            p = c.rootPosition()
+            p.h = '@file 1'
+            p.b = 'b1'
+            c.save()
+            # Rename the @file node and save
+            p1 = c.rootPosition()
+            p1.h = "@file 1_renamed"
+            c.save()
+            # Remove the original "@file 1" from the disk
+            external_filename = f"{temp_dir}{os.sep}1"
+            assert os.path.exists(external_filename)
+            os.remove(external_filename)
+            assert not os.path.exists(external_filename)
+            # Change the @file contents, save and reopen the outline
+            p1.b = "b_1_changed"
+            c.save()
+            c.close()
+            c = bridge.openLeoFile(c.fileName())
+            p1 = c.rootPosition()
+            self.assertEqual(p1.h, "@file 1_renamed")
+    #@+node:ekr.20210421035527.1: *3* TestAtFile.test_bug_1889
+    def test_bug_1889(self):
+        # Test #1889: Honor ~ in ancestor @path nodes.
+        # Create a new outline with @file node and save it
+        bridge = self.bridge()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            filename = f"{temp_dir}{os.sep}test_file.leo"
+            c = bridge.openLeoFile(filename)
+            root = c.rootPosition()
+            root.h = '@path ~/sub-directory/'
+            child = root.insertAsLastChild()
+            child.h = '@file test_bug_1889.py'
+            child.b = '@language python\n# test #1889'
+            path = g.fullPath(c, child)
+            assert '~' not in path, repr(path)
     #@+node:ekr.20210901140645.13: *3* TestAtFile.test_checkPythonSyntax
     def test_checkPythonSyntax(self):
 
@@ -100,21 +159,6 @@ class TestAtFile(LeoUnitTest):
         for expected, s in table:
             result = at.directiveKind4(s, 0)
             self.assertEqual(expected, result, msg=repr(s))
-    #@+node:ekr.20210905052021.21: *3* TestAtFile.test_setPathUa
-    def test_setPathUa(self):
-
-        at, p = self.at, self.c.p
-        at.setPathUa(p, 'abc')
-        d = p.v.tempAttributes
-        d2 = d.get('read-path')
-        val1 = d2.get('path')
-        val2 = at.getPathUa(p)
-        table = (
-            ('d2.get', val1),
-            ('at.getPathUa', val2),
-        )
-        for kind, val in table:
-            self.assertEqual(val, 'abc', msg=kind)
     #@+node:ekr.20210905052021.23: *3* TestAtFile.test_parseLeoSentinel
     def test_parseLeoSentinel(self):
 
@@ -141,186 +185,6 @@ class TestAtFile(LeoUnitTest):
                 self.assertEqual(at.encoding, encoding, msg=repr(s))
         finally:
             at.encoding = 'utf-8'
-    #@+node:ekr.20211104142459.1: *3* TestAtFile.test_putLine
-    def test_putLine(self):
-
-        at, p = self.at, self.c.p
-        at.initWriteIvars(p)
-
-        class Status:  # at.putBody defines the status class.
-            at_comment_seen=False
-            at_delims_seen=False
-            at_warning_given=True  # Always suppress warning messages.
-            has_at_others=False
-            in_code=True
-            
-        # For now, test only the case that hasn't been covered:
-        # kind == at.othersDirective and not status.in_code
-        status = Status()
-        status.in_code = False
-        i, kind = 0, at.othersDirective
-        s = 'A doc line\n'
-        at.putLine(i, kind, p, s, status)
-        
-            
-    #@+node:ekr.20210905052021.24: *3* TestAtFile.test_remove
-    def test_remove(self):
-        
-        at = self.at
-        exists = g.os_path_exists
-
-        path = g.os_path_join(g.app.testDir, 'xyzzy')
-        if exists(path):
-            os.remove(path)
-
-        assert not exists(path)
-        assert not at.remove(path)
-
-        f = open(path, 'w')
-        f.write('test')
-        f.close()
-
-        assert exists(path)
-        assert at.remove(path)
-        assert not exists(path)
-    #@+node:ekr.20210905052021.25: *3* TestAtFile.test_replaceFile_different_contents
-    def test_replaceFile_different_contents(self):
-        
-        at, c = self.at, self.c
-        # Duplicate init logic...
-        at.initCommonIvars()
-        at.scanAllDirectives(c.p)
-        encoding = 'utf-8'
-        try:
-            # https://stackoverflow.com/questions/23212435
-            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
-            fn = f.name
-            contents = 'test contents'
-            val = at.replaceFile(contents, encoding, fn, at.root)
-            assert val, val
-        finally:
-            f.close()
-            os.unlink(f.name)
-    #@+node:ekr.20210905052021.26: *3* TestAtFile.test_replaceFile_no_target_file
-    def test_replaceFile_no_target_file(self):
-        
-        at, c = self.at, self.c
-        # Duplicate init logic...
-        at.initCommonIvars()
-        at.scanAllDirectives(c.p)
-        encoding = 'utf-8'
-        at.outputFileName = None  # The point of this test, but I'm not sure it matters.
-        try:
-            # https://stackoverflow.com/questions/23212435
-            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
-            fn = f.name
-            contents = 'test contents'
-            val = at.replaceFile(contents, encoding, fn, at.root)
-            assert val, val
-        finally:
-            f.close()
-            os.unlink(f.name)
-    #@+node:ekr.20210905052021.27: *3* TestAtFile.test_replaceFile_same_contents
-    def test_replaceFile_same_contents(self):
-
-        at, c = self.at, self.c
-        # Duplicate init logic...
-        at.initCommonIvars()
-        at.scanAllDirectives(c.p)
-        encoding = 'utf-8'
-        try:
-            # https://stackoverflow.com/questions/23212435
-            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
-            fn = f.name
-            contents = 'test contents'
-            f.write(contents)
-            f.flush()
-            val = at.replaceFile(contents, encoding, fn, at.root)
-            assert not val, val
-        finally:
-            f.close()
-            os.unlink(f.name)
-    #@+node:ekr.20210905052021.28: *3* TestAtFile.test_at_scanAllDirectives
-    def test_at_scanAllDirectives(self):
-        
-        at, c = self.at, self.c
-        d = at.scanAllDirectives(c.p)
-        # These are the commander defaults, without any settings.
-        self.assertEqual(d.get('language'), 'python')
-        self.assertEqual(d.get('tabwidth'), -4)
-        self.assertEqual(d.get('pagewidth'), 132)
-    #@+node:ekr.20210905052021.29: *3* TestAtFile.test_at_scanAllDirectives_minimal_
-    def test_at_scanAllDirectives_minimal_(self):
-        
-        at, c = self.at, self.c
-        d = at.scanAllDirectives(c.p)
-        d = c.atFileCommands.scanAllDirectives(c.p)
-        assert d
-    #@+node:ekr.20210901140645.14: *3* TestAtFile.test_tabNannyNode
-    def test_tabNannyNode(self):
-        
-        at, p = self.at, self.c.p
-        # Test 1.
-        s = textwrap.dedent("""\
-            # no error
-            def spam():
-                pass
-        """)
-        at.tabNannyNode(p, body=s)
-        # Test 2.
-        s2 = textwrap.dedent("""\
-            # syntax error
-            def spam:
-                pass
-              a = 2
-        """)
-        try:
-            at.tabNannyNode(p, body=s2)
-        except IndentationError:
-            pass
-    #@+node:ekr.20200204094139.1: *3* TestAtFile.test_bug_1469
-    def test_bug_1469(self):
-        # Test #1469: saves renaming an external file
-        # Create a new outline with @file node and save it
-        bridge = self.bridge()
-        with tempfile.TemporaryDirectory() as temp_dir:
-            filename = f"{temp_dir}{os.sep}test_file.leo"
-            c = bridge.openLeoFile(filename)
-            p = c.rootPosition()
-            p.h = '@file 1'
-            p.b = 'b1'
-            c.save()
-            # Rename the @file node and save
-            p1 = c.rootPosition()
-            p1.h = "@file 1_renamed"
-            c.save()
-            # Remove the original "@file 1" from the disk
-            external_filename = f"{temp_dir}{os.sep}1"
-            assert os.path.exists(external_filename)
-            os.remove(external_filename)
-            assert not os.path.exists(external_filename)
-            # Change the @file contents, save and reopen the outline
-            p1.b = "b_1_changed"
-            c.save()
-            c.close()
-            c = bridge.openLeoFile(c.fileName())
-            p1 = c.rootPosition()
-            self.assertEqual(p1.h, "@file 1_renamed")
-    #@+node:ekr.20210421035527.1: *3* TestAtFile.test_bug_1889
-    def test_bug_1889(self):
-        # Test #1889: Honor ~ in ancestor @path nodes.
-        # Create a new outline with @file node and save it
-        bridge = self.bridge()
-        with tempfile.TemporaryDirectory() as temp_dir:
-            filename = f"{temp_dir}{os.sep}test_file.leo"
-            c = bridge.openLeoFile(filename)
-            root = c.rootPosition()
-            root.h = '@path ~/sub-directory/'
-            child = root.insertAsLastChild()
-            child.h = '@file test_bug_1889.py'
-            child.b = '@language python\n# test #1889'
-            path = g.fullPath(c, child)
-            assert '~' not in path, repr(path)
     #@+node:ekr.20211102110237.1: *3* TestAtFile.test_put_body_adjacent_at_doc_part
     def test_put_body_adjacent_at_doc_part(self):
 
@@ -446,6 +310,150 @@ class TestAtFile(LeoUnitTest):
         at.putBody(root)
         result = ''.join(at.outputList)
         self.assertEqual(result, expected)
+    #@+node:ekr.20211104142459.1: *3* TestAtFile.test_putLine
+    def test_putLine(self):
+
+        at, p = self.at, self.c.p
+        at.initWriteIvars(p)
+
+        class Status:  # at.putBody defines the status class.
+            at_comment_seen=False
+            at_delims_seen=False
+            at_warning_given=True  # Always suppress warning messages.
+            has_at_others=False
+            in_code=True
+            
+        # For now, test only the case that hasn't been covered:
+        # kind == at.othersDirective and not status.in_code
+        status = Status()
+        status.in_code = False
+        i, kind = 0, at.othersDirective
+        s = 'A doc line\n'
+        at.putLine(i, kind, p, s, status)
+        
+            
+    #@+node:ekr.20210905052021.24: *3* TestAtFile.test_remove
+    def test_remove(self):
+        
+        at = self.at
+        exists = g.os_path_exists
+
+        path = g.os_path_join(g.app.testDir, 'xyzzy')
+        if exists(path):
+            os.remove(path)
+
+        assert not exists(path)
+        assert not at.remove(path)
+
+        f = open(path, 'w')
+        f.write('test')
+        f.close()
+
+        assert exists(path)
+        assert at.remove(path)
+        assert not exists(path)
+    #@+node:ekr.20210905052021.25: *3* TestAtFile.test_replaceFile_different_contents
+    def test_replaceFile_different_contents(self):
+        
+        at, c = self.at, self.c
+        # Duplicate init logic...
+        at.initCommonIvars()
+        at.scanAllDirectives(c.p)
+        encoding = 'utf-8'
+        try:
+            # https://stackoverflow.com/questions/23212435
+            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
+            fn = f.name
+            contents = 'test contents'
+            val = at.replaceFile(contents, encoding, fn, at.root)
+            assert val, val
+        finally:
+            f.close()
+            os.unlink(f.name)
+    #@+node:ekr.20210905052021.26: *3* TestAtFile.test_replaceFile_no_target_file
+    def test_replaceFile_no_target_file(self):
+        
+        at, c = self.at, self.c
+        # Duplicate init logic...
+        at.initCommonIvars()
+        at.scanAllDirectives(c.p)
+        encoding = 'utf-8'
+        at.outputFileName = None  # The point of this test, but I'm not sure it matters.
+        try:
+            # https://stackoverflow.com/questions/23212435
+            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
+            fn = f.name
+            contents = 'test contents'
+            val = at.replaceFile(contents, encoding, fn, at.root)
+            assert val, val
+        finally:
+            f.close()
+            os.unlink(f.name)
+    #@+node:ekr.20210905052021.27: *3* TestAtFile.test_replaceFile_same_contents
+    def test_replaceFile_same_contents(self):
+
+        at, c = self.at, self.c
+        # Duplicate init logic...
+        at.initCommonIvars()
+        at.scanAllDirectives(c.p)
+        encoding = 'utf-8'
+        try:
+            # https://stackoverflow.com/questions/23212435
+            f = tempfile.NamedTemporaryFile(delete=False, encoding=encoding, mode='w')
+            fn = f.name
+            contents = 'test contents'
+            f.write(contents)
+            f.flush()
+            val = at.replaceFile(contents, encoding, fn, at.root)
+            assert not val, val
+        finally:
+            f.close()
+            os.unlink(f.name)
+    #@+node:ekr.20210905052021.21: *3* TestAtFile.test_setPathUa
+    def test_setPathUa(self):
+
+        at, p = self.at, self.c.p
+        at.setPathUa(p, 'abc')
+        d = p.v.tempAttributes
+        d2 = d.get('read-path')
+        val1 = d2.get('path')
+        val2 = at.getPathUa(p)
+        table = (
+            ('d2.get', val1),
+            ('at.getPathUa', val2),
+        )
+        for kind, val in table:
+            self.assertEqual(val, 'abc', msg=kind)
+    #@+node:ekr.20210901140645.14: *3* TestAtFile.test_tabNannyNode
+    def test_tabNannyNode(self):
+        
+        at, p = self.at, self.c.p
+        # Test 1.
+        s = textwrap.dedent("""\
+            # no error
+            def spam():
+                pass
+        """)
+        at.tabNannyNode(p, body=s)
+        # Test 2.
+        s2 = textwrap.dedent("""\
+            # syntax error
+            def spam:
+                pass
+              a = 2
+        """)
+        try:
+            at.tabNannyNode(p, body=s2)
+        except IndentationError:
+            pass
+    #@+node:ekr.20211104154115.1: *3* TestAtFile.test_validInAtOthers
+    def test_validInAtOthers(self):
+
+        at, p = self.at, self.c.p
+        
+        # Just test the last line.
+        at.sentinels = False
+        at.validInAtOthers(p)
     #@-others
 #@+node:ekr.20211031085414.1: ** class TestFastAtRead(LeoUnitTest)
 class TestFastAtRead(LeoUnitTest):
