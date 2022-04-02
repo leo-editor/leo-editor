b0d793e0cf68eef78aad937da0787499a56c9bf3
Edward K. Ream <edreamleo@gmail.com>
2022-04-02 14:58:01 -0500

    Add test_iterative_ast.py

---
+++ leo/unittests/core/test_iterative_ast.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#@+leo-ver=5-thin
+#@+node:ekr.20220402144737.1: * @file ../unittests/core/test_iterative_ast.py
+#@@first
+"""Tests of iterative_ast.py"""
+from leo.core.test_leoAst import TestTOG
+# # # v1, v2, junk1, junk2, junk3 = sys.version_info
+# # # py_version = (v1, v2)
+
+class TestIterative(TestTOG):
+    """
+    Tests for the IterativeTokenGenerator class.
+    
+    This class inherits all the tests from the TestTOG class.
+    """
+
+    ### debug_list = ['unit-test']
+
+    ### @others
+
+#@-leo
