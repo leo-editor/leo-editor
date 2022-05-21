875f84c50f89c2478a8ff4485755ad009b084c55
Edward K. Ream <edreamleo@gmail.com>
2022-05-21 17:12:09 -0500

    Reindent

--- leo/core/leoGlobals.py
+++ leo/core/leoGlobals.py
@@ -418,7 +418,7 @@ class BindingInfo:
         # result = [f"BindingInfo kind: {self.kind}"]
         result = ['BindingInfo']
         # Print all existing ivars.
-        table = ('commandName', 'stroke') # 'pane', 'func', 'nextMode', 
+        table = ('commandName', 'stroke') # 'pane', 'func', 'nextMode',
         for ivar in table:
             if hasattr(self, ivar):
                 val = getattr(self, ivar)
@@ -7248,7 +7248,7 @@ def run_unit_tests(tests: str=None, verbose: bool=False) -> None:
     Run *all* unit tests if "tests" is not given.
     """
     # Note: `pip install leo` does not create a leo-editor folder, so this code will fail.
-    #       The workaround: run `python -m unittest` from python/Lib/sitecustomize/leo.   
+    #       The workaround: run `python -m unittest` from python/Lib/sitecustomize/leo.
     leo_editor_dir = g.os_path_finalize_join(g.app.loadDir, '..', '..')
     os.chdir(leo_editor_dir)
     verbosity = '-v' if verbose else ''
