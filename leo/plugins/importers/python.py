9232721582eca1772ace54fdd8913e19556fd0c4
Edward K. Ream <edreamleo@gmail.com>
2022-07-26 12:22:15 -0500

    Tweak format

--- leo/plugins/importers/python.py
+++ leo/plugins/importers/python.py
@@ -298,50 +298,40 @@ class Python_Importer(Importer):
     #@@nobeautify
 
     def get_new_dict(self, context):
-        """
-        Return a *general* state dictionary for the given context.
-        Subclasses may override...
-        """
+        """Return the state dictionary for python."""
         comment, block1, block2 = self.single_comment, self.block1, self.block2
         assert (comment, block1, block2) == ('#', '', ''), f"python: {comment!r} {block1!r} {block2!r}"
 
         d: Dict
-
         if context:
             d = {
                 # key   kind    pattern ends?
-                '\\':   [('len+1', '\\',None),],
-                '"':[
-                        ('len', '"""',  context == '"""'),
-                        ('len', '"',    context == '"'),
-                    ],
-                "'":[
-                        ('len', "'''",  context == "'''"),
-                        ('len', "'",    context == "'"),
-                    ],
+                '\\':   [('len+1', '\\', None),],
+                '"':    [
+                            ('len', '"""', context == '"""'),
+                            ('len', '"', context == '"'),
+                        ],
+                "'":    [
+                            ('len', "'''", context == "'''"),
+                            ('len', "'", context == "'"),
+                        ],
             }
         else:
             # Not in any context.
             d = {
                 # key    kind pattern new-ctx  deltas
                 '\\': [('len+1','\\', context, None),],
-                '#':  [('all', '#',   context, None),],
-                '"':[
+                '#':  [('all', '#', context, None),],
+                '"':  [
                         # order matters.
-                        ('len', '"""',  '"""', None),
-                        ('len', '"',    '"',   None),
-                    ],
-                "'":[
+                        ('len', '"""', '"""', None),
+                        ('len', '"', '"', None),
+                      ],
+                "'":  [
                         # order matters.
                         ('len', "'''",  "'''", None),
-                        ('len', "'",    "'",   None),
-                    ],
-                # '{':    [('len', '{', context, (1,0,0)),],
-                # '}':    [('len', '}', context, (-1,0,0)),],
-                # '(':    [('len', '(', context, (0,1,0)),],
-                # ')':    [('len', ')', context, (0,-1,0)),],
-                # '[':    [('len', '[', context, (0,0,1)),],
-                # ']':    [('len', ']', context, (0,0,-1)),],
+                        ('len', "'", "'", None),
+                      ],
             }
         return d
     #@-others
