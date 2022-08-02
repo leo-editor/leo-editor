aebfc769253b6ccb26611ef9fd5763c03c395648
Edward K. Ream <edreamleo@gmail.com>
2022-08-02 11:32:52 -0500

    Remove two methods from .ini importer

--- leo/plugins/importers/ini.py
+++ leo/plugins/importers/ini.py
@@ -17,20 +17,7 @@ class Ini_Importer(Importer):
         )
 
     #@+others
-    #@+node:ekr.20161123143008.1: *3* ini_i.gen_lines & helpers (*** to do)
-    def gen_lines(self, lines, parent):
-        """
-        Non-recursively parse all lines of s into parent, creating descendant
-        nodes as needed.
-        """
-        self.at_others_flag = False
-        p = self.root
-        for line in lines:
-            if self.starts_block(line):
-                p = self.start_block(line)
-            else:
-                self.add_line(p, line)
-    #@+node:ekr.20161123103554.1: *4* ini_i.starts_block
+    #@+node:ekr.20161123103554.1: *3* ini_i.starts_block
     ini_pattern = re.compile(r'^\s*\[(.*)\]')
 
     def starts_block(self, line):
@@ -38,18 +25,6 @@ class Ini_Importer(Importer):
         # pylint: disable=arguments-differ
         m = self.ini_pattern.match(line)
         return bool(m and m.group(1).strip())
-    #@+node:ekr.20161123112121.1: *4* ini_i.start_block
-    def start_block(self, line):
-        """Start a block consisting of a new child of self.root."""
-        # Insert @others if needed.
-        if not self.at_others_flag:
-            self.at_others_flag = True
-            self.add_line(self.root, '@others\n')
-        # Create the new node.
-        return self.create_child_node(
-            parent=self.root,
-            line=line,
-            headline=line.strip())
     #@-others
 #@-others
 importer_dict = {
