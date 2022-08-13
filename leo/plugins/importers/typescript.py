1eb5a65b8b9bee8b183a25f2f5b67ef9ffd2235c
Edward K. Ream <edreamleo@gmail.com>
2022-08-13 13:05:40 -0500

    typescript importer: full coverage after removing several methods

--- leo/plugins/importers/typescript.py
+++ leo/plugins/importers/typescript.py
@@ -2,7 +2,7 @@
 #@+node:ekr.20140723122936.18152: * @file ../plugins/importers/typescript.py
 """The @auto importer for TypeScript."""
 import re
-from typing import Any, Dict, List
+from typing import Dict
 from leo.core import leoGlobals as g  # Required.
 from leo.core.leoCommands import Commands as Cmdr
 from leo.core.leoNodes import Position
@@ -67,13 +67,12 @@ class TS_Importer(Importer):
         """Return a cleaned up headline s."""
         s = s.strip()
         # Don't clean a headline twice.
-        if s.endswith('>>') and s.startswith('<<'):
+        if s.endswith('>>') and s.startswith('<<'):  # pragma: no cover.
             return s
         # Try to match patterns.
         for group_n, pattern in self.function_patterns:
             m = pattern.match(s)
             if m:
-                # g.trace('group %s: %s' % (group_n, m.group(group_n)))
                 return m.group(group_n)
         # Final cleanups, if nothing matches.
         for ch in '{(=':
@@ -82,45 +81,6 @@ class TS_Importer(Importer):
         s = s.replace('  ', ' ')
         s = s.replace(' (', '(')
         return g.truncate(s, 100)
-    #@+node:ekr.20161118093751.2: *3* ts_i.skip_possible_regex
-    def skip_possible_regex(self, s: str, i: int) -> int:
-        """look ahead for a regex /"""
-        assert s[i] in '=(', repr(s[i])
-        i += 1
-        while i < len(s) and s[i] in ' \t':
-            i += 1
-        if i < len(s) and s[i] == '/':
-            i += 1
-            while i < len(s):
-                progress = i
-                ch = s[i]
-                if ch == '\\':
-                    i += 2
-                elif ch == '/':
-                    i += 1
-                    break
-                else:
-                    i += 1
-                assert progress < i
-
-        return i - 1
-    #@+node:ekr.20180523170649.1: *3* ts_i.starts_block
-    def starts_block(self, i: int, lines: List[str], new_state: Any, prev_state: Any) -> bool:
-        """True if the new state starts a block."""
-        if new_state.level() <= prev_state.level():
-            return False
-        line = lines[i].strip()
-        for word in ('do', 'else', 'for', 'if', 'switch', 'try', 'while'):
-            if line.startswith(word):
-                return False
-        # #1617: Chained calls look like functions, but aren't.
-        for pattern in self.non_function_patterns:
-            if pattern.match(line) is not None:
-                return False
-        for group_n, pattern in self.function_patterns:
-            if pattern.match(line) is not None:
-                return True
-        return False
     #@-others
 #@+node:ekr.20161118071747.14: ** class Typescript_ScanState
 class Typescript_ScanState:
@@ -146,12 +106,6 @@ class Typescript_ScanState:
     #@+node:ekr.20220731103111.1: *3* typescript_state.in_context
     def in_context(self) -> bool:
         return bool(self.context)
-    #@+node:ekr.20161118082821.1: *3* typescript_state.is_ws_line
-    ws_pattern = re.compile(r'^\s*$|^\s*#')
-
-    def is_ws_line(self, s: str) -> bool:
-        """Return True if s is nothing but whitespace and single-line comments."""
-        return bool(self.ws_pattern.match(s))
     #@+node:ekr.20161119115736.1: *3* typescript_state.level
     def level(self) -> int:
         """Typescript_ScanState.level."""
