762438db156a29ab3ab381700d5485ee1109ce29
Edward K. Ream <edreamleo@gmail.com>
2022-08-23 21:13:07 -0500

    Mystery solved. Convert to @file

--- leo/plugins/leofts.py
+++ leo/plugins/leofts.py
@@ -1,3 +1,5 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20220823200700.1: * @file ../plugins/leofts.py
 import os
 from whoosh.index import create_in, open_dir
 from whoosh.fields import ID, TEXT, Schema
@@ -6,11 +8,14 @@ from whoosh.analysis import RegexTokenizer, LowercaseFilter, StopFilter
 
 g = None
 
+#@+others
+#@+node:ekr.20220823205609.1: ** set_leo
 def set_leo(gg):
     global g
     g = gg
     g._fts = None
 
+#@+node:ekr.20220823205609.2: ** init
 def init():
 
     print("bigdash init")
@@ -23,20 +28,26 @@ def init():
 
     return ok
 
+#@+node:ekr.20220823205609.3: ** get_fts
 def get_fts():
     if g._fts is None:
         g._fts = LeoFts(g.app.homeLeoDir + "/fts_index")
     return g._fts
 
+#@+node:ekr.20220823205609.4: ** all_positions_global
 def all_positions_global():
     for c in g.app.commanders():
         for p in c.all_unique_positions():
             yield(c, p)
 
+#@+node:ekr.20220823205609.5: ** class GnxCache
 class GnxCache:
     """ map gnx => vnode """
+    #@+others
+    #@+node:ekr.20220823205610.1: *3* __init__
     def __init__(self):
         self.clear()
+    #@+node:ekr.20220823205610.2: *3* update_new_cs
     def update_new_cs(self):
         for c in g.app.commanders():
             if c.hash() not in self.cs:
@@ -45,11 +56,13 @@ class GnxCache:
                     self.ps[k] = c, p.v
                 self.cs.add(c.hash())
 
+    #@+node:ekr.20220823205610.3: *3* get
     def get(self, gnx):
         if not self.ps:
             self.update_new_cs()
         res = self.ps.get(gnx, None)
         return res
+    #@+node:ekr.20220823205610.4: *3* get_p
     def get_p(self, gnx):
         r = self.get(gnx)
         if r:
@@ -66,11 +79,16 @@ class GnxCache:
                 return c, p.copy()
         return None
 
+    #@+node:ekr.20220823205610.5: *3* clear
     def clear(self):
         self.ps = {}
         self.cs = set()
 
+    #@-others
+#@+node:ekr.20220823205610.6: ** class LeoFts
 class LeoFts:
+    #@+others
+    #@+node:ekr.20220823205610.7: *3* __init__
     def __init__(self, idx_dir):
         self.idx_dir = idx_dir
         if not os.path.exists(idx_dir):
@@ -79,6 +97,7 @@ class LeoFts:
         else:
             self.ix = open_dir(idx_dir)
 
+    #@+node:ekr.20220823205610.8: *3* schema
     def schema(self):
         my_analyzer = RegexTokenizer("[a-zA-Z_]+") | LowercaseFilter() | StopFilter()
         schema = Schema(
@@ -89,12 +108,14 @@ class LeoFts:
         )
         return schema
 
+    #@+node:ekr.20220823205610.9: *3* create
     def create(self):
 
         schema = self.schema()
         self.ix = create_in(self.idx_dir, schema)
 
 
+    #@+node:ekr.20220823205610.10: *3* index_nodes
     def index_nodes(self, c):
         writer = self.ix.writer()
         doc = c.mFileName
@@ -110,12 +131,14 @@ class LeoFts:
         writer.commit()
         g._gnxcache.clear()
 
+    #@+node:ekr.20220823205610.11: *3* drop_document
     def drop_document(self, docfile):
         writer = self.ix.writer()
         print("Drop index", docfile)
         writer.delete_by_term("doc", docfile)
         writer.commit()
 
+    #@+node:ekr.20220823205610.12: *3* statistics
     def statistics(self):
         r = {}
         with self.ix.searcher() as s:
@@ -124,6 +147,7 @@ class LeoFts:
         return r
 
 
+    #@+node:ekr.20220823205610.13: *3* search
     def search(self, searchstring, limit=30):
 
         res = []
@@ -151,14 +175,21 @@ class LeoFts:
 
         return res
 
+    #@+node:ekr.20220823205610.14: *3* close
     def close(self):
         self.ix.close()
 
+    #@-others
+#@+node:ekr.20220823205610.15: ** main
 def main():
     fts = LeoFts("c:/t/ltest")
     fts.create()
 
 
+#@-others
 if __name__ == '__main__':
     main()
 
+#@@language python
+#@@tabwidth -4
+#@-leo
