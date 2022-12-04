804cd846635cea3a3ffa65c821115a0acbc71754
Edward K. Ream <edreamleo@gmail.com>
2022-12-04 12:56:33 -0600

    Remove an indirection from one script

--- leo/scripts/pylint-leo.cmd
+++ leo/scripts/pylint-leo.cmd
@@ -1,4 +1,4 @@
 echo off
 time /T
-call pylint leo --extension-pkg-allow-list=PyQt6.QtCore,PyQt6.QtGui,PyQt6.QtWidgets %*
+call python -m pylint leo --extension-pkg-allow-list=PyQt6.QtCore,PyQt6.QtGui,PyQt6.QtWidgets %*
 time /T
