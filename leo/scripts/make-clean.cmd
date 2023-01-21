4002e230d9f4e99a64dd3969909b6f7546b02f0e
Edward K. Ream <edreamleo@gmail.com>
2023-01-21 11:06:34 -0600

    Add make-clean and make-html scripts

---
+++ leo/scripts/make-clean.cmd
@@ -0,0 +1,8 @@
+echo off
+echo off
+cd C:\Repos\leo-editor\leo\doc\html
+echo make clean
+call make clean
+echo .
+echo make html
+call make html
---
+++ leo/scripts/make-html.cmd
@@ -0,0 +1,5 @@
+echo off
+echo off
+cd C:\Repos\leo-editor\leo\doc\html
+echo make html
+make html
