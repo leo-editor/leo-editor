71a83314d4fecf61caec7794597394df6dce983f
Edward K. Ream <edreamleo@gmail.com>
2023-01-26 03:02:12 -0600

    Add TestHtml.test_slideshow_slide, illustrating serious problems in the generated outline

--- leo/scripts/test-one-leo.cmd
+++ leo/scripts/test-one-leo.cmd
@@ -2,26 +2,14 @@ echo off
 cls
 cd c:\Repos\leo-editor
 
-rem echo test-one-leo: TestPlugins.test_cursesGui2
-rem call python -m unittest leo.unittests.test_plugins.TestPlugins.test_cursesGui2 %*
+rem echo test-one-leo: test_leoFind.TestFind
+rem call python -m unittest leo.unittests.core.test_leoFind.TestFind
 
-rem echo test-one-leo: test_leoAst.TestFstringify
-rem call python -m unittest leo.unittests.core.test_leoAst.TestFstringify
+echo test-one-leo: TestHtml.test_slideshow_slide
+call python -m unittest leo.unittests.test_importers.TestHtml.test_slideshow_slide
 
-rem echo test-one-leo: test_leoAst.TestOrange
-rem call python -m unittest leo.unittests.core.test_leoAst.TestOrange
 
-rem echo test-one-leo: test_importers.TestHtml.test_structure
-rem call python -m unittest leo.unittests.test_importers.TestHtml.test_structure
 
-rem echo test-one-leo: test_leoFind.TestFind.test_find_all
-rem call python -m unittest leo.unittests.core.test_leoFind.TestFind.test_find_all
-
-REM  echo test-one-leo: test_leoFind.TestFind.test_find_all_regex
-REM  call python -m unittest leo.unittests.core.test_leoFind.TestFind.test_find_all_regex
-
-echo test-one-leo: test_leoFind.TestFind
-call python -m unittest leo.unittests.core.test_leoFind.TestFind
 
 
 
