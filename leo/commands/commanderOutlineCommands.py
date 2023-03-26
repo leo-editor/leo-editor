5b55e5bf7eb28e3c973e18c635f15280f80a425e
Félix <felixgraphx@gmail.com>
2023-03-26 00:50:17 -0400

    Removed the unused 'paste_c' property from g.app.

--- leo/commands/commanderOutlineCommands.py
+++ leo/commands/commanderOutlineCommands.py
@@ -29,7 +29,6 @@ def copyOutline(self: Self, event: Event = None) -> str:
     c = self
     c.endEditing()
     s = c.fileCommands.outline_to_clipboard_string()
-    g.app.paste_c = c
     if g.app.inBridge:
         return s
     g.app.gui.replaceClipboardWith(s)
@@ -42,7 +41,6 @@ def copyOutlineAsJSON(self: Self, event: Event = None) -> Optional[str]:
     c = self
     c.endEditing()
     s = c.fileCommands.outline_to_clipboard_json_string()
-    g.app.paste_c = c
     if g.app.inBridge:
         return s
     g.app.gui.replaceClipboardWith(s)
