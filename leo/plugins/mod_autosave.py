2a0b27c820f835bf45ef02c4b72f178b52bf85a4
Edward K. Ream <edreamleo@gmail.com>
2023-03-27 04:50:45 -0500

    Refactor: never touch the UI

--- leo/plugins/mod_autosave.py
+++ leo/plugins/mod_autosave.py
@@ -13,16 +13,19 @@ This plugin is active only if::
 """
 
 # By Paul Paterson. Rewritten by EKR.
+from __future__ import annotations
 import time
-from typing import Any, Dict
+from typing import Dict, TYPE_CHECKING
 from leo.core import leoGlobals as g
-from leo.core.leoQt import QtWidgets
-#
+# from leo.core.leoQt import QtWidgets
+if TYPE_CHECKING:  # pragma: no cover
+    from leo.core.leoCommands import Commands as Cmdr
+
 # Fail fast, right after all imports.
 g.assertUi('qt')  # May raise g.UiTypeException, caught by the plugins manager.
-#
-# The global settings dict.
-gDict: Dict[Any, Any] = {}  # Keys are commanders, values are settings dicts.
+
+# The global settings dict: Keys are commanders. Values are settings dicts.
+gDict: Dict[Cmdr, Dict] = {}
 
 #@+others
 #@+node:ekr.20060108123141.2: ** init
@@ -61,33 +64,68 @@ def onCreate(tag, keywords):
 def onIdle(tag, keywords):
     """Save the current document if it has a name"""
     global gDict
-    guiName = g.app.gui.guiName()
-    if guiName not in ('qt', 'qttabs'):
+    if g.app.killed or g.unitTesting:
         return
+    # if g.app.gui.guiName() not in ('qt', 'qttabs'):
+        # return
     c = keywords.get('c')
+    if not c or not c.exists:
+        return
     d = gDict.get(c.hash())
-    if c and d and c.exists and c.mFileName and not g.app.killed and not g.unitTesting:
-        # Wait the entire interval after c is first changed or saved.
-        # Imo (EKR) this is the desired behavior.
-        # It gives the user a chance to revert changes before they are changed.
-        if c.changed:
-            w = c.get_focus()
-            if isinstance(w, QtWidgets.QLineEdit):
-                # Saving now would destroy the focus.
-                # There is **no way** to recreate outline editing after a redraw.
-                pass
-            else:
-                last = d.get('last')
-                interval = d.get('interval')
-                if time.time() - last >= interval:
-                    g.es_print("Autosave: %s" % time.ctime(), color="orange")
-                    c.fileCommands.save(c.mFileName)
-                    c.set_focus(w)
-                    d['last'] = time.time()
-                    gDict[c.hash()] = d
-        else:
-            d['last'] = time.time()
-            gDict[c.hash()] = d
+    if not d:
+        return
+    if not c.mFileName:
+        return
+    # Wait the entire interval after c is first changed or saved.
+    # This wait gives the user a chance to revert changes before they are saved.
+    if not c.changed:
+        d['last'] = time.time()
+        gDict[c.hash()] = d
+        return
+    # Do not save if the user is editing a headline.
+    # w = c.get_focus()
+    # if isinstance(w, QtWidgets.QLineEdit):
+        # # Saving now would destroy the focus.
+        # # There is **no way** to recreate outline editing after a redraw.
+        # return
+    last = d.get('last')
+    interval = d.get('interval')
+    if time.time() - last >= interval:
+        # g.es_print("Autosave: %s" % time.ctime(), color="orange")
+        print(f"Autosave: {time.ctime()}")
+        # c.fileCommands.save(c.mFileName)
+        # c.set_focus(w)
+        save(c)
+        d['last'] = time.time()
+        gDict[c.hash()] = d
+    
+#@+node:ekr.20230327042532.1: ** save (mode_autosave.py)
+def save(c: Cmdr) -> None:
+    """Save c's outlines without changing any part of the UI."""
+    fc = c.fileCommands
+    fileName = c.mFileName
+    g.app.commander_cacher.save(c, fileName)
+    # ok = c.checkFileTimeStamp(fileName)
+    # if not ok:
+    #    return
+    if c.checkOutline():
+        print('mod_autosave: structural errors in outline! outline not saved')
+        return
+    ###
+        # if c.sqlite_connection:
+            # c.sqlite_connection.close()
+            # c.sqlite_connection = None
+    ### ok = fc.write_Leo_file(fileName)
+    # g.app.recentFilesManager.writeRecentFilesFile(c)
+    fc.writeAllAtFileNodes()  # Ignore any errors.
+    fc.writeOutline(fileName)
+    # if ok:
+        # if not silent:
+            # self.putSavedMessage(fileName)
+        # c.clearChanged()  # Clears all dirty bits.
+        # if c.config.getBool('save-clears-undo-buffer'):
+            # g.es("clearing undo")
+            # c.undoer.clearUndoState()
 #@-others
 #@@language python
 #@@tabwidth -4
