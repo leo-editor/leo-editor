05103f57374c01b968fac20b830cce827e9ad89b
Edward K. Ream <edreamleo@gmail.com>
2023-03-27 05:38:07 -0500

    Cleanups. Redraw if possible

--- leo/plugins/mod_autosave.py
+++ leo/plugins/mod_autosave.py
@@ -17,7 +17,7 @@ from __future__ import annotations
 import time
 from typing import Dict, TYPE_CHECKING
 from leo.core import leoGlobals as g
-# from leo.core.leoQt import QtWidgets
+from leo.core.leoQt import QtWidgets
 if TYPE_CHECKING:  # pragma: no cover
     from leo.core.leoCommands import Commands as Cmdr
 
@@ -28,15 +28,15 @@ g.assertUi('qt')  # May raise g.UiTypeException, caught by the plugins manager.
 gDict: Dict[Cmdr, Dict] = {}
 
 #@+others
-#@+node:ekr.20060108123141.2: ** init
+#@+node:ekr.20060108123141.2: ** init (mod_autosave.py)
 def init():
     """Return True if the plugin has loaded successfully."""
-    ok = not g.unitTesting  # Don't want autosave after unit testing.
-    if ok:
-        # Register the handlers...
-        g.registerHandler('after-create-leo-frame', onCreate)
-        g.plugin_signon(__name__)
-    return ok
+    if g.unitTesting:
+        return False
+    # Register the handlers...
+    g.registerHandler('after-create-leo-frame', onCreate)
+    g.plugin_signon(__name__)
+    return True
 #@+node:edream.110203113231.726: ** onCreate (mod_autosave.py)
 def onCreate(tag, keywords):
     """Handle the per-Leo-file settings."""
@@ -46,86 +46,58 @@ def onCreate(tag, keywords):
         return
     # Do nothing here if we already have registered the idle-time hook.
     d = gDict.get(c.hash())
-    if not d:
-        active = c.config.getBool('mod-autosave-active', default=False)
-        interval = c.config.getInt('mod-autosave-interval')
-        if active:
-            # Create an entry in the global settings dict.
-            gDict[c.hash()] = {
-                'last': time.time(),
-                'interval': interval,
-            }
-            message = "auto save %s sec. after changes" % (interval)
-            g.registerHandler('idle', onIdle)
-        else:
-            message = "@bool mod_autosave_active=False"
-        g.es(message, color='orange')
-#@+node:ekr.20100904062957.10654: ** onIdle
+    if d:
+        return
+    active = c.config.getBool('mod-autosave-active', default=False)
+    interval = c.config.getInt('mod-autosave-interval')
+    if active:
+        # Create an entry in the global settings dict.
+        gDict[c.hash()] = {
+            'last': time.time(),
+            'interval': interval,
+        }
+        message = f"auto save {interval} sec. after changes"
+        g.registerHandler('idle', onIdle)
+    else:
+        message = "@bool mod_autosave_active=False"
+    print(message)
+#@+node:ekr.20100904062957.10654: ** onIdle (mod_autosave.py)
 def onIdle(tag, keywords):
     """Save the current document if it has a name"""
     global gDict
     if g.app.killed or g.unitTesting:
         return
-    # if g.app.gui.guiName() not in ('qt', 'qttabs'):
-        # return
     c = keywords.get('c')
-    if not c or not c.exists:
-        return
     d = gDict.get(c.hash())
-    if not d:
+    if not c or not c.exists or not d or not c.mFileName:
         return
-    if not c.mFileName:
-        return
-    # Wait the entire interval after c is first changed or saved.
-    # This wait gives the user a chance to revert changes before they are saved.
+    # Redraw the screen only if we aren't editing a headline.
+    redraw_flag = (
+        g.app.gui.guiName() in ('qt', 'qttabs')
+        and not isinstance(c.get_focus(), QtWidgets.QLineEdit)
+    )
+    # Wait the entire interval after c is changed.
     if not c.changed:
         d['last'] = time.time()
         gDict[c.hash()] = d
         return
-    # Do not save if the user is editing a headline.
-    # w = c.get_focus()
-    # if isinstance(w, QtWidgets.QLineEdit):
-        # # Saving now would destroy the focus.
-        # # There is **no way** to recreate outline editing after a redraw.
-        # return
     last = d.get('last')
     interval = d.get('interval')
     if time.time() - last >= interval:
-        # g.es_print("Autosave: %s" % time.ctime(), color="orange")
-        print(f"Autosave: {time.ctime()}")
-        # c.fileCommands.save(c.mFileName)
-        # c.set_focus(w)
         save(c)
+        print(f"Autosave: {time.ctime()} {c.shortFileName()}")
+        if redraw_flag:
+            c.redraw()
+        c.changed = False  # Clear the flag *without* updating the UI.
         d['last'] = time.time()
         gDict[c.hash()] = d
-    
 #@+node:ekr.20230327042532.1: ** save (mode_autosave.py)
 def save(c: Cmdr) -> None:
     """Save c's outlines without changing any part of the UI."""
     fc = c.fileCommands
-    fileName = c.mFileName
-    g.app.commander_cacher.save(c, fileName)
-    # ok = c.checkFileTimeStamp(fileName)
-    # if not ok:
-    #    return
-    if c.checkOutline():
-        print('mod_autosave: structural errors in outline! outline not saved')
-        return
-    ###
-        # if c.sqlite_connection:
-            # c.sqlite_connection.close()
-            # c.sqlite_connection = None
-    ### ok = fc.write_Leo_file(fileName)
-    # g.app.recentFilesManager.writeRecentFilesFile(c)
+    g.app.commander_cacher.save(c, c.mFileName)
     fc.writeAllAtFileNodes()  # Ignore any errors.
-    fc.writeOutline(fileName)
-    # if ok:
-        # if not silent:
-            # self.putSavedMessage(fileName)
-        # c.clearChanged()  # Clears all dirty bits.
-        # if c.config.getBool('save-clears-undo-buffer'):
-            # g.es("clearing undo")
-            # c.undoer.clearUndoState()
+    fc.writeOutline(c.mFileName)
 #@-others
 #@@language python
 #@@tabwidth -4
