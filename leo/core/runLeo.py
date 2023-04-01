a5144bcd07c1a6a2877ebabe5db743f2159725ec
Edward K. Ream <edreamleo@gmail.com>
2023-04-01 06:40:39 -0500

    Use same logic in runLeo.py

--- leo/core/runLeo.py
+++ leo/core/runLeo.py
@@ -31,9 +31,11 @@ if sys.executable.endswith("pythonw.exe"):
         os.path.join(os.getenv("TEMP", default=""),  # #1557.
         "stderr-" + os.path.basename(sys.argv[0])),
         "w")
+# Like g.appendToSysPath.
 path = os.getcwd()
+isWindows = sys.platform.startswith('win')
+theDir2 = path.replace('/', '\\') if isWindows else path
 if path not in sys.path:
-    # print('appending %s to sys.path' % path)
     sys.path.append(path)
 try:
     # #1472: bind to g immediately.
