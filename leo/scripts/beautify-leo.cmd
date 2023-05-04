f739f406540a9f9eb3b9e29d54c6c364fe040641
Edward K. Ream <edreamleo@gmail.com>
2023-05-04 05:54:39 -0500

    Beautify one mode file

--- leo/scripts/beautify-leo.cmd
+++ leo/scripts/beautify-leo.cmd
@@ -6,3 +6,5 @@ call py -m leo.core.leoAst --orange --recursive leo\core
 call py -m leo.core.leoAst --orange --recursive leo\commands
 call py -m leo.core.leoAst --orange --recursive leo\plugins\importers
 call py -m leo.core.leoAst --orange --recursive leo\plugins\writers
+rem call py -m leo.core.leoAst --orange --recursive leo\modes
+
