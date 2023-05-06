6804bbf79b58f4a0c0851ebd4acafe59fcc011c9
Edward K. Ream <edreamleo@gmail.com>
2023-05-06 12:48:02 -0500

    Complete annotations test. Add AnnotationsTraverser.test_annotations

--- leo/unittests/test_design.py
+++ leo/unittests/test_design.py
@@ -3,33 +3,20 @@
 """Global design tests."""
 import ast
 from ast import NodeVisitor
-from ast import AST as Node
-from collections import defaultdict
 import glob
 import os
 import re
-from typing import Dict ###, List
 import unittest
 from leo.core import leoGlobals as g
-#@+<< define patterns >>
-#@+node:ekr.20230506110036.1: ** << define patterns >>
-c_pat_s = r'\b(c[0-9]?|[\w_]+_c)\b'
-p_pat_s = r'\b(p[0-9]?|[\w_]+_p)\b'
-s_pat_s = r'\b(s[0-9]?|[\w_]+_s)\b'
-v_pat_s = r'\b(v[0-9]?|[\w_]+_v)\b'
-var_patterns_s = '|'.join([c_pat_s, p_pat_s, v_pat_s])
-var_patterns = re.compile(var_patterns_s)
-#@-<< define patterns >>
 #@+<< define test files >>
 #@+node:ekr.20230506103755.1: ** << define test files >>
-unittests_dir = os.path.dirname(__file__)
-leo_dir = g.finalize_join(unittests_dir, '..')
-
 def compute_files(pattern, root_dir):
     return [g.finalize_join(root_dir, z)
         for z in glob.glob(pattern, root_dir=root_dir)]
 
 # Compute directories.
+unittests_dir = os.path.dirname(__file__)
+leo_dir = g.finalize_join(unittests_dir, '..')
 core_dir = g.finalize_join(leo_dir, 'core')
 commands_dir = g.finalize_join(leo_dir, 'commands')
 plugins_dir = g.finalize_join(leo_dir, 'plugins')
@@ -46,70 +33,89 @@ all_files = core_files + commands_files + qt_files
 #@+node:ekr.20230506111649.1: *3* class AnnotationsTraverser
 class AnnotationsTraverser(NodeVisitor):
 
-    d: Dict[Node, Dict] = defaultdict(dict)
-    
-    def __init__(self, path):
+    def __init__(self, tester, path):
         super().__init__()
         self.path = path
-    
+        self.tester = tester
+
     #@+others
+    #@+node:ekr.20230506123402.1: *4* test_annotation
+    annotation_table = (
+        (re.compile(r'\b(c[0-9]?|[\w_]+_c)\b'), 'Cmdr'),
+        (re.compile(r'\b(p[0-9]?|[\w_]+_p)\b'), 'Position'),
+        (re.compile(r'\b(s[0-9]?|[\w_]+_s)\b'), 'str'),
+        (re.compile(r'\b(v[0-9]?|[\w_]+_v)\b'), 'VNode'),
+    )
+
+    def test_annotation(self, identifier: str, annotation: ast.Expr) -> None:
+        """Test the annotation of identifier."""
+        for pattern, expected_annotation in self.annotation_table:
+            m = pattern.match(identifier)
+            if m:
+                self.tester.assertTrue(identifier, ast.unparse(annotation))
     #@+node:ekr.20230506111649.3: *4* visit_AnnAssign
     def visit_AnnAssign(self, node):
-        path = self.path
+        ### path = self.path
         # AnnAssign(expr target, expr annotation, expr? value, int simple)
         if isinstance(node.target, ast.Name):
-            id_s = node.target.id
-            if var_patterns.match(id_s):
-                self.d [path] [id_s] = node.annotation
-            
+            if node.annotation:
+                id_s = node.target.id
+                self.test_annotation(id_s, node.annotation)
+            ###
+                # id_s = node.target.id
+                # if var_patterns.match(id_s):
+                    # self.d [path] [id_s] = node.annotation
     #@+node:ekr.20230506111649.4: *4* visit_FunctionDef
     def visit_FunctionDef(self, node):
-        path = self.path
+        ### path = self.path
         arguments = node.args
         for arg in arguments.args:
             assert isinstance(arg, ast.arg)
             # arg = (identifier arg, expr? annotation, string? type_comment)
             annotation = getattr(arg, 'annotation', None)
-            id_s = arg.arg
-            if annotation and var_patterns.match(id_s):
-                ### dump_annotation???
-                if isinstance(annotation, ast.Name):
-                    ann_s = annotation.id
-                elif isinstance(annotation, ast.Constant):
-                    ann_s = annotation.value
-                elif isinstance(annotation, ast.Subscript):
-                    # Subscript(expr value, expr slice, expr_context ctx)
-                    if isinstance(annotation.value, ast.Name):
-                         ann_s = f"Subscript: [{annotation.value.id}]"
-                    else:
-                        ### dump_subscript???
-                        ann_s = f"Subscript: [{annotation.value!r}]"
-                else:
-                    ann_s = repr(annotation)
-                aList = self.d [path].get(id_s, [])
-                aList.append(ann_s)
-                self.d [path] [id_s] = aList
+            if annotation:
+                id_s = arg.arg
+                self.test_annotation(id_s, annotation)
+            ###
+            # if annotation and var_patterns.match(id_s):
+                # ### dump_annotation???
+                # if isinstance(annotation, ast.Name):
+                    # ann_s = annotation.id
+                # elif isinstance(annotation, ast.Constant):
+                    # ann_s = annotation.value
+                # elif isinstance(annotation, ast.Subscript):
+                    # # Subscript(expr value, expr slice, expr_context ctx)
+                    # if isinstance(annotation.value, ast.Name):
+                         # ann_s = f"Subscript: [{annotation.value.id}]"
+                    # else:
+                        # ### dump_subscript???
+                        # ann_s = f"Subscript: [{annotation.value!r}]"
+                # else:
+                    # ann_s = repr(annotation)
+                # aList = self.d [path].get(id_s, [])
+                # aList.append(ann_s)
+                # self.d [path] [id_s] = aList
     #@-others
-#@+node:ekr.20230506111927.1: *3* class ChainsTraverser(NodeVisitor) To do
+#@+node:ekr.20230506111927.1: *3* class ChainsTraverser(NodeVisitor)
 class ChainsTraverser(NodeVisitor):
-    
-    def __init__(self, path):
+
+    def __init__(self, tester, path):
         super().__init__()
         self.path = path
-        
+
     #@+others
     #@-others
 #@+node:ekr.20230506095516.1: ** class TestAnnotations(unittest.TestCase)
 class TestAnnotations(unittest.TestCase):
     """Test that annotations of c, g, p, s, v are as expected."""
-    
+
     def test_all_paths(self):
         for path in all_files:
             self.assertTrue(os.path.exists(path))
             with open(path, 'rb') as f:
                 source = g.toUnicode(f.read())
             tree = ast.parse(source, filename=path)
-            traverser = AnnotationsTraverser(path)
+            traverser = AnnotationsTraverser(self, path)
             traverser.visit(tree)
 
     #@+others
@@ -124,7 +130,7 @@ class TestChains(unittest.TestCase):
             with open(path, 'rb') as f:
                 source = g.toUnicode(f.read())
             tree = ast.parse(source, filename=path)
-            traverser = ChainsTraverser(path)
+            traverser = ChainsTraverser(self, path)
             traverser.visit(tree)
 #@-others
 #@-leo
