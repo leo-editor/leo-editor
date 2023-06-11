39c5349dfddc17c4f9bfb478fe487b0ea2a1f54f
Edward K. Ream <edreamleo@gmail.com>
2023-06-11 15:42:27 -0500

    Remove test_bug_3270_at_path

--- leo/scripts/test-one-leo.cmd
+++ leo/scripts/test-one-leo.cmd
@@ -2,5 +2,5 @@
 cls
 call %~dp0\set-repo-dir
 
-echo test-one-leo: test_leoFind.TestFind
+echo test-one-leo
 call py -m unittest leo.unittests.core.test_leoGlobals.TestGlobals.test_g_handleScriptException
