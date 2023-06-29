7921f4f30d753a99dd89ffbbc737d0deffc95fe1
Edward K. Ream <edreamleo@gmail.com>
2023-06-29 09:25:37 -0500

    Change python to py

--- leo/scripts/blacken-leo.cmd
+++ leo/scripts/blacken-leo.cmd
@@ -3,7 +3,3 @@ cd %~dp0..\..
 
 echo black leo.core
 call py -m black --skip-string-normalization leo\core
-
-rem echo.
-rem echo black leo.commands
-rem call python -m black --skip-string-normalization leo\commands
--- leo/scripts/ruff-leo.cmd
+++ leo/scripts/ruff-leo.cmd
@@ -2,7 +2,7 @@
 cd %~dp0..\..
 
 echo ruff leo/core
-call python -m ruff leo/core
+call py -m ruff leo/core
 
 echo ruff leo/commands
-call python -m ruff leo/commands
+call py -m ruff leo/commands
