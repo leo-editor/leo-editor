448447408c7ec4aba9e43ed451b92a5b3f81340d
Edward K. Ream <edreamleo@gmail.com>
2023-07-05 14:07:22 -0500

    Improve test_gdc_node_history

--- leo/unittests/commands/test_editFileCommands.py
+++ leo/unittests/commands/test_editFileCommands.py
@@ -1,6 +1,7 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20230705083159.1: * @file ../unittests/commands/test_editFileCommands.py
 """Tests for leo.commands.editFileCommands."""
+import textwrap
 from leo.core import leoGlobals as g
 from leo.core.leoTest2 import LeoUnitTest
 assert g
@@ -21,8 +22,26 @@ class TestEditFileCommands(LeoUnitTest):
         # g.parsePathData:  unl:gnx://leoPy.leo#ekr.20230630132341.1
 
         # git log -L/start/,/end/:filename.
-        regex1 = r'@\+node:ekr\.20230626064652\.1'  # g.findUnl
-        regex1 = r'@\+node:ekr\.20230630132341.1'  # g.parsePathData (last def in the file).
+        table = (
+            r'@\+node:ekr\.20230626064652\.1',  # g.findUnl
+            r'@\+node:ekr\.20230630132341.1',  # g.parsePathData (last def in the file).
+        )
+        assert table  ### later.
+        
+        s = textwrap.dedent("""
+            leoPy.leo:   c:/Repos/leo-editor/leo/core
+            # test.leo:    c:/Repos/leo-editor/leo/test
+            # LeoDocs.leo: c:/Repos/leo-editor/leo/doc
+        """)
+        lines = g.splitLines(s)
+        self._set_setting(c, kind='data', name='unl-path-prefixes', val=lines)
+        lines2 = c.config.getData('unl-path-prefixes')
+        expected_lines = [
+            'leoPy.leo:   c:/Repos/leo-editor/leo/core',
+            # 'test.leo:    c:/Repos/leo-editor/leo/test',
+            # 'LeoDocs.leo: c:/Repos/leo-editor/leo/doc',
+        ]
+        self.assertEqual(lines2, expected_lines)
     #@-others
 
 #@-others
