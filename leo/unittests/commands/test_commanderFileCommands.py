1aae6f291da4bb7e75e6880ed3c335d612764164
Edward K. Ream <edreamleo@gmail.com>
2023-07-10 19:26:01 -0500

    Fix write issues

--- leo/unittests/commands/test_commanderFileCommands.py
+++ leo/unittests/commands/test_commanderFileCommands.py
@@ -7,6 +7,7 @@ import textwrap
 from leo.core import leoGlobals as g
 from leo.core.leoTest2 import LeoUnitTest
 assert g
+assert textwrap  ###
 
 #@+others
 #@+node:ekr.20230710105810.1: ** class TestRefreshFromDisk (LeoUnitTest)
@@ -15,34 +16,34 @@ class TestRefreshFromDisk (LeoUnitTest):
     #@+node:ekr.20230710105853.1: *3* TestRefreshFromDisk.test_refresh_from_disk
     def test_refresh_from_disk(self):
         c = self.c
+        at = c.atFileCommands
         p = c.p
+        
+        # Monkey-patch at.preCheck.
+        from typing import Any
+
+        def dummy_precheck(fileName: str, root: Any) -> bool:
+            return True
+            
         refresh = c.refreshFromDisk
-        # Create contents.
-        at_clean_contents = textwrap.dedent(
-            '''
-            """Test file"""
-            ''')
-        at_file_contents = textwrap.dedent(
-            # Not ready yet. Must add valid sentinels.
-            '''
-            """Test file"""
-            ''')
-        assert at_file_contents  ###
-        table = (
-            ('clean', at_clean_contents),
-            #('file', at_file_contents),
-        )
+        raw_contents = '"""Test File"""\n'
         directory = tempfile.gettempdir()  # A writable directory.
-        for (kind, contents) in table:
+        for kind in ('clean', 'file'):
             file_name = f"{directory}{os.sep}test_at_{kind}.py"
             p.h = f"@{kind} {file_name}"
+            p.b = contents = raw_contents
+            if kind == 'file':
+                # Create the sentinels.
+                at.precheck = dummy_precheck  # Force the write.
+                at.writeOneAtFileNode(p)
+                contents = ''.join(at.outputList)
+            g.printObj(contents, tag=kind)
             with open(file_name, 'w') as f:
                 f.write(contents)
             with open(file_name, 'r') as f:
                 contents2 = f.read()
             self.assertEqual(contents, contents2)
-            if 1:
-                refresh(event=None)
+            refresh(event=None)
     #@-others
 #@-others
 #@-leo
