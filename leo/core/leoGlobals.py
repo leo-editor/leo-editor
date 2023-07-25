3b4f1dfca0c7869e5fcba37b2ec0a30f1ede3347
HaveF <iamaplayer@gmail.com>
2023-07-25 12:53:33 +0800

    add private repo support for func get_one_issue

--- leo/core/leoGlobals.py
+++ leo/core/leoGlobals.py
@@ -4814,7 +4814,10 @@ class GitIssueController:
         page_url = self.base_url + '?labels=%s&state=%s&page=%s'
         while True:
             url = page_url % (label, state, page)
-            r = requests.get(url, timeout=2.0)
+            headers = {}
+            if 'GITHUB_TOKEN' in os.environ:
+                headers['Authorization'] = 'Bearer ' + os.environ['GITHUB_TOKEN']
+            r = requests.get(url, timeout=2.0, headers=headers)
             try:
                 done, n = self.get_one_page(label, page, r, root)
                 # Do not remove this trace. It's reassuring.
