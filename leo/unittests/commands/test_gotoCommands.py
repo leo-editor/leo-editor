5a70934c1524ced36de48202780f77fcc47957dc
Edward K. Ream <edreamleo@gmail.com>
2023-08-02 06:08:11 -0500

    Add failing unit test

---
+++ leo/unittests/commands/test_gotoCommands.py
@@ -0,0 +1,21 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20230802060212.1: * @file ../unittests/commands/test_gotoCommands.py
+"""Tests of leo.commands.gotoCommands."""
+from leo.core import leoGlobals as g
+from leo.core.leoTest2 import LeoUnitTest
+assert g
+
+#@+others
+#@+node:ekr.20230802060212.2: ** class TestGotoCommands(LeoUnitTest)
+class TestGotoCommands(LeoUnitTest):
+    """Unit tests for leo/commands/gotoCommands.py."""
+
+    #@+others
+    #@+node:ekr.20230802060444.1: *3* TestGotoCommands.test_show_file_line
+    def test_show_file_line(self):
+        
+        self.fail('Not ready yet')
+    #@-others
+
+#@-others
+#@-leo
