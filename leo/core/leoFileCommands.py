8dd3d25cd990324289f579f8b2edf863bd91d85a
Edward K. Ream <edreamleo@gmail.com>
2023-09-10 01:43:20 -0500

    fc.exportToSqlite clears the 'dirty' bit

--- leo/core/leoFileCommands.py
+++ leo/core/leoFileCommands.py
@@ -1398,7 +1398,8 @@ class FileCommands:
                 ' '.join(x.gnx for x in v.children),
                 ' '.join(x.gnx for x in v.parents),
                 v.iconVal,
-                v.statusBits,
+                # #3550: Clear the dirty bit.
+                v.statusBits & ~v.dirtyBit,
                 dump_u(v)
             )
 
@@ -1456,6 +1457,7 @@ class FileCommands:
             '''create table if not exists extra_infos(name primary key, value)''')
     #@+node:vitalije.20170701161851.1: *6* fc.exportVnodesToSqlite
     def exportVnodesToSqlite(self, conn: Any, rows: Any) -> None:
+        """Called only from fc.exportToSqlite."""
         conn.executemany(
             '''insert into vnodes
             (gnx, head, body, children, parents,
