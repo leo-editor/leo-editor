e6524b26099b04351e6f1506400df96bc558b0b8
Edward K. Ream <edreamleo@gmail.com>
2023-09-10 10:54:47 -0500

    Revert one method to the legacy version (except for traces)

--- leo/core/leoAtFile.py
+++ leo/core/leoAtFile.py
@@ -2915,6 +2915,7 @@ class AtFile:
 
         See #50: https://github.com/leo-editor/leo-editor/issues/50
         """
+        c = self.c
         efc = g.app.externalFilesController
         if p.isAtNoSentFileNode():
             # No danger of overwriting a file: It was never read.
@@ -2925,8 +2926,14 @@ class AtFile:
             return False
 
         # Prompt if the external file is newer.
-        if efc and efc.has_changed(fn):  # has_changed handles all special cases.
-            return True
+        if efc:
+            # Like c.checkFileTimeStamp.
+            if c.sqlite_connection and c.mFileName == fn:
+                # sqlite database file is never actually overwritten by Leo,
+                # so do *not* check its timestamp.
+                pass
+            elif efc.has_changed(fn):
+                return True
 
         if hasattr(p.v, 'at_read'):
             # Fix bug #50: body text lost switching @file to @auto-rst
