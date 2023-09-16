79f15471d31092b17439a9a9ae984378b6895c8e
Edward K. Ream <edreamleo@gmail.com>
2023-09-16 14:26:00 -0500

    Add do-nothing unit test

---
+++ leo/unittests/commands/test_spellCommands.py
@@ -0,0 +1,27 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20230916141635.1: * @file ../unittests/commands/test_spellCommands.py
+"""
+New unit tests for Leo's outline commands.
+
+Older tests are in unittests/core/test_leoNodes.py
+"""
+from leo.core.leoTest2 import LeoUnitTest
+from leo.core import leoGlobals as g
+assert g
+
+#@+others
+#@+node:ekr.20230916141635.2: ** class TestSpellCommands(LeoUnitTest)
+class TestSpellCommands(LeoUnitTest):
+    """
+    Unit tests for Leo's outline commands.
+    """
+
+    #@+others
+    #@+node:ekr.20230916141635.3: *3* TestSpellCommands.test_SpellTabHandler_find
+    def test_SpellTabHandler_find(self):
+
+        # g.trace('***')
+        pass
+    #@-others
+#@-others
+#@-leo
