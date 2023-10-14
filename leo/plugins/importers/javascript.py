736b9e7192a34d4d9227cd3178a7fd93f8abd979
Edward K. Ream <edreamleo@gmail.com>
2023-10-14 08:34:04 -0500

    Add JsLexer code from a previous version of Leo

--- leo/plugins/importers/javascript.py
+++ leo/plugins/importers/javascript.py
@@ -3,7 +3,7 @@
 """The @auto importer for JavaScript."""
 from __future__ import annotations
 import re
-from typing import TYPE_CHECKING
+from typing import Any, Generator, TYPE_CHECKING
 from leo.core import leoGlobals as g
 from leo.plugins.importers.base_importer import Importer
 
@@ -96,6 +96,195 @@ class JS_Importer(Importer):
         assert len(result) == len(lines)  # A crucial invariant.
         return result
     #@-others
+#@+node:ekr.20231014081529.1: ** JsLex...
+# JsLex: a lexer for Javascript
+# Written by Ned Batchelder. Used by permission.
+
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://bitbucket.org/ned/jslex/src/default/NOTICE.txt
+#@+node:ekr.20231014081617.1: *3* class Tok
+class Tok:
+    """A specification for a token class."""
+
+    num = 0
+
+    def __init__(self, name: str, regex: str, next: str = None) -> None:
+        self.id = Tok.num
+        Tok.num += 1
+        self.name = name
+        self.regex = regex
+        self.next = next
+#@+node:ekr.20231014081649.1: *3* class Lexer
+class Lexer:
+    """A generic multi-state regex-based lexer."""
+
+    def __init__(self, states: dict, first: Any) -> None:
+        self.regexes = {}
+        self.toks = {}
+        for state, rules in states.items():
+            parts = []
+            for tok in rules:
+                groupid = "t%d" % tok.id
+                self.toks[groupid] = tok
+                parts.append("(?P<%s>%s)" % (groupid, tok.regex))
+            self.regexes[state] = re.compile("|".join(parts), re.MULTILINE | re.VERBOSE)  # |re.UNICODE)
+        self.state = first
+
+    #@+others
+    #@+node:ekr.20231014082415.1: *4* Lexer.lex
+    def lex(self, text: str) -> Generator:
+        """Lexically analyze `text`.
+
+        Yields pairs (`name`, `tokentext`).
+        """
+        end = len(text)
+        state = self.state
+        regexes = self.regexes
+        toks = self.toks
+        start = 0
+        while start < end:
+            for match in regexes[state].finditer(text, start):
+                # g.trace(state, start, text, match)
+                # g.printObj(regexes[state])
+                name = match.lastgroup
+                tok = toks[name]
+                toktext = match.group(name)
+                start += len(toktext)
+                yield(tok.name, toktext)
+                if tok.next:
+                    state = tok.next
+                    break
+        self.state = state
+    #@-others
+#@+node:ekr.20231014081748.1: *3* function: literals
+def literals(choices: str, prefix: str = "", suffix: str = "") -> str:
+    """
+    Create a regex from a space-separated list of literal `choices`.
+
+    If provided, `prefix` and `suffix` will be attached to each choice
+    individually.
+
+    """
+    return "|".join(prefix + re.escape(c) + suffix for c in choices.split())
+#@+node:ekr.20231014081748.2: *3* class JsLexer(Lexer)
+class JsLexer(Lexer):
+    """A Javascript lexer
+
+    >>> lexer = JsLexer()
+    >>> list(lexer.lex("a = 1"))
+    [('id', 'a'), ('ws', ' '), ('punct', '='), ('ws', ' '), ('dnum', '1')]
+
+    This doesn't properly handle non-Ascii characters in the Javascript source.
+    """
+    # EKR: Happily, the JS importer doesn't need to handle id's carefully.
+
+    #@+<< constants: JsLexer>>
+    #@+node:ekr.20231014082018.1: *4* << constants: JsLexer>>
+    # Because these tokens are matched as alternatives in a regex, longer possibilities
+    # must appear in the list before shorter ones, for example, '>>' before '>'.
+
+    # Note that we don't have to detect malformed Javascript, so much of this is simplified.
+
+    # Details of Javascript lexical structure are taken from
+    # http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-262.pdf
+
+    # A useful explanation of automatic semicolon insertion is at:
+    # http://inimino.org/~inimino/blog/javascript_semicolons
+
+    # See https://stackoverflow.com/questions/6314614/match-any-unicode-letter
+
+    both_before = [
+        Tok("comment", r"/\*(.|\n)*?\*/"),
+        Tok("linecomment", r"//.*?$"),
+        Tok("ws", r"\s+"),
+        Tok("keyword", literals("""
+                                async await
+                                break case catch class const continue debugger
+                                default delete do else enum export extends
+                                finally for function if import in instanceof new
+                                return super switch this throw try typeof var
+                                void while with
+                                """, suffix=r"\b"), next='reg'),
+        Tok("reserved", literals("null true false", suffix=r"\b"), next='div'),
+        #
+        # EKR: This would work if patterns were compiled with the re.UNICODE flag.
+        #      However, \w is not the same as valid JS characters.
+        #      In any case, the JS importer doesn't need to handle id's carefully.
+        #
+        # Tok("id",           r"""([\w$])([\w\d]*)""", next='div'),
+        #
+        Tok("id", r"""
+                            ([a-zA-Z_$   ]|\\u[0-9a-fA-Z]{4})       # first char
+                            ([a-zA-Z_$0-9]|\\u[0-9a-fA-F]{4})*      # rest chars
+                            """, next='div'),
+        Tok("hnum", r"0[xX][0-9a-fA-F]+", next='div'),
+        Tok("onum", r"0[0-7]+"),
+        Tok("dnum", r"""
+                            (   (0|[1-9][0-9]*)         # DecimalIntegerLiteral
+                                \.                      # dot
+                                [0-9]*                  # DecimalDigits-opt
+                                ([eE][-+]?[0-9]+)?      # ExponentPart-opt
+                            |
+                                \.                      # dot
+                                [0-9]+                  # DecimalDigits
+                                ([eE][-+]?[0-9]+)?      # ExponentPart-opt
+                            |
+                                (0|[1-9][0-9]*)         # DecimalIntegerLiteral
+                                ([eE][-+]?[0-9]+)?      # ExponentPart-opt
+                            )
+                            """, next='div'),
+        Tok("punct", literals("""
+                                >>>= === !== >>> <<= >>= <= >= == != << >> &&
+                                || += -= *= %= &= |= ^=
+                                """), next="reg"),
+        Tok("punct", literals("++ -- ) ]"), next='div'),
+        Tok("punct", literals("{ } ( [ . ; , < > + - * % & | ^ ! ~ ? : ="), next='reg'),
+        Tok("string", r'"([^"\\]|(\\(.|\n)))*?"', next='div'),
+        Tok("string", r"'([^'\\]|(\\(.|\n)))*?'", next='div'),
+        ]
+
+    both_after = [
+        Tok("other", r"."),
+        ]
+
+    states = {
+        'div':  # slash will mean division
+            both_before + [
+            Tok("punct", literals("/= /"), next='reg'),
+            ] + both_after,
+
+        'reg':  # slash will mean regex
+            both_before + [
+            Tok("regex",
+                r"""
+                    /                       # opening slash
+                    # First character is..
+                    (   [^*\\/[]            # anything but * \ / or [
+                    |   \\.                 # or an escape sequence
+                    |   \[                  # or a class, which has
+                            (   [^\]\\]     #   anything but \ or ]
+                            |   \\.         #   or an escape sequence
+                            )*              #   many times
+                        \]
+                    )
+                    # Following characters are same, except for excluding a star
+                    (   [^\\/[]             # anything but \ / or [
+                    |   \\.                 # or an escape sequence
+                    |   \[                  # or a class, which has
+                            (   [^\]\\]     #   anything but \ or ]
+                            |   \\.         #   or an escape sequence
+                            )*              #   many times
+                        \]
+                    )*                      # many times
+                    /                       # closing slash
+                    [a-zA-Z0-9]*            # trailing flags
+                """, next='div'),
+            ] + both_after,
+        }
+    #@-<< constants: JsLexer>>
+
+    def __init__(self) -> None:
+        super().__init__(self.states, 'reg')
 #@-others
 
 def do_import(c: Cmdr, parent: Position, s: str) -> None:
