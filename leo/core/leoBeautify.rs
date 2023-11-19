c42e0b4acfa277e0d1e3645b216bf06151d34c12
Edward K. Ream <edreamleo@gmail.com>
2023-11-19 15:19:06 -0600

    Add leoBeautify.rs

---
+++ leo/core/leoBeautify.rs
@@ -0,0 +1,21 @@
+//@+leo-ver=5-thin
+//@+node:ekr.20231119151610.1: * @file leoBeautify.rs
+# This file is part of Leo: https://leo-editor.github.io/leo-editor
+# Leo's copyright notice is based on the MIT license:
+# https://leo-editor.github.io/leo-editor/license.html
+
+//@+<< leoBeautify.rs docstring >>
+//@+node:ekr.20231119151801.1: ** << leoBeautify.rs docstring >>
+//@-<< leoBeautify.rs docstring >>
+//@+<< leoBeautify.rs imports & annotations >>
+//@+node:ekr.20231119151804.1: ** << leoBeautify.rs imports & annotations >>
+//@-<< leoBeautify.rs imports & annotations >>
+//@+<< leoBeautify.rs data >>
+//@+node:ekr.20231119151807.1: ** << leoBeautify.rs data >>
+//@-<< leoBeautify.rs data >>
+
+//@+others
+//@-others
+
+//@@language rust
+//@-leo
