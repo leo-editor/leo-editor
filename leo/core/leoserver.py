76ebff13a324a8c48e1ecd5f276beaa1229de16d
Félix <felixgraphx@gmail.com>
2023-12-02 19:38:24 -0500

    Removed extraneous parameters when raising websockets.exceptions.ConnectionClosed.

--- leo/core/leoserver.py
+++ leo/core/leoserver.py
@@ -5446,7 +5446,7 @@ def main() -> None:  # pragma: no cover (tested in client)
                         print(f"{tag}: got: {d}", flush=True)
                     answer = controller._do_message(d)
                 except TerminateServer as e:
-                    raise websockets.exceptions.ConnectionClosed(1000, e.__str__())
+                    raise websockets.exceptions.ConnectionClosed()
                 except ServerError as e:
                     data = f"{d}" if d else f"json syntax error: {json_message!r}"
                     error = f"{tag}:  ServerError: {e}...\n{tag}:  {data}"
