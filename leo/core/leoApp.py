444773ad2f3c65b08bffdebae6fe31886abeadbd
Edward K. Ream <edreamleo@gmail.com>
2023-12-11 14:01:03 -0600

    Simplify version logic. Deprecate g.CheckVersion

--- leo/core/leoApp.py
+++ leo/core/leoApp.py
@@ -12,7 +12,6 @@ import string
 import sys
 import textwrap
 import time
-import traceback
 from typing import Any, Optional, TYPE_CHECKING
 import zipfile
 import platform
@@ -2951,10 +2950,10 @@ class LoadManager:
             return True
         message = (
             f"Leo requires Python {g.minimum_python_version} or higher"
-            f"You may download Python from http://python.org/download/")
+            "You may download Python from http://python.org/download/"
+        )
         try:
-            version = '.'.join([str(sys.version_info[i]) for i in (0, 1, 2)])
-            ok = g.CheckVersion(version, g.minimum_python_version)
+            ok = g.python_version_tuple >= g.minimum_python_version_tuple
             if not ok:
                 print(message)
                 try:
@@ -2966,9 +2965,8 @@ class LoadManager:
                 except Exception:
                     g.es_exception()
             return ok
-        except Exception:
-            print("isValidPython: unexpected exception: g.CheckVersion")
-            traceback.print_exc()
+        except Exception as e:
+            print(f"LM.isValidPython: unexpected exception {e}")
             return False
     #@+node:ekr.20120223062418.10393: *4* LM.openWithFileName & helpers
     def openWithFileName(self, fn: str, gui: LeoGui, old_c: Cmdr) -> Optional[Cmdr]:
