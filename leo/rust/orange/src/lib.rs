54af1849529ac92cef23d3af0dd59f1a55c591c2
Edward K. Ream <edreamleo@gmail.com>
2023-12-19 18:16:34 -0600

    Add the test file, lib.rs

---
+++ leo/rust/orange/src/lib.rs
@@ -0,0 +1,14 @@
+//@+leo-ver=5-thin
+//@+node:ekr.20231219180929.1: * @file ../rust/orange/src/lib.rs
+//@@language rust
+
+// Test file for main.rs.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+//@-leo
