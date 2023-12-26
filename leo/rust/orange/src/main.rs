b87718b2e0d415aacbfbf596db2da588061a3832
Edward K. Ream <edreamleo@gmail.com>
2023-12-26 04:18:33 -0600

    Delete lib.rs. Move tests into main.rs

--- leo/rust/orange/src/main.rs
+++ leo/rust/orange/src/main.rs
@@ -1,8 +1,25 @@
 //@+leo-ver=5-thin
 //@+node:ekr.20231120070819.1: * @file ../rust/orange/src/main.rs
 //@@language rust
+// Orange in rust.
 
+//@+others
+//@+node:ekr.20231226041126.1: ** fn main
 fn main() {
     println!("main.rs: ekr-orange");
 }
+//@+node:ekr.20231226041203.1: ** mod tests
+#[cfg(test)]
+mod tests {
+    //@+others
+    //@+node:ekr.20231223154842.1: *3* fn: test_driver
+    #[test]
+    fn test_driver() {
+        println!("driver test")
+        // assert_eq!(2 + 2, 4);
+    }
+    //@-others
+}
+//@-others
+
 //@-leo
