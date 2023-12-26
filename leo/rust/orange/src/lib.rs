f7b68bd87516c44cc2fe4c3d24d304e46bc7603c
Edward K. Ream <edreamleo@gmail.com>
2023-12-26 06:43:45 -0600

    Add dependency. Add commented-out example

--- leo/rust/orange/src/lib.rs
+++ leo/rust/orange/src/lib.rs
@@ -4,12 +4,35 @@
 
 use std::process::ExitCode;
 
+// use rustpython::
+use rustpython_vm as vm;
+
 pub fn run() -> ExitCode {
     // The main line for orange.
     println!("orange/src/lib.rs: run");
     
+    if false {
+    
+        // From RustPython/examples/hello_embed.rs.
+        // vm::Interpreter::without_stdlib(Default::default()).enter(|vm| {
+            // let scope = vm.new_scope_with_builtins();
+            // let source = r#"print("Hello World!")"#;
+            // let code_obj = vm
+                // .compile(source, vm::compiler::Mode::Exec, "<embedded>".to_owned())
+                // // .map_err(|err| vm.new_syntax_error(&err, Some(source)))?;
+                // .map_err(|err| vm.new_syntax_error(&err, Some(source)));
+
+            // // vm.run_code_obj(code_obj, scope)?;
+            // vm.run_code_obj(code_obj, scope);
+
+            // // Ok(())
+        // });
+    
+    };
+
     ExitCode::SUCCESS
 }
+
 
 //@+others
 //@+node:ekr.20231226041203.1: ** mod tests
