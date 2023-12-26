cb0b36ddc89b03693c0f3a0085509fb70d356b3d
Edward K. Ream <edreamleo@gmail.com>
2023-12-26 05:52:32 -0600

    Milestone: use lib.rs and main.rs as in best practices. Add local .gitignore

---
+++ leo/rust/orange/src/lib.rs
@@ -0,0 +1,27 @@
+//@+leo-ver=5-thin
+//@+node:ekr.20231226052346.1: * @file ../rust/orange/src/lib.rs
+//! This is the `orange` binary.
+
+use std::process::ExitCode;
+
+pub fn run() -> ExitCode {
+    println!("orange/src/lib.rs: run");
+    
+    ExitCode::SUCCESS
+}
+
+//@+others
+//@+node:ekr.20231226041203.1: ** mod tests
+#[cfg(test)]
+mod tests {
+    //@+others
+    //@+node:ekr.20231223154842.1: *3* fn: test_driver
+    #[test]
+    fn test_driver() {
+        println!("driver test")
+        // assert_eq!(2 + 2, 4);
+    }
+    //@-others
+}
+//@-others
+//@-leo
