d9ce6a84bc354aaa97e081b3bf279d8508fba97f
Edward K. Ream <edreamleo@gmail.com>
2024-02-01 13:07:05 -0600

    Clean one more file

--- leo/unittests/core/test_leoCommands.py
+++ leo/unittests/core/test_leoCommands.py
@@ -14,22 +14,24 @@ class TestCommands(LeoUnitTest):
     #@+node:ekr.20210906075242.28: *3* TestCommands.test_add_comments_with_multiple_language_directives
     def test_add_comments_with_multiple_language_directives(self):
         c, p, w = self.c, self.c.p, self.c.frame.body.wrapper
-        p.b = textwrap.dedent("""\
+        p.b = textwrap.dedent(
+        """
             @language rest
             rest text.
             @language python
             def spam():
                 pass
             # after
-    """)
-        expected = textwrap.dedent("""\
+    """).lstrip()
+        expected = textwrap.dedent(
+        """
             @language rest
             rest text.
             @language python
             def spam():
                 # pass
             # after
-    """)
+    """).lstrip()
         i = p.b.find('pass')
         assert i > -1, 'fail1: %s' % (repr(p.b))
         w.setSelectionRange(i, i + 4)
@@ -38,18 +40,20 @@ class TestCommands(LeoUnitTest):
     #@+node:ekr.20210906075242.30: *3* TestCommands.test_add_html_comments
     def test_add_html_comments(self):
         c, p, w = self.c, self.c.p, self.c.frame.body.wrapper
-        p.b = textwrap.dedent("""\
+        p.b = textwrap.dedent(
+        """
             @language html
             <html>
                 text
             </html>
-    """)
-        expected = textwrap.dedent("""\
+        """).lstrip()
+        expected = textwrap.dedent(
+        """
             @language html
             <html>
                 <!-- text -->
             </html>
-    """)
+        """).lstrip()
         i = p.b.find('text')
         w.setSelectionRange(i, i + 4)
         c.addComments()
@@ -57,18 +61,20 @@ class TestCommands(LeoUnitTest):
     #@+node:ekr.20210906075242.32: *3* TestCommands.test_add_python_comments
     def test_add_python_comments(self):
         c, p, w = self.c, self.c.p, self.c.frame.body.wrapper
-        p.b = textwrap.dedent("""\
+        p.b = textwrap.dedent(
+        """
             @language python
             def spam():
                 pass
             # after
-    """)
-        expected = textwrap.dedent("""\
+        """).lstrip()
+        expected = textwrap.dedent(
+        """
             @language python
             def spam():
                 # pass
             # after
-    """)
+        """).lstrip()
         i = p.b.find('pass')
         w.setSelectionRange(i, i + 4)
         c.addComments()
@@ -93,12 +99,13 @@ class TestCommands(LeoUnitTest):
     #@+node:ekr.20210901140645.16: *3* TestCommands.test_c_checkPythonNode
     def test_c_checkPythonNode(self):
         c, p = self.c, self.c.p
-        p.b = textwrap.dedent("""\
+        p.b = textwrap.dedent(
+        """
             @language python
 
             def abc:  # missing parens.
                 pass
-        """)
+        """).lstrip()
         result = c.checkPythonCode(event=None, checkOnSave=False, ignoreAtIgnore=True)
         self.assertEqual(result, 'error')
     #@+node:ekr.20210906075242.4: *3* TestCommands.test_c_contractAllHeadlines
@@ -324,19 +331,21 @@ class TestCommands(LeoUnitTest):
     def test_c_tabNannyNode(self):
         c, p = self.c, self.c.p
         # Test 1.
-        s = textwrap.dedent("""\
+        s = textwrap.dedent(
+        """
             # no error
             def spam():
                 pass
-        """)
+        """).lstrip()
         c.tabNannyNode(p, headline=p.h, body=s)
         # Test 2.
-        s2 = textwrap.dedent("""\
+        s2 = textwrap.dedent(
+        """
             # syntax error
             def spam:
                 pass
               a = 2
-        """)
+        """).lstrip()
         try:
             c.tabNannyNode(p, headline=p.h, body=s2)
         except IndentationError:
@@ -359,22 +368,24 @@ class TestCommands(LeoUnitTest):
     #@+node:ekr.20210906075242.29: *3* TestCommands.test_delete_comments_with_multiple_at_language_directives
     def test_delete_comments_with_multiple_at_language_directives(self):
         c, p, w = self.c, self.c.p, self.c.frame.body.wrapper
-        p.b = textwrap.dedent("""\
+        p.b = textwrap.dedent(
+        """
             @language rest
             rest text.
             @language python
             def spam():
                 pass
             # after
-    """)
-        expected = textwrap.dedent("""\
+        """).lstrip()
+        expected = textwrap.dedent(
+        """
             @language rest
             rest text.
             @language python
             def spam():
                 pass
             # after
-    """)
+        """).lstrip()
         i = p.b.find('pass')
         w.setSelectionRange(i, i + 4)
         c.deleteComments()
@@ -383,18 +394,20 @@ class TestCommands(LeoUnitTest):
     #@+node:ekr.20210906075242.31: *3* TestCommands.test_delete_html_comments
     def test_delete_html_comments(self):
         c, p, w = self.c, self.c.p, self.c.frame.body.wrapper
-        p.b = textwrap.dedent("""\
+        p.b = textwrap.dedent(
+        """
             @language html
             <html>
                 <!-- text -->
             </html>
-    """)
-        expected = textwrap.dedent("""\
+        """).lstrip()
+        expected = textwrap.dedent(
+        """
             @language html
             <html>
                 text
             </html>
-    """)
+        """).lstrip()
         i = p.b.find('text')
         w.setSelectionRange(i, i + 4)
         c.deleteComments()
@@ -402,18 +415,20 @@ class TestCommands(LeoUnitTest):
     #@+node:ekr.20210906075242.33: *3* TestCommands.test_delete_python_comments
     def test_delete_python_comments(self):
         c, p, w = self.c, self.c.p, self.c.frame.body.wrapper
-        p.b = textwrap.dedent("""\
+        p.b = textwrap.dedent(
+        """
             @language python
             def spam():
                 # pass
             # after
-    """)
-        expected = textwrap.dedent("""\
+        """).lstrip()
+        expected = textwrap.dedent(
+        """
             @language python
             def spam():
                 pass
             # after
-    """)
+        """).lstrip()
         i = p.b.find('pass')
         w.setSelectionRange(i, i + 4)
         c.deleteComments()
