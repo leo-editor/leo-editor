cc452a154eb8d8fab501a7c34360e87c524dd132
Edward K. Ream <edreamleo@gmail.com>
2024-02-28 02:47:41 -0600

    Tweak a script

--- leo/scripts/pip-uninstall-all.cmd
+++ leo/scripts/pip-uninstall-all.cmd
@@ -6,13 +6,12 @@ cd %~dp0..\..
 echo pip freeze > temp_requirements.txt
 call pip freeze > temp_requirements.txt
 
-rem A hack: Remove leo.egg-info
-
+rem A hack: Remove leo.egg-info if it exists from a legacy installation.
 echo del leo.egg-info
 del leo.egg-info
 
 echo pip uninstall -r temp_requirements.txt -y
-call pip uninstall -r temp_requirements.txt -y --verbose
+call pip uninstall -r temp_requirements.txt -y
 
 echo del temp_requirements.txt
 del temp_requirements.txt
