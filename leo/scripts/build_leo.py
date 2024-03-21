031c9ae572b3ce1b667041134870747325b40392
Edward K. Ream <edreamleo@gmail.com>
2024-03-21 16:50:27 -0500

    Change version to 6.7.9.a1. Complete build_leo.py

--- leo/scripts/build_leo.py
+++ leo/scripts/build_leo.py
@@ -1,7 +1,6 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20240321122822.1: * @file ../scripts/build_leo.py
 #@@language python
-
 import glob
 import os
 import sys
@@ -9,26 +8,21 @@ import sys
 # Make sure leo-editor is on the path.
 leo_dir = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
 if leo_dir not in sys.path:
-    print(f"add {leo_dir!r} to sys.path")
+    # print(f"add {leo_dir!r} to sys.path")
     sys.path.insert(0, leo_dir)
 from leo.core import leoGlobals as g
 
 g.cls()
-# leo_dir = g.os_path_finalize_join(g.app.loadDir, '..', '..')
-
-print(f"cd {leo_dir}")
+print('build_leo.py')
 os.chdir(leo_dir)
 
 # delete leo/dist/*.*
-print('del dist/*.*')
-files = glob.glob(f"{leo_dir}{os.sep}dist{os.sep}*.*")
-for z in files:
+for z in glob.glob(f"{leo_dir}{os.sep}dist{os.sep}*.*"):
     os.remove(z)
 
 # build *both* sdist and wheel.
 command = 'python -m build > build_log.txt'
-print(command)
 g.execute_shell_commands(command)
 
-print('build-leo: done')
+# print('build_leo.py: done')
 #@-leo
