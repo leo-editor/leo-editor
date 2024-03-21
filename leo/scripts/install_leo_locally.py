9329b148d6c8e27ba72f4984c55ce138b13fafdc
Edward K. Ream <edreamleo@gmail.com>
2024-03-21 18:31:31 -0500

    Add two more scripts

--- leo/scripts/install_leo_locally.py
+++ leo/scripts/install_leo_locally.py
@@ -1,17 +1,34 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20240321123214.1: * @file ../scripts/install_leo_locally.py
-#@verbatim
-# @language python
+#@@language python
 
-#@verbatim
-# @echo off
+import glob
+import os
+import sys
 
-# rem Do *not* install from leo-editor!
-# cls
-# cd c:\Users\Dev
+# Make sure leo-editor is on the path.
+leo_dir = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
+if leo_dir not in sys.path:
+    sys.path.insert(0, leo_dir)
+from leo.core import leoGlobals as g
 
-# call python -m pip install c:\Repos\leo-editor\dist\leo-6.7.8.post3-py3-none-any.whl
+g.cls()
+print('install_leo_locally.py')
 
-# echo ls Python3.12\Lib\site-packages\leo*.*
-# ls C:\Python\Python3.12\Lib\site-packages\leo*.*
+# Do *not* install from leo-editor!
+home_dir = os.path.expanduser("~")
+os.chdir(home_dir)
+
+# Install.
+wheel_file = 'leo-6.7.9a1-py3-none-any.whl'
+command = fr"python -m pip install {leo_dir}{os.sep}dist{os.sep}{wheel_file}"
+g.execute_shell_commands(command)
+
+# List site-packages/leo*.
+python_dir = os.path.dirname(sys.executable)
+package_dir = os.path.abspath(os.path.join(python_dir, 'Lib', 'site-packages'))
+print('')
+print('site-packages/leo*...')
+for z in glob.glob(f"{package_dir}{os.sep}leo*"):
+    print(z)
 #@-leo
--- leo/scripts/uninstall_leo.py
+++ leo/scripts/uninstall_leo.py
@@ -2,12 +2,23 @@
 #@+node:ekr.20240321123225.1: * @file ../scripts/uninstall_leo.py
 #@@language python
 
-#@verbatim
-# @echo off
+import os
+import sys
 
-# rem Do *not* install from leo-editor!
-# cls
-# cd c:\Users\Dev
+# Make sure leo-editor is on the path.
+leo_dir = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
+if leo_dir not in sys.path:
+    sys.path.insert(0, leo_dir)
+from leo.core import leoGlobals as g
 
-# pip uninstall leo
+g.cls()
+print('uninstall_leo.py')
+
+# Do *not* install from leo-editor!
+home_dir = os.path.expanduser("~")
+os.chdir(home_dir)
+
+# Uninstall.
+command = 'python -m pip uninstall leo'
+g.execute_shell_commands(command)
 #@-leo
