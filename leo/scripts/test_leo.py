8267a9ad6e9c8fee185b79211bf2e64dfdeda674
Edward K. Ream <edreamleo@gmail.com>
2024-03-22 16:46:18 -0500

    Complete test_leo.py

--- leo/scripts/test_leo.py
+++ leo/scripts/test_leo.py
@@ -8,13 +8,16 @@ import os
 import subprocess
 import sys
 
-# cd to leo-editor
-os.chdir(os.path.abspath(os.path.join(__file__, '..', '..', '..')))
+# cd to leo-editor/leo/unittests
+path = os.path.abspath(os.path.join(__file__, '..', '..', 'unittests'))
+assert path.endswith(f"leo{os.sep}unittests"), repr(path)
+# print(f"os.chdir({path})")
+os.chdir(path)
 
 args = ' '.join(sys.argv[1:])
 isWindows = sys.platform.startswith('win')
 python = 'py' if isWindows else 'python'
 
-command = fr'{python} -m unittest leo'
+command = fr'{python} -m unittest '
 subprocess.Popen(command, shell=True).communicate()
 #@-leo
