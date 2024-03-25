84fdd293e996878d49c0bd6c9bdf794a9f55b3f9
Edward K. Ream <edreamleo@gmail.com>
2024-03-24 21:04:34 -0500

    Remove more ### comments

--- leo/unittests/plugins/test_gui.py
+++ leo/unittests/plugins/test_gui.py
@@ -66,15 +66,11 @@ class TestQtGui(LeoUnitTest):
         for command in ('toggle-invisibles', 'hide-invisibles', 'show-invisibles'):
             c.doCommandByName(command)
         option = QtGui.QTextOption()
-        # Test the old code.
-        if True:  ###isQt6:
-            # Skip this test when using PyQt5.
-            with self.assertRaises(AttributeError):
-                option.ShowTabsAndSpaces  # pylint: disable=pointless-statement
-            return
-        ###
-            # # Test the new code.
-            # assert option.ShowTabsAndSpaces is not None  # pragma: no cover
+
+        # Skip this test when using PyQt5.
+        with self.assertRaises(AttributeError):
+            option.ShowTabsAndSpaces  # pylint: disable=pointless-statement
+        return
     #@+node:ekr.20210912140946.1: *3* TestQtGui.test_do_nothing1/2/3
     # These tests exist to test the startup logic.
     if 0:  # pragma: no cover
