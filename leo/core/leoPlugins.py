1cd93db9cd0cbc4fc0b6cf265833642f9c9c809b
Edward K. Ream <edreamleo@gmail.com>
2024-04-09 09:48:10 -0500

    Beautify

--- leo/core/leoPlugins.py
+++ leo/core/leoPlugins.py
@@ -487,7 +487,7 @@ class LeoPluginsController:
         Using a module name allows plugins to be loaded from outside the leo/plugins directory.
         """
         global optional_modules
-        
+
         moduleName: str
 
         trace = verbose or 'plugins' in g.app.debug
