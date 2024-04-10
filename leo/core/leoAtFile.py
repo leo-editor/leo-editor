bacb8fbbe6c7215c7a4c5301bc22dca7c401a54c
Edward K. Ream <edreamleo@gmail.com>
2024-04-10 11:34:22 -0500

    Fixed most annotations. Made LeoIOStatus visible for unit test

--- leo/core/leoAtFile.py
+++ leo/core/leoAtFile.py
@@ -20,6 +20,7 @@ if TYPE_CHECKING:  # pragma: no cover
     from leo.core.leoCommands import Commands as Cmdr
     from leo.core.leoGui import LeoKeyEvent
     from leo.core.leoNodes import Position, VNode
+    Args = Any
 #@-<< leoAtFile imports & annotations >>
 
 #@+others
@@ -560,7 +561,7 @@ class AtFile:
         g.doHook('after-reading-external-file', c=c, p=root)
         return True  # Errors not detected.
     #@+node:ekr.20150204165040.7: *6* at.dump_lines
-    def dump(self, lines: list[str], tag: Any) -> None:  # pragma: no cover
+    def dump(self, lines: list[str], tag: str) -> None:  # pragma: no cover
         """Dump all lines."""
         print(f"***** {tag} lines...\n")
         for s in lines:
@@ -1717,24 +1718,8 @@ class AtFile:
             if not s.endswith('\n'):
                 s = s + '\n'
 
-
-        class Status:
-            at_comment_seen = False
-            at_delims_seen = False
-            at_warning_given = False
-            has_at_others = False
-            in_code = True
-
-            def __repr__(self) -> str:  # pragma: no cover (testing)
-                return (
-                    f"code? {int(self.in_code)} "
-                    # f"comment? {int(self.at_comment_seen)} "
-                    # f"delims? {int(self.at_delims_seen)} "
-                    # f"@others? {int(self.has_at_others)} "
-                )
-
         i = 0
-        status = Status()
+        status = LeoIOStatus()
         while i < len(s):
             next_i = g.skip_line(s, i)
             assert next_i > i, 'putBody'
@@ -1745,7 +1730,7 @@ class AtFile:
             at.putEndDocLine()
         return status.has_at_others
     #@+node:ekr.20041005105605.163: *6* at.putLine
-    def putLine(self, i: int, kind: int, p: Position, s: str, status: Any) -> None:
+    def putLine(self, i: int, kind: int, p: Position, s: str, status: LeoIOStatus) -> None:
         """Put the line at s[i:] of the given kind, updating the status."""
         at = self
         if kind == at.noDirective:
@@ -2052,7 +2037,7 @@ class AtFile:
             at.os(at.endSentinelComment)
             at.onl()  # Note: no trailing whitespace.
     #@+node:ekr.20041005105605.182: *6* at.putStartDocLine
-    def putStartDocLine(self, s: str, i: int, kind: Any) -> None:
+    def putStartDocLine(self, s: str, i: int, kind: int) -> None:
         """Write the start of a doc part."""
         at = self
         sentinel = "@+doc" if kind == at.docDirective else "@+at"
@@ -2209,7 +2194,7 @@ class AtFile:
         if not ok:
             g.app.syntax_error_files.append(g.shortFileName(fileName))
     #@+node:ekr.20090514111518.5663: *6* at.checkPythonSyntax
-    def checkPythonSyntax(self, p: Position, body: Any) -> bool:
+    def checkPythonSyntax(self, p: Position, body: str) -> bool:
         at = self
         try:
             body = body.replace('\r', '')
@@ -2224,7 +2209,7 @@ class AtFile:
             g.es_exception()
         return False
     #@+node:ekr.20090514111518.5666: *7* at.syntaxError (leoAtFile)
-    def syntaxError(self, p: Position, body: Any) -> None:  # pragma: no cover
+    def syntaxError(self, p: Position, body: str) -> None:  # pragma: no cover
         """Report a syntax error."""
         g.error(f"Syntax error in: {p.h}")
         typ, val, tb = sys.exc_info()
@@ -2352,7 +2337,7 @@ class AtFile:
             return True, i + len(at.section_delim2)
         return False, -1
     #@+node:ekr.20190111112442.1: *5* at.isWritable
-    def isWritable(self, path: Any) -> bool:  # pragma: no cover
+    def isWritable(self, path: str) -> bool:  # pragma: no cover
         """Return True if the path is writable."""
         try:
             # os.access() may not exist on all platforms.
@@ -2776,12 +2761,12 @@ class AtFile:
         at.addToOrphanList(root)
     #@+node:ekr.20041005105605.219: *3* at.Utilities
     #@+node:ekr.20041005105605.220: *4* at.error & printError
-    def error(self, *args: Any) -> None:  # pragma: no cover
+    def error(self, *args: Args) -> None:  # pragma: no cover
         at = self
         at.printError(*args)
         at.errors += 1
 
-    def printError(self, *args: Any) -> None:  # pragma: no cover
+    def printError(self, *args: Args) -> None:  # pragma: no cover
         """Print an error message that may contain non-ascii characters."""
         at = self
         if at.errors:
@@ -2795,7 +2780,7 @@ class AtFile:
     #@+node:ekr.20050104131929: *4* at.file operations...
     # Error checking versions of corresponding functions in Python's os module.
     #@+node:ekr.20050104131820: *5* at.chmod
-    def chmod(self, fileName: str, mode: Any) -> None:  # pragma: no cover
+    def chmod(self, fileName: str, mode: int) -> None:  # pragma: no cover
         # Do _not_ call self.error here.
         if mode is None:
             return
@@ -2819,7 +2804,7 @@ class AtFile:
                 g.es_exception()
             return False
     #@+node:ekr.20050104132026: *5* at.stat
-    def stat(self, fileName: str) -> Any:  # pragma: no cover
+    def stat(self, fileName: str) -> int:  # pragma: no cover
         """Return the access mode of named file, removing any setuid, setgid, and sticky bits."""
         # Do _not_ call self.error here.
         try:
@@ -2835,7 +2820,7 @@ class AtFile:
             return d.get('path')  # type:ignore
         return ''
 
-    def setPathUa(self, p: Position, path: Any) -> None:
+    def setPathUa(self, p: Position, path: str) -> None:
         if not hasattr(p.v, 'tempAttributes'):
             p.v.tempAttributes = {}
         d = p.v.tempAttributes.get('read-path', {})
@@ -3054,7 +3039,7 @@ class FastAtRead:
     #@+node:ekr.20180602103135.3: *3* fast_at.get_patterns
     #@@nobeautify
 
-    def get_patterns(self, comment_delims: Any) -> None:
+    def get_patterns(self, comment_delims: tuple) -> None:
         """Create regex patterns for the given comment delims."""
 
         # This must be a function, because of @comments & @delims.
@@ -3107,7 +3092,7 @@ class FastAtRead:
         re.VERBOSE,
     )
 
-    def scan_header(self, lines: Any) -> Optional[tuple[Any, Any, Any]]:
+    def scan_header(self, lines: list[str]) -> Optional[tuple[Any, Any, Any]]:
         """
         Scan for the header line, which follows any @first lines.
         Return (delims, first_lines, i+1) or None
@@ -3123,7 +3108,13 @@ class FastAtRead:
             first_lines.append(line)
         return None  # pragma: no cover (defensive)
     #@+node:ekr.20180602103135.8: *3* fast_at.scan_lines
-    def scan_lines(self, comment_delims: Any, first_lines: Any, lines: Any, path: Any, start: Any) -> None:
+    def scan_lines(self,
+        comment_delims: tuple,
+        first_lines: list[str],
+        lines: list[str],
+        path: str,
+        start: int,
+    ) -> None:
         """Scan all lines of the file, creating vnodes."""
         #@+<< init scan_lines >>
         #@+node:ekr.20180602103135.9: *4* << init scan_lines >>
@@ -3151,7 +3142,7 @@ class FastAtRead:
         sentinel = comment_delim1 + '@'
 
         # at+others, at+<section>, and at+all update the stack.
-        stack: list[tuple[str, int, str]] = []  # Entries are (gnx, indent, body)
+        stack: list[tuple[str, int, list[str]]] = []  # Entries are (gnx, indent, body)
         verbatim_line = comment_delim1 + '@verbatim' + comment_delim2
         verbatim = False  # True: the next line must be added without change.
 
@@ -3626,6 +3617,22 @@ class FastAtRead:
         self.scan_lines(comment_delims, first_lines, lines, path, start_i)
         return True
     #@-others
+#@+node:ekr.20240410111658.3: ** class LeoIOStatus
+class LeoIOStatus:
+    """A class representing the status of a Leo file operation"""
+    at_comment_seen = False
+    at_delims_seen = False
+    at_warning_given = False
+    has_at_others = False
+    in_code = True
+
+    def __repr__(self) -> str:  # pragma: no cover (testing)
+        return (
+            f"code? {int(self.in_code)} "
+            # f"comment? {int(self.at_comment_seen)} "
+            # f"delims? {int(self.at_delims_seen)} "
+            # f"@others? {int(self.has_at_others)} "
+        )
 #@-others
 #@@language python
 #@@tabwidth -4
