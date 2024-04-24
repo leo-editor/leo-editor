24c3e527d75f76326b79bbc7915d61a5553a78e7
Edward K. Ream <edreamleo@gmail.com>
2024-04-24 10:46:55 -0500

    Use popen. Advantage: no need for quotes

--- leo/commands/commanderFileCommands.py
+++ leo/commands/commanderFileCommands.py
@@ -6,6 +6,7 @@
 from __future__ import annotations
 import os
 import sys
+import subprocess
 import time
 from typing import TYPE_CHECKING
 from leo.core import leoGlobals as g
@@ -123,23 +124,31 @@ def restartLeo(self: Self, event: LeoKeyEvent = None) -> None:
     sys.stdout.flush()
     sys.stderr.flush()
     # Create the command to restart Leo.
-    leo_editor_dir = os.path.normpath(os.path.join(g.app.loadDir, '..', '..'))
-    launchLeo_s = fr'"{leo_editor_dir}{os.sep}launchLeo.py"'
-    quoted_paths = [f'"{z}"' for z in restart_paths]
-    args = ['unused-program-name', launchLeo_s] + quoted_paths + ['--no-splash']
     executable_s = f'"{sys.executable}"'
-    args_s = '\n  ' + '\n  '.join(args)
-    command = fr"  {executable_s}{args_s}"
-    # Python 3.9 does not allow newlines within f-strings.
-    print(
-        '\n'
-        f"Restarting Leo with this command:"
-        '\n\n'
-        f"{command}"
-        '\n'
-    )
-    print('Note: You may have to hit <return> to continue!')
-    os.execv(sys.executable, args)
+    leo_editor_dir = os.path.normpath(os.path.join(g.app.loadDir, '..', '..'))
+
+    print('\nRestarting Leo with this command:\n')
+    return_message = '\n' + 'Note: You may have to hit <return> to continue!'
+
+    # Warning: Python 3.9 does not allow newlines within f-strings.
+    if 1:  # Use Popen.
+        launchLeo_s = fr'{leo_editor_dir}{os.sep}launchLeo.py'
+        popen_args = [sys.executable, launchLeo_s] + restart_paths + ['--no-splash']
+        popen_args_s = '\n  ' + '\n  '.join(popen_args)
+        popen_command_s = fr"  sys.executable {popen_args_s} --no-splash"
+        print(popen_command_s)
+        print(return_message)
+        subprocess.Popen(popen_args)
+        sys.exit()
+    else:
+        quoted_paths = [f'"{z}"' for z in restart_paths]
+        launchLeo_s = fr'"{leo_editor_dir}{os.sep}launchLeo.py"'
+        execv_args = ['unused-program-name', launchLeo_s] + quoted_paths + ['--no-splash']
+        execv_args_s = '\n  ' + '\n  '.join(execv_args)
+        execv_command_s = fr"  {executable_s}{execv_args_s}"
+        print(execv_command_s)
+        print(return_message)
+        os.execv(sys.executable, execv_args)
 #@+node:ekr.20031218072017.2820: ** c_file.top level
 #@+node:ekr.20031218072017.2833: *3* c_file.close
 @g.commander_command('close-window')
