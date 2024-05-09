e29450bb3f6565eed4749a2f7fb54bab9fabd292
X Caminhante <xcaminhante@i2pmail.org>
2024-05-09 18:43:10 -0300

    Oops

--- leo/plugins/stickynotes.py
+++ leo/plugins/stickynotes.py
@@ -1,5 +1,5 @@
 #@+leo-ver=5-thin
-#@+node:ekr.20100103093121.5329: * @file /home/user/bin/leo-editor/leo/plugins/stickynotes.py
+#@+node:ekr.20100103093121.5329: * @file ../plugins/stickynotes.py
 #@+<< docstring >>
 #@+node:vivainio2.20091008133028.5821: ** << docstring >>
 """ Adds simple "sticky notes" feature (popout editors) for Qt gui.
