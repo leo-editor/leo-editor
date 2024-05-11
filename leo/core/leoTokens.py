a244fc7698030ff24ac9773bdaaa963e0f4145b8
Edward K. Ream <edreamleo@gmail.com>
2024-05-11 06:05:47 -0500

    Improve tab-related message

--- leo/core/leoTokens.py
+++ leo/core/leoTokens.py
@@ -1045,10 +1045,10 @@ class TokenBasedOrange:  # Orange is the new Black.
         """Handle indent token."""
 
         # Only warn about indentation errors.
-        if (
-            '\t' in self.input_token.value or
-            (len(self.input_token.value) % self.tab_width) != 0
-        ):  # pragma: no cover
+        if '\t' in self.input_token.value:  # pragma: no cover
+            print(f"Found tab character in {self.filename}")
+            print(self.consider_message)
+        elif (len(self.input_token.value) % self.tab_width) != 0:  # pragma: no cover
             print(f"Indentation error in {self.filename}")
             print(self.consider_message)
 
