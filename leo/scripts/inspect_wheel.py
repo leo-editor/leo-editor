28b2dd5f3c5ccabb8b86ad45f49ab9a0b6954118
Edward K. Ream <edreamleo@gmail.com>
2024-06-16 13:25:27 -0500

    Adjust two scripts

--- leo/scripts/inspect_wheel.py
+++ leo/scripts/inspect_wheel.py
@@ -6,6 +6,8 @@
 inspect_wheel.py: Inspect the metadata of wheel files.
                   Output goes to inspect_wheel.txt.
 
+`pip install wheel-inspect' is not part of Leo's requirements.
+
 See info item #3837 for full documentation.
 https://github.com/leo-editor/leo-editor/issues/3837
 """
@@ -16,9 +18,14 @@ import subprocess
 print(os.path.basename(__file__))
 
 # cd to leo-editor
-os.chdir(os.path.abspath(os.path.join(__file__, '..', '..', '..')))
+### os.chdir(os.path.abspath(os.path.join(__file__, '..', '..', '..')))
+leo_editor_dir = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
+assert leo_editor_dir.endswith('leo-editor'), repr(leo_editor_dir)
+assert os.path.exists(leo_editor_dir), repr(leo_editor_dir)
+assert os.path.isdir(leo_editor_dir), repr(leo_editor_dir)
+os.chdir(leo_editor_dir)
 
-command = 'python -m wheel_inspect dist\leo-6.7.9a1-py3-none-any.whl >inspect_wheel.txt'
+command = r'python -m wheel_inspect dist\leo-6.8.0b1-py3-none-any.whl >inspect_wheel.txt'
 print(command)
 subprocess.Popen(command, shell=True).communicate()
 
