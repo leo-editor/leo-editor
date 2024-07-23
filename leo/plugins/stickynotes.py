2dcaa1e70fdffdb8d93ef0e391d07aa391ecf362
Edward K. Ream <edreamleo@gmail.com>
2024-07-23 04:10:39 -0500

    Suppress all mypy complaints

--- leo/plugins/stickynotes.py
+++ leo/plugins/stickynotes.py
@@ -1,5 +1,6 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20100103093121.5329: * @file ../plugins/stickynotes.py
+# type:ignore
 #@+<< docstring >>
 #@+node:vivainio2.20091008133028.5821: ** << docstring >>
 """ Adds simple "sticky notes" feature (popout editors) for Qt gui.
