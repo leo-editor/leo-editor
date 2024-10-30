349871f16df3c62fbbee63b00b321a2e4b521da6
Edward K. Ream <edreamleo@gmail.com>
2024-10-30 15:57:01 -0500

    Create leo/modes/notebook.py

---
+++ leo/modes/notebook.py
@@ -0,0 +1,14 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20241030151621.1: * @file ../modes/notebook.py
+#@@language python
+# Leo colorizer control file for @language notebook.
+# This file is in the public domain.
+
+# This colorizer must switch between markdown and python colorizing.
+
+# Properties for notebook mode.
+properties = {
+    # "indentNextLines": "\\s*[^#]{3,}:\\s*(#.*)?",
+    # "lineComment": "#",
+}
+#@-leo
