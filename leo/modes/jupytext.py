b3701eb50c7f5399366007b54726e699a4d94f3e
Edward K. Ream <edreamleo@gmail.com>
2024-11-01 03:53:38 -0500

    Eliminate comment_helper!

--- leo/modes/jupytext.py
+++ leo/modes/jupytext.py
@@ -9,30 +9,10 @@ This file is in the public domain.
 #@+<< jupytext.py: imports >>
 #@+node:ekr.20241031140333.1: ** << jupytext.py: imports >>
 from __future__ import annotations
-import string
-from typing import Any
 
 from leo.core import leoGlobals as g
 assert g
 #@-<< jupytext.py: imports >>
-global_state = '??'
-#@+<< jupytext.py: global data >>
-#@+node:ekr.20241031140131.1: ** << jupytext.py: global data >>
-delegate_dict = {
-    'md': 'md:md_main',
-    'py': 'python:python_main',
-    '??': '',
-}
-marker_dict = {
-    'md': '# %% [markdown]',
-    'py': '# %%',
-    '??': '',
-}
-markup_table = (
-    ('md', '# %% [markdown]'),
-    ('py', '# %%'),
-)
-#@-<< jupytext.py: global data >>
 #@+<< jupytext.py: rules >>
 #@+node:ekr.20241031024909.1: ** << jupytext.py: rules >>
 
@@ -41,30 +21,6 @@ markup_table = (
 # n < 0: total failure, skip n chars.
 
 #@+others
-#@+node:ekr.20241031043109.1: *3* comment_helper
-# This is a helper, not a rule!
-
-def predicate(s: str) -> bool:
-    return s.strip().startswith('# %%)')  # Also matches '# %% [markdown]'.
-
-def comment_helper(colorer: Any, s: str, i: int) -> int:
-    """Continue coloring."""
-    global global_state
-
-    if 1:  ###
-        print('')
-        g.trace(global_state, i, s)
-
-    # Colorize *this* line.
-    colorer.match_line(s, i, kind='comment1')
-
-    # Continue colorizing on the *next* line.
-    colorer.match_span_delegated_lines(s, i,
-        delegate=delegate_dict.get(global_state),
-        predicate=predicate)
-
-    # We have completely colorized *this* line.
-    return -1
 #@+node:ekr.20241031024939.2: *3* jupytext_comment
 def jupytext_comment(colorer, s, i) -> int:
     """
@@ -73,20 +29,27 @@ def jupytext_comment(colorer, s, i) -> int:
     Return: n > 1 if n characters match, otherwise -1.
     """
     assert s[i] == '#'
-    global global_state
-    # Check for the next target.
-    line = s.strip()
-    if True:  ### global_state == '??':
-        for (state, target) in markup_table:
-            if line.startswith(target):
-                # Switch to a new state.
-                global_state = state
-                g.trace('   NEW STATE', global_state, line)  ###
-                colorer.clearState()
-                return comment_helper(colorer, s, i)
-    # This is not an error.
-    return comment_helper(colorer, s, i)
+    marker = '# %%'  # Matches the start of Python and Markdown modes.
 
+    # Colorize *this* line.
+    colorer.match_line(s, i, kind='comment1')
+
+    line = s.strip()
+
+    if line.startswith(marker):
+        # Colorize the *next* lines until the predicate matches.
+
+        def predicate(s: str) -> bool:
+            return s.strip().startswith(marker)
+
+        delegate = (
+            'md:md_main' if line.startswith('# %% [markdown]')
+            else 'python:python_main'
+        )
+        colorer.match_span_delegated_lines(s, i,
+            delegate=delegate, predicate=predicate)
+
+    return -1  # This line has been completely handled.
 #@+node:ekr.20241031024936.1: *3* jupytext_keyword
 def jupytext_keyword(colorer, s, i):
     return colorer.match_keywords(s, i)
