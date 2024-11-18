875fc7c0e39b1a0368d478e7e5c9604bf3c50d06
Edward K. Ream <edreamleo@gmail.com>
2024-11-18 15:55:09 -0600

    Add PyQt6.QtWidgets to the list of extensions

--- leo/scripts/pylint_leo.py
+++ leo/scripts/pylint_leo.py
@@ -25,7 +25,8 @@ python = 'py' if isWindows else 'python'
 rc_file_name = PylintCommand(c=None).get_rc_file()  # #4191.
 rc_file = rf"--rcfile {rc_file_name}"
 # print(__file__, rc_file)
-extension_pkg = '--extension-pkg-allow-list=PyQt6.QtCore,PyQt6.QtGui,PyQt6.QtWidgets'
+extensions = 'PyQt6.QtCore,PyQt6.QtGui,PyQt6.QtWidgets,PyQt6.QtWebEngineWidgets'
+extension_pkg = f"--extension-pkg-allow-list={extensions}"
 command = fr"{python} -m pylint leo {rc_file} {extension_pkg}"
 
 subprocess.Popen(command, shell=True).communicate()
