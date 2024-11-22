d2743a5308a61cb1d25caa0401db420c23415d3a
Edward K. Ream <edreamleo@gmail.com>
2024-11-22 09:13:23 -0600

    Remove unused rules and dicts

--- leo/modes/html.py
+++ leo/modes/html.py
@@ -10,16 +10,14 @@ from typing import Any
 from leo.core import leoGlobals as g
 assert g
 #@-<< html.py: imports >>
-#@+<< html.py: rules >>
-#@+node:ekr.20241120012017.1: ** << html.py: rules >>
+#@+others  # Rules
+#@+node:ekr.20230419051223.1: ** main ruleset
 #@+others
-#@+node:ekr.20230419051223.1: *3* main ruleset
-#@+others
-#@+node:ekr.20230419050050.1: *4* html_rule_comment <!--..-->
+#@+node:ekr.20230419050050.1: *3* html_rule_comment <!--..-->
 def html_rule_comment(colorer, s, i):
     return colorer.match_span(s, i, kind="comment1", begin="<!--", end="-->")
 
-#@+node:ekr.20230419050050.2: *4* html_rule_script <script..</script>
+#@+node:ekr.20230419050050.2: *3* html_rule_script <script..</script>
 def html_rule_script(colorer: Any, s: str, i: int) -> int:
 
     if i != 0 or not s.startswith("<script"):
@@ -34,7 +32,7 @@ def html_rule_script(colorer: Any, s: str, i: int) -> int:
 
 
 
-#@+node:ekr.20230419050050.4: *4* html_rule_style <style..</style>
+#@+node:ekr.20230419050050.4: *3* html_rule_style <style..</style>
 def html_rule_style(colorer, s, i):
 
     if i != 0 or not s.startswith("<style"):
@@ -46,31 +44,31 @@ def html_rule_style(colorer, s, i):
     # Start css mode.
     colorer.push_delegate('css')
     return len(s)  # Success.
-#@+node:ekr.20230419050050.6: *4* html_rule3 <!..>
+#@+node:ekr.20230419050050.6: *3* html_rule3 <!..>
 def html_rule3(colorer, s, i):
     return colorer.match_span(s, i, kind="keyword2", begin="<!", end=">")
 
-#@+node:ekr.20230419050050.7: *4* html_rule4 <..>
+#@+node:ekr.20230419050050.7: *3* html_rule4 <..>
 def html_rule4(colorer, s, i):
     return colorer.match_span(s, i, kind="markup", begin="<", end=">",
         delegate="html::tags")
 
-#@+node:ekr.20230419050050.8: *4* html_rule5 &..;
+#@+node:ekr.20230419050050.8: *3* html_rule5 &..;
 def html_rule5(colorer, s, i):
     return colorer.match_span(s, i, kind="literal2", begin="&", end=";",
         no_word_break=True)
 
-#@+node:ekr.20230419050050.9: *4* html_rule_handlebar {{..}}
+#@+node:ekr.20230419050050.9: *3* html_rule_handlebar {{..}}
 # New rule for handlebar markup, colored with the literal3 color.
 def html_rule_handlebar(colorer, s, i):
     return colorer.match_span(s, i, kind="literal3", begin="{{", end="}}")
-#@+node:ekr.20241121083510.1: *4* html_rule_at_language @language
+#@+node:ekr.20241121083510.1: *3* html_rule_at_language @language
 def html_rule_at_language(colorer, s, i):
 
     if i == 0 and s.startswith("@language "):
         return colorer.match_at_language(s, i)
     return 0  # Fail, but allow other matches.
-#@+node:ekr.20241120172252.1: *4* html_rule_end_template </template>
+#@+node:ekr.20241120172252.1: *3* html_rule_end_template </template>
 def html_rule_end_template(colorer, s, i):
 
     if i != 0 or not s.startswith("</template>"):
@@ -84,7 +82,7 @@ def html_rule_end_template(colorer, s, i):
     return len(s)  # Success.
 #@-others
 
-#@+node:ekr.20230419050351.1: *3* html_tags ruleset
+#@+node:ekr.20230419050351.1: ** html_tags ruleset
 # Rules for html_tags ruleset.
 
 def html_rule6(colorer, s, i):
@@ -95,31 +93,7 @@ def html_rule7(colorer, s, i):
 
 def html_rule8(colorer, s, i):
     return colorer.match_plain_seq(s, i, kind="operator", seq="=")
-#@+node:ekr.20230419050529.1: *3* html_javascript ruleset (to be removed?)
-# Rules for html_javascript ruleset.
-
-def html_rule9(colorer, s, i):
-    return colorer.match_seq(s, i, kind="markup", seq=">",
-        delegate="javascript::main")
-
-def html_rule10(colorer, s, i):
-    return colorer.match_seq(s, i, kind="markup", seq="SRC=",
-        delegate="html::back_to_html")
-#@+node:ekr.20230419050552.1: *3* back_to_html ruleset (to be removed?)
-# Rules for html_back_to_html ruleset.
-
-def html_rule11(colorer, s, i):
-    return colorer.match_seq(s, i, kind="markup", seq=">",
-        delegate="html::main")
-
-#@+node:ekr.20230419051037.1: *3* html_css ruleset (to be removed?)
-# Rules for html_css ruleset.
-
-def html_rule12(colorer, s, i):
-    return colorer.match_seq(s, i, kind="markup", seq=">",
-        delegate="css::main")
 #@-others
-#@-<< html.py: rules >>
 #@+<< html.py: dictionaries >>
 #@+node:ekr.20241120012038.1: ** << html.py: dictionaries >>
 #@+others
@@ -235,24 +209,6 @@ rulesDict2 = {
     "'": [html_rule7],
     "=": [html_rule8],
 }
-
-# Rules dict for html_javascript ruleset.
-rulesDict3 = {
-    ">": [html_rule9],
-    "S": [html_rule10],
-}
-
-# Rules dict for html_back_to_html ruleset.
-rulesDict4 = {
-    ">": [html_rule11],
-}
-
-# Rules dict for html_css ruleset.
-rulesDict5 = {
-    ">": [html_rule12],
-}
-
-
 #@-others
 
 # Import dict for html mode.
