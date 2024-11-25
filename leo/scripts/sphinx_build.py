1d161a5a1073347485eaadbf4b96df4395439362
Edward K. Ream <edreamleo@gmail.com>
2024-11-25 11:20:11 -0600

    A milestone: sphinx_build.py works when called from EKR's sphinx-build.cmd

--- leo/scripts/sphinx_build.py
+++ leo/scripts/sphinx_build.py
@@ -4,20 +4,21 @@ leo/scripts/sphinx_build.py.
 
 Invoke python/scripts/sphinx_build.exe
 """
-g.cls()  ###
 import os
 import subprocess
 import sys
-from leo.core import leoGlobals as g
 
-python_exe = sys.executable
+# Find python/Scripts/sphinx-build.exe.
 python_folder = os.path.dirname(sys.executable)
-# script = os.path.normpath(os.path.join(python_folder, 'Scripts', 'sphinx-build.exe'))
-script = g.os_path_finalize_join(python_folder, 'Scripts', 'sphinx-build.exe')
+script = os.path.normpath(os.path.join(
+    python_folder, 'Scripts', 'sphinx-build.exe'))
 assert os.path.exists(script), repr(script)
 
-args = sys.argv
-script_s = f'"{script}"'
+# Create a command that executes python/Scripts/sphinx-build.exe.
+args_s = ' '.join(sys.argv[1:])
+script_s = f'"{script}" {args_s}'
 command = fr"python {script_s}"
-print(command, args)
-### subprocess.Popen(command, shell=True).communicate()
+
+# Print and execute the command!
+print(f"sphinx_build.py: {command}\n")
+subprocess.Popen(command, shell=True).communicate()
