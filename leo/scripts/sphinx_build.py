65f2bcec82f29a0bb8b4b9a051eaa9be3274db36
Edward K. Ream <edreamleo@gmail.com>
2024-11-25 12:27:47 -0600

    Improve docstring and error checking

--- leo/scripts/sphinx_build.py
+++ leo/scripts/sphinx_build.py
@@ -3,7 +3,11 @@
 leo/scripts/sphinx_build.py.
 
 Invoke python/scripts/sphinx_build.exe
+
+Note: This straightforward script works on EKR's Windows 11 machine.
+      There is *no* guarantee that it will work elsewhere.
 """
+
 import os
 import subprocess
 import sys
@@ -12,13 +16,18 @@ import sys
 python_folder = os.path.dirname(sys.executable)
 script = os.path.normpath(os.path.join(
     python_folder, 'Scripts', 'sphinx-build.exe'))
-assert os.path.exists(script), repr(script)
 
-# Create a command that executes python/Scripts/sphinx-build.exe.
-args_s = ' '.join(sys.argv[1:])
-script_s = f'"{script}" {args_s}'
-command = fr"python {script_s}"
 
-# Print and execute the command!
-print(f"sphinx_build.py: {command}\n")
-subprocess.Popen(command, shell=True).communicate()
+if os.path.exists(script):
+    # Create a command that executes python/Scripts/sphinx-build.exe.
+    args_s = ' '.join(sys.argv[1:])
+    script_s = f'"{script}" {args_s}'
+    command = fr"python {script_s}"
+    # Print and execute the command!
+    print(f"sphinx_build.py: {command}\n")
+    subprocess.Popen(command, shell=True).communicate()
+else:
+    print('')
+    print(f"Not found: {script!r}")
+    print('pip install sphinx')
+    print('')
