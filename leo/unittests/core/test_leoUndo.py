3a5b42769f0dfa228fb0f8974500c1275283ab9e
Edward K. Ream <edreamleo@gmail.com>
2025-02-04 06:59:15 -0600

    Disable no-member test

--- leo/unittests/core/test_leoUndo.py
+++ leo/unittests/core/test_leoUndo.py
@@ -1,7 +1,7 @@
 #@+leo-ver=5-thin
 #@+node:ekr.20210906141410.1: * @file ../unittests/core/test_leoUndo.py
 """Tests of leoUndo.py"""
-
+# pylint: disable=no-member
 from leo.core import leoGlobals as g
 from leo.core.leoTest2 import LeoUnitTest
 assert g
