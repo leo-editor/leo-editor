590db1ae272afc37699443edb8f88a75e87a44ba
Edward K. Ream <edreamleo@gmail.com>
2025-03-24 05:55:44 -0500

    Improve annotation of g.objToString

--- leo/core/leoGlobals.py
+++ leo/core/leoGlobals.py
@@ -2250,7 +2250,7 @@ def pdb(message: str = '') -> None:
     breakpoint()  # New in Python 3.7.
 #@+node:ekr.20050819064157: *4* g.objToString & aliases
 def objToString(
-    obj: Any,
+    obj: object,
     *,
     indent: int = 0,
     tag: str = None,
