b11965b919d0c221f1e0a2f39588830d5be1e959
Edward K. Ream <edreamleo@gmail.com>
2025-03-27 06:12:46 -0500

    Replace 'Any' with 'Value'

--- leo/core/leoFind.py
+++ leo/core/leoFind.py
@@ -25,6 +25,7 @@ if TYPE_CHECKING:  # pragma: no cover
     MatchGroups = tuple  # Best we can do so far.
     Settings = g.Bunch
     UndoData = g.Bunch
+    Value = Any
 #@-<< leoFind imports & annotations >>
 #@+<< Theory of operation of find/change >>
 #@+node:ekr.20031218072017.2414: ** << Theory of operation of find/change >>
@@ -1583,7 +1584,7 @@ class LeoFind:
         c.widgetWantsFocusNow(w)
         self.do_change_all(settings)  # Correct: convert to change-all.
     #@+node:ekr.20031218072017.3073: *5* find.do_find_all & helpers
-    def do_find_all(self, settings: Settings) -> dict[str, Any]:
+    def do_find_all(self, settings: Settings) -> dict[str, Value]:
         """
         Top-level helper for find-all command.
 
@@ -1612,7 +1613,7 @@ class LeoFind:
         self.node_only = self.suboutline_only = False
         return result_dict
     #@+node:ekr.20160422073500.1: *6* find._find_all_helper & helpers
-    def _find_all_helper(self, settings: Settings) -> dict[str, Any]:
+    def _find_all_helper(self, settings: Settings) -> dict[str, Value]:
         """
         Handle the find-all command from p to after.
 
