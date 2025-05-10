dc2e1fa66c3be505804e7fd197164f2c1c174fbc
Edward K. Ream <edreamleo@gmail.com>
2025-05-10 17:38:59 -0500

    Fix typo in upload script

--- leo/scripts/upload_leo_to_testpypi.py
+++ leo/scripts/upload_leo_to_testpypi.py
@@ -21,7 +21,7 @@ assert os.path.exists(leo_editor_dir), repr(leo_editor_dir)
 assert os.path.isdir(leo_editor_dir), repr(leo_editor_dir)
 os.chdir(leo_editor_dir)
 
-command = 'python -m twine upload -r testpypi dist/*.* --vebose'
+command = 'python -m twine upload -r testpypi dist/*.* --verbose'
 
 # Upload
 if 1:  # Don't do this until we are ready to release.
