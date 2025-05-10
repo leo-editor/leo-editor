ba73c75bf4efcc7d0aa38db4b841c5a3e6f30fba
Edward K. Ream <edreamleo@gmail.com>
2025-05-10 17:32:20 -0500

    Add --verbose to upload scripts:

--- leo/scripts/upload_leo_to_pypi.py
+++ leo/scripts/upload_leo_to_pypi.py
@@ -21,7 +21,7 @@ assert os.path.exists(leo_editor_dir), repr(leo_editor_dir)
 assert os.path.isdir(leo_editor_dir), repr(leo_editor_dir)
 os.chdir(leo_editor_dir)
 
-command = 'python -m twine upload -r pypi dist/*.*'
+command = 'python -m twine upload -r pypi dist/*.* --verbose'
 
 # Upload.
 if 1:  # Don't do this until we are ready to release.
