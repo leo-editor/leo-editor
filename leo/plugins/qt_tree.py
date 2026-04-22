cef2c3506cd4b2e619ed3978f194c1675615a6e6
Edward K. Ream <edreamleo@gmail.com>
2026-04-22 10:06:25 -0500

    Add back a crucial assignment

--- leo/plugins/qt_tree.py
+++ leo/plugins/qt_tree.py
@@ -66,6 +66,7 @@ class LeoQtTree(leoFrame.LeoTree):
         self.editWidgetsDict: dict[QLineEdit, QHeadlineWrapper] = {}
         self.reloadSettings()
         # Components...
+        self.canvas = self  # An official ivar used by Leo's core.  ###
         self.treeWidget: LeoQTreeWidget = frame.top.treeWidget
         w = self.treeWidget
         # Declutter data...
