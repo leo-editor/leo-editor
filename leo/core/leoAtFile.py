0b1299924619bb37972c8466fd79f533904019d8
Edward K. Ream <edreamleo@gmail.com>
2026-06-06 04:40:19 -0500

    Simplify at.warnAboutOrphandAndIgnoredNodes

--- leo/core/leoAtFile.py
+++ leo/core/leoAtFile.py
@@ -3240,25 +3240,19 @@ class AtFile:
     def warnAboutOrphandAndIgnoredNodes(self) -> None:  # pragma: no cover
         # Always warn, even when language=="cweb"
         at, root = self, self.root
+        message = 'The node is included neither by an @others directive nor a section reference'
         if at.errors:
             return  # No need to repeat this.
-        for p in root.self_and_subtree(copy=False):
+        if root.isAtAllNode():  # #4729.
+            return
+        if root.isOrphan():  # #4729.
+            return
+        for p in root.subtree(copy=False):
             if not p.v.isVisited():
                 at.writeError("Orphan node:  " + p.h)
+                g.es_print_unique_message(message)
                 if p.hasParent():
                     g.blue("parent node:", p.parent().h)
-        p = root.copy()
-        after = p.nodeAfterTree()
-        while p and p != after:
-            if p.isAtAllNode():
-                p.moveToNodeAfterTree()
-            else:
-                # #1050: test orphan bit.
-                if p.isOrphan():
-                    at.writeError("Orphan node: " + p.h)
-                    if p.hasParent():
-                        g.blue("parent node:", p.parent().h)
-                p.moveToThreadNext()
 
     # @+node:ekr.20041005105605.217: *5* at.writeError
     def writeError(self, message: str) -> None:  # pragma: no cover
