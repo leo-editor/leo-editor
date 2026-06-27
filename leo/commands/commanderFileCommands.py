bfb0ed2d4b1156013582c2714e4a5bd48bac5e48
Edward K. Ream <edreamleo@gmail.com>
2026-06-26 20:39:34 -0500

    Reformat one file

--- leo/commands/commanderFileCommands.py
+++ leo/commands/commanderFileCommands.py
@@ -429,7 +429,7 @@ def refreshFromDisk(
     at.changed_roots = []
     c.fileCommands.handleNodeConflicts()
     c.setChanged()
-    # We may be coming from idle_check_commander and not already have p selected, 
+    # We may be coming from idle_check_commander and not already have p selected,
     # if the selected position was inside the expanded & refreshed outline, it may not be valid.
     c.redraw(update_p)  # Select a valid update_p position or p
     c.undoer.clearAndWarn('refresh-from-disk')
