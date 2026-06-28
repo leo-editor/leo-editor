68bd11e4ac48ba4acefae0d1a99825c2b1200c15
Edward K. Ream <edreamleo@gmail.com>
2026-06-28 09:41:26 -0500

    Getting Things Done

--- leo/scripts/mypy_leo.py
+++ leo/scripts/mypy_leo.py
@@ -32,10 +32,10 @@ else:  # Test only specific files.
     files = [
         'leo/core/leoApp.py',
         'leo/core/leoGlobals.py',
-        'leo/core/leoNodes.py',
-        'leo/core/leoCommands.py',
-        'leo/core/leoAtFile.py',
-        'leo/core/leoKeys.py',
+        # 'leo/core/leoNodes.py',
+        # 'leo/core/leoCommands.py',
+        # 'leo/core/leoAtFile.py',
+        # 'leo/core/leoKeys.py',
     ]
 # Apparently 'strict-optional' doesn't work.
 incremental = False
