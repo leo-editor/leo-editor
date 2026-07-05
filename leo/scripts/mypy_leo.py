a94c91c8cbc631ab0d8af54818b24ef68653539d
Edward K. Ream <edreamleo@gmail.com>
2026-07-05 06:42:25 -0500

    Fix all annotation problems in abbrevCommand.py

--- leo/scripts/mypy_leo.py
+++ leo/scripts/mypy_leo.py
@@ -25,33 +25,38 @@ os.chdir(leo_editor_dir)
 # @-<< mypy_leo.py: imports & startup >>
 incremental = False
 follow = False
-files = [
-    # 'leo',
+if 0:
+    files = [
+        'leo',
+    ]  # Test all files.
+else:
+    files = [
+        # Files to check with strict_optional in .mypy.ini.
+        'leo/core/leoGlobals.py',
+        'leo/core/leoCommands.py',
+        'leo/core/leoNodes.py',
 
-    # Files to check with strict_optional in .mypy.ini.
-    'leo/core/leoGlobals.py',
-    'leo/core/leoCommands.py',
-    'leo/core/leoNodes.py',
-    
-    # Follow-on files, with errors uncovered by checking leoCommands.py.
-    'leo/core/leoExternalFiles.py',
-    'leo/core/leoserver.py',
+        # Follow-on files, with errors uncovered by checking leoCommands.py.
+        'leo/commands/abbrevCommands.py',
+        'leo/core/leoExternalFiles.py',
+        'leo/core/leoserver.py',
 
-    # To be checked in other PRs...
-    #   'leo/core/leoApp.py',
-    #   'leo/core/leoAtFile.py',
-    #   'leo/core/leoKeys.py',
-    #   'leo/plugins/mod_scripting.py',
-    #   'leo/plugins/qt_commands.py',
-    #   'leo/plugins/qt_frame.py',
-    #   'leo/plugins/qt_gui.py',
-    #   'leo/plugins/qt_idle_time.py',
-    #   'leo/plugins/qt_layout.py',
-    #   'leo/plugins/qt_text.py',
-    #   'leo/plugins/qt_tree.py',
-    # 'leo/plugins/viewrendered.py',
-    #   'leo/plugins/viewrendered3.py',
-]  # fmt: skip
+        # To be checked in other PRs...
+        #   'leo/core/leoApp.py',
+        #   'leo/core/leoAtFile.py',
+        #   'leo/core/leoKeys.py',
+        #   'leo/plugins/mod_scripting.py',
+        #   'leo/plugins/qt_commands.py',
+        #   'leo/plugins/qt_frame.py',
+        #   'leo/plugins/qt_gui.py',
+        #   'leo/plugins/qt_idle_time.py',
+        #   'leo/plugins/qt_layout.py',
+        #   'leo/plugins/qt_text.py',
+        #   'leo/plugins/qt_tree.py',
+        # 'leo/plugins/viewrendered.py',
+        #   'leo/plugins/viewrendered3.py',
+    ]  # fmt: skip
+
 python = sys.executable
 incremental_arg = '' if incremental else '--no-incremental'
 follow_kind = 'normal' if follow else 'skip'
