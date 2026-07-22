5799c703aeb48ffbaeb0260a3dea16c7533ff762
Edward K. Ream <edreamleo@gmail.com>
2026-07-22 11:59:03 -0500

    Fix leoMarkup.py

--- leo/core/leoMarkup.py
+++ leo/core/leoMarkup.py
@@ -259,7 +259,7 @@ class MarkupCommands:
 
     def __init__(self, c: Cmdr) -> None:
         self.c = c
-        self.kind: str | None = None  # 'adoc' or 'pandoc'
+        self.kind: str = ''  # 'adoc' or 'pandoc'
         self.level_offset = 0
         self.root_level = 0
         self.reload_settings()
@@ -342,7 +342,7 @@ class MarkupCommands:
     # @+node:ekr.20190515084219.1: *4* markup.filename
     adoc_pattern = re.compile(r'^@(adoc|asciidoctor)')
 
-    def filename(self, p: Position) -> str | None:
+    def filename(self, p: Position) -> str:
         """Return the filename of the @adoc, @pandoc or @sphinx node, or None."""
         kind = self.kind
         h = p.h.rstrip()
@@ -350,14 +350,14 @@ class MarkupCommands:
             if m := self.adoc_pattern.match(h):
                 prefix = m.group(1)
                 return h[1 + len(prefix) :].strip()
-            return None
+            return ''
         if kind in ('pandoc', 'sphinx'):
             prefix = f"@{kind}"
             if g.match_word(h, 0, prefix):
                 return h[len(prefix) :].strip()
-            return None
+            return ''
         g.trace('BAD KIND', kind)
-        return None
+        return ''
 
     # @+node:ekr.20191007053522.1: *4* markup.compute_opath
     def compute_opath(self, i_path: str) -> str:
