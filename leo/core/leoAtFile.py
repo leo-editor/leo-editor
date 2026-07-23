0012773b0dc7dcf85c7abcfcc4baebea628cb41e
Edward K. Ream <edreamleo@gmail.com>
2026-07-23 05:56:36 -0500

    Enshrine the big little lie

--- leo/core/leoAtFile.py
+++ leo/core/leoAtFile.py
@@ -3750,7 +3750,7 @@ class FastAtRead:
                     continue  # End of case 1.
 
                 # Case 2: We are scanning the descendants of a clone.
-                parent_v, clone_v = level_stack[level - 2]
+                parent_v, clone_v = level_stack[level - 2]  # type:ignore # The big little lie.
                 if v and clone_v:
                     # The last version of the body and headline wins..
                     gnx2body[gnx] = body = []
