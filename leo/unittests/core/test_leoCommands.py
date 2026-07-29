346fe37803fe1ebc67cbff687b21bec96dfbcde8
Edward K. Ream <edreamleo@gmail.com>
2026-07-29 15:04:32 -0500

    Remove first suppression

--- leo/unittests/core/test_leoCommands.py
+++ leo/unittests/core/test_leoCommands.py
@@ -212,7 +212,7 @@ class TestCommands(LeoUnitTest):
         c = self.c
         abs_base = '/leo_base'
         c.mFileName = f"{abs_base}/test.leo"
-        os.environ = {
+        os.environ = {  # noqa  # Doesn't *clear* os.environ, but that's fine.
             'HOME': '/home',  # Linux.
             'USERPROFILE': r'c:\EKR',  # Windows.
             'LEO_BASE': abs_base,
