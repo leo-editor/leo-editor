1063364394436e15eae7cab3c5afed847beee1ea
Edward K. Ream <edreamleo@gmail.com>
2026-07-31 06:58:46 -0500

    Restore a 'useless' pass: it's the target of a comment

--- leo/core/leoBeautify.py
+++ leo/core/leoBeautify.py
@@ -205,6 +205,7 @@ def should_beautify(p: Position) -> bool:
                 return False
             # The ambiguous node has no effect.
             # Look up the tree.
+            pass  # noqa
         elif beautify:
             return True
         if no_beautify:
