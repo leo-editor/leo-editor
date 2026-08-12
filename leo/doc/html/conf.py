cb575c843b0d127f6cfed9b54b877483bff49f0c
Edward K. Ream <edreamleo@gmail.com>
2026-08-12 06:47:17 -0500

    Merge LeoDocs.leo into devel

--- leo/doc/html/conf.py
+++ leo/doc/html/conf.py
@@ -4,34 +4,43 @@
 # https://www.sphinx-doc.org/en/master/usage/theming.html#builtin-themes
 
 project = 'Leo'
+author = 'Edward K. Ream and contributors'
 copyright = '1997-2026, Edward K. Ream'
 version = '6.8.9'
 release = '6.8.9'
 
-html_theme = 'classic'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    'repository_url': 'https://github.com/leo-editor/leo-editor',
+    'use_issues_button': True,
+    'use_repository_button': True,
+    'home_page_in_toc': True,
+    'show_navbar_depth': 2,
+    # Everything in the navbar is already available in the primary sidebar.
+    # Leaving all slots empty also lets the theme bind the visible sidebar toggle.
+    'navbar_start': [],
+    'navbar_center': [],
+    'navbar_end': [],
+    'navbar_persistent': [],
+    'logo': {
+        'image_light': '../_static/LeoLogo.svg',
+        'image_dark': '../_static/LeoLogo-dark.svg',
+    },
+}
 pygments_style = 'sphinx'
 source_suffix = '.html.txt'
 
 html_last_updated_fmt = '%B %d, %Y'
-html_logo = '../_static/LeoLogo.svg'
+html_title = 'Leo'
 html_use_index = False  # Not necessary for glossary.
 html_use_smartypants = False
-master_doc = 'leo_toc'
+master_doc = 'index'
 
 # These folders are copied to the documentation's HTML output.
 html_static_path = ['../_static']  # 'screen-shots' aren't going to change.
 html_css_files = ['custom.css']  # Relative to html_static_paths.
 
 # Options...
-html_sidebars = {
-    '**': [
-        'relations.html',  # Enable links to the previous and next documents.
-        'searchbox.html',  # Enable “quick search” box.
-        # 'localtoc.html',  # Use a fine-grained table of contents.
-        # 'globaltoc.html,  # Use a coarse-grained table of contents.
-    ]
-}
-
 # extensions = []
 # templates_path = []
 # exclude_trees = []              # Don't search these for source files.
