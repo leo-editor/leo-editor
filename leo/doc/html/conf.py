d14eb0a34089b053d8b99903f9eae8aa6e6ac60c
Edward K. Ream <edreamleo@gmail.com>
2026-08-13 07:24:24 -0500

    Make release notes more visible. Remove one footer line

--- leo/doc/html/conf.py
+++ leo/doc/html/conf.py
@@ -4,7 +4,7 @@
 # https://www.sphinx-doc.org/en/master/usage/theming.html#builtin-themes
 
 project = 'Leo'
-author = 'Edward K. Ream and contributors'
+author = '' # Edward K. Ream and contributors'
 copyright = '1997-2026, Edward K. Ream'
 version = '6.8.10'
 release = '6.8.10'
