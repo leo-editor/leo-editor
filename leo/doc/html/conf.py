0e0e94bed269737b528d276c56cf1d4001edef1c
Edward K. Ream <edreamleo@gmail.com>
2026-08-13 05:04:39 -0500

    Restore flake8 suppression, adding comment.

--- leo/doc/html/conf.py
+++ leo/doc/html/conf.py
@@ -1,9 +1,10 @@
+# flake8: noqa  # Required for ci.yml.
 """leo/doc/html/conf.py"""
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 # https://www.sphinx-doc.org/en/master/usage/theming.html#builtin-themes
 
 project = 'Leo'
-author = '' # Edward K. Ream and contributors'
+author = 'Edward K. Ream and contributors'
 copyright = '1997-2026, Edward K. Ream'
 version = '6.8.10'
 release = '6.8.10'
