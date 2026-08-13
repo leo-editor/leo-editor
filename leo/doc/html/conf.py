508f88e1f41b348c1b8ad38ef27ee1b461c543df
Edward K. Ream <edreamleo@gmail.com>
2026-08-13 04:57:45 -0500

    Use 6.8.10 as the version, again

--- leo/doc/html/conf.py
+++ leo/doc/html/conf.py
@@ -6,8 +6,8 @@
 project = 'Leo'
 author = 'Edward K. Ream and contributors'
 copyright = '1997-2026, Edward K. Ream'
-version = '6.8.9'
-release = '6.8.9'
+version = '6.8.10'
+release = '6.8.10'
 
 html_theme = 'sphinx_book_theme'
 html_theme_options = {
