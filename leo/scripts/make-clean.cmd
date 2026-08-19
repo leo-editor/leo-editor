5e19c684dece6fd26f4ffad5c38abf1acb550eb0
Edward K. Ream <edreamleo@gmail.com>
2026-08-19 11:56:17 -0500

    Delete various no-longer-used scripts and .cmd files

--- leo/scripts/make-clean.cmd
+++
@@ -1,5 +0,0 @@
-echo off
-echo off
-cd C:\Repos\leo-editor\leo\doc\html
-echo make clean
-call make clean
--- leo/scripts/make-html.cmd
+++
@@ -1,5 +0,0 @@
-echo off
-echo off
-cd C:\Repos\leo-editor\leo\doc\html
-echo make html
-call make html
