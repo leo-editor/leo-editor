3496937b48f678b5fece149e3a773cc1f29a6978
Edward K. Ream <edreamleo@gmail.com>
2026-08-21 11:05:21 -0500

    Tweak qt_gui.py

--- leo/plugins/qt_gui.py
+++ leo/plugins/qt_gui.py
@@ -2309,7 +2309,7 @@ class StyleSheetManager:
         """
         if top is None:
             top = self.c.frame.top
-        master = top.leo_master or top  # type:ignore
+        master = top.leo_master or top
         return master
 
     # @+node:ekr.20140913054442.19391: *4* StyleSheetManager.set selected_style_sheet
