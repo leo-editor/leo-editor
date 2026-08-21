5081b7a1d871293c32a102b17dfdde1b288dee0e
Edward K. Ream <edreamleo@gmail.com>
2026-08-21 09:29:32 -0500

    Fix leoBeautify.py

--- leo/core/leoBeautify.py
+++ leo/core/leoBeautify.py
@@ -14,7 +14,7 @@ from typing import overload, Literal, TYPE_CHECKING
 try:
     import black
 except Exception:
-    black = None  # type:ignore
+    black = None
 
 # Leo imports.
 from leo.core import leoGlobals as g
