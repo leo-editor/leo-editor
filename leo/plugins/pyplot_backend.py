5363524d472ea9b3e72dc32c92600db5c23ff452
Edward K. Ream <edreamleo@gmail.com>
2026-08-28 06:50:58 -0500

    Don't assume c.vr exists

--- leo/plugins/pyplot_backend.py
+++ leo/plugins/pyplot_backend.py
@@ -102,7 +102,7 @@ class LeoFigureManagerQT(FigureManager):
         self.canvas = canvas
 
         # New code for Leo: embed the canvas in the viewrendered area.
-        self.vr_controller = vc = c.vr
+        self.vr_controller = vc = getattr(c, 'vr', None)
         self.splitter = gui.find_widget_by_name(c, 'main_splitter')
         self.frame = w = QtWidgets.QFrame()
         w.setLayout(QtWidgets.QVBoxLayout())
