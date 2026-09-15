834debfc218b715ba6bdad07121f4299f0436b1f
Edward K. Ream <edreamleo@gmail.com>
2026-09-15 04:52:47 -0500

    Add setUp method

--- leo/unittests/core/test_leoMarkdown.py
+++ leo/unittests/core/test_leoMarkdown.py
@@ -2,15 +2,21 @@
 # @+node:ekr.20260915043529.1: * @file ../unittests/core/test_leoMarkdown.py
 """Tests of leoMarkdown.py"""
 
-### import string
+from shutil import which
 from leo.core import leoGlobals as g
 from leo.core.leoTest2 import LeoUnitTest
+
+assert g
 
 
 # @+others
 # @+node:ekr.20260915043622.1: ** class TestMarkdown(LeoUnitTest)
 class TestMarkdown(LeoUnitTest):
     """Test cases for leoMarkdown.py"""
+
+    def setUp(self):
+        if not which('asciidoctor') and not which('asciidoc3'):
+            self.skipTest('Markdown tests require asciidoctor or asciidoc3')
 
     # @+others
     # @+node:ekr.20260915043717.1: *3* TestMarkdown.test_adoc
