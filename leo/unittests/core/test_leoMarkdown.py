b3cb5c9e62671392b4ab56afa1c272b298a44733
Edward K. Ream <edreamleo@gmail.com>
2026-09-15 04:41:01 -0500

    Add one failing unit test

---
+++ leo/unittests/core/test_leoMarkdown.py
@@ -0,0 +1,24 @@
+# @+leo-ver=5-thin
+# @+node:ekr.20260915043529.1: * @file ../unittests/core/test_leoMarkdown.py
+"""Tests of leoMarkdown.py"""
+
+### import string
+from leo.core import leoGlobals as g
+from leo.core.leoTest2 import LeoUnitTest
+
+
+# @+others
+# @+node:ekr.20260915043622.1: ** class TestMarkdown(LeoUnitTest)
+class TestMarkdown(LeoUnitTest):
+    """Test cases for leoMarkdown.py"""
+
+    # @+others
+    # @+node:ekr.20260915043717.1: *3* TestMarkdown.test_adoc
+    def test_adoc(self):
+        assert False
+
+    # @-others
+
+
+# @-others
+# @-leo
