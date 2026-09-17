36938f7aa8234bb83c5247dadb58b9ed8cf659c5
Edward K. Ream <edreamleo@gmail.com>
2026-09-17 06:59:30 -0500

    Give the 'plugins disabled' message only once

--- leo/core/leoGlobals.py
+++ leo/core/leoGlobals.py
@@ -5526,7 +5526,7 @@ def doHook(tag: str, *args: Args, **kwargs: KWargs) -> Any:
         g.pr(f"***ignoring args param.  tag = {tag}")
     if not g.app.enablePlugins:
         if tag in ('open0', 'start1'):
-            g.warning("Plugins disabled: use_plugins is 0 in a leoSettings.leo file.")
+            g.es_print_unique_message('Plugins disabled')
         return None
     # Get the hook handler function.  Usually this is doPlugins.
     c = kwargs.get("c")
